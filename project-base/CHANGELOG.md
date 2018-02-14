2d47efaeeb81f4d8a17a7c8b6041af1d7f7a3550
Milan Stano <milan.stano@shopsys.com>
2018-02-14 13:45:08 +0100

    gdpr site: shows personal data from email

--- project-base/CHANGELOG.md
+++ project-base/CHANGELOG.md
@@ -22,6 +22,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - `services_command.yml` to set Commands as services (@TomasLudvik)
 - admin legal condition: added gdpr content site (@stanoMilan) 
 - added gdpr site (@stanoMilan) 
+- gdpr site now shows personal data (@stanoMilan) 
 
 ### Changed
 - cache is cleared before PHPUnit tests only when run via [Phing targets](docs/introduction/phing-targets.md), not when run using `phpunit` directly (@PetrHeinz)
--- project-base/src/Shopsys/ShopBundle/Controller/Front/GdprController.php
+++ project-base/src/Shopsys/ShopBundle/Controller/Front/GdprController.php
@@ -6,6 +6,7 @@ use Shopsys\ShopBundle\Component\Controller\FrontBaseController;
 use Shopsys\ShopBundle\Component\Domain\Domain;
 use Shopsys\ShopBundle\Component\Setting\Setting;
 use Shopsys\ShopBundle\Form\Front\Gdpr\GdprFormType;
+use Shopsys\ShopBundle\Model\Customer\CustomerFacade;
 use Symfony\Component\HttpFoundation\Request;
 
 class GdprController extends FrontBaseController
@@ -20,12 +21,19 @@ class GdprController extends FrontBaseController
      */
     private $domain;
 
+    /**
+     * @var \ShopSys\ShopBundle\Model\Customer\CustomerFacade
+     */
+    private $customerFacade;
+
     public function __construct(
         Setting $setting,
-        Domain $domain
+        Domain $domain,
+        CustomerFacade $customerFacade
     ) {
         $this->setting = $setting;
         $this->domain = $domain;
+        $this->customerFacade = $customerFacade;
     }
 
     public function indexAction(Request $request)
@@ -43,10 +51,16 @@ class GdprController extends FrontBaseController
     {
         $gdprContent = $this->setting->getForDomain(Setting::GDPR_SITE_CONTENT, $this->domain->getId());
         $form = $this->createForm(GdprFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isValid() and $form->isSubmitted()) {
+            $user = $this->customerFacade->findUserByEmailAndDomain($form->getData()['email'], $this->domain->getId());
+        }
 
         return $this->render('@ShopsysShop/Front/Content/Gdpr/gdpr-list.html.twig', [
             'gdprContent' => $gdprContent,
             'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
