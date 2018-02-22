8fd052b69890572781ab07c25fa08d773f1e22f3
Milan Stano <milan.stano@shopsys.com>
2018-02-22 10:35:11 +0100

    DateTimeFormatterExtension: added DateTimeImmutable support

--- project-base/src/Shopsys/ShopBundle/Twig/DateTimeFormatterExtension.php
+++ project-base/src/Shopsys/ShopBundle/Twig/DateTimeFormatterExtension.php
@@ -3,6 +3,7 @@
 namespace Shopsys\ShopBundle\Twig;
 
 use DateTime;
+use DateTimeImmutable;
 use IntlDateFormatter;
 use Shopsys\ShopBundle\Component\Localization\DateTimeFormatter;
 use Shopsys\ShopBundle\Model\Localization\Localization;
@@ -149,6 +150,8 @@ class DateTimeFormatterExtension extends Twig_Extension
     {
         if ($value instanceof DateTime) {
             return $value;
+        } elseif ($value instanceof DateTimeImmutable) {
+            return new DateTime($value->format(DATE_ISO8601));
         } else {
             return new DateTime($value);
         }
