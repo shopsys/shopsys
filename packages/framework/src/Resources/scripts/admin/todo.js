601c857341b7e0d8de5f282916ef003ad6392a75
Jan Kulda <jan.kulda@shopsys.com>
2018-06-28 15:02:05 +0200

    js error build corection

--- packages/framework/src/Resources/scripts/admin/todo.js
+++ packages/framework/src/Resources/scripts/admin/todo.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.js-web-header-menu-toggle-button').on('click', function(e) {
-        $('body').toggleClass("js-side-menu-open");
-        $(this).toggleClass("active");
+    $('.js-web-header-menu-toggle-button').on('click', function (e) {
+        $('body').toggleClass('js-side-menu-open');
+        $(this).toggleClass('active');
         e.preventDefault();
     });
 
