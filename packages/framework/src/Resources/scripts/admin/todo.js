67fa17f665c435314617406b289f4596000fa6bb
Jan Kulda <jan.kulda@shopsys.com>
2018-07-17 11:35:06 +0200

    Fixing JS build error

--- packages/framework/src/Resources/scripts/admin/todo.js
+++ packages/framework/src/Resources/scripts/admin/todo.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
-    $('.js-products-picker-button-add').click( function($container) {
-        setTimeout( function() {
+    $('.js-products-picker-button-add').click(function ($container) {
+        setTimeout(function () {
             $('.mfp-iframe-scaler').addClass('mfp-iframe-scaler--responsive');
         }, 2000);
     });
 
-    $('#js-order-item-add-product').click( function($container) {
-        setTimeout( function() {
+    $('#js-order-item-add-product').click(function ($container) {
+        setTimeout(function () {
             $('.mfp-iframe-scaler').addClass('mfp-iframe-scaler--responsive');
         }, 2000);
     });
