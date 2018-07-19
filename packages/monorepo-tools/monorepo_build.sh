e5edea876623bb36646e6d0cafc2237f94bc25bc
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2018-07-19 14:43:50 +0100

    Refactored to have 2 commands

--- packages/monorepo-tools/monorepo_build.sh
+++ packages/monorepo-tools/monorepo_build.sh
@@ -10,58 +10,15 @@
 # Example: monorepo_build.sh main-repository package-alpha:packages/alpha package-beta:packages/beta
 
 # Check provided arguments
-if [ "$1" == "-p" ] || [ "$1" == "--partial" ]; then
-    shift
-    PARTIAL=true
-else
-    PARTIAL=false
-fi
-if [[ ( "$#" -lt "2"  && "$PARTIAL" == "false" ) || "$#" -lt "1" ]]; then
-    echo 'Please provide at least 2 remotes to be merged into a new monorepo or one remote and request a partial load'
+PARTIAL=false
+if [ "$#" -lt "2" ]; then
+    echo 'Please provide at least 2 remotes to be merged into a new monorepo'
     echo 'Usage: monorepo_build.sh [OPTION] <remote-name>[:<subdirectory>] <remote-name>[:<subdirectory>] ...'
-    echo ''
-    echo '  -p  --partial Add a repo to an existing monorepo (will only use master branch)'
     echo ''
     echo 'Example: monorepo_build.sh main-repository package-alpha:packages/alpha package-beta:packages/beta'
     exit
 fi
+COMMIT_MSG="merge multiple repositories into a monorepo"$'\n'$'\n'"- merged using: 'monorepo_build.sh $@'"$'\n'"- see https://github.com/shopsys/monorepo-tools"
 # Get directory of the other scripts
 MONOREPO_SCRIPT_DIR=$(dirname "$0")
-# Wipe original refs (possible left-over back-up after rewriting git history)
-$MONOREPO_SCRIPT_DIR/original_refs_wipe.sh
-for PARAM in $@; do
-    # Parse parameters in format <remote-name>[:<subdirectory>]
-    PARAM_ARR=(${PARAM//:/ })
-    REMOTE=${PARAM_ARR[0]}
-    SUBDIRECTORY=${PARAM_ARR[1]}
-    if [ "$SUBDIRECTORY" == "" ]; then
-        SUBDIRECTORY=$REMOTE
-    fi
-    # Rewrite all branches from the first remote, only master branches from others
-    if [ "$PARAM" == "$1" ] && [ "$PARTIAL" == "false" ]; then
-        echo "Building all branches of the remote '$REMOTE'"
-        $MONOREPO_SCRIPT_DIR/load_branches_from_remote.sh $REMOTE
-        $MONOREPO_SCRIPT_DIR/rewrite_history_into.sh $SUBDIRECTORY --branches
-        MERGE_REFS='master'
-    else
-        echo "Building branch 'master' of the remote '$REMOTE'"
-        git checkout --detach $REMOTE/master
-        $MONOREPO_SCRIPT_DIR/rewrite_history_into.sh $SUBDIRECTORY
-        MERGE_REFS="$MERGE_REFS $(git rev-parse HEAD)"
-    fi
-    # Wipe the back-up of original history
-    $MONOREPO_SCRIPT_DIR/original_refs_wipe.sh
-done
-# Merge all master branches
-COMMIT_MSG="merge multiple repositories into a monorepo"$'\n'$'\n'"- merged using: 'monorepo_build.sh $@'"$'\n'"- see https://github.com/shopsys/monorepo-tools"
-git checkout master
-echo "Merging refs: $MERGE_REFS"
-git merge --no-commit -q $MERGE_REFS --allow-unrelated-histories
-echo 'Resolving conflicts using trees of all parents'
-for REF in $MERGE_REFS; do
-    # Add all files from all master branches into index
-    # "git read-tree" with multiple refs cannot be used as it is limited to 8 refs
-    git ls-tree -r $REF | git update-index --index-info
-done
-git commit -m "$COMMIT_MSG"
-git reset --hard
+. $MONOREPO_SCRIPT_DIR/build_or_add_repos.sh $@
