1c0182f0e52268b484fcd27f203813c04e868c3b
Miroslav Stopka <stopka.miroslav.sk@gmail.com>
2018-08-10 11:11:24 +0200

    fixup! working microservice integrated into Shopsys Framework

--- microservices/product-search/src/Repository/ProductSearchRepository.php
+++ microservices/product-search/src/Repository/ProductSearchRepository.php
@@ -2,74 +2,78 @@
 
 namespace Shopsys\MicroserviceProductSearch\Repository;
 
-use Shopsys\MicroserviceProductSearch\Component\String\DatabaseSearching;
-use Shopsys\MicroserviceProductSearch\Component\String\TsqueryFactory;
+use Elasticsearch\Client;
 
 class ProductSearchRepository
 {
     /**
-     * @var \Shopsys\MicroserviceProductSearch\Component\String\TsqueryFactory
+     * @var \Elasticsearch\Client
      */
-    private $tsqueryFactory;
+    private $client;
 
     /**
-     * @var string
+     * @param \Elasticsearch\Client $client
      */
-    private $pgConnectionString;
-
-    public function __construct(TsqueryFactory $tsqueryFactory, string $pgConnectionString)
+    public function __construct(Client $client)
     {
-        $this->tsqueryFactory = $tsqueryFactory;
-        $this->pgConnectionString = $pgConnectionString;
+        $this->client = $client;
+    }
+
+    public function getFoundProductIds(int $domainId, string $searchText): array
+    {
+        if (!$searchText) {
+            return [];
+        }
+
+        $parameters = $this->createQuery($domainId, $searchText);
+        $result = $this->client->search($parameters);
+        return $this->extractIds($result);
     }
 
     /**
      * @param int $domainId
      * @param string $searchText
+     * @return array
+     */
+    protected function createQuery(int $domainId, string $searchText): array
+    {
+        return [
+            'index' => $domainId,
+            'type' => '_doc',
+            'size' => 1000,
+            'body' => [
+                '_source' => false,
+                'query' => [
+                    'multi_match' => [
+                        'query' => $searchText,
+                        'fields' => [
+                            'name.full_with_diacritic^60',
+                            'name.full_without_diacritic^50',
+                            'name^45',
+                            'name.edge_ngram_with_diacritic^40',
+                            'name.edge_ngram_without_diacritic^35',
+                            'catnum^50',
+                            'catnum.edge_ngram^25',
+                            'partno^40',
+                            'partno.edge_ngram^20',
+                            'ean^60',
+                            'ean.edge_ngram^30',
+                            'short_description^5',
+                            'description^5',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param array $result
      * @return int[]
      */
-    public function getFoundProductIds(int $domainId, string $searchText): array
+    protected function extractIds(array $result): array
     {
-        if (!$this->tsqueryFactory->isValidSearchText($searchText)) {
-            return [];
-        }
-
-        $connection = pg_connect($this->pgConnectionString);
-        $sql = '
-          SELECT p.id, CASE
-                WHEN (
-                    NORMALIZE(pt.name) LIKE NORMALIZE($1)
-                    OR
-                    NORMALIZE(pt.name) LIKE NORMALIZE($2)
-                ) THEN 1
-                WHEN pt.name_tsvector @@ to_tsquery($3) = TRUE THEN 2
-                WHEN p.catnum_tsvector @@ to_tsquery($4) = TRUE THEN 3
-                WHEN p.partno_tsvector @@ to_tsquery($4) = TRUE THEN 4
-                WHEN NORMALIZE(pt.name) LIKE NORMALIZE($5) THEN 5
-                WHEN pt.name_tsvector @@ to_tsquery($6) = TRUE THEN 6
-                WHEN pd.description_tsvector @@ to_tsquery($3) = TRUE THEN 7
-                WHEN pt.name_tsvector @@ to_tsquery($4) = TRUE THEN 8
-                WHEN pt.name_tsvector @@ to_tsquery($7) = TRUE THEN 9
-                ELSE 10
-            END AS relevance 
-          FROM products p
-          JOIN product_translations pt ON pt.translatable_id = p.id AND pt.locale = get_domain_locale($8)
-          JOIN product_domains pd ON pd.product_id = p.id AND pd.domain_id = $8
-          WHERE pd.fulltext_tsvector @@ to_tsquery($6) = TRUE
-          ORDER BY relevance';
-        $parameters = [
-            '%' . DatabaseSearching::getLikeSearchString($searchText) . ' %',
-            '%' . DatabaseSearching::getLikeSearchString($searchText),
-            $this->tsqueryFactory->getTsqueryWithAndConditions($searchText),
-            $this->tsqueryFactory->getTsqueryWithOrConditions($searchText),
-            DatabaseSearching::getFullTextLikeSearchString($searchText),
-            $this->tsqueryFactory->getTsqueryWithAndConditionsAndPrefixMatchForLastWord($searchText),
-            $this->tsqueryFactory->getTsqueryWithOrConditionsAndPrefixMatchForLastWord($searchText),
-            $domainId,
-        ];
-        $result = pg_query_params($connection, $sql, $parameters);
-        $productsIds = pg_fetch_all_columns($result, 0);
-
-        return array_map('intval', $productsIds);
+        $hits = $result['hits']['hits'];
+        return array_column($hits, '_id');
     }
 }
