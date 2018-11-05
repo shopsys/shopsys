6cb98940b07f1a8f2a005d130d072e28634d33af
Tomas Votruba <tomas.vot@gmail.com>
2018-11-05 13:29:15 +0100

    fixup! fix monday

--- packages/coding-standards/tests/Unit/CsFixer/Phpdoc/OrderedParamAnnotationsFixer/OrderedParamAnnotationsFixerTest.php
+++ packages/coding-standards/tests/Unit/CsFixer/Phpdoc/OrderedParamAnnotationsFixer/OrderedParamAnnotationsFixerTest.php
@@ -25,6 +25,29 @@ final class OrderedParamAnnotationsFixerTest extends AbstractCheckerTestCase
         yield [__DIR__ . '/wrong/wrong.php', __DIR__ . '/fixed/fixed.php'];
     }
 
+    /**
+     * @dataProvider provideCorrectFiles()
+     * @param string $correctFile
+     */
+    public function testCorrect(string $correctFile): void
+    {
+        $this->doTestCorrectFile($correctFile);
+    }
+
+    /**
+     * @return \Iterator
+     */
+    public function provideCorrectFiles(): Iterator
+    {
+        yield [__DIR__ . '/correct/correct.php'];
+        yield [__DIR__ . '/correct/correct2.php'];
+        yield [__DIR__ . '/correct/correct3.php'];
+        yield [__DIR__ . '/correct/correct4.php'];
+    }
+
+    /**
+     * @return string
+     */
     protected function provideConfig(): string
     {
         return __DIR__ . '/config.yml';
