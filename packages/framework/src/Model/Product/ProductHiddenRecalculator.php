d33cf64fc407de2d4b218b0842424161ac30059a
Jaroslav Spozdil <dzarda@gmail.com>
2018-12-05 18:15:39 +0100

    Solution for issue #654 - ProductHiddenRecalculator service takes into account stockQuantities of variants in case of mainVariant product type

--- packages/framework/src/Model/Product/ProductHiddenRecalculator.php
+++ packages/framework/src/Model/Product/ProductHiddenRecalculator.php
@@ -42,14 +42,28 @@ class ProductHiddenRecalculator
             ->update(Product::class, 'p')
             ->set('p.calculatedHidden', '
                 CASE
-                    WHEN p.usingStock = TRUE
+                    WHEN 
+                        (
+                        p.variantType <> :mainType
+                        AND p.usingStock = TRUE
                         AND p.stockQuantity <= 0
                         AND p.outOfStockAction = :outOfStockActionHide
+                        ) OR (
+                            p.variantType = :mainType 
+                            AND 
+                            ( 
+                                SELECT SUM(pv.stockQuantity)
+                                FROM '.Product::class.' AS pv
+                                WHERE pv.mainVariant = p.id
+                                AND pv.usingStock = TRUE
+                            ) <= 0
+                        )
                     THEN TRUE
                     ELSE p.hidden
                 END
                 ')
-            ->setParameter('outOfStockActionHide', Product::OUT_OF_STOCK_ACTION_HIDE);
+            ->setParameter('outOfStockActionHide', Product::OUT_OF_STOCK_ACTION_HIDE)
+            ->setParameter('mainType', Product::VARIANT_TYPE_MAIN);
 
         if ($product !== null) {
             $qb->where('p = :product')->setParameter('product', $product);
