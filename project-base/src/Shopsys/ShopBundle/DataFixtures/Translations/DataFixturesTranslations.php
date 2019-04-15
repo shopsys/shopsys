efd48a6137ccc4fba4d4441aabc89c98e0444217
Miroslav Stopka <stopka.miroslav.sk@gmail.com>
2019-04-15 22:48:09 +0200

    DataFixtures: preparation for translations by specific domain - used for multidomain attributes

--- project-base/src/Shopsys/ShopBundle/DataFixtures/Translations/DataFixturesTranslations.php
+++ project-base/src/Shopsys/ShopBundle/DataFixtures/Translations/DataFixturesTranslations.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Shopsys\ShopBundle\DataFixtures\Translations;
 
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\ShopBundle\DataFixtures\Translations\Exception\DataFixturesAttributeTranslationNotFound;
 
 class DataFixturesTranslations
@@ -14,10 +15,25 @@ class DataFixturesTranslations
     public const TRANSLATED_ATTRIBUTE_NAME = 'name';
     public const TRANSLATED_ATTRIBUTE_DESCRIPTION = 'description';
 
+    public const DEFAULT_LOCALE_IF_NOT_AVAILABLE_REQUESTED_LOCALE = 'en';
+
     /**
      * @var \Shopsys\ShopBundle\DataFixtures\Translations\DataFixturesTranslationInterface[]
      */
     private $registeredLanguageTranslations;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    private $domain;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     */
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
 
     /**
      * @param \Shopsys\ShopBundle\DataFixtures\Translations\DataFixturesTranslationInterface $translationService
@@ -88,5 +104,56 @@ class DataFixturesTranslations
         }
 
         return $entityAttributeTranslationsIndexedByLocale;
+    }
+
+    /**
+     * @param string $translatedEntity
+     * @param string $translatedAttribute
+     * @param int $domainId
+     * @return string
+     */
+    public function getEntityAttributeTranslationForDomainId(
+        string $translatedEntity,
+        string $translatedAttribute,
+        int $domainId
+    ): string {
+        $entityAttributeTranslationsIndexedByLocale = $this->getEntityAttributeTranslations(
+            $translatedEntity,
+            $translatedAttribute
+        );
+
+        $localeByDomainId = $this->domain->getDomainConfigById($domainId)->getLocale();
+        if (array_key_exists($localeByDomainId, $entityAttributeTranslationsIndexedByLocale)) {
+            return $entityAttributeTranslationsIndexedByLocale[$localeByDomainId];
+        }
+
+        return $entityAttributeTranslationsIndexedByLocale[self::DEFAULT_LOCALE_IF_NOT_AVAILABLE_REQUESTED_LOCALE];
+    }
+
+    /**
+     * @param string $translatedEntity
+     * @param string $translatedAttribute
+     * @param string $referenceName
+     * @param int $domainId
+     * @return string
+     */
+    public function getEntityAttributeTranslationByReferenceNameForDomainId(
+        string $translatedEntity,
+        string $translatedAttribute,
+        string $referenceName,
+        int $domainId
+    ): string {
+        $entityAttributeTranslationsIndexedByLocale = $this->getEntityAttributeTranslationsByReferenceName(
+            $translatedEntity,
+            $translatedAttribute,
+            $referenceName
+        );
+
+        $localeByDomainId = $this->domain->getDomainConfigById($domainId)->getLocale();
+        if (array_key_exists($localeByDomainId, $entityAttributeTranslationsIndexedByLocale)) {
+            return $entityAttributeTranslationsIndexedByLocale[$localeByDomainId];
+        }
+
+        return $entityAttributeTranslationsIndexedByLocale[self::DEFAULT_LOCALE_IF_NOT_AVAILABLE_REQUESTED_LOCALE];
     }
 }
