4907edb0decccb063c061eef010f2bbaea104bf6
Petr Heinz <petr.heinz@shopsys.com>
2019-06-07 11:26:12 +0200

    fixup! ProductAvailabilityCalculation: fix calculation of not-yet-persisted products

--- project-base/tests/ShopBundle/Functional/Model/Product/ProductVariantCreationTest.php
+++ project-base/tests/ShopBundle/Functional/Model/Product/ProductVariantCreationTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\ShopBundle\Functional\Model\Product;
 
-use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityFacade;
 use Shopsys\FrameworkBundle\Model\Product\Product;
 use Shopsys\FrameworkBundle\Model\Product\ProductDataFactory;
 use Shopsys\FrameworkBundle\Model\Product\ProductFacade;
