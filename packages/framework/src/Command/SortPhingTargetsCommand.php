275f614b8146aa577f0a8b5091b699aeb20df346
Petr Heinz <petr.heinz@shopsys.com>
2019-06-07 12:42:13 +0200

    the shopsys:phing-targets:sort command has better output
    - SymfonyStyle is used for input / output
    - return codes are used instead of throwing Exceptions
    - messages were tweaked for better understandability
    - command will not fail when a block is not found (a warning will be displayed instead)

--- packages/framework/src/Command/SortPhingTargetsCommand.php
+++ packages/framework/src/Command/SortPhingTargetsCommand.php
@@ -2,16 +2,18 @@
 
 namespace Shopsys\FrameworkBundle\Command;
 
-use Exception;
 use SimpleXMLElement;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class SortPhingTargetsCommand extends Command
 {
+    protected const RETURN_CODE_OK = 0;
+    protected const RETURN_CODE_ERROR = 1;
     protected const ARG_XML_PATH = 'xml';
     protected const OPTION_ONLY_CHECK = 'check';
 
@@ -31,31 +33,52 @@ class SortPhingTargetsCommand extends Command
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $returnCode = static::RETURN_CODE_OK;
+        $io = new SymfonyStyle($input, $output);
+
+        $checkOnly = $input->getOption(static::OPTION_ONLY_CHECK);
         $paths = $input->getArgument(static::ARG_XML_PATH);
         foreach ($paths as $path) {
             $content = file_get_contents($path);
 
-            $sortedContent = $this->sortTargetBlocks($content);
+            $sortedContent = $this->sortTargetBlocks($content, $io);
 
-            if (!$input->getOption(static::OPTION_ONLY_CHECK)) {
+            $isContentChanged = $content !== $sortedContent;
+            if ($checkOnly && $isContentChanged) {
+                $returnCode = static::RETURN_CODE_ERROR;
+
+                $io->error(sprintf('The targets in "%s" are not alphabetically sorted.', $path));
+            } elseif ($isContentChanged) {
                 file_put_contents($path, $sortedContent);
-            } elseif ($content !== $sortedContent) {
-                throw new Exception('The targets are not alphabetically sorted. Re-run the command without the "' . static::OPTION_ONLY_CHECK . '" option to fix it.');
+
+                $io->success(sprintf('The targets in "%s" were alphabetically sorted.', $path));
             }
         }
+
+        if ($returnCode === static::RETURN_CODE_OK) {
+            $io->success('All targets are alphabetically sorted.');
+        } elseif ($returnCode === static::RETURN_CODE_ERROR) {
+            $io->error('Some targets are not alphabetically sorted.');
+
+            $io->comment(sprintf('Re-run the command without the "%s" option to fix it automatically.', static::OPTION_ONLY_CHECK));
+        }
+
+        return $returnCode;
     }
 
     /**
      * @param string $content
+     * @param \Symfony\Component\Console\Style\SymfonyStyle $io
      * @return string
      */
-    protected function sortTargetBlocks(string $content): string
+    protected function sortTargetBlocks(string $content, SymfonyStyle $io): string
     {
         $targetBlocks = $this->extractTargetBlocksIndexedByName($content);
-        $content = $this->replaceTargetsByPlaceholders($content, $targetBlocks);
+        $content = $this->replaceTargetsByPlaceholders($content, $targetBlocks, $io);
         $content = $this->normalizeWhitespaceBetweenPlaceholders($content);
 
         ksort($targetBlocks);
@@ -104,9 +127,10 @@ class SortPhingTargetsCommand extends Command
     /**
      * @param string $content
      * @param string[] $targetBlocks
+     * @param \Symfony\Component\Console\Style\SymfonyStyle $io
      * @return string
      */
-    protected function replaceTargetsByPlaceholders(string $content, array $targetBlocks): string
+    protected function replaceTargetsByPlaceholders(string $content, array $targetBlocks, SymfonyStyle $io): string
     {
         $position = 1;
         foreach ($targetBlocks as $targetBlock) {
@@ -115,7 +139,9 @@ class SortPhingTargetsCommand extends Command
             $replacedContent = str_replace($targetBlock, $targetPlaceholder, $content);
 
             if ($content === $replacedContent) {
-                throw new Exception("Target block was not found in the original XML, probably because of unexpected formatting:\n\n" . $targetBlock);
+                $io->warning("This target block was not found in the original XML:\n\n" . $targetBlock);
+
+                $io->warning('This is probably because of unexpected formatting (eg. excessive whitespace). Position of this target will not be checked nor fixed.');
             }
 
             $content = $replacedContent;
