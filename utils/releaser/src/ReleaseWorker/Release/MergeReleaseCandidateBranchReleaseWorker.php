0fd44d320500a72a4091099d5be2b960773a8721
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-07-15 17:16:20 +0200

    MergeBranchToMasterReleaseWorker renamed to MergeReleaseCandidateBranchReleaseWorker and generalized
    - sometimes we need to merge the branch to another base branch than master

--- utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
@@ -8,7 +8,7 @@ use PharIo\Version\Version;
 use Shopsys\Releaser\ReleaseWorker\AbstractShopsysReleaseWorker;
 use Shopsys\Releaser\Stage;
 
-final class MergeBranchToMasterReleaseWorker extends AbstractShopsysReleaseWorker
+final class MergeReleaseCandidateBranchReleaseWorker extends AbstractShopsysReleaseWorker
 {
     /**
      * @param \PharIo\Version\Version $version
@@ -16,7 +16,7 @@ final class MergeBranchToMasterReleaseWorker extends AbstractShopsysReleaseWorke
      */
     public function getDescription(Version $version): string
     {
-        return '[Manually] Merge branch into master';
+        return sprintf('[Manually] Merge "%s" branch into "%s"', $this->createBranchName($version), $this->initialBranchName);
     }
 
     /**
@@ -34,7 +34,7 @@ final class MergeBranchToMasterReleaseWorker extends AbstractShopsysReleaseWorke
     public function work(Version $version): void
     {
         $this->symfonyStyle->note('You need to create a merge commit, see https://github.com/shopsys/shopsys/blob/master/docs/contributing/merging-on-github.md for detailed instructions.');
-        $this->confirm('Confirm the release branch was merged to master');
+        $this->confirm(sprintf('Confirm "%s" branch was merged to "%s"', $this->createBranchName($version), $this->initialBranchName));
     }
 
     /**
