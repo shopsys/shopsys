51a8c156a6a14837f8b1b9eadf2b962395d53436
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-08-19 16:03:10 +0200

    added ClassExtensionRegistry that holds information about all the project classes that extend the classes (or implement interfaces) from framework
    - the registry will be used for automated annotations fixes and additions in project classes
    - this will make PHPStan and PHPStorm understand the code of Shopsys Framework based project properly
    - the extensions information is not collected in production environment as it is not necessary there

---
+++ packages/framework/src/Component/ClassExtension/ClassExtensionRegistry.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Component\ClassExtension;
+
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
+use Symfony\Component\Finder\Finder;
+
+class ClassExtensionRegistry
+{
+    /**
+     * @var string[]
+     */
+    protected $entityExtensionMap = [];
+
+    /**
+     * @var string[]
+     */
+    protected $serviceExtensionMap = [];
+
+    /**
+     * @var string[]
+     */
+    protected $dataObjectExtensionMap = [];
+
+    /**
+     * @var string
+     */
+    protected $frameworkRootDir;
+
+    /**
+     * @param string[] $entityExtensionMap
+     * @param string $frameworkRootDir
+     */
+    public function __construct(array $entityExtensionMap, string $frameworkRootDir)
+    {
+        $this->entityExtensionMap = $entityExtensionMap;
+        $this->frameworkRootDir = $frameworkRootDir;
+        $this->dataObjectExtensionMap = $this->getDataObjectExtensionMap();
+    }
+
+    /**
+     * @param string $parentClassName
+     * @param string $childClassName
+     */
+    public function addExtendedService(string $parentClassName, string $childClassName): void
+    {
+        $this->serviceExtensionMap[$parentClassName] = $childClassName;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getDataObjectExtensionMap(): array
+    {
+        $finder = Finder::create()
+            ->files()
+            ->ignoreUnreadableDirs()
+            ->in($this->frameworkRootDir)
+            ->name('/.*Data\.php/');
+
+        $dataObjectsMap = [];
+        foreach ($finder as $file) {
+            /** @var \Symfony\Component\Finder\SplFileInfo $file */
+            $frameworkClassFqcn = $this->getFqcn($file->getPathname());
+            $projectClassFqcn = str_replace('Shopsys\FrameworkBundle', 'Shopsys\ShopBundle', $frameworkClassFqcn);
+            if (class_exists($projectClassFqcn)) {
+                $dataObjectsMap[$frameworkClassFqcn] = $projectClassFqcn;
+            }
+        }
+
+        return $dataObjectsMap;
+    }
+
+    /**
+     * @param string $pathname
+     * @return string
+     */
+    protected function getFqcn(string $pathname): string
+    {
+        $astLocator = (new BetterReflection())->astLocator();
+        $reflector = new ClassReflector(new SingleFileSourceLocator($pathname, $astLocator));
+        return $reflector->getAllClasses()[0]->getName();
+    }
+}
---
+++ packages/framework/src/DependencyInjection/Compiler/RegisterProjectFrameworkClassExtensionsCompilerPass.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\DependencyInjection\Compiler;
+
+use Shopsys\FrameworkBundle\Component\ClassExtension\ClassExtensionRegistry;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RegisterProjectFrameworkClassExtensionsCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        $classExtensionRegistryDefinition = $container->findDefinition(ClassExtensionRegistry::class);
+        foreach ($container->getServiceIds() as $serviceId) {
+            if ($this->isFrameworkClassWithAlias($container, $serviceId)) {
+                $aliasId = (string)$container->getAlias($serviceId);
+                if ($this->isProjectClass($aliasId)) {
+                    $classExtensionRegistryDefinition->addMethodCall('addExtendedService', [$serviceId, $aliasId]);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param string $serviceId
+     * @return bool
+     */
+    private function isFrameworkClassWithAlias(ContainerBuilder $container, string $serviceId): bool
+    {
+        return strpos($serviceId, 'Shopsys\FrameworkBundle') !== false && $container->hasAlias($serviceId);
+    }
+
+    /**
+     * @param string $serviceId
+     * @return bool
+     */
+    private function isProjectClass(string $serviceId): bool
+    {
+        return strpos($serviceId, 'Shopsys\ShopBundle') !== false;
+    }
+}
