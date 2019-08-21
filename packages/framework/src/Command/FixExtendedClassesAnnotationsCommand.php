7616b55e83919647a8fc7942d14733461b498101
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-08-21 15:26:14 +0200

    FixExtendedClassesAnnotationsCommand now also adds @property annotations where needed
    - the annotations are added to project classes when there exists a property in parent class that is extended in the project
    - see e.g. https://github.com/shopsys/demoshop/commit/b0135a25e8abb2e0123c6205099d76797810328f
    - see e.g. https://github.com/shopsys/demoshop/commit/4cd68d79ecaccdb1b6608efefa7122248bf354d4

--- packages/framework/src/Command/FixExtendedClassesAnnotationsCommand.php
+++ packages/framework/src/Command/FixExtendedClassesAnnotationsCommand.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Command;
 
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflection\ReflectionClass;
+use Roave\BetterReflection\Reflection\ReflectionProperty;
 use Shopsys\FrameworkBundle\Component\ClassExtension\ClassExtensionRegistry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -33,7 +36,10 @@ class FixExtendedClassesAnnotationsCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Replaces the framework with the project annotations in all project files when there exists a project extension of a given framework class.');
+            ->setDescription('
+                Fixes and adds annotations in project classes to improve static analysis and DX with extended classes:
+                - Replaces the framework with the project annotations in all project files when there exists a project extension of a given framework class.
+                - Adds @property annotations to project classes when there exists a property in parent class that is extended in the project.');
     }
 
     /**
@@ -53,6 +59,7 @@ class FixExtendedClassesAnnotationsCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->replaceFrameworkWithProjectAnnotations();
+        $this->addPropertyAnnotationsToProjectClasses();
         $output->writeln('Annotations fixed successfully');
     }
 
@@ -110,16 +117,190 @@ class FixExtendedClassesAnnotationsCommand extends Command
     {
         $annotationsReplacementsMap = [];
         foreach ($classExtensionMap as $frameworkClass => $projectClass) {
-            /**
-             * We want to match the FQCN followed by space or an array declaration but want to exclude the substrings.
-             * E.g. for "\Shopsys\FrameworkBundle\Model\Product\Product" we do not want to match "\Shopsys\FrameworkBundle\Model\Product\ProductFacade"
-             * but we want to match "\Shopsys\FrameworkBundle\Model\Product\Product[]"
-             */
-            $index = '/(?P<fqcn>\\\\' . preg_quote($frameworkClass, '/') . ')(?!\w)(?P<brackets>(\[\])*)/';
+            $index = $this->getEscapedFqcnWithLeadingSlashPattern($frameworkClass);
             $value = '$1$2|\\' . $projectClass . '$2';
             $annotationsReplacementsMap[$index] = $value;
         }
 
         return $annotationsReplacementsMap;
+    }
+
+    protected function addPropertyAnnotationsToProjectClasses(): void
+    {
+        $classExtensionMap = $this->classExtensionRegistry->getClassExtensionMap();
+        foreach ($classExtensionMap as $frameworkClass => $projectClass) {
+            $frameworkClassBetterReflection = $this->getBetterReflectionClass($frameworkClass);
+            foreach ($frameworkClassBetterReflection->getProperties() as $property) {
+                $this->addPropertyAnnotationToProjectClassIfNecessary($property, $projectClass, array_keys($classExtensionMap));
+            }
+        }
+    }
+
+    /**
+     * @param \Roave\BetterReflection\Reflection\ReflectionProperty $reflectionProperty
+     * @param string $projectClass
+     * @param string[] $extendedFrameworkClasses
+     */
+    protected function addPropertyAnnotationToProjectClassIfNecessary(
+        ReflectionProperty $reflectionProperty,
+        string $projectClass,
+        array $extendedFrameworkClasses
+    ): void {
+        $projectClassBetterReflection = $this->getBetterReflectionClass($projectClass);
+        foreach ($extendedFrameworkClasses as $extendedFrameworkClass) {
+            $isPropertyOfTypeThatIsExtendedInProject = preg_match(
+                $this->getEscapedFqcnWithLeadingSlashPattern($extendedFrameworkClass),
+                $reflectionProperty->getDocComment()
+            );
+            if (!$this->isPropertyDeclaredInClass($reflectionProperty->getName(), $projectClassBetterReflection)
+                && $isPropertyOfTypeThatIsExtendedInProject
+            ) {
+                $this->addPropertyAnnotationToClass($reflectionProperty, $projectClassBetterReflection);
+            }
+        }
+    }
+
+    /**
+     * We want to match the FQCN followed by space or an array declaration but want to exclude the substrings.
+     * E.g. for "\Shopsys\FrameworkBundle\Model\Product\Product" we do not want to match "\Shopsys\FrameworkBundle\Model\Product\ProductFacade"
+     * but we want to match "\Shopsys\FrameworkBundle\Model\Product\Product[]"
+     *
+     * @param string $fullyQualifiedClassName
+     * @return string
+     */
+    protected function getEscapedFqcnWithLeadingSlashPattern(string $fullyQualifiedClassName): string
+    {
+        return '/(?P<fqcn>\\\\' . preg_quote($fullyQualifiedClassName, '/') . ')(?!\w)(?P<brackets>(\[\])*)/';
+    }
+
+    /**
+     * @param string $fullyQualifiedClassName
+     * @return \Roave\BetterReflection\Reflection\ReflectionClass
+     */
+    protected function getBetterReflectionClass(string $fullyQualifiedClassName): ReflectionClass
+    {
+        $projectClassBetterReflection = (new BetterReflection())
+            ->classReflector()
+            ->reflect($fullyQualifiedClassName);
+
+        return $projectClassBetterReflection;
+    }
+
+    /**
+     * @param \Roave\BetterReflection\Reflection\ReflectionProperty $reflectionProperty
+     * @param \Roave\BetterReflection\Reflection\ReflectionClass $projectClassBetterReflection
+     */
+    protected function addPropertyAnnotationToClass(ReflectionProperty $reflectionProperty, ReflectionClass $projectClassBetterReflection): void
+    {
+        $replacedTypeForProperty = $this->getReplacedTypeForProperty($reflectionProperty);
+        $projectClassName = $projectClassBetterReflection->getShortName();
+        $projectClassFileName = $projectClassBetterReflection->getFileName();
+        $projectClassDocBlock = $projectClassBetterReflection->getDocComment();
+        if ($projectClassDocBlock !== '') {
+            $this->updateClassAnnotationWithProperty(
+                $reflectionProperty,
+                $replacedTypeForProperty,
+                $projectClassDocBlock,
+                $projectClassFileName
+            );
+        } else {
+            $this->addClassAnnotationWithProperty(
+                $reflectionProperty,
+                $replacedTypeForProperty,
+                $projectClassName,
+                $projectClassFileName
+            );
+        }
+    }
+
+    /**
+     * @param string $propertyName
+     * @param \Roave\BetterReflection\Reflection\ReflectionClass $reflectionClass
+     * @return bool
+     */
+    protected function isPropertyDeclaredInClass(string $propertyName, ReflectionClass $reflectionClass)
+    {
+        $reflectionProperty = $reflectionClass->getProperty($propertyName);
+        if ($reflectionProperty === null) {
+            return false;
+        }
+
+        return $reflectionProperty->getDeclaringClass()->getName() === $reflectionClass->getName();
+    }
+
+    /**
+     * @param \Roave\BetterReflection\Reflection\ReflectionProperty $reflectionProperty
+     * @return string
+     */
+    protected function getReplacedTypeForProperty(ReflectionProperty $reflectionProperty): string
+    {
+        $propertyType = implode('|', $reflectionProperty->getDocBlockTypeStrings());
+
+        $annotationsReplacementsMap = $this->getAnnotationsReplacementsMap($this->classExtensionRegistry->getClassExtensionMap());
+        $replacedPropertyType = preg_replace(
+            array_keys($annotationsReplacementsMap),
+            $annotationsReplacementsMap,
+            $propertyType
+        );
+
+        return $replacedPropertyType;
+    }
+
+    /**
+     * @param string $fileName
+     * @param string $search
+     * @param string $replace
+     */
+    protected function replaceInFile(string $fileName, string $search, string $replace): void
+    {
+        $fileContent = file_get_contents($fileName);
+        $replacedContent = str_replace($search, $replace, $fileContent);
+        file_put_contents($fileName, $replacedContent);
+    }
+
+    /**
+     * @param \Roave\BetterReflection\Reflection\ReflectionProperty $reflectionProperty
+     * @param string $replacedTypeForProperty
+     * @param string $projectClassDocBlock
+     * @param string $projectClassFileName
+     */
+    protected function updateClassAnnotationWithProperty(
+        ReflectionProperty $reflectionProperty,
+        string $replacedTypeForProperty,
+        string $projectClassDocBlock,
+        string $projectClassFileName
+    ): void {
+        $propertyAnnotationNewLine = sprintf(
+            "* @property %s%s $%s\n",
+            $reflectionProperty->isStatic() ? 'static ' : '',
+            $replacedTypeForProperty,
+            $reflectionProperty->getName()
+        );
+        if (strpos($projectClassDocBlock, $propertyAnnotationNewLine) === false) {
+            $extendedDocBlock = str_replace('*/', $propertyAnnotationNewLine . ' */', $projectClassDocBlock);
+            $this->replaceInFile($projectClassFileName, $projectClassDocBlock, $extendedDocBlock);
+        }
+    }
+
+    /**
+     * @param \Roave\BetterReflection\Reflection\ReflectionProperty $reflectionProperty
+     * @param string $replacedTypeForProperty
+     * @param string $projectClassShortName
+     * @param string $projectClassFileName
+     */
+    protected function addClassAnnotationWithProperty(
+        ReflectionProperty $reflectionProperty,
+        string $replacedTypeForProperty,
+        string $projectClassShortName,
+        string $projectClassFileName
+    ): void {
+        $replacement = sprintf(
+            "/**\n * @property %s%s $%s\n */\nclass %s",
+            $reflectionProperty->isStatic() ? 'static ' : '',
+            $replacedTypeForProperty,
+            $reflectionProperty->getName(),
+            $projectClassShortName
+        );
+        $this->replaceInFile($projectClassFileName, 'class ' . $projectClassShortName, $replacement);
     }
 }
