09e07be3602d456f75a61f4ac1c599426fd5f5d3
David Stritezsky <david.stritezsky@shopsys.com>
2019-08-22 10:57:58 +0200

    extended OrderItemFactory to project-base

---
+++ project-base/src/Shopsys/ShopBundle/Model/Order/Item/OrderItemFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Model\Order\Item;
+
+use Shopsys\FrameworkBundle\Model\Order\Item\OrderItemFactory as BaseOrderItemFactory;
+
+class OrderItemFactory extends BaseOrderItemFactory
+{
+}
