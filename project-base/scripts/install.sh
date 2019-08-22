50d4f0719d25e37d42a2724c6c23c0de5317dda7
David Stritezsky <david.stritezsky@shopsys.com>
2019-08-22 13:14:40 +0200

    removed option --no-interaction when composer install

--- project-base/scripts/install.sh
+++ project-base/scripts/install.sh
@@ -81,7 +81,7 @@ docker-compose up -d --build
 
 echo "Installing application inside a php-fpm container"
 
-docker-compose exec -T php-fpm composer install --no-interaction
+docker-compose exec -T php-fpm composer install
 docker-compose exec -T php-fpm ./phing db-create test-db-create build-demo-dev-quick error-pages-generate
 
 echo "Your application is now ready under http://127.0.0.1:8000 and second domain under http://127.0.0.2:8000"
