3d6e09c40fb2666d58247375e57e3283656e8c2f
Peter Turek <peter.turek@shopsys.com>
2019-08-30 10:55:38 +0200

    replace constants from other classes by literal values (#1340)

--- packages/framework/src/Migrations/Version20181008000001.php
+++ packages/framework/src/Migrations/Version20181008000001.php
@@ -3,7 +3,6 @@
 namespace Shopsys\FrameworkBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Shopsys\FrameworkBundle\Component\Setting\Setting;
 use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 class Version20181008000001 extends AbstractMigration
@@ -13,8 +12,8 @@ class Version20181008000001 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $this->sql('UPDATE setting_values SET value = 0 WHERE value IS NULL AND type = \'integer\' AND  name = \'' . Setting::DEFAULT_AVAILABILITY_IN_STOCK . '\'');
-        $this->sql('UPDATE setting_values SET value = 0 WHERE value IS NULL AND type = \'integer\' AND  name = \'' . Setting::DEFAULT_UNIT . '\'');
+        $this->sql('UPDATE setting_values SET value = 0 WHERE value IS NULL AND type = \'integer\' AND  name = \'defaultAvailabilityInStockId\'');
+        $this->sql('UPDATE setting_values SET value = 0 WHERE value IS NULL AND type = \'integer\' AND  name = \'defaultUnitId\'');
         $this->sql('UPDATE setting_values SET type = \'none\' where value IS NULL');
     }
 
