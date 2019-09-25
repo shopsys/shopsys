b8df3bfc3d6c7edf3d4b77133b63da5290f5e931
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-09-25 14:31:16 +0200

    FlagFilterChoiceRepositoryTest: use first domain locale instead of hardcoded "en" locale
    - as a result, there is no need for fixing this test when a domain with a different locale (e.g. "es") is set

--- project-base/tests/ShopBundle/Functional/Model/Product/Filter/FlagFilterChoiceRepositoryTest.php
+++ project-base/tests/ShopBundle/Functional/Model/Product/Filter/FlagFilterChoiceRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\ShopBundle\Functional\Model\Product\Filter;
 
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Model\Product\Filter\FlagFilterChoiceRepository;
 use Shopsys\FrameworkBundle\Model\Product\Flag\Flag;
 use Shopsys\ShopBundle\DataFixtures\Demo\CategoryDataFixture;
@@ -85,7 +86,11 @@ class FlagFilterChoiceRepositoryTest extends TransactionFunctionalTestCase
         /** @var \Shopsys\ShopBundle\Model\Category\Category $category */
         $category = $this->getReference($categoryReferenceName);
 
-        return $repository->getFlagFilterChoicesInCategory(1, $pricingGroup, 'en', $category);
+        /** @var \Shopsys\FrameworkBundle\Component\Domain\Domain $domain */
+        $domain = $this->getContainer()->get(Domain::class);
+        $domainConfig1 = $domain->getDomainConfigById(1);
+
+        return $repository->getFlagFilterChoicesInCategory($domainConfig1->getId(), $pricingGroup, $domainConfig1->getLocale(), $category);
     }
 
     /**
@@ -99,7 +104,11 @@ class FlagFilterChoiceRepositoryTest extends TransactionFunctionalTestCase
         /** @var \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup $pricingGroup */
         $pricingGroup = $this->getReferenceForDomain(PricingGroupDataFixture::PRICING_GROUP_ORDINARY, 1);
 
-        return $repository->getFlagFilterChoicesForSearch(1, $pricingGroup, 'en', $searchText);
+        /** @var \Shopsys\FrameworkBundle\Component\Domain\Domain $domain */
+        $domain = $this->getContainer()->get(Domain::class);
+        $domainConfig1 = $domain->getDomainConfigById(1);
+
+        return $repository->getFlagFilterChoicesForSearch($domainConfig1->getId(), $pricingGroup, $domainConfig1->getLocale(), $searchText);
     }
 
     /**
