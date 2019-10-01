b3567152422b43518f3b02108921f9debc643a40
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2019-10-01 18:34:43 +0200

    ParameterFilterChoiceRepositoryTest is no longer language dependant
    - testParameterFilterChoicesFromCategory does no longer require strict order of parameters and its values
    - ParameterValueIds are now loaded by its translations

--- project-base/tests/ShopBundle/Functional/Model/Product/Filter/ParameterFilterChoiceRepositoryTest.php
+++ project-base/tests/ShopBundle/Functional/Model/Product/Filter/ParameterFilterChoiceRepositoryTest.php
@@ -5,33 +5,27 @@ declare(strict_types=1);
 namespace Tests\ShopBundle\Functional\Model\Product\Filter;
 
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
-use Shopsys\FrameworkBundle\Model\Product\Filter\ParameterFilterChoice;
 use Shopsys\FrameworkBundle\Model\Product\Filter\ParameterFilterChoiceRepository;
 use Shopsys\ShopBundle\DataFixtures\Demo\CategoryDataFixture;
 use Shopsys\ShopBundle\DataFixtures\Demo\PricingGroupDataFixture;
-use Tests\ShopBundle\Test\TransactionFunctionalTestCase;
+use Tests\ShopBundle\Test\ParameterTransactionFunctionalTestCase;
 
-class ParameterFilterChoiceRepositoryTest extends TransactionFunctionalTestCase
+class ParameterFilterChoiceRepositoryTest extends ParameterTransactionFunctionalTestCase
 {
     public function testParameterFilterChoicesFromCategoryWithNoParameters(): void
     {
-        $parameterFilterChoices = $this->getChoicesForCategoryReference(CategoryDataFixture::CATEGORY_GARDEN_TOOLS);
+        $parameterFilterChoices = $this->getParameterValueIdsForCategoryReferenceIndexedByParameterId(CategoryDataFixture::CATEGORY_GARDEN_TOOLS);
 
         $this->assertCount(0, $parameterFilterChoices);
     }
 
     public function testParameterFilterChoicesFromCategory(): void
     {
-        $parameterFilterChoices = $this->getChoicesForCategoryReference(CategoryDataFixture::CATEGORY_BOOKS);
+        $parameterFilterChoices = $this->getParameterValueIdsForCategoryReferenceIndexedByParameterId(CategoryDataFixture::CATEGORY_BOOKS);
 
         $this->assertCount(3, $parameterFilterChoices);
 
-        $ids = array_map(
-            static function (ParameterFilterChoice $parameterFilterChoice) {
-                return $parameterFilterChoice->getParameter()->getId();
-            },
-            $parameterFilterChoices
-        );
+        $ids = array_keys($parameterFilterChoices);
 
         $this->assertContains(50, $ids);
         $this->assertContains(51, $ids);
@@ -43,31 +37,45 @@ class ParameterFilterChoiceRepositoryTest extends TransactionFunctionalTestCase
             10 => [$this->getParameterValueIdForFirstDomain('150 g'), $this->getParameterValueIdForFirstDomain('250 g'), $this->getParameterValueIdForFirstDomain('50 g')],
         ];
 
-        foreach ($parameterParameterValuePair as $i => $parameterValues) {
-            foreach ($parameterValues as $j => $value) {
-                $this->assertSame($value, $parameterFilterChoices[$i]->getValues()[$j]->getId());
+        foreach ($parameterParameterValuePair as $parameterId => $parameterValues) {
+            foreach ($parameterValues as $parameterValue) {
+                $this->assertContains($parameterValue, $parameterFilterChoices[$parameterId]);
             }
         }
     }
 
     /**
      * @param string $categoryReferenceName
-     * @return \Shopsys\FrameworkBundle\Model\Product\Filter\ParameterFilterChoice[]
+     * @return array
      */
-    protected function getChoicesForCategoryReference(string $categoryReferenceName): array
+    protected function getParameterValueIdsForCategoryReferenceIndexedByParameterId(string $categoryReferenceName): array
     {
         $repository = $this->getParameterFilterChoiceRepository();
 
         /** @var \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup $pricingGroup */
-        $pricingGroup = $this->getReferenceForDomain(PricingGroupDataFixture::PRICING_GROUP_ORDINARY, 1);
+        $pricingGroup = $this->getReferenceForDomain(PricingGroupDataFixture::PRICING_GROUP_ORDINARY, Domain::FIRST_DOMAIN_ID);
 
         /** @var \Shopsys\ShopBundle\Model\Category\Category $category */
         $category = $this->getReference($categoryReferenceName);
 
         /** @var \Shopsys\FrameworkBundle\Component\Domain\Domain $domain */
         $domain = $this->getContainer()->get(Domain::class);
-        $domainConfig1 = $domain->getDomainConfigById(1);
-        return $repository->getParameterFilterChoicesInCategory($domainConfig1->getId(), $pricingGroup, $domainConfig1->getLocale(), $category);
+        $domainConfig1 = $domain->getDomainConfigById(Domain::FIRST_DOMAIN_ID);
+
+        $parameterFilterChoices = $repository->getParameterFilterChoicesInCategory($domainConfig1->getId(), $pricingGroup, $domainConfig1->getLocale(), $category);
+
+        $parameterValuesByParameterId = [];
+
+        foreach ($parameterFilterChoices as $parameterFilterChoice) {
+            $parameterValuesByParameterId[$parameterFilterChoice->getParameter()->getId()] = array_map(
+                function ($parameterValue) {
+                    return $parameterValue->getId();
+                },
+                $parameterFilterChoice->getValues()
+            );
+        }
+
+        return $parameterValuesByParameterId;
     }
 
     /**
