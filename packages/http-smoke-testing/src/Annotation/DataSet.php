539d7cd46693d66374aadbcc545e11921bfe377b
Marius Büscher <marius@gridscale.io>
2019-10-05 16:53:51 +0200

    Add data set annotation

---
+++ packages/http-smoke-testing/src/Annotation/DataSet.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Shopsys\HttpSmokeTesting\Annotation;
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class DataSet
+{
+    /** @var int */
+    public $statusCode;
+
+    /** @var array<\Shopsys\HttpSmokeTesting\Annotation\Parameter> */
+    public $parameters;
+}
