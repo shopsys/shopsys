e9fbc8df026775edf6eb1357c284b677ab87ea10
Marius Büscher <marius@gridscale.io>
2019-10-05 16:53:06 +0200

    Add annotation to skip the smoke test of this controller action

---
+++ packages/http-smoke-testing/src/Annotation/Skipped.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Shopsys\HttpSmokeTesting\Annotation;
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class Skipped
+{
+
+}
