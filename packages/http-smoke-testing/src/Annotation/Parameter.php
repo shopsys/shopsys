d96212dc485e2d8e70c7fb06eacf5d89a4d7c8c3
Marius Büscher <marius@gridscale.io>
2019-10-05 16:53:32 +0200

    Add annotation for a single parameter

---
+++ packages/http-smoke-testing/src/Annotation/Parameter.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Shopsys\HttpSmokeTesting\Annotation;
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target({"ANNOTATION"})
+ */
+class Parameter
+{
+    /** @var string */
+    public $name;
+
+    /** @var string */
+    public $value;
+}
