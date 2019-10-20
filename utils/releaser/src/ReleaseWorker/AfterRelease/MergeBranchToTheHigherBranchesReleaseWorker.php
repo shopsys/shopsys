0f14c8d9e06d9a3ff60291eefb729eb2422a5dd4
Rostislav Vítek <rostislav.vitek@shopsys.com>
2019-10-20 22:42:11 +0200

    release process: added instruction for fixing docs links when merging to higher branches (MergeBranchToTheHigherBranchesReleaseWorker) (#1467)

--- utils/releaser/src/ReleaseWorker/AfterRelease/MergeBranchToTheHigherBranchesReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/AfterRelease/MergeBranchToTheHigherBranchesReleaseWorker.php
@@ -33,7 +33,8 @@ final class MergeBranchToTheHigherBranchesReleaseWorker extends AbstractShopsysR
     {
         $this->symfonyStyle->note('You need to gradually merge the branch with the new release to all the higher development branches.');
         $this->symfonyStyle->note('E.g. when releasing "7.3.x" version, you need to merge the "7.3" branch to the master branch, and then merge the master branch to the "9.0" branch provided current release series is "8.0"');
-        $this->confirm('Confirm the branch with the released version is merged to all the higher development branches.');
+        $this->symfonyStyle->note('When merging to the higher branch, check the docs and fix the links if necessary. Articles and files should be always linked in the corresponding version, see https://docs.shopsys.com/en/latest/contributing/guidelines-for-writing-documentation/.');
+        $this->confirm('Confirm the branch with the released version is merged to all the higher development branches and the links in the docs are fixed.');
     }
 
     /**
