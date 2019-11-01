e1c584c7ddec56022621943f037a90a2cc370190
Tomas Pesek <tomas.pesek@shopsys.com>
2019-11-01 10:44:09 +0100

    Added option for get main variations and variations

--- packages/frontend-api/src/Model/Resolver/Products/ProductResolverMap.php
+++ packages/frontend-api/src/Model/Resolver/Products/ProductResolverMap.php
@@ -38,16 +38,37 @@ class ProductResolverMap extends ResolverMap
     {
         return [
             'Product' => [
-                'shortDescription' => function (Product $product) {
-                    return $product->getShortDescription($this->domain->getId());
-                },
-                'link' => function (Product $product) {
-                    return $this->getProductLink($product);
-                },
-                'categories' => function (Product $product) {
-                    return $product->getCategoriesIndexedByDomainId()[$this->domain->getId()];
+                self::RESOLVE_TYPE => function (Product $product) {
+                    if ($product->isMainVariant()) {
+                        return 'MainVariant';
+                    } elseif ($product->isVariant()) {
+                        return 'Variant';
+                    } else {
+                        return 'RegularProduct';
+                    }
                 },
             ],
+            'RegularProduct' => $this->mapProduct(),
+            'Variant' => $this->mapProduct(),
+            'MainVariant' => $this->mapProduct(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function mapProduct(): array
+    {
+        return [
+            'shortDescription' => function (Product $product) {
+                return $product->getShortDescription($this->domain->getId());
+            },
+            'link' => function (Product $product) {
+                return $this->getProductLink($product);
+            },
+            'categories' => function (Product $product) {
+                return $product->getCategoriesIndexedByDomainId()[$this->domain->getId()];
+            },
         ];
     }
 
