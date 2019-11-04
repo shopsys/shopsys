96bbb0aee43136611b5c08644db3b6fb5f208c88
tomasgottvald <tomas.gottvald@shopsys.com>
2019-11-04 08:52:30 +0100

    design - new placeholder, update manual lazy call

--- project-base/src/Shopsys/ShopBundle/Resources/scripts/frontend/lazyLoadInit.js
+++ project-base/src/Shopsys/ShopBundle/Resources/scripts/frontend/lazyLoadInit.js
@@ -1,17 +1,17 @@
-useNativeLazyload(new MiniLazyload({
-    rootMargin: '500px',
-    threshold: 0.5,
-    placeholder: 'placeholder.png'
-}, '.lazy'));
-
 (function ($) {
+
+    new MiniLazyload({
+        rootMargin: '500px',
+        threshold: 0.5,
+        placeholder: '/assets/frontend/images/noimage.png'
+    }, '.lazy');
 
     Shopsys = window.Shopsys || {};
     Shopsys.lazyLoadCall = Shopsys.lazyLoadCall || {};
 
     Shopsys.lazyLoadCall.inContainer = function (container) {
         $(container).find('.lazy').each(function () {
-            $(this).attr('src', $(this).data('src'));
+            $(this).attr('src', $(this).data('src')).addClass('loaded');
         });
     };
 
