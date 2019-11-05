ba2a4aabae9a89a0130bcfd8819e76bf01200d14
Alex Golubtsov <alex.8fmi@gmail.com>
2019-11-05 11:28:46 +0300

    Enabled to rewrite exact commits

--- packages/monorepo-tools/rewrite_history_into.sh
+++ packages/monorepo-tools/rewrite_history_into.sh
@@ -11,12 +11,31 @@
 
 SUBDIRECTORY=$1
 REV_LIST_PARAMS=${@:2}
+[[ ${@:3} == "--filter-commits" ]] && FILTER_COMMITS=true
+if [[ $REV_LIST_PARAMS == "--filter-commits" ]]; then
+    REV_LIST_PARAMS=''
+    FILTER_COMMITS=true
+fi
+
 echo "Rewriting history into a subdirectory '$SUBDIRECTORY'"
+
+if [[ $FILTER_COMMITS == true ]]; then
+    echo "Filter commits list:"
+    COMMITS_LIST=$(cat)
+    if [ -z "$COMMITS_LIST" ]; then
+        echo "Commits list should be passed. E.g. git rev-list <options> | rewrite_history_into.sh"
+        exit 1
+    fi
+    echo "$(echo "$COMMITS_LIST" | wc -l) item(s)"
+fi
+
 # All paths in the index are prefixed with a subdirectory and the index is updated
 # Previous index file is replaced by a new one (otherwise each file would be in the index twice)
 # The tags are rewritten as well as commits (the "cat" command will use original name without any change)
-SUBDIRECTORY_SED=${SUBDIRECTORY//-/\\-} TAB=$'\t' git filter-branch \
+COMMITS_LIST=$COMMITS_LIST SUBDIRECTORY_SED=${SUBDIRECTORY//-/\\-} TAB=$'\t' git filter-branch \
     --index-filter '
-    git ls-files -s | sed "s-$TAB\"*-&$SUBDIRECTORY_SED/-" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && if [ -f "$GIT_INDEX_FILE.new" ]; then mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"; fi' \
+    if [ -z "$COMMITS_LIST" ] || [ ! -z $(echo "$COMMITS_LIST" | grep "$GIT_COMMIT") ]; then
+        git ls-files -s | sed "s-$TAB\"*-&$SUBDIRECTORY_SED/-" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && if [ -f "$GIT_INDEX_FILE.new" ]; then mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"; fi
+    fi' \
     --tag-name-filter 'cat' \
     -- $REV_LIST_PARAMS
