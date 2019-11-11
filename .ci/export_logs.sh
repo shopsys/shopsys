7413dc1c8b6236998d88c1f2b782be7a1bb5ff12
David Stritezsky <david.stritezsky@shopsys.com>
2019-11-11 21:53:48 +0100

    fixed copying codeception logs from pod

--- .ci/export_logs.sh
+++ .ci/export_logs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 # used to copy logs of codeception which are not streamed
 WEBSERVER_PHP_FPM_CONTAINER_NAME="webserver-php-fpm"
@@ -15,6 +15,8 @@ for POD_NAME in $(kubectl get pods -n ${JOB_NAME} | grep -v ^NAME | cut -f 1 -d 
     if [[ ${POD_NAME} == *${WEBSERVER_PHP_FPM_CONTAINER_NAME}* ]]; then
         # copy codeception logs from php-fpm pod to local
         # we do not need to specify the php-fpm container because it is picked by default
-        kubectl cp ${JOB_NAME}/${POD_NAME}:/var/www/html/project-base/var/logs/codeception/ ${WORKSPACE}/logs/codeception
+        if [[ -n $(kubectl -n ${JOB_NAME} exec ${POD_NAME} -- ls -1 var/logs |grep '^codeception$') ]]; then
+            kubectl cp ${JOB_NAME}/${POD_NAME}:/var/www/html/project-base/var/logs/codeception/ ${WORKSPACE}/logs/codeception
+        fi
     fi
 done
