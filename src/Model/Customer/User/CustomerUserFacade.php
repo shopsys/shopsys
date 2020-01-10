25e6f3221df526cb67c9b211f49a86a9883dda43
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-01-10 08:27:32 +0100

    add customer to newsletter table if he approve newsletter

--- src/Model/Customer/User/CustomerUserFacade.php
+++ src/Model/Customer/User/CustomerUserFacade.php
@@ -6,10 +6,43 @@ declare(strict_types=1);
 namespace App\Model\Customer\User;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+use Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Customer\BillingAddressFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
+use Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Customer\Mail\CustomerMailFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserFacade as BaseCustomerUserFacade;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserPasswordFacade;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserRepository;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserUpdateDataFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Newsletter\NewsletterFacade;
 
 class CustomerUserFacade extends BaseCustomerUserFacade
 {
+    /**
+     * @var NewsletterFacade
+     */
+    private $newsletterFacade;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        CustomerUserRepository $customerUserRepository,
+        CustomerUserUpdateDataFactoryInterface $customerUserUpdateDataFactory,
+        CustomerMailFacade $customerMailFacade,
+        BillingAddressFactoryInterface $billingAddressFactory,
+        DeliveryAddressFactoryInterface $deliveryAddressFactory,
+        BillingAddressDataFactoryInterface $billingAddressDataFactory,
+        CustomerUserFactoryInterface $customerUserFactory,
+        CustomerUserPasswordFacade $customerUserPasswordFacade,
+        CustomerFacade $customerFacade,
+        NewsletterFacade $newsletterFacade
+    ){
+        parent::__construct($em, $customerUserRepository, $customerUserUpdateDataFactory, $customerMailFacade, $billingAddressFactory, $deliveryAddressFactory, $billingAddressDataFactory, $customerUserFactory, $customerUserPasswordFacade, $customerFacade);
+        $this->newsletterFacade = $newsletterFacade;
+    }
+
 
     /**
      * @param \App\Model\Customer\User\CustomerUserData $customerUserData
@@ -27,12 +60,18 @@ class CustomerUserFacade extends BaseCustomerUserFacade
         $customer = $this->customerFacade->createCustomerWithBillingAddress($billingAddress);
         $customerUserData->customer = $customer;
         $deliveryAddress = null;
-
+        /**
+         * @var CustomerUser $customerUser
+         */
         $customerUser = $this->customerUserFactory->create($customerUserData, $deliveryAddress);
         $this->setEmail($customerUserData->email, $customerUser);
 
         $this->em->persist($customerUser);
         $this->em->flush();
+
+        if($customerUser->isAdvertisingApproval()){
+            $this->newsletterFacade->addSubscribedEmail($customerUser->getEmail(), $customerUser->getDomainId());
+        }
 
         $this->customerMailFacade->sendRegistrationMail($customerUser);
         return $customerUser;
