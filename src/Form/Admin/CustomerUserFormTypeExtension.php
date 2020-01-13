9aa4d06880a2468e9c5c6e746e5d9787464e8072
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-01-13 12:39:31 +0100

    add gender to admin customer form

--- src/Form/Admin/CustomerUserFormTypeExtension.php
+++ src/Form/Admin/CustomerUserFormTypeExtension.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Form\Admin;
 
+use App\Model\Customer\User\CustomerUser;
 use Shopsys\FrameworkBundle\Form\Admin\Customer\User\CustomerUserFormType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CustomerUserFormTypeExtension extends AbstractTypeExtension
 {
@@ -15,6 +18,14 @@ class CustomerUserFormTypeExtension extends AbstractTypeExtension
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $personalDataBuilder = $builder->get('personalData');
+        $personalDataBuilder->add('gender', ChoiceType::class, [
+            'choices' => array_flip(CustomerUser::getAllGenders()),
+            'placeholder' => t('-- Vyber oslovení --'),
+            'constraints' => [
+                new NotBlank(['message' => 'Please choose your gender']),
+            ],
+        ]);
     }
 
     /**
