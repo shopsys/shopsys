151d18121354c635c0fc9317473b72b4e0348b70
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-01-27 10:49:51 +0100

    friendly url

---
+++ src/Model/Product/Series/ProductSeriesFriendlyUrlProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Model\Product\Series;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
+use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
+use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrl;
+use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlDataFactoryInterface;
+use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlDataProviderInterface;
+use Shopsys\FrameworkBundle\Model\Article\Article;
+
+class ProductSeriesFriendlyUrlProvider implements FriendlyUrlDataProviderInterface
+{
+    public const ROUTE_NAME = 'front_productseries_detail';
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $em;
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlDataFactoryInterface
+     */
+    private $friendlyUrlDataFactory;
+
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlDataFactoryInterface $friendlyUrlDataFactory
+     */
+    public function __construct(EntityManagerInterface $em, FriendlyUrlDataFactoryInterface $friendlyUrlDataFactory)
+    {
+        $this->em = $em;
+        $this->friendlyUrlDataFactory = $friendlyUrlDataFactory;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig $domainConfig
+     * @return array
+     */
+    public function getFriendlyUrlData(DomainConfig $domainConfig): array
+    {
+        $queryBuilder = $this->em->createQueryBuilder()
+            ->select('psd.productSeries, psd.seoH1')
+            ->distinct()
+            ->from(ProductSeriesDomain::class, 'psd')
+            ->leftJoin(FriendlyUrl::class, 'f', Join::WITH, 'psd.productSeriesId = f.entityId AND f.routeName = :routeName AND f.domainId = psd.domainId')
+            ->setParameter('routeName', static::ROUTE_NAME)
+            ->where('f.entityId IS NULL AND psd.domainId = :domainId')
+            ->setParameter('domainId', $domainConfig->getId());
+        $scalarData = $queryBuilder->getQuery()->getScalarResult();
+
+        $friendlyUrlsData = [];
+
+        foreach ($scalarData as $data) {
+            $friendlyUrlsData[] = $this->friendlyUrlDataFactory->createFromIdAndName($data['id'], $data['name']);
+        }
+        return $friendlyUrlsData;
+    }
+
+    public function getRouteName(): string
+    {
+        return self::ROUTE_NAME;
+    }
+
+
+}
