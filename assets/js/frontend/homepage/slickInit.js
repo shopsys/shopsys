92b7e100df0b5cc5401e6479ad63696d7ea7edce
Tomáš Fejfar <tomas.fejfar@scontodigital.cz>
2020-02-02 17:17:05 +0100

    ESHOP-228 - slider na titulce + leták a prodejny

--- assets/js/frontend/homepage/slickInit.js
+++ assets/js/frontend/homepage/slickInit.js
@@ -3,11 +3,37 @@ import 'slick-carousel';
 import Register from 'framework/common/utils/register';
 
 export default function slickInit () {
-    $('#js-slider-homepage').not('.slick-initialized').slick({
-        dots: true,
+    const $hpSlider = $('#js-slider-homepage');
+    const $hpSliderThumbnails = $('#js-slider-homepage-thumbnails');
+
+    $hpSlider.not('.slick-initialized').slick({
+        dots: false,
         arrows: false,
+        fade: false,
         autoplay: true,
-        autoplaySpeed: 4000
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: $hpSliderThumbnails
+    });
+
+    $hpSliderThumbnails.not('.slick-initialized').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: $hpSlider,
+        dots: true,
+        focusOnSelect: true
+    });    
+
+    // slick-active-current - slick carousel always gives slick-active to asNavFor element,
+    // so we must give our special active class.
+    $hpSliderThumbnails.find('.slick-slide').removeClass('slick-active-current');
+    $hpSliderThumbnails.find('.slick-slide').eq(0).addClass('slick-active-current');
+
+    // On before slide change match active thumbnail to current slide
+    $hpSlider.on('afterChange', function (event, slick, currentSlide, nextSlide) {                
+        $hpSliderThumbnails.find('.slick-slide').removeClass('slick-active-current');
+        $hpSliderThumbnails.find('.slick-slide').eq(currentSlide).addClass('slick-active-current');
     });
 }
 
