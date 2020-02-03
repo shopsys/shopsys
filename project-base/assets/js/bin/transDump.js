8fd4a7cbcf611546f0069ea3c27981533d053ace
Tomas Pesek <tomas.pesek@shopsys.com>
2020-02-03 10:52:19 +0100

    set export folder path as a cli parameter

--- project-base/assets/js/bin/transDump.js
+++ project-base/assets/js/bin/transDump.js
@@ -3,21 +3,31 @@
 const fs = require('fs');
 const processTransDump = require('../commands/translations/processDump');
 const defaultDirWithJsFiles = './assets/js/';
-const dirForExportDumpFile = './var/translations/';
 
 const dirsWithJsFiles = process.argv.slice(2).map(parameter => {
     const keyValueParameter = parameter.split('=');
     if (keyValueParameter[0] === 'source-dir') {
         return keyValueParameter[1];
     }
-});
+}).filter(item => item !== undefined);
 
 if (dirsWithJsFiles.length === 0) {
     dirsWithJsFiles.push(defaultDirWithJsFiles + '**/*.js');
 }
 
-if (!fs.existsSync(dirForExportDumpFile)) {
-    fs.mkdirSync(dirForExportDumpFile);
+let dirForExport = process.argv.slice(2).map(parameter => {
+    const keyValueParameter = parameter.split('=');
+    if (keyValueParameter[0] === 'export-dir') {
+        return keyValueParameter[1];
+    }
+}).filter(item => item !== undefined)[0];
+
+if (Array.isArray(dirForExport)) {
+    dirForExport = dirForExport[0];
 }
 
-processTransDump(dirsWithJsFiles, dirForExportDumpFile);
+if (!fs.existsSync(dirForExport)) {
+    fs.mkdirSync(dirForExport);
+}
+
+processTransDump(dirsWithJsFiles, dirForExport);
