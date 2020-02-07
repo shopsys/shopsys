79ba8d40801670ac485874c3ac2b7f12076580fe
Martin Grossmann <martin@vyvoj.net>
2020-02-07 08:49:22 +0100

    added parameter events

---
+++ packages/framework/src/Model/Product/Parameter/ParameterEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Product\Parameter;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class ParameterEvent extends Event
+{
+    /**
+     * The CREATE event occurs once a parameter was created.
+     *
+     * This event allows you to run jobs dependent on the parameter creation.
+     */
+    public const CREATE = 'parameter.create';
+    /**
+     * The UPDATE event occurs once a parameter was changed.
+     *
+     * This event allows you to run jobs dependent on the parameter change.
+     */
+    public const UPDATE = 'parameter.update';
+    /**
+     * The DELETE event occurs once a parameter was deleted.
+     *
+     * This event allows you to run jobs dependent on the parameter deletion.
+     */
+    public const DELETE = 'parameter.delete';
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Product\Parameter\Parameter
+     */
+    protected $parameter;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Parameter\Parameter $parameter
+     */
+    public function __construct(Parameter $parameter)
+    {
+        $this->parameter = $parameter;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Product\Parameter\Parameter
+     */
+    public function getParameter(): Parameter
+    {
+        return $this->parameter;
+    }
+}
