f32e0d484460a8d7413130b2dc65a741343547d3
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2020-02-12 13:16:07 +0100

    design - side menu open/close function

--- packages/framework/assets/js/admin/components/SideMenu.js
+++ packages/framework/assets/js/admin/components/SideMenu.js
@@ -3,19 +3,23 @@ import Register from '../../common/utils/Register';
 export default class SideMenu {
 
     constructor ($sideMenu) {
-        this.$elementPanelClass = '.js-web-panel';
-
+        this.$webPanel = $('.js-web-panel');
         this.$sideMenu = $sideMenu;
         this.$items = this.$sideMenu.filterAllNodes('.js-side-menu-item');
+        const _this = this;
 
-        this.$items.click(function () {
-            if ($(this).hasClass('open')) {
-                $(this).parents(this.$elementPanelClass).removeClass('open');
+        this.$items.click( event => {
+            if($(event.currentTarget).hasClass('open')){
+                $(event.currentTarget).filterAllNodes('.js-side-menu-submenu').addClass('hidden');
+                $(event.currentTarget).removeClass('open');
+                _this.$webPanel.removeClass('open');
+                _this.$webPanel.filterAllNodes('.js-side-menu-submenu').addClass('hidden');
+                _this.$webPanel.filterAllNodes('.js-side-menu-item').removeClass('open');
             } else {
-                $(this).parents(this.$elementPanelClass).addClass('open');
+                $(event.currentTarget).filterAllNodes('.js-side-menu-submenu').removeClass('hidden');
+                $(event.currentTarget).addClass('open');
+                _this.$webPanel.addClass('open');
             }
-            $(this).filterAllNodes('.js-side-menu-submenu').toggle();
-            $(this).toggleClass('open');
         });
 
         if (this.$sideMenu.hasClass('js-side-menu-close-after-mouseleave')) {
@@ -32,13 +36,13 @@ export default class SideMenu {
     }
 
     closeMenus () {
-        this.$sideMenu.removeClass('open');
-        this.$items.filterAllNodes('.js-side-menu-submenu').hide();
+        this.$webPanel.filterAllNodes('.js-side-menu-submenu').addClass('hidden');
         this.$items.removeClass('open');
+        this.$webPanel.removeClass('open');
     }
 
     static init ($container) {
-        $container.filterAllNodes(this.$elementPanelClass).each(function () {
+        $container.filterAllNodes('.js-side-menu').each(function () {
             // eslint-disable-next-line no-new
             new SideMenu($(this));
         });
