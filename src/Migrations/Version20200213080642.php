38679476812a5e13ab5654f470e4af4ac2b7489a
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-02-13 11:55:22 +0100

    update tests by changed main VAT calculation e-shop setup(inserted prices are with VAT)

--- src/Migrations/Version20200213080642.php
+++ src/Migrations/Version20200213080642.php
@@ -9,7 +9,6 @@ use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 class Version20200213080642 extends AbstractMigration
 {
-
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
