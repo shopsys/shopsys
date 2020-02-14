023a616aaf7ac55f175ac79876bc35a0c0be888f
Adam Tyroň <adam.tyron@shopsys.com>
2020-02-14 15:39:11 +0100

    ScontoBridge: disable fields in admin

---
+++ src/Form/Admin/Customer/DeliveryAddressFormTypeExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Admin\Customer;
+
+use App\Component\Form\FormBuilderHelper;
+use Shopsys\FrameworkBundle\Form\Admin\Customer\DeliveryAddressFormType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DeliveryAddressFormTypeExtension extends AbstractTypeExtension
+{
+    private const DISABLED_FIELDS = [
+        'addressFilled',
+        'deliveryAddressFields',
+    ];
+
+    /**
+     * @var \App\Component\Form\FormBuilderHelper
+     */
+    private $formBuilderHelper;
+
+    /**
+     * @param \App\Component\Form\FormBuilderHelper $formBuilderHelper
+     */
+    public function __construct(FormBuilderHelper $formBuilderHelper)
+    {
+        $this->formBuilderHelper = $formBuilderHelper;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $this->formBuilderHelper->disableFieldsByConfigurations($builder, self::DISABLED_FIELDS);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExtendedType(): string
+    {
+        return DeliveryAddressFormType::class;
+    }
+}
