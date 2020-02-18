74ec85e5bbb2390f2b95e4e2ac5d408338e2dedf
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2020-02-18 13:19:55 +0100

    design - left menu for mobile, collapse for desktop

--- packages/framework/assets/js/admin/components/SideMenu.js
+++ packages/framework/assets/js/admin/components/SideMenu.js
@@ -4,6 +4,9 @@ export default class SideMenu {
 
     constructor ($sideMenu) {
         this.$webPanel = $('.js-web-panel');
+        this.$sideMenuMobile = $('.js-side-menu-mobile');
+        this.$sideMenuOverlay = $('.js-side-menu-overlay');
+        this.$sideMenuCollapseButton = $('.js-side-menu-collapse-button');
         this.$sideMenu = $sideMenu;
         this.$items = this.$sideMenu.filterAllNodes('.js-side-menu-item');
         const _this = this;
@@ -25,6 +28,33 @@ export default class SideMenu {
         if (this.$sideMenu.hasClass('js-side-menu-close-after-mouseleave')) {
             this.closeMenusAfterMouseleave(500);
         }
+
+        // show left menu on mobile devices
+        this.$sideMenuMobile.click(event => {
+            if (!$('body').hasClass('active-menu')) {
+                $('body').addClass('active-menu');
+                this.$webPanel.addClass('open');
+            } else {
+                $('body').removeClass('active-menu');
+                this.$webPanel.removeClass('open');
+            }
+        });
+
+        // close left menu on overlay click
+        this.$sideMenuOverlay.click(event => {
+            $('body').removeClass('active-menu');
+            this.$webPanel.removeClass('open');
+        });
+
+        // show/hide left menu on desktop
+        this.$sideMenuCollapseButton.click(event => {
+            $('body').toggleClass('menu-collapsed');
+            if (!this.$webPanel.hasClass('active-menu')) {
+                this.$webPanel.removeClass('open');
+            } else {
+                this.$webPanel.addClass('open');
+            }
+        });
     }
 
     closeMenusAfterMouseleave (timoutMilliseconds) {
