66c703b1cf6ba3bfb0298f2932d211ec320c15a7
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-02-19 16:09:05 +0100

    correct generators usage suggestion

--- src/Model/Category/Transfer/Akeneo/AkeneoImportCategoryFacade.php
+++ src/Model/Category/Transfer/Akeneo/AkeneoImportCategoryFacade.php
@@ -120,15 +120,6 @@ class AkeneoImportCategoryFacade extends AbstractAkeneoImportTransfer
      */
     protected function processItem($akeneoCategoryData): void
     {
-        $this->logger->addInfo('Test info');
-        $this->logger->addNotice('Test notice');
-        $this->logger->addError('Test error');
-        $this->logger->addAlert('Test alert');
-        $this->logger->addDebug('Test debug');
-        $this->logger->addEmergency('Test emergency');
-        $this->logger->addCritical('Test critical');
-        $this->logger->addWarning('Test warning');
-
         if ($akeneoCategoryData['code'] === self::ROOT_CATEGORY_CODE) {
             return;
         }
