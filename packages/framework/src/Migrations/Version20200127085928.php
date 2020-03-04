b897f2ce98ed62782bf58adc90d087a159e83a8c
Petr Kadlec <petr.kadlec@shopsys.com>
2020-03-04 14:39:53 +0100

    fix wrong migration on database with data (#1712)

--- packages/framework/src/Migrations/Version20200127085928.php
+++ packages/framework/src/Migrations/Version20200127085928.php
@@ -15,13 +15,13 @@ class Version20200127085928 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->sql('ALTER TABLE delivery_addresses ADD customer_id INT NOT NULL DEFAULT 0');
+        $this->sql('UPDATE delivery_addresses SET customer_id = (SELECT customer_id FROM customer_users WHERE delivery_address_id = delivery_addresses.id)');
         $this->sql('
             ALTER TABLE
                 delivery_addresses
             ADD
                 CONSTRAINT FK_2BAF39849395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->sql('CREATE INDEX IDX_2BAF39849395C3F3 ON delivery_addresses (customer_id)');
-        $this->sql('UPDATE delivery_addresses SET customer_id = (SELECT customer_id FROM customer_users WHERE delivery_address_id = delivery_addresses.id)');
         $this->sql('ALTER TABLE customer_users DROP delivery_address_id');
         $this->sql('ALTER TABLE delivery_addresses ALTER customer_id DROP DEFAULT');
     }
