cffe3fa2f93d2ec83d150a49fad0342a5ba17b52
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2020-03-17 12:02:38 +0100

    WIP: design - webpack config

--- project-base/webpack.config.js
+++ project-base/webpack.config.js
@@ -1,8 +1,13 @@
 const Encore = require('@symfony/webpack-encore');
 const EventHooksPlugin = require('event-hooks-webpack-plugin');
 const processTrans = require('./assets/js/commands/translations/process');
+const optimizeSvg = require('./assets/js/commands/svg/optimizeSvg');
+const GenerateWebFont = require('./assets/js/commands/svg/generateWebFont');
 const CopyPlugin = require('copy-webpack-plugin');
+const yaml = require('js-yaml');
+const fs = require('fs');
 const path = require('path');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
@@ -48,131 +53,38 @@ Encore
     }))
     .addPlugin(new CopyPlugin([
         { from: 'web/bundles/fpjsformvalidator', to: '../../assets/js/bundles/fpjsformvalidator', force: true },
-        { from: 'web/bundles/shopsysframework', to: '../../web/assets/admin/images', force: true }
+        { from: 'assets/public', to: '../../web/public', force: true }
     ]))
 ;
 
-// Frontend Config
-const WebfontsGenerator = require('webfonts-generator');
-const StylelintPlugin = require('stylelint-webpack-plugin');
-var requireContext = require('require-context');
-yaml = require('js-yaml');
-fs = require('fs');
-
-SVGO = require('svgo');
-const svgo = new SVGO();
-
-const svgFilesFrontend = requireContext('../../src/Resources/svg/front', false, '.svg');
-var svgFilesFrontendPath = [];
-svgFilesFrontend.keys().forEach((filePathFrontend) => {
-    var newFileFrontendPath = './src/Resources/svg/front/' + filePathFrontend;
-    svgFilesFrontendPath.push(newFileFrontendPath);
-
-    fs.readFile(newFileFrontendPath, 'utf8', function (err, data) {
-        if (err) {
-            throw err;
-        }
-        svgo.optimize(data, { path: newFileFrontendPath }).then(function (result) {
-            fs.writeFile(newFileFrontendPath, result.data, function (err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('Frontend SVG icon ' + newFileFrontendPath + ' optimized');
-            });
-        });
-    });
-});
-
-const svgFilesAdmin = requireContext('../../../packages/framework/src/Resources/svg/admin', false, '.svg');
-var svgFilesAdminPath = [];
-svgFilesAdmin.keys().forEach((filePathAdmin) => {
-    var newFileAdminPath = '../packages/framework/src/Resources/svg/admin/' + filePathAdmin;
-    svgFilesAdminPath.push(newFileAdminPath);
-
-    fs.readFile(newFileAdminPath, 'utf8', function (err, data) {
-        if (err) {
-            throw err;
-        }
-        svgo.optimize(data, { path: newFileAdminPath }).then(function (result) {
-            fs.writeFile(newFileAdminPath, result.data, function (err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('Admin SVG icon ' + newFileAdminPath + ' optimized');
-            });
-        });
-    });
-});
+var svgFilesFrontendPath = optimizeSvg('frontend');
+var svgFilesAdminPath = optimizeSvg('admin');
 
 const domainFile = './config/domains.yml';
-var domains = yaml.safeLoad(fs.readFileSync(domainFile, 'utf8'));
+const domains = yaml.safeLoad(fs.readFileSync(domainFile, 'utf8'));
 
 domains.domains.forEach((domain) => {
     if (!domain.styles_directory) {
         domain.styles_directory = 'common';
     }
     Encore
-        .addEntry('frontend-style-' + domain.styles_directory, './src/Resources/styles/front/' + domain.styles_directory + '/main.less')
-        .addEntry('frontend-print-style-' + domain.styles_directory, './src/Resources/styles/front/' + domain.styles_directory + '/print/main.less');
+        .addEntry('frontend-style-' + domain.styles_directory, './assets/styles/frontend/' + domain.styles_directory + '/main.less')
+        .addEntry('frontend-print-style-' + domain.styles_directory, './assets/styles/frontend/' + domain.styles_directory + '/print/main.less');
 });
 
 Encore
-    .addEntry('admin-style', '../packages/framework/src/Resources/styles/admin/main.less')
+    .addEntry('admin-style', './assets/styles/admin/main.less')
     .addPlugin(
         new StylelintPlugin({
             configFile: '.stylelintrc',
-            files: 'src/**/*.less'
+            files: 'assets/styles/**/*.less'
         })
     )
     .addLoader(
-        new WebfontsGenerator({
-            files: svgFilesFrontendPath,
-            dest: 'web/assets/frontend/fonts',
-            cssDest: 'src/Resources/styles/front/common/libs/svg.less',
-            cssFontsUrl: '/assets/frontend/fonts',
-            fontName: 'svg',
-            fontHeight: '512',
-            fixedWidth: '512',
-            centerHorizontally: true,
-            normalize: true,
-            html: true,
-            htmlDest: 'docs/generated/webfont-frontend-svg.html',
-            templateOptions: {
-                baseSelector: '.svg',
-                classPrefix: 'svg-'
-            }
-        }, function (error) {
-            if (error) {
-                console.log('Frontend SVG Fail!', error);
-            } else {
-                console.log('Frontend SVG generated!');
-            }
-        })
+        new GenerateWebFont('frontend', svgFilesFrontendPath)
     )
     .addLoader(
-        new WebfontsGenerator({
-            files: svgFilesAdminPath,
-            dest: 'web/assets/admin/fonts',
-            cssDest: '../packages/framework/src/Resources/styles/admin/libs/svg.less',
-            cssFontsUrl: '/assets/admin/fonts',
-            fontName: 'svg',
-            fontHeight: '512',
-            fixedWidth: '512',
-            centerHorizontally: true,
-            normalize: true,
-            html: true,
-            htmlDest: 'docs/generated/webfont-admin-svg.html',
-            templateOptions: {
-                baseSelector: '.svg',
-                classPrefix: 'svg-'
-            }
-        }, function (error) {
-            if (error) {
-                console.log('Admin SVG Fail!', error);
-            } else {
-                console.log('Admin SVG generated!');
-            }
-        })
+        new GenerateWebFont('admin', svgFilesAdminPath)
     )
     .addLoader({
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
@@ -185,8 +97,9 @@ Encore
 const config = Encore.getWebpackConfig();
 
 config.resolve.alias = {
-    'jquery-ui': 'jquery-ui/ui/widgets/',
-    'framework': '@shopsys/framework/js',
+    'jquery-ui': 'jquery-ui/ui/widgets',
+    'jquery-ui-styles': 'jquery-ui',
+    'framework': '@shopsys/framework/assets/js',
     'jquery': path.resolve(path.join(__dirname, 'node_modules', 'jquery'))
 };
 module.exports = config;
