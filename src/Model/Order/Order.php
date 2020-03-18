b33e79b28f9ff7da95ddad9206978f5daa59d404
Petr Kadlec <petr.kadlec@shopsys.com>
2020-03-18 12:36:15 +0100

    remove unnecessary method overriding

--- src/Model/Order/Order.php
+++ src/Model/Order/Order.php
@@ -136,19 +136,4 @@ class Order extends BaseOrder
 
         return $transports;
     }
-
-    /**
-     * @return \App\Model\Order\Item\OrderItem[]
-     */
-    public function getTransportAndPaymentItems(): array
-    {
-        $items = [];
-        foreach ($this->getItems() as $item) {
-            if ($item->isTypeTransport() === true || $item->isTypePayment() === true) {
-                $items[] = $item;
-            }
-        }
-
-        return $items;
-    }
 }
