e6461973d90f79b3e73a4b7a5d40412e6da231bb
Tomas Pesek <tomas.pesek@shopsys.com>
2020-04-14 12:17:56 +0200

    removed duplicity svg files (files are copied from framework in webpack config)

--- project-base/assets/js/commands/svg/optimizeSvg.js
+++ project-base/assets/js/commands/svg/optimizeSvg.js
@@ -1,33 +1,42 @@
-const glob = require('glob');
 const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
 const SVGO = require('svgo');
 
-const svgo = new SVGO();
+const writeSvg = (svgOptimizedFile, optimizedSvg) => {
+    try {
+        fs.writeFileSync(svgOptimizedFile, optimizedSvg);
+    } catch (err) {
+        console.log('ERROR: SVG icon ' + svgOptimizedFile + ' optimize failed');
+    }
+};
 
-function optimizeSvg (svgSourceFolder) {
+function optimizeSvg (svgSourceFolder, svgDestinationFolder = null) {
+    const svgo = new SVGO();
+
+    if (svgDestinationFolder === null) {
+        svgDestinationFolder = svgSourceFolder;
+    }
+
+    if (!fs.existsSync(svgDestinationFolder)) {
+        fs.mkdirSync(svgDestinationFolder, { recursive: true });
+    }
+
     return new Promise((resolve, reject) => {
-        return glob(svgSourceFolder, null, (err, svgFiles) => {
+        glob(svgSourceFolder + '**/*.svg', null, (err, svgFiles) => {
             if (err) {
                 reject(err);
             }
 
-            svgFiles.forEach(svgFile => {
-                fs.readFile(svgFile, 'utf8', function (err, data) {
-                    if (err) {
-                        throw err;
-                    }
-                    svgo.optimize(data, { path: svgFile }).then(function (result) {
-                        fs.writeFile(svgFile, result.data, function (errorOptimize) {
-                            if (errorOptimize) {
-                                console.log('ERROR: SVG icon ' + svgFile + ' optimize failed');
-                                throw errorOptimize;
-                            }
-                        });
-                    });
-                });
+            const svgOptimizedFiles = svgFiles.map(svgFile => {
+                const svgOptimizedFile = svgDestinationFolder + path.basename(svgFile);
+                const optimizedSvg = fs.readFileSync(svgFile, 'utf8');
+                svgo.optimize(optimizedSvg, { path: svgFile }).then(result => writeSvg(svgOptimizedFile, result.data));
+
+                return svgOptimizedFile;
             });
 
-            resolve(svgFiles);
+            resolve(svgOptimizedFiles);
         });
     });
 }
