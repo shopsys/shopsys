ab1f68532e84203045c75fd909c23c62e0ceb241
Tomas Pesek <tomas.pesek@shopsys.com>
2020-04-22 15:53:06 +0200

    fixup! fix dump function

--- project-base/app/autoload.php
+++ project-base/app/autoload.php
@@ -6,7 +6,13 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $symfonyDumpFunctionPath = 'vendor/symfony/var-dumper/Resources/functions/dump.php';
 
-file_exists(__DIR__ . '/../' . $symfonyDumpFunctionPath) ? require_once __DIR__ . '/../' . $symfonyDumpFunctionPath : require_once __DIR__ . '/../../' . $symfonyDumpFunctionPath;
+if (file_exists(__DIR__ . '/../' . $symfonyDumpFunctionPath)) {
+    require_once __DIR__ . '/../' . $symfonyDumpFunctionPath;
+}
+
+if (file_exists(__DIR__ . '/../../' . $symfonyDumpFunctionPath)) {
+    require_once __DIR__ . '/../../' . $symfonyDumpFunctionPath;
+}
 
 /* @var \Composer\Autoload\ClassLoader $loader */
 $loader = file_exists(__DIR__ . '/../vendor/autoload.php') ? require __DIR__ . '/../vendor/autoload.php' : require __DIR__ . '/../../vendor/autoload.php';
