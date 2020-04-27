b19c8922c92438edcd2f1c6a15c589f41dba8eec
Tomas Pesek <tomas.pesek@shopsys.com>
2020-04-27 15:28:58 +0200

    import jquery-ui-touch-punch in admin and frontend js endpoits

--- project-base/assets/js/admin/admin.js
+++ project-base/assets/js/admin/admin.js
@@ -1,4 +1,6 @@
 import '../jQuery/registerJquery';
+import 'jquery-ui/ui/widgets/mouse';
+import 'jquery-ui-touch-punch';
 import registerAdmin from 'framework/admin/registerAdmin';
 import '../loadTranslations';
 
