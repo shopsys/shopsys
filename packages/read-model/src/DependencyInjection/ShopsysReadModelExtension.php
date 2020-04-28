9cdb1f5c04665538e021add286f3c46a7c8e111d
Martin Grossmann <martin@vyvoj.net>
2020-04-28 15:36:35 +0200

    unified config extensions to yaml in read-model package

--- packages/read-model/src/DependencyInjection/ShopsysReadModelExtension.php
+++ packages/read-model/src/DependencyInjection/ShopsysReadModelExtension.php
@@ -17,6 +17,6 @@ class ShopsysReadModelExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
