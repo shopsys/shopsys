672983fe6fab7b9993a1906fd41a31c11769b6b4
Tomas Pesek <tomas.pesek@shopsys.com>
2020-05-04 14:37:01 +0200

    added undefined correction in number.js

--- packages/framework/assets/js/common/utils/number.js
+++ packages/framework/assets/js/common/utils/number.js
@@ -1,4 +1,8 @@
 export const parseNumber = (value) => {
+    if (!value) {
+        return null;
+    }
+
     const compareValue = value.toString().replace(',', '.');
     const regexpNumber = /^[-+]?[0-9]+((\.|,)?[0-9]+)?$/;
     if (regexpNumber.test(compareValue)) {
