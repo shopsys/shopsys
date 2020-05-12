33e2bf182534b0136522a2e6075e3e7427c0519e
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-05-12 10:44:04 +0200

    trailing slash remove from all routes

---
+++ src/Migrations/Version20200512082050.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20200512082050 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('
+            UPDATE friendly_urls 
+            SET slug = LEFT(slug, -1) 
+            WHERE RIGHT(slug, 1) = \'/\'
+        ');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
