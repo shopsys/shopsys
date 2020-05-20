b1dc5e6a781b5226a5e43c1bdda951ea56114ea0
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-05-20 13:30:20 +0200

    MANAGE-65 fixed rounding for form transformer, too

--- src/Form/Front/Product/ProductFilterFormType.php
+++ src/Form/Front/Product/ProductFilterFormType.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Form\Front\Product;
 
+use App\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Form\Constraints\NotNegativeMoneyAmount;
+use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
 use Shopsys\FrameworkBundle\Model\Product\Filter\ProductFilterConfig;
 use Shopsys\FrameworkBundle\Model\Product\Filter\ProductFilterData;
 use Symfony\Component\Form\AbstractType;
@@ -18,6 +20,26 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ProductFilterFormType extends AbstractType
 {
     /**
+     * @var \App\Component\Domain\Domain
+     */
+    private $domain;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade
+     */
+    private $currencyFacade;
+
+    /**
+     * @param \App\Component\Domain\Domain $domain
+     * @param \Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade $currencyFacade
+     */
+    public function __construct(Domain $domain, CurrencyFacade $currencyFacade)
+    {
+        $this->domain = $domain;
+        $this->currencyFacade = $currencyFacade;
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
      */
@@ -26,6 +48,7 @@ class ProductFilterFormType extends AbstractType
         /** @var \Shopsys\FrameworkBundle\Model\Product\Filter\ProductFilterConfig $config */
         $config = $options['product_filter_config'];
 
+        $currency = $this->currencyFacade->getDomainDefaultCurrencyByDomainId($this->domain->getId());
         $builder
             ->add('minimalPrice', MoneyType::class, [
                 'required' => false,
@@ -33,6 +56,8 @@ class ProductFilterFormType extends AbstractType
                 'constraints' => [
                     new NotNegativeMoneyAmount(['message' => 'Price must be greater or equal to zero']),
                 ],
+                'scale' => $currency->getMinFractionDigits(),
+                'currency' => $currency->getCode(),
             ])
             ->add('maximalPrice', MoneyType::class, [
                 'required' => false,
@@ -40,6 +65,8 @@ class ProductFilterFormType extends AbstractType
                 'constraints' => [
                     new NotNegativeMoneyAmount(['message' => 'Price must be greater or equal to zero']),
                 ],
+                'scale' => $currency->getMinFractionDigits(),
+                'currency' => $currency->getCode(),
             ])
             ->add('parameters', ParameterFilterFormType::class, [
                 'required' => false,
