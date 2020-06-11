2dc61068b674035978cea4bdced48f29d68556d0
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-06-11 10:21:00 +0200

    mobile images for advertising system

--- src/Form/Admin/AdvertFormTypeExtension.php
+++ src/Form/Admin/AdvertFormTypeExtension.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Form\Admin;
 
 use App\Component\DateTimeHelper\DateTimeHelper;
+use App\Model\Advert\AdvertFacade;
 use Shopsys\FrameworkBundle\Form\Admin\Advert\AdvertFormType;
 use Shopsys\FrameworkBundle\Form\DatePickerType;
+use Shopsys\FrameworkBundle\Form\ImageUploadType;
+use Shopsys\FrameworkBundle\Model\Advert\Advert;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints;
 
 class AdvertFormTypeExtension extends AbstractTypeExtension
 {
@@ -18,6 +22,63 @@ class AdvertFormTypeExtension extends AbstractTypeExtension
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $this->buildVisibilityIntervalForm($builder);
+        $this->buildImageGroup($builder, $options);
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    private function buildImageGroup(FormBuilderInterface $builder, array $options): void
+    {
+        $builderImageGroup = $builder->get('image_group');
+
+        $imageConstraints = [
+            new Constraints\NotBlank([
+                'message' => 'Choose image',
+                'groups' => [AdvertFormType::VALIDATION_GROUP_TYPE_IMAGE],
+            ]),
+        ];
+
+        $builderImageGroup
+            ->add('image', ImageUploadType::class, [
+                'required' => false,
+                'image_entity_class' => Advert::class,
+                'image_type' => AdvertFacade::IMAGE_TYPE_WEB,
+                'file_constraints' => [
+                    new Constraints\Image([
+                        'mimeTypes' => ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'],
+                        'mimeTypesMessage' => 'Image can be only in JPG, GIF or PNG format',
+                        'maxSize' => '2M',
+                        'maxSizeMessage' => 'Uploaded image is to large ({{ size }} {{ suffix }}). '
+                            . 'Maximum size of an image is {{ limit }} {{ suffix }}.',
+                    ]),
+                ],
+                'constraints' => ($options['image_exists'] ? [] : $imageConstraints),
+                'label' => t('Upload new image'),
+                'entity' => $options['advert'],
+                'info_text' => t('You can upload following formats: PNG, JPG, GIF'),
+            ]);
+
+        $builderImageGroup
+            ->add('mobileImage', ImageUploadType::class, [
+                'required' => false,
+                'image_entity_class' => Advert::class,
+                'image_type' => AdvertFacade::IMAGE_TYPE_MOBILE,
+                'file_constraints' => [
+                    new Constraints\Image([
+                        'mimeTypes' => ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'],
+                        'mimeTypesMessage' => 'Image can be only in JPG, GIF or PNG format',
+                        'maxSize' => '2M',
+                        'maxSizeMessage' => 'Uploaded image is to large ({{ size }} {{ suffix }}). '
+                            . 'Maximum size of an image is {{ limit }} {{ suffix }}.',
+                    ]),
+                ],
+                'constraints' => ($options['image_exists'] ? [] : $imageConstraints),
+                'label' => t('Nahrát nový obrázek pro mobilní zařízení'),
+                'entity' => $options['advert'],
+                'info_text' => t('You can upload following formats: PNG, JPG, GIF'),
+            ]);
     }
 
     /**
