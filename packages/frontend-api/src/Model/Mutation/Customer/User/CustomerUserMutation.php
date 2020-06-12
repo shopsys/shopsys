6ee0311573b7ac399bd2b5441239a9c1e3d5cbd1
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2020-06-12 10:44:24 +0200

    added changePersonalData frontend api mutation

--- packages/frontend-api/src/Model/Mutation/Customer/User/CustomerUserMutation.php
+++ packages/frontend-api/src/Model/Mutation/Customer/User/CustomerUserMutation.php
@@ -8,9 +8,15 @@ use GraphQL\Error\UserError;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Validator\InputValidator;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser;
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserPasswordFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserRefreshTokenChainFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User\FrontendCustomerUserProvider;
+use Shopsys\FrontendApiBundle\Model\Customer\User\CustomerUserUpdateDataFactory;
+use Shopsys\FrontendApiBundle\Model\User\FrontendApiUser;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
@@ -37,21 +43,45 @@ class CustomerUserMutation implements MutationInterface, AliasedInterface
     protected $customerUserRefreshTokenChainFacade;
 
     /**
+     * @var \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
+     */
+    protected $tokenStorage;
+
+    /**
+     * @var \Shopsys\FrontendApiBundle\Model\Customer\User\CustomerUserUpdateDataFactory
+     */
+    protected $customerUserUpdateDataFactory;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserFacade
+     */
+    protected $customerUserFacade;
+
+    /**
      * @param \Shopsys\FrameworkBundle\Model\Customer\User\FrontendCustomerUserProvider $frontendCustomerUserProvider
      * @param \Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface $userPasswordEncoder
      * @param \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserPasswordFacade $customerUserPasswordFacade
      * @param \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserRefreshTokenChainFacade $customerUserRefreshTokenChainFacade
+     * @param \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface $tokenStorage
+     * @param \Shopsys\FrontendApiBundle\Model\Customer\User\CustomerUserUpdateDataFactory $customerUserUpdateDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserFacade $customerUserFacade
      */
     public function __construct(
         FrontendCustomerUserProvider $frontendCustomerUserProvider,
         UserPasswordEncoderInterface $userPasswordEncoder,
         CustomerUserPasswordFacade $customerUserPasswordFacade,
-        CustomerUserRefreshTokenChainFacade $customerUserRefreshTokenChainFacade
+        CustomerUserRefreshTokenChainFacade $customerUserRefreshTokenChainFacade,
+        TokenStorageInterface $tokenStorage,
+        CustomerUserUpdateDataFactory $customerUserUpdateDataFactory,
+        CustomerUserFacade $customerUserFacade
     ) {
         $this->frontendCustomerUserProvider = $frontendCustomerUserProvider;
         $this->userPasswordEncoder = $userPasswordEncoder;
         $this->customerUserPasswordFacade = $customerUserPasswordFacade;
         $this->customerUserRefreshTokenChainFacade = $customerUserRefreshTokenChainFacade;
+        $this->tokenStorage = $tokenStorage;
+        $this->customerUserUpdateDataFactory = $customerUserUpdateDataFactory;
+        $this->customerUserFacade = $customerUserFacade;
     }
 
     /**
@@ -67,14 +97,42 @@ class CustomerUserMutation implements MutationInterface, AliasedInterface
         try {
             $customerUser = $this->frontendCustomerUserProvider->loadUserByUsername($input['email']);
         } catch (UsernameNotFoundException $e) {
-            throw new UserError('User does not exists or provided password is not valid.');
+            throw new UserError('This account doesn\'t exist or password is incorrect');
         }
 
         if (!$this->userPasswordEncoder->isPasswordValid($customerUser, $input['oldPassword'])) {
-            throw new UserError('User does not exists or provided password is not valid.');
+            throw new UserError('This account doesn\'t exist or password is incorrect');
         }
 
         $this->customerUserPasswordFacade->changePassword($customerUser, $input['newPassword']);
+
+        return $customerUser;
+    }
+
+    /**
+     * @param \Overblog\GraphQLBundle\Definition\Argument $argument
+     * @param \Overblog\GraphQLBundle\Validator\InputValidator $validator
+     * @return \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser
+     */
+    public function changePersonalData(Argument $argument, InputValidator $validator): CustomerUser
+    {
+        $validator->validate();
+        $token = $this->tokenStorage->getToken();
+
+        if ($token === null) {
+            throw new UserError('Token is not valid.');
+        }
+
+        /** @var \Shopsys\FrontendApiBundle\Model\User\FrontendApiUser $user */
+        $user = $token->getUser();
+
+        if (!($user instanceof FrontendApiUser)) {
+            throw new UserError('Token is not valid.');
+        }
+
+        $customerUser = $this->customerUserFacade->getByUuid($user->getUuid());
+        $customerUserUpdateData = $this->customerUserUpdateDataFactory->createFromCustomerUserWithArgument($customerUser, $argument);
+        $this->customerUserFacade->editByCustomerUser($customerUser->getId(), $customerUserUpdateData);
 
         return $customerUser;
     }
@@ -86,6 +144,7 @@ class CustomerUserMutation implements MutationInterface, AliasedInterface
     {
         return [
             'changePassword' => 'customer_user_change_password',
+            'changePersonalData' => 'customer_user_change_personal_data',
         ];
     }
 }
--- project-base/tests/FrontendApiBundle/Functional/Customer/User/CurrentCustomerUserTest.php
+++ project-base/tests/FrontendApiBundle/Functional/Customer/User/CurrentCustomerUserTest.php
@@ -35,4 +35,68 @@ class CurrentCustomerUserTest extends GraphQlWithLoginTestCase
 
         $this->assertQueryWithExpectedJson($query, $jsonExpected);
     }
+
+    public function testChangePersonalData(): void
+    {
+        $query = '
+mutation {
+    ChangePersonalData(input: {
+        telephone: "123456321"
+        firstName: "John"
+        lastName: "Doe"
+    }) {
+        firstName
+        lastName,
+        telephone,
+        email
+    }
+}';
+
+        $jsonExpected = '
+{
+    "data": {
+        "ChangePersonalData": {
+            "firstName": "John",
+            "lastName": "Doe",
+            "telephone": "123456321",
+            "email": "no-reply@shopsys.com"
+        }
+    }
+}';
+
+        $this->assertQueryWithExpectedJson($query, $jsonExpected);
+    }
+
+    public function testChangePersonalDataWithWrongData(): void
+    {
+        $query = '
+mutation {
+    ChangePersonalData(input: {
+        telephone: "1234567890123456789012345678901"
+        firstName: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit ultrices molestie. Donec s"
+        lastName: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent suscipit ultrices molestie. Donec s"
+    }) {
+    firstName
+        lastName,
+        telephone,
+        email
+    }
+}';
+
+        $expectedViolationMessages = [
+            0 => 'First name cannot be longer than 100 characters',
+            1 => 'Last name cannot be longer than 100 characters',
+            2 => 'Telephone number cannot be longer than 30 characters',
+        ];
+
+        $responseData = $this->getResponseContentForQuery($query)['errors'][0]['extensions']['validation'];
+
+        $i = 0;
+        foreach ($responseData as $responseRow) {
+            foreach ($responseRow as $validationError) {
+                $this->assertEquals($expectedViolationMessages[$i], $validationError['message']);
+                $i++;
+            }
+        }
+    }
 }
