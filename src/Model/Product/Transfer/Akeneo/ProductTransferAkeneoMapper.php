92bd13326eabeb99db9d758e1321fd105f60807b
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-06-22 12:58:25 +0200

    SD-1023 updated akeneo mapper

--- src/Model/Product/Transfer/Akeneo/ProductTransferAkeneoMapper.php
+++ src/Model/Product/Transfer/Akeneo/ProductTransferAkeneoMapper.php
@@ -359,15 +359,15 @@ class ProductTransferAkeneoMapper
         $productData->packageNotIncluded = AkeneoProductHelper::mapDomainDataString($productData->packageNotIncluded, $akeneoProductData['values']['not_included'] ?? null);
 
         $productData->mountingState = AkeneoProductHelper::mapDataToAllDomains($productData->mountingState, $akeneoProductData['values']['mounting_state'][0]['data'] ?? 'false');
-        $productData->packagingUnit = AkeneoProductHelper::mapDataToAllDomains($productData->packagingUnit, $akeneoProductData['values']['packaging_unit'][0]['data'] ?? null);
-        $productData->countPackages = AkeneoProductHelper::mapDataToAllDomains($productData->countPackages, $akeneoProductData['values']['number_package'][0]['data'] ?? null);
+        //todo: ['amount'] + ['unit'] => momentalne opraveno jenom kvuli importu - ale bude asi potreba vsude (entity atd.) predelat
+        $productData->packagingUnit = AkeneoProductHelper::mapDataToAllDomains($productData->packagingUnit, $akeneoProductData['values']['packaging_unit'][0]['data']['amount'] ?? null);
+        $productData->countPackages = AkeneoProductHelper::mapDataToAllDomains($productData->countPackages, $akeneoProductData['values']['number_package'][0]['data']['amount'] ?? null);
         $productData->totalPackageWeight = AkeneoProductHelper::mapDataToAllDomains($productData->totalPackageWeight, $akeneoProductData['values']['package_weight'][0]['data']['amount'] ?? null);
 
         foreach ($productData->mountingState as $domainId => $state) {
             $productData->mountingState[$domainId] = AkeneoProductHelper::convertStingToType(str_replace('mounting_state__', '', $state), AkeneoProductHelper::TYPE_BOOLEAN);
-            //todo: ['amount'] + ['unit'] => momentalne opraveno jenom kvuli importu - ale bude asi potreba vsude (entity atd.) predelat
-            $productData->packagingUnit[$domainId] = AkeneoProductHelper::convertStingToType($productData->packagingUnit[$domainId]['amount'], AkeneoProductHelper::TYPE_INT);
-            $productData->countPackages[$domainId] = AkeneoProductHelper::convertStingToType($productData->countPackages[$domainId]['amount'], AkeneoProductHelper::TYPE_INT);
+            $productData->packagingUnit[$domainId] = AkeneoProductHelper::convertStingToType($productData->packagingUnit[$domainId], AkeneoProductHelper::TYPE_INT);
+            $productData->countPackages[$domainId] = AkeneoProductHelper::convertStingToType($productData->countPackages[$domainId], AkeneoProductHelper::TYPE_INT);
             $productData->totalPackageWeight[$domainId] = AkeneoProductHelper::convertStingToType($productData->totalPackageWeight[$domainId], AkeneoProductHelper::TYPE_FLOAT);
         }
     }
