44509cda1be6c1bc5e5be3ffc7809205fd6f0a98
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2020-06-25 15:38:59 +0200

    select2 is no longer hiding behind bottom bar in administration

--- packages/framework/assets/js/admin/components/initSelect2.js
+++ packages/framework/assets/js/admin/components/initSelect2.js
@@ -2,10 +2,19 @@ import Register from '../../common/utils/Register';
 import 'select2/dist/js/select2.full';
 
 export function initSelect2 ($container) {
+    const select2BottomMaxOffset = 250;
+
     $container.filterAllNodes('select').select2({
         minimumResultsForSearch: 5,
         width: 'computedstyle'
+    }).on('select2:open', function () {
+        const $select2Container = $('.select2-container').last();
+        const bottom = $select2Container[0].getBoundingClientRect().bottom;
+        if (($(window).outerHeight() - bottom) < select2BottomMaxOffset) {
+            $select2Container.find('.select2-dropdown').removeClass('select2-dropdown--below').addClass('select2-dropdown--above');
+        }
     });
+
 }
 
 (new Register()).registerCallback(initSelect2, 'initSelect2');
