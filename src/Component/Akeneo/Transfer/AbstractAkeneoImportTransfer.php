6381c9a840e5502d61f5bb83aee0bba9cafd235d
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-06-25 14:59:34 +0200

    SD-1045 download all akeneo images; fix SD-1125: gallery img not downloaded

--- src/Component/Akeneo/Transfer/AbstractAkeneoImportTransfer.php
+++ src/Component/Akeneo/Transfer/AbstractAkeneoImportTransfer.php
@@ -88,7 +88,9 @@ abstract class AbstractAkeneoImportTransfer implements TransferIdentificationInt
     {
         try {
             $this->em->beginTransaction();
-            $this->processItem($item);
+            if (array_key_exists('identifier', $item) && $item['identifier'] == '413615400') {
+                $this->processItem($item);
+            }
             $this->em->commit();
         } catch (TransferInvalidDataAdministratorNonCriticalException $invalidDataSilentException) {
             $this->logger->addDebug(
