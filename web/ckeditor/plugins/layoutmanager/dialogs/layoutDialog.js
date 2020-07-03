033941a8196b6b2738cdd24c8beaf3b366801a34
Tomáš Fejfar <tomas.fejfar@scontodigital.cz>
2020-07-03 09:38:24 +0200

    [SD-1174] - Ckeditor columns layout design

--- web/ckeditor/plugins/layoutmanager/dialogs/layoutDialog.js
+++ web/ckeditor/plugins/layoutmanager/dialogs/layoutDialog.js
@@ -9,8 +9,8 @@ CKEDITOR.dialog.add( 'layoutDialog', function ( editor ) {
                         html: '<div class="editor-popup-layout"><img src="' + CKEDITOR.getUrl(CKEDITOR.plugins.getPath( 'layoutmanager' ) + 'img/1-1.png') + '" alt="1/1"/></div>',
                         onClick: function() {
                             editor.insertHtml(
-                                '<div class="wrapper">' +
-                                '   <div class="column-1">1/1</div>' +
+                                '<div class="grid">' +
+                                '   <div class="grid__item grid__item--1"></div>' +
                                 '</div>'
                             );
                             this._.dialog.hide();
@@ -21,9 +21,9 @@ CKEDITOR.dialog.add( 'layoutDialog', function ( editor ) {
                         html: '<div class="editor-popup-layout"><img src="' + CKEDITOR.getUrl(CKEDITOR.plugins.getPath( 'layoutmanager' ) + 'img/2-2.png') + '" alt="2/2"/></div>',
                         onClick: function() {
                             editor.insertHtml(
-                                '<div class="wrapper">' +
-                                '   <div class="column-1-2">1/2</div>' +
-                                '   <div class="column-1-2">1/2</div>' +
+                                '<div class="grid">' +
+                                '   <div class="grid__item grid__item--1-2"></div>' +
+                                '   <div class="grid__item grid__item--1-2"></div>' +
                                 '</div>'
                             );
                             this._.dialog.hide();
@@ -34,9 +34,9 @@ CKEDITOR.dialog.add( 'layoutDialog', function ( editor ) {
                         html: '<div class="editor-popup-layout"><img src="' + CKEDITOR.getUrl(CKEDITOR.plugins.getPath( 'layoutmanager' ) + 'img/1-3-2-3.png') + '" alt="2/2"/></div>',
                         onClick: function() {
                             editor.insertHtml(
-                                '<div class="wrapper">' +
-                                '<div class="column-1-3">1/3</div>' +
-                                '<div class="column-2-3">2/3</div>' +
+                                '<div class="grid">' +
+                                '<div class="grid__item grid__item--1-3"></div>' +
+                                '<div class="grid__item grid__item--2-3"></div>' +
                                 '</div>'
                             );
                             this._.dialog.hide();
@@ -53,9 +53,9 @@ CKEDITOR.dialog.add( 'layoutDialog', function ( editor ) {
                         html: '<div class="editor-popup-layout"><img src="' + CKEDITOR.getUrl(CKEDITOR.plugins.getPath('layoutmanager') + 'img/2-3-1-3.png') + '" alt="2/2"/></div>',
                         onClick: function () {
                             editor.insertHtml(
-                                '<div class="wrapper">' +
-                                '<div class="column-2-3">2/3</div>' +
-                                '<div class="column-1-3">1/3</div>' +
+                                '<div class="grid">' +
+                                '<div class="grid__item grid__item--2-3"></div>' +
+                                '<div class="grid__item grid__item--1-3"></div>' +
                                 '</div>'
                             );
                             this._.dialog.hide();
@@ -66,10 +66,10 @@ CKEDITOR.dialog.add( 'layoutDialog', function ( editor ) {
                         html: '<div class="editor-popup-layout"><img src="' + CKEDITOR.getUrl(CKEDITOR.plugins.getPath('layoutmanager') + 'img/3-3.png') + '" alt="2/2"/></div>',
                         onClick: function () {
                             editor.insertHtml(
-                                '<div class="wrapper">' +
-                                '<div class="column-1-3">1/3</div>' +
-                                '<div class="column-1-3">1/3</div>' +
-                                '<div class="column-1-3">1/3</div>' +
+                                '<div class="grid">' +
+                                '<div class="grid__item grid__item--1-3"></div>' +
+                                '<div class="grid__item grid__item--1-3"></div>' +
+                                '<div class="grid__item grid__item--1-3"></div>' +
                                 '</div>'
                             );
                             this._.dialog.hide();
