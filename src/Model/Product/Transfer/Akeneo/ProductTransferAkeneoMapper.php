946b6c5ad470f263d4beef6ef27b72dc61a6b86d
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-07-09 08:50:54 +0200

    SD-1197 fixed return value

--- src/Model/Product/Transfer/Akeneo/ProductTransferAkeneoMapper.php
+++ src/Model/Product/Transfer/Akeneo/ProductTransferAkeneoMapper.php
@@ -555,7 +555,7 @@ class ProductTransferAkeneoMapper
                 $parameter->getAkeneoCode(),
                 $akeneoParameterValueCode
             );
-            if (array_key_exists($locale, $valueTextsByLocale) === false) {
+            if (array_key_exists($locale, $valueTextsByLocale) === false || $valueTextsByLocale[$locale] === null) {
                 throw TransferInvalidDataException::createWithViolation(
                     sprintf('Parameter value %s for parameter code %s does not have localized label', $akeneoParameterValueCode, $parameter->getAkeneoCode()),
                     ''
