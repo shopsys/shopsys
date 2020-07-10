6186f854f6e461d84d84ce7cdcb43fd5e408ebf5
David Střítežský <53976269+s3tezsky@users.noreply.github.com>
2020-07-10 14:03:17 +0200

    fixed renaming files in wysiwyg (#1920)

--- packages/framework/src/Component/Filesystem/Flysystem/VolumeDriver.php
+++ packages/framework/src/Component/Filesystem/Flysystem/VolumeDriver.php
@@ -119,7 +119,7 @@ class VolumeDriver extends Driver
      */
     protected function rmTmb($stat)
     {
-        $path = $stat['realpath'];
+        $path = $this->tmbPath . DIRECTORY_SEPARATOR . $this->tmbname($stat);
         if ($this->tmbURL) {
             $thumbnailName = $this->gettmb($path, $stat);
             $stat['tmb'] = $thumbnailName ? $thumbnailName : 1;
