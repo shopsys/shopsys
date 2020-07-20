ac038a51704af7fd86181f2397ae5d6c1a57bff5
Tomáš Fejfar <tomas.fejfar@scontodigital.cz>
2020-07-20 08:06:33 +0200

    [SD-1149] - eslint fix

--- assets/js/frontend/components/ResetCustomerpassword.js
+++ assets/js/frontend/components/ResetCustomerpassword.js
@@ -40,7 +40,7 @@ export default class ResetCustomerpassword {
             buttonContinue: true,
             cssClass: 'window-popup--wide window-popup--reset-password',
             cssClassContinue: 'window-popup__actions__btn--continue window-popup__actions__btn--continue--outline',
-            textContinue: Translator.trans('Potvrdit'),
+            textContinue: Translator.trans('Potvrdit')
         });
     }
 
