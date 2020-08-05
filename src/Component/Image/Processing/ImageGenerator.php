56bd5963b2b669dc8cf7f6e459d094d02ff33a16
Adam Tyroň <adam.tyron@scontodigital.cz>
2020-08-05 16:33:38 +0200

    [SD-1379] kraken : logging errors for generating by request

--- src/Component/Image/Processing/ImageGenerator.php
+++ src/Component/Image/Processing/ImageGenerator.php
@@ -12,6 +12,7 @@ use Shopsys\FrameworkBundle\Component\Image\Image;
 use Shopsys\FrameworkBundle\Component\Image\ImageLocator;
 use Shopsys\FrameworkBundle\Component\Image\Processing\ImageGenerator as BaseImageGenerator;
 use Shopsys\FrameworkBundle\Component\Image\Processing\ImageProcessor;
+use Symfony\Bridge\Monolog\Logger;
 
 /**
  * @method string generateAdditionalImageSizeAndGetFilepath(\App\Component\Image\Image $image, int $additionalIndex, string|null $sizeName)
@@ -28,21 +29,29 @@ class ImageGenerator extends BaseImageGenerator
     private $imageKrakenProcessor;
 
     /**
+     * @var \Symfony\Bridge\Monolog\Logger
+     */
+    private $logger;
+
+    /**
      * @param \App\Component\Image\Processing\ImageProcessor $imageProcessor
      * @param \App\Component\Image\ImageLocator $imageLocator
      * @param \App\Component\Image\Config\ImageConfig $imageConfig
      * @param \League\Flysystem\FilesystemInterface $filesystem
      * @param \App\Component\Image\Kraken\Processing\ImageKrakenProcessor $imageKrakenProcessor
+     * @param \Symfony\Bridge\Monolog\Logger $logger
      */
     public function __construct(
         ImageProcessor $imageProcessor,
         ImageLocator $imageLocator,
         ImageConfig $imageConfig,
         FilesystemInterface $filesystem,
-        ImageKrakenProcessor $imageKrakenProcessor
+        ImageKrakenProcessor $imageKrakenProcessor,
+        Logger $logger
     ) {
         parent::__construct($imageProcessor, $imageLocator, $imageConfig, $filesystem);
         $this->imageKrakenProcessor = $imageKrakenProcessor;
+        $this->logger = $logger;
     }
 
     /**
@@ -59,7 +68,7 @@ class ImageGenerator extends BaseImageGenerator
         $sizeConfig = $this->imageConfig->getImageSizeConfigByImage($image, $sizeName);
 
         if ($this->imageKrakenProcessor->isEnabled()) {
-            $this->processImageInKraken($sourceImageFilepath, $targetImageFilepath, $sizeConfig);
+            $this->processImageInKraken($sourceImageFilepath, $targetImageFilepath, $sizeConfig, $image);
         } else {
             $this->processImageInFramework($sourceImageFilepath, $targetImageFilepath, $sizeConfig);
         }
@@ -86,10 +95,18 @@ class ImageGenerator extends BaseImageGenerator
      * @param string $sourceImageFilepath
      * @param string $targetImageFilepath
      * @param \Shopsys\FrameworkBundle\Component\Image\Config\ImageSizeConfig $sizeConfig
+     * @param \App\Component\Image\Image $image
      */
-    private function processImageInKraken(string $sourceImageFilepath, string $targetImageFilepath, ImageSizeConfig $sizeConfig): void
+    private function processImageInKraken(string $sourceImageFilepath, string $targetImageFilepath, ImageSizeConfig $sizeConfig, Image $image): void
     {
         $krakenImageData = $this->imageKrakenProcessor->resizeBySizeConfig($sourceImageFilepath, [$sizeConfig]);
+
+        if ($krakenImageData['success'] === false) {
+            $this->logger->addError(sprintf('Generating image by kraken error: image id: %s with result: %s', $image->getId(), $krakenImageData['message']));
+            $this->processImageInFramework($sourceImageFilepath, $targetImageFilepath, $sizeConfig);
+            return;
+        }
+
         $krakenImageDataResult = current($krakenImageData['results']);
 
         $file = file_get_contents($krakenImageDataResult['kraked_url']);
