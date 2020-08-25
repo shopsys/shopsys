00927e701729f5eee9da85833d3bc82be9a8e2e0
David Stritezsky <david.stritezsky@shopsys.com>
2020-08-25 13:21:32 +0200

    added special articles into FEAPI

--- packages/frontend-api/src/Model/Resolver/Article/ArticleResolver.php
+++ packages/frontend-api/src/Model/Resolver/Article/ArticleResolver.php
@@ -9,6 +9,7 @@ use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Overblog\GraphQLBundle\Error\UserError;
 use Ramsey\Uuid\Uuid;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Component\Setting\Setting;
 use Shopsys\FrameworkBundle\Model\Article\Article;
 use Shopsys\FrameworkBundle\Model\Article\ArticleFacade;
 use Shopsys\FrameworkBundle\Model\Article\Exception\ArticleNotFoundException;
@@ -26,15 +27,23 @@ class ArticleResolver implements ResolverInterface, AliasedInterface
     protected $domain;
 
     /**
+     * @var \Shopsys\FrameworkBundle\Component\Setting\Setting
+     */
+    protected $setting;
+
+    /**
      * @param \Shopsys\FrameworkBundle\Model\Article\ArticleFacade $articleFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     * @param \Shopsys\FrameworkBundle\Component\Setting\Setting $setting
      */
     public function __construct(
         ArticleFacade $articleFacade,
-        Domain $domain
+        Domain $domain,
+        Setting $setting
     ) {
         $this->articleFacade = $articleFacade;
         $this->domain = $domain;
+        $this->setting = $setting;
     }
 
     /**
@@ -55,12 +64,63 @@ class ArticleResolver implements ResolverInterface, AliasedInterface
     }
 
     /**
+     * @return \Shopsys\FrameworkBundle\Model\Article\Article
+     */
+    public function termsAndConditionsArticle(): Article
+    {
+        try {
+            $articleId = $this->setting->getForDomain(
+                Setting::TERMS_AND_CONDITIONS_ARTICLE_ID,
+                $this->domain->getId()
+            );
+            return $this->articleFacade->getById($articleId);
+        } catch (ArticleNotFoundException $articleNotFoundException) {
+            throw new UserError($articleNotFoundException->getMessage());
+        }
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Article\Article
+     */
+    public function privacyPolicyArticle(): Article
+    {
+        try {
+            $articleId = $this->setting->getForDomain(
+                Setting::PRIVACY_POLICY_ARTICLE_ID,
+                $this->domain->getId()
+            );
+            return $this->articleFacade->getById($articleId);
+        } catch (ArticleNotFoundException $articleNotFoundException) {
+            throw new UserError($articleNotFoundException->getMessage());
+        }
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Article\Article
+     */
+    public function cookiesArticle(): Article
+    {
+        try {
+            $articleId = $this->setting->getForDomain(
+                Setting::COOKIES_ARTICLE_ID,
+                $this->domain->getId()
+            );
+            return $this->articleFacade->getById($articleId);
+        } catch (ArticleNotFoundException $articleNotFoundException) {
+            throw new UserError($articleNotFoundException->getMessage());
+        }
+    }
+
+    /**
      * @return string[]
      */
     public static function getAliases(): array
     {
         return [
             'resolver' => 'article',
+            'termsAndConditionsArticle' => 'termsAndConditionsArticle',
+            'privacyPolicyArticle' => 'privacyPolicyArticle',
+            'cookiesArticle' => 'cookiesArticle',
         ];
     }
 }
