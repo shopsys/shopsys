1f8fe50f2b8bcffccca17c89d262664ebb3bc734
David Stritezsky <david.stritezsky@shopsys.com>
2020-09-01 11:44:01 +0000

    ShopsysFrameworkBundle: version updated to "9.0.3-dev"

--- packages/framework/src/ShopsysFrameworkBundle.php
+++ packages/framework/src/ShopsysFrameworkBundle.php
@@ -21,7 +21,7 @@ class ShopsysFrameworkBundle extends Bundle
     /**
      * @var string
      */
-    public const VERSION = '9.0.2';
+    public const VERSION = '9.0.3-dev';
 
     /**
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
