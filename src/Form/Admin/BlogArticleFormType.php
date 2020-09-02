3fd2b8f0f8e01b58dd73760919de3ef35317843b
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-09-02 15:31:07 +0200

    remove products from blog article

--- src/Form/Admin/BlogArticleFormType.php
+++ src/Form/Admin/BlogArticleFormType.php
@@ -43,23 +43,15 @@ class BlogArticleFormType extends AbstractType
     private $seoSettingFacade;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Form\Transformers\RemoveDuplicatesFromArrayTransformer
-     */
-    private $removeDuplicatesTransformer;
-
-    /**
      * @param \App\Component\Domain\Domain $domain
      * @param \Shopsys\FrameworkBundle\Model\Seo\SeoSettingFacade $seoSettingFacade
-     * @param \Shopsys\FrameworkBundle\Form\Transformers\RemoveDuplicatesFromArrayTransformer $removeDuplicatesTransformer
      */
     public function __construct(
         Domain $domain,
-        SeoSettingFacade $seoSettingFacade,
-        RemoveDuplicatesFromArrayTransformer $removeDuplicatesTransformer
+        SeoSettingFacade $seoSettingFacade
     ) {
         $this->domain = $domain;
         $this->seoSettingFacade = $seoSettingFacade;
-        $this->removeDuplicatesTransformer = $removeDuplicatesTransformer;
     }
 
     /**
@@ -76,7 +68,6 @@ class BlogArticleFormType extends AbstractType
         $builderDescriptionGroup = $this->createDescriptionGroup($builder);
         $builderImageGroup = $this->createImageGroup($builder, $options);
         $builderPerexGroup = $this->createPerexGroup($builder);
-        $builderProductGroup = $this->createProductGroup($builder);
 
         $builder
             ->add($builderSettingsGroup)
@@ -84,7 +75,6 @@ class BlogArticleFormType extends AbstractType
             ->add($builderPerexGroup)
             ->add($builderDescriptionGroup)
             ->add($builderImageGroup)
-            ->add($builderProductGroup)
             ->add('save', SubmitType::class);
     }
 
@@ -340,26 +330,5 @@ class BlogArticleFormType extends AbstractType
         }
 
         return [$seoTitlesOptionsByDomainId, $seoMetaDescriptionsOptionsByDomainId, $seoH1OptionsByDomainId];
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     * @return \Symfony\Component\Form\FormBuilderInterface
-     */
-    private function createProductGroup(FormBuilderInterface $builder): FormBuilderInterface
-    {
-        $builderProductGroup = $builder->create('products', GroupType::class, [
-            'label' => t('Produkty k článku blogu'),
-        ]);
-
-        $builderProductGroup
-            ->add('products', ProductsType::class, [
-                'required' => false,
-                'allow_main_variants' => true,
-                'allow_variants' => false,
-            ])
-            ->addViewTransformer($this->removeDuplicatesTransformer);
-
-        return $builderProductGroup;
     }
 }
