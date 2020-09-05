da00804f40f80ec06c6c0e9f56000b58871c104e
Tomáš Fejfar <tomas.fejfar@scontodigital.cz>
2020-09-05 15:51:49 +0200

    [sd-1534] - Kitchen index page

---
+++ src/Controller/Front/KitchenController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Front;
+
+use App\Component\Domain\Domain;
+use Symfony\Component\HttpFoundation\Response;
+
+class KitchenController extends FrontBaseController
+{
+    /**
+     * @var \App\Component\Domain\Domain
+     */
+    private Domain $domain;
+
+    /**
+     * KitchenController constructor.
+     * @param \App\Component\Domain\Domain $domain
+     */
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(): Response
+    {
+        return $this->render('Front/Content/Kitchen/index.html.twig');
+    }
+}
