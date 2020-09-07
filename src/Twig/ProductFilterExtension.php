5c3a7d7b93e840759c32dd8d4481d6c58a5dfd90
Ondřej Petr <ondrej.petr@scontodigital.cz>
2020-09-07 22:21:42 +0200

    [SD-1497] Icons in parametric filter

--- src/Twig/ProductFilterExtension.php
+++ src/Twig/ProductFilterExtension.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Twig;
 
 use App\Model\Product\Filter\ProductFilterFacade;
+use App\Model\Product\Flag\Flag;
+use App\Model\Product\Flag\FlagFacade;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -16,11 +18,18 @@ class ProductFilterExtension extends AbstractExtension
     private $productFilterFacade;
 
     /**
-     * @param \App\Model\Product\Filter\ProductFilterFacade $productFilterFacade
+     * @var \App\Model\Product\Flag\FlagFacade
      */
-    public function __construct(ProductFilterFacade $productFilterFacade)
+    private $flagFacade;
+
+    /**
+     * @param \App\Model\Product\Filter\ProductFilterFacade $productFilterFacade
+     * @param \App\Model\Product\Flag\FlagFacade $flagFacade
+     */
+    public function __construct(ProductFilterFacade $productFilterFacade, FlagFacade $flagFacade)
     {
         $this->productFilterFacade = $productFilterFacade;
+        $this->flagFacade = $flagFacade;
     }
 
     public function getFunctions()
@@ -30,6 +39,7 @@ class ProductFilterExtension extends AbstractExtension
         $functions[] = new TwigFunction('getUrlByFlagValue', [$this, 'getUrlByFlagValue']);
         $functions[] = new TwigFunction('getUrlByBrandValue', [$this, 'getUrlByBrandValue']);
         $functions[] = new TwigFunction('getUrlByAvailabilityValue', [$this, 'getUrlByAvailabilityValue']);
+        $functions[] = new TwigFunction('getFlagIconClass', [$this, 'getFlagIconClass']);
 
         return $functions;
     }
@@ -101,5 +111,45 @@ class ProductFilterExtension extends AbstractExtension
         $this->productFilterFacade->setYesNoToProductFilterSetup($tmpProductFilterSetup, 'inStock', $checked ? 0 : 1);
 
         return $this->productFilterFacade->getUrlByProductFilterSetup($tmpProductFilterSetup);
+    }
+
+    /**
+     * @param int $flagId
+     * @return string
+     */
+    public function getFlagIconClass(int $flagId): string
+    {
+        $flag = $this->flagFacade->getById($flagId);
+        switch ($flag->getAkeneoCode()) {
+            case Flag::AKENEO_CODE_NEW:
+                $class = 'flag-icon_new';
+                break;
+            case Flag::AKENEO_CODE_SCONTO:
+                $class = 'flag-icon_sconto';
+                break;
+            case Flag::AKENEO_CODE_ACTION:
+                $class = 'flag-icon_action';
+                break;
+            case Flag::AKENEO_CODE_HIT:
+                $class = 'flag-icon_hit';
+                break;
+            case Flag::AKENEO_CODE_SALE:
+                $class = 'flag-icon_sale';
+                break;
+            case Flag::AKENEO_CODE_MADE_IN_CZ:
+                $class = 'flag-icon_made-in-cz';
+                break;
+            case Flag::AKENEO_CODE_MADE_IN_DE:
+                $class = 'flag-icon_made-in-de';
+                break;
+            case Flag::AKENEO_CODE_MADE_IN_SK:
+                $class = 'flag-icon_made-in-sk';
+                break;
+            default:
+                $class = '';
+                break;
+        }
+
+        return $class;
     }
 }
