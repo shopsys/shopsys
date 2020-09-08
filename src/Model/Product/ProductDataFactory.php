c4310f6a5c5c777017104fd92ccf244c7ff91097
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-09-08 11:29:38 +0200

    fix vat replace cron by new price working

--- src/Model/Product/ProductDataFactory.php
+++ src/Model/Product/ProductDataFactory.php
@@ -82,7 +82,7 @@ class ProductDataFactory extends BaseProductDataFactory
 
     /**
      * @param \Shopsys\FrameworkBundle\Model\Pricing\Vat\VatFacade $vatFacade
-     * @param \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductInputPriceFacade $productInputPriceFacade
+     * @param \App\Model\Product\Pricing\ProductInputPriceFacade $productInputPriceFacade
      * @param \Shopsys\FrameworkBundle\Model\Product\Unit\UnitFacade $unitFacade
      * @param \App\Component\Domain\Domain $domain
      * @param \App\Model\Product\ProductRepository $productRepository
