2dfd311f931fb3db842b94909e57b7bcd6eb7771
David Stritezsky <david.stritezsky@shopsys.com>
2020-09-10 15:40:27 +0200

    fixed coding standard of regex in EmailValidator.php

--- packages/framework/src/Form/Constraints/EmailValidator.php
+++ packages/framework/src/Form/Constraints/EmailValidator.php
@@ -41,11 +41,11 @@ class EmailValidator extends ConstraintValidator
         $atom = "[-a-z0-9!#$%&'*+/=?^_`{|}~]"; // RFC 5322 unquoted characters in local-part
         $alpha = "a-z\x80-\xFF"; // superset of IDN
 
-        return (bool)preg_match("(^
-            (\"([ !#-[\\]-~]*|\\\\[ -~])+\"|$atom+(\\.$atom+)*) # quoted or unquoted
+        return (bool)preg_match('(^
+            ("([ !#-[\\]-~]*|\\\\[ -~])+"|' . $atom . '+(\\.' . $atom . '+)*) # quoted or unquoted
             @
-            ([0-9$alpha]([-0-9$alpha]{0,61}[0-9$alpha])?\\.)+ # domain - RFC 1034
-            [$alpha]([-0-9$alpha]{0,17}[$alpha])? # top domain
-            \\z)ix", $value);
+            ([0-9' . $alpha . ']([-0-9' . $alpha . ']{0,61}[0-9' . $alpha . '])?\\.)+ # domain - RFC 1034
+            [' . $alpha . ']([-0-9' . $alpha . ']{0,17}[' . $alpha . '])? # top domain
+            \\z)ix', $value);
     }
 }
