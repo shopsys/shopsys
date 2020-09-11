655c93a6e2fcc34f193300fbc1d99987e80796ab
David Stritezsky <david.stritezsky@shopsys.com>
2020-09-11 11:48:23 +0200

    fixed indentation in maintenance.php

--- project-base/app/maintenance.php
+++ project-base/app/maintenance.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-    header('HTTP/1.1 503 Service Temporarily Unavailable');
-    header('Status: 503 Service Temporarily Unavailable');
-    header('Retry-after: 300');
-    header('Expires: Thu, 01 Dec 1994 16:00:00 GMT');
-    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
-    header('Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate');
+header('HTTP/1.1 503 Service Temporarily Unavailable');
+header('Status: 503 Service Temporarily Unavailable');
+header('Retry-after: 300');
+header('Expires: Thu, 01 Dec 1994 16:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
+header('Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate');
 
-    echo file_get_contents(__DIR__ . '/maintenance.html');
+echo file_get_contents(__DIR__ . '/maintenance.html');
