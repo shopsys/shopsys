c8fd723976d5071fb1a96e5ce07d3bfec80b0125
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-09-11 07:46:27 +0200

    set product visibility recalculation crons to importAkeneoProducts class

---
+++ src/Model/Feed/FeedFacade.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Model\Feed;
+
+use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
+use Shopsys\FrameworkBundle\Model\Feed\FeedExport;
+use Shopsys\FrameworkBundle\Model\Feed\FeedFacade as BaseFeedFacade;
+
+class FeedFacade extends BaseFeedFacade
+{
+    /**
+     * @param string $feedName
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig $domainConfig
+     * @param int|null $lastSeekId
+     * @return \Shopsys\FrameworkBundle\Model\Feed\FeedExport
+     */
+    public function createFeedExport(string $feedName, DomainConfig $domainConfig, ?int $lastSeekId = null): FeedExport
+    {
+        $feed = $this->feedRegistry->getFeedByName($feedName);
+
+        return $this->feedExportFactory->create($feed, $domainConfig, $lastSeekId);
+    }
+}
