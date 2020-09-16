4d58cf407dd0fafd304416540471b598d3e663e0
David Stritezsky <david.stritezsky@shopsys.com>
2020-09-16 17:30:25 +0200

    fixed using admin_locale in tests

--- packages/framework/tests/Test/TestTranslatableListener.php
+++ packages/framework/tests/Test/TestTranslatableListener.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\FrameworkBundle\Test;
 
+use Exception;
 use Metadata\MetadataFactory;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Administration\AdministrationFacade;
 use Shopsys\FrameworkBundle\Model\Localization\TranslatableListener;
 
 class TestTranslatableListener extends TranslatableListener
@@ -16,19 +18,49 @@ class TestTranslatableListener extends TranslatableListener
     protected $domain;
 
     /**
+     * @var \Shopsys\FrameworkBundle\Model\Administration\AdministrationFacade
+     */
+    protected $administrationFacade;
+
+    /**
+     * @var string
+     */
+    protected $adminLocale;
+
+    /**
      * @param \Metadata\MetadataFactory $factory
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     * @param \Shopsys\FrameworkBundle\Model\Administration\AdministrationFacade $administrationFacade
+     * @param string $adminLocale
      */
-    public function __construct(MetadataFactory $factory, Domain $domain)
+    public function __construct(MetadataFactory $factory, Domain $domain, AdministrationFacade $administrationFacade, string $adminLocale)
     {
         parent::__construct($factory);
         $this->domain = $domain;
+        $this->administrationFacade = $administrationFacade;
+        $this->adminLocale = $adminLocale;
     }
 
     /**
      * @return string
      */
     public function getCurrentLocale()
+    {
+        try {
+            if ($this->administrationFacade->isInAdmin()) {
+                return $this->adminLocale;
+            }
+        } catch (Exception $exception) {
+            return $this->getFirstDomainLocale();
+        }
+
+        return $this->getFirstDomainLocale();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFirstDomainLocale(): string
     {
         return $this->domain->getDomainConfigById(Domain::FIRST_DOMAIN_ID)->getLocale();
     }
