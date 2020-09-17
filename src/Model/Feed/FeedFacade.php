dfb447eeff1a120590dfccee88ad89d139766649
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2020-09-17 09:14:44 +0200

    [SD-1665]fix Feed generating

--- src/Model/Feed/FeedFacade.php
+++ src/Model/Feed/FeedFacade.php
@@ -9,6 +9,10 @@ use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
 use Shopsys\FrameworkBundle\Model\Feed\FeedExport;
 use Shopsys\FrameworkBundle\Model\Feed\FeedFacade as BaseFeedFacade;
 
+/**
+ * @property \App\Model\Feed\FeedExportFactory $feedExportFactory
+ * @method __construct(\Shopsys\FrameworkBundle\Model\Feed\FeedRegistry $feedRegistry, \Shopsys\FrameworkBundle\Model\Product\ProductVisibilityFacade $productVisibilityFacade, \App\Model\Feed\FeedExportFactory $feedExportFactory, \Shopsys\FrameworkBundle\Model\Feed\FeedPathProvider $feedPathProvider, \League\Flysystem\FilesystemInterface $filesystem)
+ */
 class FeedFacade extends BaseFeedFacade
 {
     /**
