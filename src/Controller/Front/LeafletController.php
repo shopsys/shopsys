a5ba7fcdaeab1b429395796f863ca90e8f20b114
Tomáš Fejfar <tomas.fejfar@scontodigital.cz>
2020-09-22 00:18:22 +0200

    [SD-1729] - PHP ECS FIX

--- src/Controller/Front/LeafletController.php
+++ src/Controller/Front/LeafletController.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Controller\Front;
 
 use App\Component\Domain\Domain;
-use Symfony\Component\HttpFoundation\Response;
 use Shopsys\FrameworkBundle\Model\Category\TopCategory\TopCategoryFacade;
+use Symfony\Component\HttpFoundation\Response;
 
 class LeafletController extends FrontBaseController
 {
