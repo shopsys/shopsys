ac18e9c5f4cafa94b7b72e31e21a703126e021c2
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2020-09-24 14:35:26 +0200

    added test

--- packages/framework/src/DependencyInjection/Compiler/RegisterMultiDesignFilesystemLoaderCompilerPass.php
+++ packages/framework/src/DependencyInjection/Compiler/RegisterMultiDesignFilesystemLoaderCompilerPass.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\DependencyInjection\Compiler;
 
 use Shopsys\FrameworkBundle\Component\Domain\Multidomain\Twig\FilesystemLoader;
+use Shopsys\FrameworkBundle\Component\Environment\EnvironmentType;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -18,5 +19,10 @@ class RegisterMultiDesignFilesystemLoaderCompilerPass implements CompilerPassInt
         $twigFilesystemLoaderDefinition = $container->getDefinition('twig.loader.native_filesystem');
         $twigFilesystemLoaderDefinition->setClass(FilesystemLoader::class);
         $twigFilesystemLoaderDefinition->setAutowired(true);
+
+        $environment = $container->getParameter('kernel.environment');
+        if ($environment === EnvironmentType::TEST) {
+            $twigFilesystemLoaderDefinition->setPublic(true);
+        }
     }
 }
