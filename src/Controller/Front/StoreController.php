0f43288b2cbd7031dcb6fc133e54d2b0ef22be7a
kopd <kop@bartonstudio.cz>
2020-09-24 15:05:45 +0200

    insert api key for google maps

--- src/Controller/Front/StoreController.php
+++ src/Controller/Front/StoreController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Front;
 
 use App\Component\Domain\Domain;
 use App\Model\Stock\StockFacade;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class StoreController extends FrontBaseController
@@ -21,14 +22,21 @@ class StoreController extends FrontBaseController
     private Domain $domain;
 
     /**
+     * @var string
+     */
+    private $googleMapApiKey;
+
+    /**
      * StoreController constructor.
      * @param \App\Model\Stock\StockFacade $stockFacade
      * @param \App\Component\Domain\Domain $domain
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
      */
-    public function __construct(StockFacade $stockFacade, Domain $domain)
+    public function __construct(StockFacade $stockFacade, Domain $domain, ContainerInterface $container)
     {
         $this->stockFacade = $stockFacade;
         $this->domain = $domain;
+        $this->googleMapApiKey = $container->getParameter('google_map_api_key');
     }
 
     /**
@@ -38,6 +46,7 @@ class StoreController extends FrontBaseController
     {
         return $this->render('Front/Content/Store/store.html.twig', [
             'stores' => $this->stockFacade->getStocksWithoutCentralByDomainIdIndexedByStockId($this->domain->getId()),
+            'googleMapApiKey' => $this->googleMapApiKey,
         ]);
     }
 
@@ -51,6 +60,7 @@ class StoreController extends FrontBaseController
 
         return $this->render('Front/Content/Store/detail.html.twig', [
             'store' => $store,
+            'googleMapApiKey' => $this->googleMapApiKey,
         ]);
     }
 }
