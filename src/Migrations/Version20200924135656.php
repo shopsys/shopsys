398039e8a583204a043847433a2718707485ee8e
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-09-26 13:27:27 +0200

    SD-1897 reworked migrations to domains

--- src/Migrations/Version20200924135656.php
+++ src/Migrations/Version20200924135656.php
@@ -1,51 +1,75 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Shopsys\FrameworkBundle\Migrations\MultidomainMigrationTrait;
 use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 final class Version20200924135656 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    use MultidomainMigrationTrait;
+
+    public function up(Schema $schema): void
     {
-        $this->createOrderStatusWithEnglishAndCzechTranslations(5, 'Overlimit', 'Nadlimitní');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(6, 'IM - sent', 'IM - odesláno');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(7, 'IM - error', 'IM - chyba');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(8, 'ERP - in stock', 'ERP - skladem');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(9, 'ERP - in transit', 'ERP - v převozu');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(10, 'ERP - waiting', 'ERP - čeká na dodání');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(11, 'ERP - ordered', 'ERP - Objednáno u dodavatele');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(12, 'ERP - error', 'ERP - chyba');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(6, 'IM - odesláno');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(7, 'IM - chyba');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(8, 'ERP - skladem');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(9, 'ERP - v převozu');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(10, 'ERP - čeká na dodání');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(11, 'ERP - Objednáno u dodavatele');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(12, 'ERP - chyba');
     }
 
     /**
     * @param int $orderStatusType
-    * @param string $orderStatusEnglishName
     * @param string $orderStatusCzechName
     */
     private function createOrderStatusWithEnglishAndCzechTranslations(
         $orderStatusType,
-        $orderStatusEnglishName,
         $orderStatusCzechName
-    ) {
+    ): void {
         $this->sql('INSERT INTO order_statuses (type) VALUES (:type)', [
             'type' => $orderStatusType,
         ]);
         $id = $this->connection->lastInsertId();
-        $this->sql('INSERT INTO order_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [
-            'translatableId' => $id,
-            'name' => $orderStatusEnglishName,
-            'locale' => 'en',
-        ]);
-        $this->sql('INSERT INTO order_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [
-            'translatableId' => $id,
-            'name' => $orderStatusCzechName,
-            'locale' => 'cs',
-        ]);
+        foreach (['cs','en','sk'] as $locale) {
+            $this->sql('INSERT INTO order_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [
+                'translatableId' => $id,
+                'name' => $orderStatusCzechName,
+                'locale' => $locale,
+            ]);
+        }
+        $mailTemplateName = sprintf('order_status_%d', $id);
+        foreach ([1,2] as $domainId) {
+            $this->createMailTemplateIfNotExist($mailTemplateName, $domainId, 'false');
+        }
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
+    }
+
+    /**
+     * @param string $mailTemplateName
+     * @param int $domainId
+     * @param string $sendMail
+     */
+    private function createMailTemplateIfNotExist($mailTemplateName, $domainId, $sendMail)
+    {
+        $mailTemplateCount = $this->sql('SELECT count(*) FROM mail_templates WHERE name = :mailTemplateName AND domain_id = :domainId', [
+            'mailTemplateName' => $mailTemplateName,
+            'domainId' => $domainId
+        ])->fetchColumn(0);
+
+        if ($mailTemplateCount <= 0) {
+            $this->sql('INSERT INTO mail_templates (name, domain_id, send_mail) VALUES (:mailTemplateName, :domainId, :sendMail)', [
+                'mailTemplateName' => $mailTemplateName,
+                'domainId' => $domainId,
+                'sendMail' => $sendMail,
+            ]);
+        }
     }
 }
