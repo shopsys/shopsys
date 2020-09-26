ff6e7502c1b1a4bb267678336fb1aecd49c0c014
Martin Poschl <martinposchl@Martin-MacBook-Pro.local>
2020-09-26 13:13:32 +0200

    SD-1897 moved order status to type

--- src/Migrations/Version20200924135656.php
+++ src/Migrations/Version20200924135656.php
@@ -9,40 +9,38 @@ final class Version20200924135656 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->createOrderStatusWithEnglishAndCzechTranslations(5, 2, 'Overlimit', 'Nadlimitní');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(6, 2, 'IM - sent', 'IM - odesláno');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(7, 2, 'IM - error', 'IM - chyba');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(8, 2, 'ERP - in stock', 'ERP - skladem');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(9, 2, 'ERP - in transit', 'ERP - v převozu');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(10, 2, 'ERP - waiting', 'ERP - čeká na dodání');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(11, 2, 'ERP - ordered', 'ERP - Objednáno u dodavatele');
-        $this->createOrderStatusWithEnglishAndCzechTranslations(12, 2, 'ERP - error', 'ERP - chyba');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(5, 'Overlimit', 'Nadlimitní');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(6, 'IM - sent', 'IM - odesláno');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(7, 'IM - error', 'IM - chyba');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(8, 'ERP - in stock', 'ERP - skladem');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(9, 'ERP - in transit', 'ERP - v převozu');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(10, 'ERP - waiting', 'ERP - čeká na dodání');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(11, 'ERP - ordered', 'ERP - Objednáno u dodavatele');
+        $this->createOrderStatusWithEnglishAndCzechTranslations(12, 'ERP - error', 'ERP - chyba');
         $this->sql('ALTER SEQUENCE order_statuses_id_seq RESTART WITH 13');
     }
 
     /**
-    * @param int $orderStatusId
     * @param int $orderStatusType
     * @param string $orderStatusEnglishName
     * @param string $orderStatusCzechName
     */
     private function createOrderStatusWithEnglishAndCzechTranslations(
-        $orderStatusId,
         $orderStatusType,
         $orderStatusEnglishName,
         $orderStatusCzechName
     ) {
-        $this->sql('INSERT INTO order_statuses (id, type) VALUES (:id, :type)', [
-            'id' => $orderStatusId,
+        $this->sql('INSERT INTO order_statuses (type) VALUES (:type)', [
             'type' => $orderStatusType,
         ]);
+        $id = $this->connection->lastInsertId();
         $this->sql('INSERT INTO order_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [
-            'translatableId' => $orderStatusId,
+            'translatableId' => $id,
             'name' => $orderStatusEnglishName,
             'locale' => 'en',
         ]);
         $this->sql('INSERT INTO order_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [
-            'translatableId' => $orderStatusId,
+            'translatableId' => $id,
             'name' => $orderStatusCzechName,
             'locale' => 'cs',
         ]);
