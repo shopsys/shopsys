f17e42bd6b8742cacb10db833d6243996315a2df
Petr Kadlec <petr.kadlec@shopsys.com>
2020-09-26 11:37:10 +0200

    revert some commits (see previous commits :) )

--- assets/js/gtm.js
+++ assets/js/gtm.js
@@ -180,10 +180,6 @@ export default class Gtm {
                 localStorage.clear(LOCALSTORAGE_FILTER_TAG);
             }
         }
-
-        $('.js-transport_and_payment_form_save').click(function (e) {
-            Gtm.pushTransportAndPayment($(this.closest('form')), e);
-        });
     }
 
     static compare (otherArray) {
