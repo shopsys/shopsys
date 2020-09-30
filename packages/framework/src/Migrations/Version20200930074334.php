be9c97bdfed79d3ad8d817ad932bbb5611eed0f3
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2020-09-30 09:52:23 +0200

    added uuid to Parameter and ParameterValue

---
+++ packages/framework/src/Migrations/Version20200930074334.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20200930074334 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('ALTER TABLE parameter_values ADD uuid UUID DEFAULT NULL');
+        $this->sql('UPDATE parameter_values SET uuid = uuid_generate_v4()');
+        $this->sql('ALTER TABLE parameter_values ALTER uuid SET NOT NULL');
+        $this->sql('CREATE UNIQUE INDEX UNIQ_DED94617D17F50A6 ON parameter_values (uuid)');
+
+        $this->sql('ALTER TABLE parameters ADD uuid UUID DEFAULT NULL');
+        $this->sql('UPDATE parameters SET uuid = uuid_generate_v4()');
+        $this->sql('ALTER TABLE parameters ALTER uuid SET NOT NULL');
+        $this->sql('CREATE UNIQUE INDEX UNIQ_69348FED17F50A6 ON parameters (uuid)');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
