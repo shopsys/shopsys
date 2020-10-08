e23d36e931c03a488b0b498b4df006e18b3478d0
David Stritezsky <david.stritezsky@shopsys.com>
2020-10-08 20:46:02 +0200

    added seo information to category

---
+++ packages/frontend-api/src/Model/Resolver/Category/CategoryResolverMap.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Category;
+
+use Overblog\GraphQLBundle\Resolver\ResolverMap;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Category\Category;
+
+class CategoryResolverMap extends ResolverMap
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    protected $domain;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     */
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * @return array
+     */
+    protected function map(): array
+    {
+        return [
+            'Category' => [
+                'seoH1' => function (Category $category) {
+                    return $category->getSeoH1($this->domain->getId());
+                },
+                'seoTitle' => function (Category $category) {
+                    return $category->getSeoTitle($this->domain->getId());
+                },
+                'seoMetaDescription' => function (Category $category) {
+                    return $category->getSeoMetaDescription($this->domain->getId());
+                },
+            ],
+        ];
+    }
+}
