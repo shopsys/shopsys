29e897090be795f98edc056138ff6e1ba16e5ed4
Martin Grossmann <martin@vyvoj.net>
2020-10-15 11:50:37 +0200

    frontend api: added promoted products (#2066)

---
+++ packages/frontend-api/src/Model/Resolver/Products/PromotedProductsResolver.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Products;
+
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser;
+use Shopsys\FrameworkBundle\Model\Product\TopProduct\TopProductFacade;
+
+class PromotedProductsResolver implements ResolverInterface, AliasedInterface
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Product\TopProduct\TopProductFacade
+     */
+    protected $topProductFacade;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    protected $domain;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser
+     */
+    protected $currentCustomerUser;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\TopProduct\TopProductFacade $topProductFacade
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
+     */
+    public function __construct(
+        TopProductFacade $topProductFacade,
+        Domain $domain,
+        CurrentCustomerUser $currentCustomerUser
+    ) {
+        $this->topProductFacade = $topProductFacade;
+        $this->domain = $domain;
+        $this->currentCustomerUser = $currentCustomerUser;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product[]
+     */
+    public function resolve(): array
+    {
+        return $this->topProductFacade->getAllOfferedProducts(
+            $this->domain->getId(),
+            $this->currentCustomerUser->getPricingGroup()
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getAliases(): array
+    {
+        return [
+            'resolve' => 'promotedProducts',
+        ];
+    }
+}
