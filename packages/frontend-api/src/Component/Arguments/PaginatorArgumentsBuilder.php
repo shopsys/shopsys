e88f8a81c9b66349e3fe1b36a3468a5311d72789
Michal Tomka <michal.tomka@shopsys.com>
2020-11-09 20:15:07 +0100

    check PaginatorArgumentsBuilder mandatory field

--- packages/frontend-api/src/Component/Arguments/PaginatorArgumentsBuilder.php
+++ packages/frontend-api/src/Component/Arguments/PaginatorArgumentsBuilder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrontendApiBundle\Component\Arguments;
 
 use Overblog\GraphQLBundle\Definition\Builder\MappingInterface;
+use Shopsys\FrontendApiBundle\Component\Arguments\Exception\MandatoryArgumentMissingException;
 
 class PaginatorArgumentsBuilder implements MappingInterface
 {
@@ -16,6 +17,8 @@ class PaginatorArgumentsBuilder implements MappingInterface
      */
     public function toMappingDefinition(array $config): array
     {
+        $this->checkMandatoryFields($config);
+
         return [
             'after' => [
                 'type' => 'String',
@@ -33,5 +36,21 @@ class PaginatorArgumentsBuilder implements MappingInterface
                 'type' => $config[self::CONFIG_ORDER_TYPE_KEY],
             ],
         ];
+    }
+
+    /**
+     * @param array $config
+     * @throws \Shopsys\FrontendApiBundle\Component\Arguments\Exception\MandatoryArgumentMissingException
+     */
+    protected function checkMandatoryFields(array $config): void
+    {
+        if (array_key_exists(self::CONFIG_ORDER_TYPE_KEY, $config) === false) {
+            $message = sprintf(
+                'Using the `%s`, the key `%s` defining the GraphQL type of the node is required.',
+                self::class,
+                self::CONFIG_ORDER_TYPE_KEY
+            );
+            throw new MandatoryArgumentMissingException($message);
+        }
     }
 }
