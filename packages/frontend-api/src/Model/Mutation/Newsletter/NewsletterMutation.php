3a36b16a8cb1900c44357c827296f684a0c716ee
Michal Tomka <michal.tomka@shopsys.com>
2020-11-13 15:57:03 +0100

    added support for subscribing for e-mail newsletter to FE API

---
+++ packages/frontend-api/src/Model/Mutation/Newsletter/NewsletterMutation.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrontendApiBundle\Model\Mutation\Newsletter;
+
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Validator\InputValidator;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Newsletter\NewsletterFacade;
+
+class NewsletterMutation implements MutationInterface, AliasedInterface
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Newsletter\NewsletterFacade
+     */
+    protected NewsletterFacade $newsletterFacade;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    protected Domain $domain;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Newsletter\NewsletterFacade $newsletterFacade
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     */
+    public function __construct(NewsletterFacade $newsletterFacade, Domain $domain)
+    {
+        $this->newsletterFacade = $newsletterFacade;
+        $this->domain = $domain;
+    }
+
+    /**
+     * @param \Overblog\GraphQLBundle\Definition\Argument $argument
+     * @param \Overblog\GraphQLBundle\Validator\InputValidator $validator
+     * @return bool[]
+     */
+    public function newsletterSubscribe(Argument $argument, InputValidator $validator): array
+    {
+        $validator->validate();
+
+        $input = $argument['input'];
+        $email = trim($input['email']);
+        $this->newsletterFacade->addSubscribedEmail($email, $this->domain->getId());
+
+        return [
+            true,
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getAliases(): array
+    {
+        return [
+            'newsletterSubscribe' => 'newsletter_subscribe',
+        ];
+    }
+}
