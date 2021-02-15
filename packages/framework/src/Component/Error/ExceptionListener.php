b92de566f232200701d78caeb68ed102fbaab8a9
Martin Grossmann <martin@vyvoj.net>
2021-02-15 12:39:21 +0100

    NotFoundHttpException thrown from twig is now converted to proper 404 (#2236)

--- packages/framework/src/Component/Error/ExceptionListener.php
+++ packages/framework/src/Component/Error/ExceptionListener.php
@@ -3,6 +3,8 @@
 namespace Shopsys\FrameworkBundle\Component\Error;
 
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Error\RuntimeError;
 
 class ExceptionListener
 {
@@ -16,6 +18,13 @@ class ExceptionListener
      */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
+        $throwable = $event->getThrowable();
+
+        // NotFoundHttpException during Twig rendering is converted to standard 404 page
+        if ($throwable instanceof RuntimeError && $throwable->getPrevious() instanceof NotFoundHttpException) {
+            $event->setThrowable($throwable->getPrevious());
+        }
+
         $this->lastException = $event->getException();
     }
 
