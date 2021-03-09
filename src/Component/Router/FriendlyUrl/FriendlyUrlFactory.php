af33c511bdd24e3668ee9e0caed9895f80589dd1
Martin Grossmann <martin@vyvoj.net>
2021-03-09 14:43:14 +0100

    removed prefixes from friendly url classes

--- src/Component/Router/FriendlyUrl/FriendlyUrlFactory.php
+++ src/Component/Router/FriendlyUrl/FriendlyUrlFactory.php
@@ -8,44 +8,8 @@ use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrl;
 use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlFactory as BaseFriendlyUrlFactory;
 use Shopsys\FrameworkBundle\Component\String\TransformString;
 
-class FriendlyUrlFactory extends BaseFriendlyUrlFactory implements FriendlyUrlFactoryInterface
+class FriendlyUrlFactory extends BaseFriendlyUrlFactory
 {
-    /**
-     * @param string $routeName
-     * @param int $entityId
-     * @param string $entityName
-     * @param int $domainId
-     * @param int|null $indexPostfix
-     * @param string[] $prefixes
-     * @return \Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrl|null
-     */
-    public function createFromPartsIfValid(
-        string $routeName,
-        int $entityId,
-        string $entityName,
-        int $domainId,
-        ?int $indexPostfix = null,
-        array $prefixes = []
-    ): ?FriendlyUrl {
-        if ($entityName === '') {
-            return null;
-        }
-
-        $slug = '';
-        foreach ($prefixes as $prefix) {
-            $slug .= TransformString::stringToFriendlyUrlSlug($prefix) . '/';
-        }
-
-        $nameForUrl = $entityName . ($indexPostfix === null ? '' : '-' . $indexPostfix);
-        $slug .= TransformString::stringToFriendlyUrlSlug($nameForUrl);
-
-        if ($slug === '') {
-            return null;
-        }
-
-        return $this->create($routeName, $entityId, $domainId, $slug);
-    }
-
     /**
      * @param string $routeName
      * @param int $entityId
