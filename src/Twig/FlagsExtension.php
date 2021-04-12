eaad23d61e1f53fd3e7d36a7b0e050831424ee86
Martin Grossmann <martin@vyvoj.net>
2021-04-12 13:11:44 +0200

    removed flag price notice

--- src/Twig/FlagsExtension.php
+++ src/Twig/FlagsExtension.php
@@ -40,7 +40,6 @@ class FlagsExtension extends AbstractExtension
     {
         return [
             new TwigFunction('renderFlagsByIds', [$this, 'renderFlagsByIds'], ['is_safe' => ['html']]),
-            new TwigFunction('renderAsterisk', [$this, 'renderAsterisk'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -56,22 +55,6 @@ class FlagsExtension extends AbstractExtension
             [
                 'flags' => $this->flagsProvider->getFlagsByIds($flagIds),
                 'classAddition' => $classAddition,
-            ]
-        );
-    }
-
-    /**
-     * @param int[] $flagIds
-     * @param string $priceType
-     * @return string
-     */
-    public function renderAsterisk(array $flagIds, string $priceType): string
-    {
-        return $this->twigEnvironment->render(
-            'Front/Inline/Product/productFlagAsterisk.html.twig',
-            [
-                'flags' => $this->flagsProvider->getFlagsByIds($flagIds),
-                'priceType' => $priceType,
             ]
         );
     }
