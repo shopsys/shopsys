f3b537f2c3e2e751702f67aa67e94a671f3a45c4
MichalSrnicek <michal.srnicek@shopsys.com>
2021-04-15 14:31:06 +0200

    design: edited mobile dropdown

--- assets/js/frontend/components/MobileDropdown.js
+++ assets/js/frontend/components/MobileDropdown.js
@@ -2,19 +2,21 @@ import Register from 'framework/common/utils/Register';
 
 export default class MobileDropdown {
     constructor () {
+        this.dropdownWrapper = '.js-mobile-dropdown';
         this.dropdownButton = '.js-mobile-dropdown-button';
-        this.dropdownContainer = $('.js-mobile-dropdown-container');
-        this.overlay = $('.web__overlay');
+        this.dropdownContainer = '.js-mobile-dropdown-container';
+        this.overlay = '.web__overlay';
         this.isToggleFinished = true;
     }
 
     static toggleDropdown (event, mobileDropdown) {
         if (mobileDropdown.isToggleFinished) {
+            const currentContainer = $(event.currentTarget).closest(mobileDropdown.dropdownWrapper).find(mobileDropdown.dropdownContainer);
             mobileDropdown.isToggleFinished = false;
+            
             event.stopPropagation(); 
-
-            mobileDropdown.overlay.fadeToggle();
-            mobileDropdown.dropdownContainer.slideToggle(function(){
+            $(mobileDropdown.overlay).fadeToggle();
+            currentContainer.slideToggle(function(){
                 mobileDropdown.isToggleFinished = true;
             });
         }
@@ -26,13 +28,13 @@ export default class MobileDropdown {
         $container.filterAllNodes(mobileDropdown.dropdownButton).on('click', (event) => MobileDropdown.toggleDropdown(event, mobileDropdown));
 
         $(document).click(function(){
-            if (mobileDropdown.dropdownContainer.is(":visible")) {
-                mobileDropdown.dropdownContainer.slideUp();
-                mobileDropdown.overlay.fadeOut();
+            if ($(mobileDropdown.dropdownContainer).is(":visible")) {
+                $(mobileDropdown.dropdownContainer).slideUp();
+                $(mobileDropdown.overlay).fadeOut();
             }
         })
 
-        mobileDropdown.dropdownContainer.click(function(e){
+        $(mobileDropdown.dropdownContainer).click(function(e){
             e.stopPropagation(); 
         });
     }
