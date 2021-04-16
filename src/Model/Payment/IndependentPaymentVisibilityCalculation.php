f4804c9f97d7184387c04734784812a4177785e3
Martin Grossmann <martin@vyvoj.net>
2021-04-16 12:17:00 +0200

    fixed failing test after #2206
    - related PR https://github.com/shopsys/shopsys/pull/2206

--- src/Model/Payment/IndependentPaymentVisibilityCalculation.php
+++ src/Model/Payment/IndependentPaymentVisibilityCalculation.php
@@ -22,8 +22,9 @@ class IndependentPaymentVisibilityCalculation extends BaseIndependentPaymentVisi
     {
         $locale = $this->domain->getDomainConfigById($domainId)->getLocale();
 
+        /** @var string|null $paymentName */
         $paymentName = $payment->getName($locale);
-        if ($paymentName === '') {
+        if ($paymentName === '' || $paymentName === null) {
             return false;
         }
 
