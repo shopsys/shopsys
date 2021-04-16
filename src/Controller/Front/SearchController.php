2b15e265bf1d66d5db7b7a7d023f995fb0c4c8ed
Martin Grossmann <martin@vyvoj.net>
2021-04-16 10:31:29 +0200

    v9.1.1 – trim search text from spaces (#2187)

--- src/Controller/Front/SearchController.php
+++ src/Controller/Front/SearchController.php
@@ -53,7 +53,7 @@ class SearchController extends FrontBaseController
      */
     public function autocompleteAction(Request $request)
     {
-        $searchText = $request->get('searchText');
+        $searchText = trim($request->get('searchText'));
         $searchUrl = $this->generateUrl('front_product_search', [ProductController::SEARCH_TEXT_PARAMETER => $searchText]);
 
         $categoriesPaginationResult = $this->categoryFacade->getSearchAutocompleteCategories(
@@ -90,7 +90,7 @@ class SearchController extends FrontBaseController
     public function boxAction(Request $request)
     {
         $searchText = TransformString::replaceInvalidUtf8CharactersByQuestionMark(
-            (string)$request->query->get(ProductController::SEARCH_TEXT_PARAMETER)
+            trim((string)$request->query->get(ProductController::SEARCH_TEXT_PARAMETER))
         );
 
         return $this->render('Front/Content/Search/searchBox.html.twig', [
