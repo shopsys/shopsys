fea7b45ba7f481fdc1360bb337fa50498fb226ff
Martin Grossmann <martin@vyvoj.net>
2021-04-19 09:54:55 +0200

    v9.1.1 – improve image lazy loading (#2268)

--- src/Twig/ImageExtension.php
+++ src/Twig/ImageExtension.php
@@ -95,4 +95,26 @@ class ImageExtension extends BaseImageExtension
             return $this->getNoimageHtml($attributes);
         }
     }
+
+    /**
+     * @deprecated Method will be moved to framework in the SSFW 10
+     * @param array $attributes
+     * @param string $entityName
+     * @param \Shopsys\FrameworkBundle\Component\Image\AdditionalImageData[] $additionalImagesData
+     * @return string
+     */
+    protected function getImageHtmlByEntityName(array $attributes, $entityName, $additionalImagesData = []): string
+    {
+        $htmlAttributes = $this->extractHtmlAttributesFromAttributes($attributes);
+
+        if ($this->isLazyLoadEnabled($attributes) === true) {
+            $htmlAttributes = $this->makeHtmlAttributesLazyLoaded($htmlAttributes);
+        }
+
+        return $this->twigEnvironment->render('@ShopsysFramework/Common/image.html.twig', [
+            'attr' => $htmlAttributes,
+            'additionalImagesData' => $additionalImagesData,
+            'imageCssClass' => $this->getImageCssClass($entityName, $attributes['type'], $attributes['size']),
+        ]);
+    }
 }
