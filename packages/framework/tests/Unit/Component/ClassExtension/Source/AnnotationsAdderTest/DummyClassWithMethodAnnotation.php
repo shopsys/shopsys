5a2b1522b134644a32a697ea6a55eb3bbe464733
Petr Heinz <petr.heinz@shopsys.com>
2021-05-05 14:20:43 +0200

    Add unit test for AnnotationsAdder::addAnnotationToClass
    - new test case for replacing an existing @method annotation
    - test currently fails

---
+++ packages/framework/tests/Unit/Component/ClassExtension/Source/AnnotationsAdderTest/DummyClassWithMethodAnnotation.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FrameworkBundle\Unit\Component\ClassExtension\Source\AnnotationsAdderTest;
+
+/**
+ * @method void setCategory(\Shopsys\FrameworkBundle\Model\Category\Category $category)
+ */
+class DummyClassWithMethodAnnotation
+{
+}
