f4775209ebc56054ea3f738b481099e16e5cafd3
David Střítežský <53976269+s3tezsky@users.noreply.github.com>
2021-06-07 08:29:02 +0200

    moved front translations to project-base (#2337)

--- packages/framework/src/Model/Breadcrumb/SimpleBreadcrumbGenerator.php
+++ packages/framework/src/Model/Breadcrumb/SimpleBreadcrumbGenerator.php
@@ -5,12 +5,28 @@ namespace Shopsys\FrameworkBundle\Model\Breadcrumb;
 use Shopsys\FrameworkBundle\Component\Breadcrumb\BreadcrumbGeneratorInterface;
 use Shopsys\FrameworkBundle\Component\Breadcrumb\BreadcrumbItem;
 
+/**
+ * @deprecated Class will be changed to abstract class in next major version. Extend this class to your project and implement corresponding methods instead.
+ */
 class SimpleBreadcrumbGenerator implements BreadcrumbGeneratorInterface
 {
     /**
      * @var string[]|null
      */
     protected $routeNameMap;
+
+    public function __construct()
+    {
+        if (static::class === self::class) {
+            trigger_error(
+                sprintf(
+                    'Class "%s" will be changed to abstract class in next major version. Extend this class to your project and implement corresponding methods instead.',
+                    self::class
+                ),
+                E_USER_DEPRECATED
+            );
+        }
+    }
 
     /**
      * @param string $routeName
@@ -41,20 +57,37 @@ class SimpleBreadcrumbGenerator implements BreadcrumbGeneratorInterface
     {
         if ($this->routeNameMap === null) {
             // Caching in order to translate breadcrumb item names only once
-            $this->routeNameMap = [
-                'front_customer_edit' => t('Edit data'),
-                'front_customer_orders' => t('Orders'),
-                'front_registration_reset_password' => t('Forgotten password'),
-                'front_customer_order_detail_registered' => t('Order detail'),
-                'front_customer_order_detail_unregistered' => t('Order detail'),
-                'front_login' => t('Login'),
-                'front_product_search' => t('Search [noun]'),
-                'front_registration_register' => t('Registration'),
-                'front_brand_list' => t('Brand overview'),
-                'front_contact' => t('Contact'),
-            ];
+            $this->routeNameMap = $this->getTranslatedBreadcrumbsByRouteNames();
         }
 
         return $this->routeNameMap;
+    }
+
+    /**
+     * @deprecated Method will be changed to abstract in next major version. Extend this class to your project and implement method by yourself instead.
+     * @return array<string, string>
+     */
+    protected function getTranslatedBreadcrumbsByRouteNames(): array
+    {
+        trigger_error(
+            sprintf(
+                'Method "%s" will be changed to abstract in next major version. Extend this class to your project and implement method by yourself instead.',
+                __METHOD__
+            ),
+            E_USER_DEPRECATED
+        );
+
+        return [
+            'front_customer_edit' => t('Edit data'),
+            'front_customer_orders' => t('Orders'),
+            'front_registration_reset_password' => t('Forgotten password'),
+            'front_customer_order_detail_registered' => t('Order detail'),
+            'front_customer_order_detail_unregistered' => t('Order detail'),
+            'front_login' => t('Login'),
+            'front_product_search' => t('Search [noun]'),
+            'front_registration_register' => t('Registration'),
+            'front_brand_list' => t('Brand overview'),
+            'front_contact' => t('Contact'),
+        ];
     }
 }
---
+++ project-base/src/Model/LegalConditions/LegalConditionsFacade.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\LegalConditions;
+
+use Shopsys\FrameworkBundle\Model\LegalConditions\LegalConditionsFacade as BaseLegalConditionsFacade;
+
+/**
+ * @method \App\Model\Article\Article|null findTermsAndConditions(int $domainId)
+ * @method setTermsAndConditions(\App\Model\Article\Article|null $termsAndConditions, int $domainId)
+ * @method \App\Model\Article\Article|null findPrivacyPolicy(int $domainId)
+ * @method setPrivacyPolicy(\App\Model\Article\Article|null $privacyPolicy, int $domainId)
+ * @method bool isArticleUsedAsLegalConditions(\App\Model\Article\Article $article)
+ * @method \App\Model\Article\Article|null findArticle(string $settingKey, int $domainId)
+ * @method setArticle(string $settingKey, \App\Model\Article\Article|null $privacyPolicy, int $domainId)
+ */
+class LegalConditionsFacade extends BaseLegalConditionsFacade
+{
+    /**
+     * @return string
+     */
+    public function getTermsAndConditionsDownloadFilename(): string
+    {
+        return t('Terms-and-conditions.html');
+    }
+}
