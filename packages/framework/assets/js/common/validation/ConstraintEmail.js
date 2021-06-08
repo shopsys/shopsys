601ec16e21fc89cf1d1f6529d8aac442eea17a6c
Tomas Ludvik <tomas.ludvik@shopsys.com>
2021-06-08 15:20:21 +0200

    javascript email validation works now same as server side validation (#2346)

--- packages/framework/assets/js/common/validation/ConstraintEmail.js
+++ packages/framework/assets/js/common/validation/ConstraintEmail.js
@@ -4,7 +4,8 @@
         this.message = '';
 
         this.validate = function (value) {
-            const regexp = /^.+@\S+\.\S+$/i;
+            const regexp = /^("([ !#-[\]-~]|\\[ -~])+"|[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*)@([0-9a-z\u00C0-\u02FF\u0370-\u1EFF]([-0-9a-z\u00C0-\u02FF\u0370-\u1EFF]{0,61}[0-9a-z\u00C0-\u02FF\u0370-\u1EFF])?\.)+[a-z\u00C0-\u02FF\u0370-\u1EFF]([-0-9a-z\u00C0-\u02FF\u0370-\u1EFF]{0,17}[a-z\u00C0-\u02FF\u0370-\u1EFF])?$/i;
+
             const errors = [];
             const f = FpJsFormValidator;
 
