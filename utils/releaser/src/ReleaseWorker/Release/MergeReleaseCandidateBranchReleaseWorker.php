9cd0abe2b6e687dd244d4440b566109877d82fc5
Rostislav Vítek <rostislav.vitek@shopsys.com>
2021-06-10 09:30:12 +0200

    fixed names of required heimdall tools for releasing new version
    - see https://github.com/shopsys/shopsys/pull/1945

--- utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
@@ -40,7 +40,7 @@ final class MergeReleaseCandidateBranchReleaseWorker extends AbstractShopsysRele
         if ($this->initialBranchName === 'master') {
             $this->symfonyStyle->note('Rest assured, after the master branch is built on Heimdall, it is split automatically (using http://heimdall:8080/view/Tools/job/tool-monorepo-split/)');
         } else {
-            $this->symfonyStyle->note(sprintf('You need split the "%s" branch it using "tool-monorepo-split-branch" on Heimdall (http://heimdall:8080/view/Tools/job/tool-monorepo-split-branch/)', $this->initialBranchName));
+            $this->symfonyStyle->note(sprintf('You need split the "%s" branch it using "tool-monorepo-force-split-branch" on Heimdall (http://heimdall:8080/view/Tools/job/tool-monorepo-split-branch/)', $this->initialBranchName));
         }
         $this->confirm('Confirm the branch is split.');
     }
