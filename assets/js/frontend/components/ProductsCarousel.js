6289ae3d7454ec7e5ad9cd232367214be1e9edcf
michalsrnicek <michal.srnicek@gmail.com>
2021-07-01 01:15:48 +0200

    design: edited function of gallery

--- assets/js/frontend/components/ProductsCarousel.js
+++ assets/js/frontend/components/ProductsCarousel.js
@@ -8,27 +8,33 @@ import Register from 'framework/common/utils/Register';
 
 export default class SlickCarousel {
     static init () {
-        const $mainImageCarousel = $('.js-main-image-carousel');
+        function mobileSlider () {
+            const $mainImageCarousel = $('.js-image-carousel');
 
-        if ($mainImageCarousel.length) {
-            $mainImageCarousel.not('.slick-initialized').slick({
-                fade: false,
-                autoplay: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: false,
-                arrows: false,
-                dots: false,
-                prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Předchozí" role="button"></button>',
-                nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Další" role="button"></button>',
-                responsive: [{
-                    breakpoint: Responsive.LG,
-                    settings: {
-                        arrows: true
-                    }
-                }]
-            });
+            if ($mainImageCarousel.length && window.innerWidth <= Responsive.LG) {
+                $mainImageCarousel.not('.slick-initialized').slick({
+                    fade: false,
+                    autoplay: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    arrows: true,
+                    dots: false,
+                    prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Předchozí" role="button"></button>',
+                    nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Další" role="button"></button>'
+                });
+            } else {
+                if ($mainImageCarousel.hasClass('slick-initialized')) {
+                    $mainImageCarousel.slick('unslick');
+                }
+            }
         }
+
+        mobileSlider();
+
+        $(window).resize(function () {
+            mobileSlider();
+        });
     }
 }
 
