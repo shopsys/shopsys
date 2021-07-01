f1962542bd9495a45d31ea8b152296aafbb292a6
David Střítežský <53976269+s3tezsky@users.noreply.github.com>
2021-07-01 09:19:26 +0200

    fixed validation of quantity in cart (#2367)

--- project-base/src/Form/Front/Cart/AddProductFormType.php
+++ project-base/src/Form/Front/Cart/AddProductFormType.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Form\Front\Cart;
 
+use Shopsys\FrameworkBundle\Form\Constraints\ConstraintValue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
@@ -27,11 +28,14 @@ class AddProductFormType extends AbstractType
                     new Constraints\Regex(['pattern' => '/^\d+$/']),
                 ],
             ])
-            ->add('quantity', TextType::class, [
+            ->add('quantity', NumberType::class, [
                 'data' => 1,
                 'constraints' => [
                     new Constraints\GreaterThan(0),
-                    new Constraints\Regex(['pattern' => '/^\d+$/']),
+                    new Constraints\LessThanOrEqual([
+                        'value' => ConstraintValue::INTEGER_MAX_VALUE,
+                        'message' => 'Please enter valid quantity',
+                    ]),
                 ],
             ])
             ->add('add', SubmitType::class);
--- project-base/src/Form/Front/Cart/CartFormType.php
+++ project-base/src/Form/Front/Cart/CartFormType.php
@@ -7,8 +7,8 @@ namespace App\Form\Front\Cart;
 use Shopsys\FrameworkBundle\Form\Constraints\ConstraintValue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
@@ -25,7 +25,7 @@ class CartFormType extends AbstractType
             ->add('quantities', CollectionType::class, [
                 'allow_add' => true,
                 'allow_delete' => true,
-                'entry_type' => TextType::class,
+                'entry_type' => NumberType::class,
                 'constraints' => [
                     new Constraints\All([
                         'constraints' => [
