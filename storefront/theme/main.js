cf4db17536b02c4f406531dfb271e8dc15741c05
TomasGottvald <tomas.gottvald@shopsys.com>
2021-07-07 00:22:40 +0200

    style - add main theme as global styling

---
+++ storefront/theme/main.js
@@ -0,0 +1,28 @@
+/* Main theme */
+export const theme = {
+    /* Colors */
+    color: {
+        white: '#fff',
+        black: '#000',
+        orange: '#ecb200',
+        orangeLight: '#fff0c4',
+        primary: '#4c5bfd',
+    },
+
+    /* Fonts */
+    fontSize: {
+        default: '16px',
+    },
+
+    /* Default border radius value */
+    radius: {
+        default: '11px',
+        medium: '4px',
+        small: '2px',
+    },
+
+    /* Buttons */
+    btnHeight: '48px',
+    /* Transition */
+    transition: '0.2s cubic-bezier(.8, .20, .48, 1.0)',
+};
