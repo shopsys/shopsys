38f36060cb6b953a4b6b13d7bd83acf3127c0bb9
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-07-21 12:42:40 +0200

    added examples + tranlsated labels in index.js

--- storefront/pages/index.js
+++ storefront/pages/index.js
@@ -5,6 +5,10 @@ import { useTranslation } from 'react-i18next';
 import SsfwButton from '../components/forms/SsfwButton';
 import ShopsysInUserText from '../components/in/ShopsysInUserText';
 import getConfig from 'next/config';
+import ShopsysTextInput from '../components/forms/ShopsysTextInput';
+import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 const { publicRuntimeConfig } = getConfig();
 
 const content = `<h1>Heading</h1>
@@ -35,6 +39,18 @@ const content = `<h1>Heading</h1>
 const Index = () => {
     const { t } = useTranslation();
 
+    const validationSchema = Yup.object().shape({
+        name: Yup.string()
+            .notRequired()
+            .matches(
+                /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
+                {
+                    message: t('name cannot contain special characters or numbers'),
+                    excludeEmptyString: true,
+                },
+            ),
+    });
+
     const [result] = useQuery({
         query: `
             query categories {
@@ -44,6 +60,13 @@ const Index = () => {
                 }
             }
             `,
+    });
+
+    const formProviderMethods = useForm({
+        mode: 'onBlur',
+        criteriaMode: 'firstError',
+        shouldFocusError: true,
+        resolver: yupResolver(validationSchema),
     });
 
     function CategoryList() {
@@ -82,6 +105,13 @@ const Index = () => {
                     {t('Secondary')}
                 </SsfwButton>
             </div>
+            <FormProvider {...formProviderMethods}>
+                <form style={{ marginTop: '15px' }}>
+                    <ShopsysTextInput id="my-form_name" name="name" label={t('name')} shouldUseSuccess={true} />
+                    <ShopsysTextInput id="my-form_password" name="password" label={t('password')} type="password" />
+                    <ShopsysTextInput id="my-form_disabled" name="disabled" label={t('disabled')} disabled={true} />
+                </form>
+            </FormProvider>
         </>
     );
 };
