ce4ae80b556651b84f516e15398cb0ea459646bc
Vojtěch Hanáček <hanacekv@gmail.com>
2021-07-23 09:52:34 +0200

    [FWCC-86] enable google feed

--- config/bundles.php
+++ config/bundles.php
@@ -18,6 +18,7 @@ return [
     Shopsys\GoogleCloudBundle\ShopsysGoogleCloudBundle::class => ['all' => true],
     Shopsys\MigrationBundle\ShopsysMigrationBundle::class => ['all' => true],
     Shopsys\ProductFeed\HeurekaBundle\ShopsysProductFeedHeurekaBundle::class => ['all' => true],
+    Shopsys\ProductFeed\GoogleBundle\ShopsysProductFeedGoogleBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
