308891f0a0d7e0063f34f967c8a35adf7a1025cf
David Stritezsky <david.stritezsky@shopsys.com>
2021-08-09 10:53:49 +0200

    used internal graphql endpoint for SSR

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -4,6 +4,9 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
     i18n,
     reactStrictMode: true,
+    serverRuntimeConfig: {
+        internalGraphqlEndpoint: process.env.INTERNAL_GRAPHQL_ENDPOINT,
+    },
     publicRuntimeConfig: {
         publicGraphqlEndpoint: process.env.PUBLIC_GRAPHQL_ENDPOINT,
     },
