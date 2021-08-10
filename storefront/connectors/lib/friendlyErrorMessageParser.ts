7c78bd17ce56ad4463aa98c84a784f8a3267a615
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-08-10 11:51:02 +0200

    moved friendlyErrorMessageParser to a separate file

---
+++ storefront/connectors/lib/friendlyErrorMessageParser.ts
@@ -0,0 +1,31 @@
+import { CombinedError } from 'urql';
+import { TFunction } from 'react-i18next';
+
+export type ParsedErrors = {
+    applicationError: undefined | string;
+    userError: undefined | { [key: string]: { message: string; code: string }[] };
+};
+
+export const getUserFriendlyErrorMessage = (originalError: CombinedError, t: TFunction): ParsedErrors => {
+    const errors: ParsedErrors = {
+        applicationError: undefined,
+        userError: undefined,
+    };
+
+    if (originalError.networkError) {
+        errors.applicationError = t('Could not connect to server. Check your network.');
+    } else if (originalError.graphQLErrors.length > 0) {
+        originalError.graphQLErrors.map((error) => {
+            if (error.message === 'validation') {
+                errors.userError = error?.extensions?.validation;
+            } else {
+                // @todo: add real exception to error handler to deal with this
+                errors.applicationError = t('Hooops, someting wrong happend.');
+            }
+        });
+    } else {
+        errors.applicationError = t('Unknown error.');
+    }
+
+    return errors;
+};
