d84453b407a920b32ed3df50a0e063231b3dfe1a
Martin Grossmann <martin@vyvoj.net>
2021-09-09 12:23:44 +0200

    added tests for FE API cart operations
    - dispatch fake kernel.response event to trigger immediate recalculations (specifically ProductAvailabilityRecalculator and ProductAvailabilityRecalculator)
    - otherwise the edited Product would remain in the scheduler and would be recalculated on the kernel.response event after GraphQL call
    - however, the GraphQL call would make the Product invalid (that is the nature of the validator) and the recalculation would end on an error
      - copy of the error from log: Uncaught PHP Exception InvalidArgumentException: "Entity has to be managed or scheduled for removal for single computation App\Model\Product\Product@000000006d3d9248000000006cced69c" at /var/www/html/vendor/shopsys/doctrine-orm/lib/Doctrine/ORM/UnitOfWork.php line 516 Error ID: quIobkiaeM
    - see https://gitlab.shopsys.cz/ss6-projects/prusa3d/-/merge_requests/104/diffs?commit_id=38eb706cd1101f6c22b738f59e71d502f93f2888

--- tests/App/Test/TransactionFunctionalTestCase.php
+++ tests/App/Test/TransactionFunctionalTestCase.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Tests\App\Test;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 abstract class TransactionFunctionalTestCase extends FunctionalTestCase
 {
@@ -13,6 +18,12 @@ abstract class TransactionFunctionalTestCase extends FunctionalTestCase
      * @inject
      */
     protected EntityManagerInterface $em;
+
+    /**
+     * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+     * @inject
+     */
+    private EventDispatcherInterface $eventDispatcher;
 
     protected function setUp(): void
     {
@@ -27,5 +38,23 @@ abstract class TransactionFunctionalTestCase extends FunctionalTestCase
         $this->em->rollback();
 
         parent::tearDown();
+    }
+
+    /**
+     * Runs scheduled recalculations that would be executed on a kernel.response event
+     * This allows to clean scheduled recalculations before making request on a client that could break the application
+     * Eg. when testing GraphQL validation that breaks consistency of the entity and disallows any operation over it afterwards
+     */
+    protected function dispatchFakeKernelResponseEventToTriggerImmediateRecalculations(): void
+    {
+        $fakeKernelResponseEvent = new ResponseEvent(
+            $this->getCurrentClient()->getKernel(),
+            new Request(),
+            HttpKernelInterface::MASTER_REQUEST,
+            new Response()
+        );
+
+        /* @phpstan-ignore-next-line */
+        $this->eventDispatcher->dispatch($fakeKernelResponseEvent, 'kernel.response');
     }
 }
