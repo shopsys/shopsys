8e56a9a5d374f7dd793c41d17ec813b8c334d154
michal.srnicek <michal.srnicek@gmail.com>
2021-09-10 01:34:09 +0200

    refactored webline

--- storefront/components/Layout/Webline/Webline.tsx
+++ storefront/components/Layout/Webline/Webline.tsx
@@ -1,27 +1,22 @@
-import { HTMLAttributes, ReactElement } from 'react';
-import PropTypes, { InferProps } from 'prop-types';
-import { StyledContainer, StyledWebline } from './Webline.style';
+import { ContainerStyled, WeblineStyled } from './Webline.style';
+import { FC, HTMLAttributes } from 'react';
 import { ExtractNativePropsFromDefault } from 'typeHelpers/ExtractNativePropsFromDefault';
 
 type NativeProps = ExtractNativePropsFromDefault<HTMLAttributes<HTMLDivElement>, 'children', 'style'>;
 
-function Webline(props: InferProps<typeof Webline.propTypes> & NativeProps): ReactElement {
-    return (
-        <StyledWebline {...props}>
-            <StyledContainer>{props.children}</StyledContainer>
-        </StyledWebline>
-    );
-}
-
-Webline.defaultProps = {
-    type: 'default',
-};
-
-Webline.propTypes = {
+type WeblineProps = NativeProps & {
     /**
      * Type for change type of webline. If you don't fill this prop then the webline will be without style.
      */
-    type: PropTypes.oneOf<'default' | 'colored' | 'dark' | 'light'>(['default', 'colored', 'dark', 'light']).isRequired,
+    type?: 'colored' | 'dark' | 'light';
+};
+
+const Webline: FC<WeblineProps> = (props) => {
+    return (
+        <WeblineStyled {...props}>
+            <ContainerStyled>{props.children}</ContainerStyled>
+        </WeblineStyled>
+    );
 };
 
 /* @component */
