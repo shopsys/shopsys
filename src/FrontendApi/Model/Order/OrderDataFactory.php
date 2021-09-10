963d36b0323d936f168adeea08b90e36bfedfb7c
Rostislav Vitek <rostislav.vitek@shopsys.com>
2021-09-10 12:59:21 +0200

    FE API: it is now possible to set personal pickup store in order
    - when the store is set (using it's uuid), then:
    	- delivery address is automatically considered to be different from the billing address
    	- delivery first name, delivery last name, delivery company name, and delivery telephone are taken from the billing data if not set explicitly
    	- delivery street, delivery city, delivery postcode, and delivery country are taken from the store

--- src/FrontendApi/Model/Order/OrderDataFactory.php
+++ src/FrontendApi/Model/Order/OrderDataFactory.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace App\FrontendApi\Model\Order;
 
+use App\Model\Order\OrderData;
+use App\Model\Store\Exception\StoreByUuidNotFoundException;
+use App\Model\Store\Store;
+use App\Model\Store\StoreFacade;
+use GraphQL\Error\UserError;
 use Overblog\GraphQLBundle\Definition\Argument;
-use Shopsys\FrameworkBundle\Model\Order\OrderData;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Country\CountryFacade;
+use Shopsys\FrameworkBundle\Model\Order\OrderData as BaseOrderData;
+use Shopsys\FrameworkBundle\Model\Order\OrderDataFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Payment\PaymentFacade;
+use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
+use Shopsys\FrameworkBundle\Model\Product\ProductFacade;
+use Shopsys\FrameworkBundle\Model\Transport\TransportFacade;
 use Shopsys\FrontendApiBundle\Model\Order\OrderDataFactory as BaseOrderDataFactory;
 
 /**
@@ -13,16 +25,45 @@ use Shopsys\FrontendApiBundle\Model\Order\OrderDataFactory as BaseOrderDataFacto
  * @property \App\Model\Transport\TransportFacade $transportFacade
  * @property \App\Model\Order\OrderDataFactory $orderDataFactory
  * @property \App\Model\Product\ProductFacade $productFacade
- * @method __construct(\App\Model\Order\OrderDataFactory $orderDataFactory, \Shopsys\FrameworkBundle\Component\Domain\Domain $domain, \App\Model\Payment\PaymentFacade $paymentFacade, \App\Model\Transport\TransportFacade $transportFacade, \Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade $currencyFacade, \Shopsys\FrameworkBundle\Model\Country\CountryFacade $countryFacade, \App\Model\Product\ProductFacade $productFacade)
  * @method \App\Model\Order\OrderData withResolvedFields(array $input, \App\Model\Order\OrderData $orderData)
  */
 class OrderDataFactory extends BaseOrderDataFactory
 {
     /**
+     * @var \App\Model\Store\StoreFacade
+     */
+    private StoreFacade $storeFacade;
+
+    /**
+     * @param \App\Model\Order\OrderDataFactory $orderDataFactory
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     * @param \App\Model\Payment\PaymentFacade $paymentFacade
+     * @param \App\Model\Transport\TransportFacade $transportFacade
+     * @param \Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade $currencyFacade
+     * @param \Shopsys\FrameworkBundle\Model\Country\CountryFacade $countryFacade
+     * @param \App\Model\Product\ProductFacade $productFacade
+     * @param \App\Model\Store\StoreFacade $storeFacade
+     */
+    public function __construct(
+        OrderDataFactoryInterface $orderDataFactory,
+        Domain $domain,
+        PaymentFacade $paymentFacade,
+        TransportFacade $transportFacade,
+        CurrencyFacade $currencyFacade,
+        CountryFacade $countryFacade,
+        ProductFacade $productFacade,
+        StoreFacade $storeFacade
+    ) {
+        parent::__construct($orderDataFactory, $domain, $paymentFacade, $transportFacade, $currencyFacade, $countryFacade, $productFacade);
+
+        $this->storeFacade = $storeFacade;
+    }
+
+    /**
      * @param \Overblog\GraphQLBundle\Definition\Argument $argument
      * @return \App\Model\Order\OrderData
      */
-    public function createOrderDataFromArgument(Argument $argument): OrderData
+    public function createOrderDataFromArgument(Argument $argument): BaseOrderData
     {
         /** @var \App\Model\Order\OrderData $orderData */
         $orderData = parent::createOrderDataFromArgument($argument);
@@ -30,7 +71,36 @@ class OrderDataFactory extends BaseOrderDataFactory
         if ($orderData->companyName !== null && $orderData->companyNumber !== null) {
             $orderData->isCompanyCustomer = true;
         }
+        $input = $argument['input'];
+        if (isset($input['personalPickupStoreUuid'])) {
+            try {
+                $store = $this->storeFacade->getByUuid($input['personalPickupStoreUuid']);
+                $this->setOrderDataByStore($orderData, $store);
+            } catch (StoreByUuidNotFoundException $exception) {
+                throw new UserError($exception->getMessage());
+            }
+        }
 
         return $orderData;
+    }
+
+    /**
+     * @param \App\Model\Order\OrderData $orderData
+     * @param \App\Model\Store\Store $store
+     */
+    private function setOrderDataByStore(OrderData $orderData, Store $store): void
+    {
+        $orderData->personalPickupStore = $store;
+        $orderData->deliveryAddressSameAsBillingAddress = false;
+
+        $orderData->deliveryFirstName = $orderData->deliveryFirstName ?? $orderData->firstName;
+        $orderData->deliveryLastName = $orderData->deliveryLastName ?? $orderData->lastName;
+        $orderData->deliveryCompanyName = $orderData->deliveryCompanyName ?? $orderData->companyName;
+        $orderData->deliveryTelephone = $orderData->deliveryTelephone ?? $orderData->telephone;
+
+        $orderData->deliveryStreet = $store->getStreet();
+        $orderData->deliveryCity = $store->getCity();
+        $orderData->deliveryPostcode = $store->getPostcode();
+        $orderData->deliveryCountry = $store->getCountry();
     }
 }
---
+++ tests/FrontendApiBundle/Functional/Order/OrderWithPersonalPickupStoreTest.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FrontendApiBundle\Functional\Order;
+
+use App\DataFixtures\Demo\PaymentDataFixture;
+use App\DataFixtures\Demo\ProductDataFixture;
+use App\DataFixtures\Demo\StoreDataFixture;
+use App\DataFixtures\Demo\TransportDataFixture;
+use App\DataFixtures\Demo\VatDataFixture;
+use Tests\FrontendApiBundle\Test\GraphQlTestCase;
+
+class OrderWithPersonalPickupStoreTest extends GraphQlTestCase
+{
+    public function testCreateOrderWithPersonalPickupStore()
+    {
+        /** @var \App\Model\Store\Store $store */
+        $store = $this->getReference(StoreDataFixture::STORE_PREFIX . 1);
+
+        $expected = [
+            'data' => [
+                'CreateOrder' => [
+                    'deliveryFirstName' => 'firstName',
+                    'deliveryLastName' => 'lastName',
+                    'deliveryCompanyName' => 'Shopsys',
+                    'deliveryTelephone' => '+53 123456789',
+                    'deliveryStreet' => $store->getStreet(),
+                    'deliveryCity' => $store->getCity(),
+                    'deliveryPostcode' => $store->getPostcode(),
+                    'deliveryCountry' => $store->getCountry()->getCode(),
+                ],
+            ],
+        ];
+
+        /** @var \Shopsys\FrameworkBundle\Model\Product\Product $product */
+        $product = $this->getReference(ProductDataFixture::PRODUCT_PREFIX . '1');
+        $mutation = 'mutation {
+            AddToCart(input: {
+                productUuid: "' . $product->getUuid() . '",
+                quantity: 1
+            }) {
+                uuid
+            }
+        }';
+        $cartUuid = $this->getResponseContentForQuery($mutation)['data']['AddToCart']['uuid'];
+
+        $this->assertQueryWithExpectedArray($this->getMutation($cartUuid, $store->getUuid()), $expected);
+    }
+
+    /**
+     * @param string $cartUuid
+     * @param string $storeUuid
+     * @return string
+     */
+    private function getMutation(string $cartUuid, string $storeUuid): string
+    {
+        $domainId = $this->domain->getId();
+        /** @var \Shopsys\FrameworkBundle\Model\Pricing\Vat\Vat $vatHigh */
+        $vatHigh = $this->getReferenceForDomain(VatDataFixture::VAT_HIGH, $domainId);
+        /** @var \Shopsys\FrameworkBundle\Model\Pricing\Vat\Vat $vatZero */
+        $vatZero = $this->getReferenceForDomain(VatDataFixture::VAT_ZERO, $domainId);
+
+        /** @var \Shopsys\FrameworkBundle\Model\Payment\Payment $paymentCard */
+        $paymentCard = $this->getReference(PaymentDataFixture::PAYMENT_CARD);
+        $paymentPrice = $this->getMutationPriceConvertedToDomainDefaultCurrency('100', $vatZero);
+
+        /** @var \Shopsys\FrameworkBundle\Model\Transport\Transport $transportPersonalPickup */
+        $transportPersonalPickup = $this->getReference(TransportDataFixture::TRANSPORT_PERSONAL);
+        $transportPrice = $this->getMutationPriceConvertedToDomainDefaultCurrency('0', $vatHigh);
+
+        return 'mutation {
+                    CreateOrder(
+                        input: {
+                            cartUuid: "' . $cartUuid . '"
+                            firstName: "firstName"
+                            lastName: "lastName"
+                            companyName: "Shopsys"
+                            email: "user@example.com"
+                            telephone: "+53 123456789"
+                            onCompanyBehalf: false
+                            street: "123 Fake Street"
+                            city: "Springfield"
+                            postcode: "12345"
+                            country: "CZ"
+                            payment: {
+                                uuid: "' . $paymentCard->getUuid() . '"
+                                price: ' . $paymentPrice . '
+                            }
+                            transport: {
+                                uuid: "' . $transportPersonalPickup->getUuid() . '"
+                                price: ' . $transportPrice . '
+                            }
+                            differentDeliveryAddress: false
+                            personalPickupStoreUuid: "' . $storeUuid . '"
+                        }
+                    ) {
+                        deliveryFirstName
+                        deliveryLastName
+                        deliveryCompanyName
+                        deliveryTelephone
+                        deliveryStreet
+                        deliveryCity
+                        deliveryPostcode
+                        deliveryCountry
+                    }
+                }';
+    }
+}
