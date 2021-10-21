6ecc293d8090a996820d35423dcafe34066206ee
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-10-21 12:56:40 +0200

    added handling of non-exisiting cart UUID
    - the error is matched to a regular expression
    - based on the match, cookie and redux are deleted

--- storefront/hooks/cart/UseHandleCartErrors.ts
+++ storefront/hooks/cart/UseHandleCartErrors.ts
@@ -1,8 +1,12 @@
+import { initCartInputCookie, updateCartInputCookie } from 'helpers/Cookies';
 import { CombinedError } from '@urql/core';
 import { showErrorMessage } from 'components/Helpers/Toasts';
+import { updateCartState } from 'utils/Cart/UpdateCartState';
 import { useEffect } from 'react';
+import { useShopsysDispatch } from 'redux/main';
 
 export const useHandleCartErrors = (resultErrors: CombinedError | undefined): void => {
+    const dispatch = useShopsysDispatch();
     useEffect(() => {
         if (resultErrors === undefined) {
             return;
@@ -10,6 +14,19 @@ export const useHandleCartErrors = (resultErrors: CombinedError | undefined): vo
 
         // TODO refactor
         for (const error of resultErrors.graphQLErrors) {
+            if (
+                /Cart "\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b" is unavailable./.test(
+                    error.message,
+                )
+            ) {
+                updateCartState(
+                    dispatch,
+                    { cart: null, payment: null, personalPickupStore: null, transport: null },
+                    { cartUuid: null, transport: null, payment: null, promoCode: null },
+                );
+                updateCartInputCookie(initCartInputCookie());
+            }
+
             if (error.extensions?.validation === undefined) {
                 return;
             }
