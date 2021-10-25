17e86f45dddb8504720725f681556d4fa045de1f
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-10-25 09:42:06 +0200

    changed useLayoutEffect to useEffect
    - useLayoutEffect cannot be used during SSR and it caused
    errors to pop up in the console

--- storefront/hooks/ui/UseGetWindowSize.ts
+++ storefront/hooks/ui/UseGetWindowSize.ts
@@ -1,9 +1,9 @@
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useGetWindowSize = (): { height: number; width: number } => {
     const [windowSize, setWindowSize] = useState({ height: -1, width: -1 });
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         if (typeof window !== 'undefined') {
             const updateSize = () => {
                 setWindowSize({ height: window.innerHeight, width: window.innerWidth });
