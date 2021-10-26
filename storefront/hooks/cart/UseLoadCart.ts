436fd2e96c4cbcf6ae41f3fd925b3e8b5fc926ce
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-10-26 14:35:58 +0200

    added refreshing mechanism to cart
    - now every time path changes, the cart is updated
    - meanin even on 1 "page", like [...all].tsx, if the url changes,
    the cart is updated

--- storefront/hooks/cart/UseLoadCart.ts
+++ storefront/hooks/cart/UseLoadCart.ts
@@ -1,7 +1,13 @@
 import { loadCart } from 'connectors/cart/Cart';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useShopsysSelector } from 'redux/main';
 
 export const useLoadCart = (): void => {
+    const router = useRouter();
     const { cartUuid, transport, payment, promoCode } = useShopsysSelector((state) => state.cartInput);
-    loadCart(cartUuid, transport, payment, promoCode);
+    const [, refreshCart] = loadCart(cartUuid, transport, payment, promoCode);
+    useEffect(() => {
+        refreshCart();
+    }, [router.asPath]);
 };
