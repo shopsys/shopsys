abf953e8c236a9196eaabd0c652c435081f6dc93
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-11-02 09:25:37 +0100

    added error popup to forms
    - added to newsletter form, registration form (after order),
    reset password form and create order form

--- storefront/pages/order/contact-information.tsx
+++ storefront/pages/order/contact-information.tsx
@@ -4,6 +4,7 @@ import { initCartInputCookie, updateCartInputCookie } from 'helpers/Cookies';
 import { initServerSideProps, ServerSidePropsType } from 'helpers/InitServerSideProps';
 import { nextReduxWrapper, useShopsysDispatch, useShopsysSelector } from 'redux/main';
 import ContactInformationForm from 'components/Pages/Order/ContactInformation';
+import ErrorPopup from 'components/Forms/Lib/ErrorPopup';
 import { FC } from 'react';
 import { getContactInformationFormResolver } from 'components/Pages/Order/ContactInformation/ContactInformationFormResolver';
 import { handleOrderPagesRedirect } from 'helpers/HandleOrderPagesRedirect';
@@ -16,6 +17,7 @@ import { updateCartState } from 'utils/Cart/UpdateCartState';
 import { useCreateOrder } from 'connectors/order/Order';
 import { useGetInternationalizedStaticUrls } from 'hooks/staticUrls/UseGetInternationalizedStaticUrls';
 import { useHandleContactInformationNonTextChanges } from 'hooks/forms/useHandleContactInformationNonTextChanges';
+import { useHandleErrorPopupVisibility } from 'hooks/forms/UseHandleErrorPopupVisibility';
 import { useHandleFormErrors } from 'hooks/forms/UseHandleFormErrors';
 import { useHandleFormSuccessfulSubmit } from 'hooks/forms/UseHandleFormSuccessfulSubmit';
 import { userActions } from 'redux/slices/user';
@@ -36,6 +38,7 @@ const ContactInformation: FC<ServerSidePropsType> = () => {
     const t = useTypedTranslationFunction();
     const [createOrderResult, createOrder] = useCreateOrder();
     const formProviderMethods = useShopsysForm(getContactInformationFormResolver(t), contactInformationValues);
+    const [isErrorPopupVisible, setErrorPopupVisibility] = useHandleErrorPopupVisibility(formProviderMethods);
     useHandleFormSuccessfulSubmit(createOrderResult, formProviderMethods, contactInformationValues, () =>
         onSuccessfullyCreatedOrderHandler(),
     );
@@ -75,7 +78,7 @@ const ContactInformation: FC<ServerSidePropsType> = () => {
     return (
         <StaticUrlGuard domainUrl={domainUrl}>
             <FormProvider {...formProviderMethods}>
-                <form onSubmit={formProviderMethods.handleSubmit(onCreateOrderHandler)}>
+                <form onSubmit={formProviderMethods.handleSubmit(onCreateOrderHandler)} noValidate>
                     <OrderLayout activeStep={3} buttonNextText={t('Submit order')}>
                         <ContactInformationForm />
                         <OrderAction
@@ -90,6 +93,59 @@ const ContactInformation: FC<ServerSidePropsType> = () => {
                     </OrderLayout>
                 </form>
             </FormProvider>
+            <ErrorPopup
+                isVisible={isErrorPopupVisible}
+                onCloseCallback={() => setErrorPopupVisibility(false)}
+                errors={[
+                    { label: t('Your e-mail'), message: formProviderMethods.formState.errors.email?.message },
+                    {
+                        label: t('I want to register with an order'),
+                        message: formProviderMethods.formState.errors.register?.message,
+                    },
+                    { label: t('Password'), message: formProviderMethods.formState.errors.passwordFirst?.message },
+                    {
+                        label: t('Password again'),
+                        message: formProviderMethods.formState.errors.passwordSecond?.message,
+                    },
+                    {
+                        label: t('You will shop with us like'),
+                        message: formProviderMethods.formState.errors.customer?.message,
+                    },
+                    { label: t('Telephone'), message: formProviderMethods.formState.errors.telephone?.message },
+                    { label: t('First name'), message: formProviderMethods.formState.errors.firstName?.message },
+                    { label: t('Last name'), message: formProviderMethods.formState.errors.lastName?.message },
+                    { label: t('Company name'), message: formProviderMethods.formState.errors.companyName?.message },
+                    {
+                        label: t('Company number'),
+                        message: formProviderMethods.formState.errors.companyNumber?.message,
+                    },
+                    { label: t('Tax number'), message: formProviderMethods.formState.errors.companyTaxNumber?.message },
+                    { label: t('Street'), message: formProviderMethods.formState.errors.street?.message },
+                    { label: t('City'), message: formProviderMethods.formState.errors.city?.message },
+                    { label: t('Postcode'), message: formProviderMethods.formState.errors.postcode?.message },
+                    {
+                        label: t('Enter the delivery address'),
+                        message: formProviderMethods.formState.errors.differentDeliveryAddress?.message,
+                    },
+                    {
+                        label: t('First name'),
+                        message: formProviderMethods.formState.errors.deliveryFirstName?.message,
+                    },
+                    { label: t('Last name'), message: formProviderMethods.formState.errors.deliveryLastName?.message },
+                    { label: t('Company'), message: formProviderMethods.formState.errors.deliveryCompanyName?.message },
+                    { label: t('Telephone'), message: formProviderMethods.formState.errors.deliveryTelephone?.message },
+                    {
+                        label: t('Street and house number'),
+                        message: formProviderMethods.formState.errors.deliveryStreet?.message,
+                    },
+                    { label: t('City'), message: formProviderMethods.formState.errors.deliveryCity?.message },
+                    { label: t('Postcode'), message: formProviderMethods.formState.errors.deliveryPostcode?.message },
+                    {
+                        label: t('I want to subscribe to the newsletter'),
+                        message: formProviderMethods.formState.errors.newsletterSubscription?.message,
+                    },
+                ]}
+            />
         </StaticUrlGuard>
     );
 };
