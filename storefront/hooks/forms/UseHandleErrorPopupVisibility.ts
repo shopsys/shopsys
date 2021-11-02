1ac33ab48acd2670eca58a30e74f5063751f8f28
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-11-02 09:45:42 +0100

    added error popup to transport and payment form
    - an optional override has been added to the popup visibility
    hook, that can be used if a special visibility condition should be
    applied

--- storefront/hooks/forms/UseHandleErrorPopupVisibility.ts
+++ storefront/hooks/forms/UseHandleErrorPopupVisibility.ts
@@ -3,13 +3,14 @@ import { UseFormReturn } from 'react-hook-form';
 
 export const useHandleErrorPopupVisibility = <T>(
     formProviderMethods: UseFormReturn<T>,
+    overrideVisibility?: boolean,
 ): [boolean, Dispatch<SetStateAction<boolean>>] => {
     const [isErrorPopupVisible, setErrorPopupVisibility] = useState(false);
 
     useEffect(() => {
         if (
             formProviderMethods.formState.isSubmitting &&
-            Object.keys(formProviderMethods.formState.errors).length > 0
+            (Object.keys(formProviderMethods.formState.errors).length > 0 || overrideVisibility)
         ) {
             setErrorPopupVisibility(true);
         }
