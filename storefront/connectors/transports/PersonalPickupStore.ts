3ec78091279b32340703577896ae8d7983e90fac
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-11-18 12:47:29 +0100

    updated getSelectedPersonalPickupStore to return the packetery store if the selected transport type is packetery

--- storefront/connectors/transports/PersonalPickupStore.ts
+++ storefront/connectors/transports/PersonalPickupStore.ts
@@ -1,4 +1,5 @@
 import { StoreType, TransportType } from './types';
+import { getPacketeryCookie } from 'helpers/packetery';
 
 export const getSelectedPersonalPickupStore = (
     transport: TransportType | null,
@@ -6,6 +7,10 @@ export const getSelectedPersonalPickupStore = (
 ): StoreType | null => {
     if (transport === null || pickupPlaceIdentifier === null) {
         return null;
+    }
+
+    if (transport.transportType.code === 'packetery') {
+        return getPacketeryCookie();
     }
 
     const personalPickupStore = transport.stores.find((store) => store.uuid === pickupPlaceIdentifier);
