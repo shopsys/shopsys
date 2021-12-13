4989c637feefb780ce1579eb5bef8d62fd96ec5a
Jan Horak <jan.horak@shopsys.com>
2021-12-13 15:14:27 +0100

    pagination reset on category change

--- storefront/components/Layout/Header/Navigation/NavigationSubList/NavigationSubList.tsx
+++ storefront/components/Layout/Header/Navigation/NavigationSubList/NavigationSubList.tsx
@@ -1,3 +1,4 @@
+import { initialState, userActions } from 'redux/slices/user';
 import {
     NavigationSubListItemLinkStyled,
     NavigationSubListItemStyled,
@@ -6,21 +7,40 @@ import {
 import { FC } from 'react';
 import { NavigationSubCategory } from 'connectors/navigation/Navigation';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { useShopsysDispatch } from 'redux/main';
 
 type NavigationSubListProps = {
     columnCategoryChildren: NavigationSubCategory[];
 };
 
 const NavigationSubList: FC<NavigationSubListProps> = (props) => {
+    const dispatch = useShopsysDispatch();
+    const router = useRouter();
     return (
         <NavigationSubListStyled>
-            {props.columnCategoryChildren.map((columnCategoryChild, subListIndex) => (
-                <NavigationSubListItemStyled key={subListIndex}>
-                    <NextLink href={columnCategoryChild.slug} passHref>
-                        <NavigationSubListItemLinkStyled>{columnCategoryChild.name}</NavigationSubListItemLinkStyled>
-                    </NextLink>
-                </NavigationSubListItemStyled>
-            ))}
+            {props.columnCategoryChildren.map((columnCategoryChild, subListIndex) =>
+                columnCategoryChild.slug === router.asPath ? (
+                    <NavigationSubListItemStyled
+                        key={subListIndex}
+                        onClick={() => dispatch(userActions.setPagination({ ...initialState.pagination }))}
+                    >
+                        <NextLink href={columnCategoryChild.slug} passHref>
+                            <NavigationSubListItemLinkStyled>
+                                {columnCategoryChild.name}
+                            </NavigationSubListItemLinkStyled>
+                        </NextLink>
+                    </NavigationSubListItemStyled>
+                ) : (
+                    <NavigationSubListItemStyled key={subListIndex}>
+                        <NextLink href={columnCategoryChild.slug} passHref>
+                            <NavigationSubListItemLinkStyled>
+                                {columnCategoryChild.name}
+                            </NavigationSubListItemLinkStyled>
+                        </NextLink>
+                    </NavigationSubListItemStyled>
+                ),
+            )}
         </NavigationSubListStyled>
     );
 };
