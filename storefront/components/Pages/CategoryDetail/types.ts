76e2565a9c60a64fa463c77334f26f03323ef278
sebastian.holesz@shopsys.com <sebastian.holesz@shopsys.com>
2021-12-14 07:32:36 +0100

    changed interfaces to types

--- storefront/components/Pages/CategoryDetail/types.ts
+++ storefront/components/Pages/CategoryDetail/types.ts
@@ -1,20 +1,21 @@
-import { BreadcrumbType } from 'connectors/breadcrumb/Breadcrumb';
+import { BreadcrumbItemType } from 'connectors/breadcrumb/Breadcrumb';
 import { ListedCategoryType } from 'connectors/categories/types';
 import { ListedProductEdgesType } from 'components/Blocks/Product/types';
-import { SlugType } from 'connectors/slug/Slug';
 
 export type ReadyCategorySeoMixLink = {
     name: string;
     slug: string;
 };
 
-export interface CategoryDetailType extends SlugType, BreadcrumbType {
+export type CategoryDetailType = {
+    breadcrumb: BreadcrumbItemType[];
     __typename: 'Category';
     uuid: string;
     name: string;
+    slug: string;
     seoH1: string | null;
     children: ListedCategoryType[];
     products: ListedProductEdgesType;
     readyCategorySeoMixLinks: ReadyCategorySeoMixLink[];
     linkedCategories: ListedCategoryType[];
-}
+};
