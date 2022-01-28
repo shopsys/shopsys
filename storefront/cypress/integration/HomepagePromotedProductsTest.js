69c435081c156aff6129d371007ca978d8c64b22
Roman Kuna <roman.kuna@shopsys.com>
2022-01-28 10:26:23 +0100

    add a message check to add the product to the cart and update name of test block

--- storefront/cypress/integration/HomepagePromotedProductsTest.js
+++ storefront/cypress/integration/HomepagePromotedProductsTest.js
@@ -1,8 +1,9 @@
 describe('Homepage promoted products', () => {
-    it('Visits homepage, clicks on Hello Kitty and checks if redirected correctly', () => {
+    it('Visits homepage, clicks on Hello Kitty, add product to the cart and checks if redirected correctly', () => {
         cy.visit('/')
         cy.contains('22" Sencor SLE 22F46DM4 HELLO KITTY').click()
         cy.url().should('contain', '/televize-22-sencor-sle-22f46dm4-hello-kitty-plazmova')
         cy.contains('Do košíku').click()
+        cy.contains('Do košíku bylo vloženo zboží 22" Sencor SLE 22F46DM4 HELLO KITTY (1 ks)')    
     })
 })
