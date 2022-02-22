28b3bf7667b3b8f6997d448f76478d6c968ba57b
Roman Kuna <romankuna@Roman-MacBook-Air.local>
2022-02-22 08:53:21 +0100

    create demodata.js - tests have now product data in variables

---
+++ storefront/cypress/fixtures/demodata.js
@@ -0,0 +1,4 @@
+export const product1_name = '22" Sencor SLE 22F46DM4 HELLO KITTY'
+export const product1_name_prefix_suffix = 'Televize 22" Sencor SLE 22F46DM4 HELLO KITTY plazmová'
+export const product1_catnum = '9177759'
+export const product1_url_prefix_suffix = '/televize-22-sencor-sle-22f46dm4-hello-kitty-plazmova'
