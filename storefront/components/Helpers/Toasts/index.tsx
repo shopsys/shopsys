d1b7b416af41a2a485afcaf72d22190ea9528f34
Martin Grossmann <martin@vyvoj.net>
2022-03-07 12:19:17 +0100

    added selector for toast messages

--- storefront/components/Helpers/Toasts/index.tsx
+++ storefront/components/Helpers/Toasts/index.tsx
@@ -2,13 +2,13 @@ import { toast } from 'react-toastify';
 
 const showMessage = (message: string, type: 'info' | 'error' | 'success'): void => {
     if (type === 'error') {
-        toast.error(() => <span dangerouslySetInnerHTML={{ __html: message }}></span>);
+        toast.error(() => <span dangerouslySetInnerHTML={{ __html: message }} data-testid={'toast-error'} />);
     }
     if (type === 'info') {
-        toast.info(() => <span dangerouslySetInnerHTML={{ __html: message }}></span>);
+        toast.info(() => <span dangerouslySetInnerHTML={{ __html: message }} data-testid={'toast-info'} />);
     }
     if (type === 'success') {
-        toast.success(() => <span dangerouslySetInnerHTML={{ __html: message }}></span>);
+        toast.success(() => <span dangerouslySetInnerHTML={{ __html: message }} data-testid={'toast-success'} />);
     }
 };
 
