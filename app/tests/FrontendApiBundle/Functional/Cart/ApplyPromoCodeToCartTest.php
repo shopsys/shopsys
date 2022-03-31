0ae32e5547e336f00e5b7d524a2201591720c923
Martin Grossmann <martin@vyvoj.net>
2022-03-31 18:49:07 +0200

    added tests for apply promocode mutation

---
+++ app/tests/FrontendApiBundle/Functional/Cart/ApplyPromoCodeToCartTest.php
@@ -0,0 +1,336 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FrontendApiBundle\Functional\Cart;
+
+use App\DataFixtures\Demo\CartDataFixture;
+use App\DataFixtures\Demo\ProductDataFixture;
+use App\DataFixtures\Demo\PromoCodeDataFixture;
+use App\FrontendApi\Model\Component\Constraints\PromoCode;
+use App\Model\Cart\CartFacade;
+use App\Model\Order\PromoCode\PromoCodeDataFactory;
+use App\Model\Order\PromoCode\PromoCodeFacade;
+use App\Model\Product\Product;
+use App\Model\Product\ProductDataFactory;
+use App\Model\Product\ProductFacade;
+use Tests\FrontendApiBundle\Test\GraphQlTestCase;
+
+class ApplyPromoCodeToCartTest extends GraphQlTestCase
+{
+    /**
+     * @var \App\Model\Product\ProductDataFactory
+     * @inject
+     */
+    private ProductDataFactory $productDataFactory;
+
+    /**
+     * @var \App\Model\Product\ProductFacade
+     * @inject
+     */
+    private ProductFacade $productFacade;
+
+    /**
+     * @var \App\Model\Order\PromoCode\PromoCodeFacade
+     * @inject
+     */
+    private PromoCodeFacade $promoCodeFacade;
+
+    /**
+     * @var \App\Model\Order\PromoCode\PromoCodeDataFactory
+     * @inject
+     */
+    private PromoCodeDataFactory $promoCodeDataFactory;
+
+    /**
+     * @var \App\Model\Cart\CartFacade
+     * @inject
+     */
+    private CartFacade $cartFacade;
+
+    public function testApplyPromoCode(): void
+    {
+        /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+        $promoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . CartDataFixture::CART_UUID . '"
+                promoCode: "' . $promoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $data = $this->getResponseDataForGraphQlType($response, 'ApplyPromoCodeToCart');
+
+        self::assertEquals(CartDataFixture::CART_UUID, $data['uuid']);
+        self::assertEquals($promoCode->getCode(), $data['promoCode']);
+    }
+
+    public function testApplyPromoCodeMultipleTimes(): void
+    {
+        /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+        $promoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . CartDataFixture::CART_UUID . '"
+                promoCode: "' . $promoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $data = $this->getResponseDataForGraphQlType($response, 'ApplyPromoCodeToCart');
+
+        self::assertEquals(CartDataFixture::CART_UUID, $data['uuid']);
+        self::assertEquals($promoCode->getCode(), $data['promoCode']);
+
+        // apply promo code again
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+
+        $this->assertResponseContainsArrayOfExtensionValidationErrors($response);
+        $violations = $this->getErrorsExtensionValidationFromResponse($response);
+
+        self::assertEquals(PromoCode::ALREADY_APPLIED_PROMO_CODE_ERROR, $violations['input.promoCode'][0]['code']);
+
+        // test promo code is applied only once in DB
+        $cart = $this->cartFacade->findCartByCartIdentifier(CartDataFixture::CART_UUID);
+        self::assertCount(1, $cart->getAllAppliedPromoCodes());
+    }
+
+    public function testApplyPromoCodeWithInvalidCart(): void
+    {
+        $invalidCartUuid = '24c11eca-a3f8-45cb-b843-861bcde847c6';
+
+        /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+        $promoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . $invalidCartUuid . '"
+                promoCode: "' . $promoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $this->assertResponseContainsArrayOfErrors($response);
+        $errors = $this->getErrorsFromResponse($response);
+
+        self::assertEquals('cart-unavailable', $errors[0]['extensions']['code']);
+        self::assertEquals(sprintf('Cart "%s" is unavailable.', $invalidCartUuid), $errors[0]['message']);
+    }
+
+    public function testApplyPromoCodeWithoutCart(): void
+    {
+        /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+        $promoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                promoCode: "' . $promoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $this->assertResponseContainsArrayOfErrors($response);
+        $errors = $this->getErrorsFromResponse($response);
+
+        self::assertEquals('cart-unavailable', $errors[0]['extensions']['code']);
+        self::assertEquals('Either cart UUID has to be provided, or the user has to be logged in.', $errors[0]['message']);
+    }
+
+    public function testModificationAfterProductIsRemoved(): void
+    {
+        /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+        $promoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+        /** @var \App\Model\Product\Product $productInCart */
+        $productInCart = $this->getReference(ProductDataFixture::PRODUCT_PREFIX . 1);
+
+        $addToCartMutation = 'mutation {
+            AddToCart(input: {
+                productUuid: "' . $productInCart->getUuid() . '",
+                quantity: 1
+            }) {
+                uuid
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($addToCartMutation);
+        $cartUuid = $this->getResponseDataForGraphQlType($response, 'AddToCart')['uuid'];
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . $cartUuid . '"
+                promoCode: "' . $promoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $data = $this->getResponseDataForGraphQlType($response, 'ApplyPromoCodeToCart');
+        self::assertEquals($promoCode->getCode(), $data['promoCode']);
+
+        // product has to be re-fetched due to identity map clearing to prevent "A new entity was found through the relationship" error
+        /** @var \App\Model\Product\Product $productInCart */
+        $productInCart = $this->getReference(ProductDataFixture::PRODUCT_PREFIX . 1);
+
+        $this->hideProduct($productInCart);
+
+        $getCartQuery = '{
+            cart(cartInput: {cartUuid: "' . $cartUuid . '"}) {
+                promoCode
+                modifications {
+                    itemModifications {
+                        noLongerListableCartItems {
+                            product {
+                                uuid
+                            }
+                        }
+                    }
+                    promoCodeModifications {
+                        noLongerApplicablePromoCode
+                    }
+                }
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($getCartQuery);
+        $data = $this->getResponseDataForGraphQlType($response, 'cart');
+        $itemModifications = $data['modifications']['itemModifications'];
+        $promoCodeModifications = $data['modifications']['promoCodeModifications'];
+
+        self::assertNull($data['promoCode']);
+
+        self::assertNotEmpty($itemModifications['noLongerListableCartItems']);
+        self::assertEquals($productInCart->getUuid(), $itemModifications['noLongerListableCartItems'][0]['product']['uuid']);
+
+        self::assertNotEmpty($promoCodeModifications['noLongerApplicablePromoCode']);
+        self::assertEquals($promoCode->getCode(), $promoCodeModifications['noLongerApplicablePromoCode'][0]);
+    }
+
+    public function testModificationAfterPromoCodeEdited(): void
+    {
+        /** @var \App\Model\Order\PromoCode\PromoCode $validPromoCode */
+        $validPromoCode = $this->getReferenceForDomain(PromoCodeDataFixture::VALID_PROMO_CODE, 1);
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . CartDataFixture::CART_UUID . '"
+                promoCode: "' . $validPromoCode->getCode() . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+        $data = $this->getResponseDataForGraphQlType($response, 'ApplyPromoCodeToCart');
+
+        self::assertEquals($validPromoCode->getCode(), $data['promoCode']);
+
+        $promoCodeData = $this->promoCodeDataFactory->createFromPromoCode($validPromoCode);
+        $promoCodeData->remainingUses = 0;
+        $this->promoCodeFacade->edit($validPromoCode->getId(), $promoCodeData);
+
+        $getCartQuery = '{
+            cart(cartInput: {cartUuid: "' . CartDataFixture::CART_UUID . '"}) {
+                promoCode
+                modifications {
+                    itemModifications {
+                        noLongerListableCartItems {
+                            product {
+                                uuid
+                            }
+                        }
+                    }
+                    promoCodeModifications {
+                        noLongerApplicablePromoCode
+                    }
+                }
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($getCartQuery);
+        $data = $this->getResponseDataForGraphQlType($response, 'cart');
+
+        $promoCodeModifications = $data['modifications']['promoCodeModifications'];
+
+        self::assertNull($data['promoCode']);
+
+        self::assertNotEmpty($promoCodeModifications['noLongerApplicablePromoCode']);
+        self::assertEquals($validPromoCode->getCode(), $promoCodeModifications['noLongerApplicablePromoCode'][0]);
+    }
+
+    /**
+     * @dataProvider getInvalidPromoCodesDataProvider
+     * @param string|null $promoCodeReferenceName
+     * @param string $expectedError
+     */
+    public function testApplyInvalidPromoCode(?string $promoCodeReferenceName, string $expectedError): void
+    {
+        $promoCodeCode = 'non-existing-promo-code';
+        if ($promoCodeReferenceName !== null) {
+            /** @var \App\Model\Order\PromoCode\PromoCode $promoCode */
+            $promoCode = $this->getReferenceForDomain($promoCodeReferenceName, 1);
+            $promoCodeCode = $promoCode->getCode();
+        }
+
+        $applyPromoCodeMutation = 'mutation {
+            ApplyPromoCodeToCart(input: {
+                cartUuid: "' . CartDataFixture::CART_UUID . '"
+                promoCode: "' . $promoCodeCode . '"
+            }) {
+                uuid
+                promoCode
+            }
+        }';
+
+        $response = $this->getResponseContentForQuery($applyPromoCodeMutation);
+
+        self::assertArrayHasKey('errors', $response);
+
+        $violations = $this->getErrorsExtensionValidationFromResponse($response);
+
+        self::assertArrayHasKey('input.promoCode', $violations);
+        self::assertEquals($expectedError, $violations['input.promoCode'][0]['code']);
+    }
+
+    /**
+     * @return iterable
+     */
+    public function getInvalidPromoCodesDataProvider(): iterable
+    {
+        yield [null, PromoCode::INVALID_ERROR];
+        yield [PromoCodeDataFixture::PROMO_CODE_FOR_PRODUCT_ID_2, PromoCode::NO_RELATION_TO_PRODUCTS_IN_CART_ERROR];
+        yield [PromoCodeDataFixture::NOT_YET_VALID_PROMO_CODE, PromoCode::NOT_YET_VALID_ERROR];
+        yield [PromoCodeDataFixture::NO_LONGER_VALID_PROMO_CODE, PromoCode::NO_LONGER_VALID_ERROR];
+        yield [PromoCodeDataFixture::PROMO_CODE_FOR_REGISTERED_ONLY, PromoCode::FOR_REGISTERED_CUSTOMER_USERS_ONLY_ERROR];
+        yield [PromoCodeDataFixture::PROMO_CODE_FOR_VIP_PRICING_GROUP, PromoCode::NOT_AVAILABLE_FOR_CUSTOMER_USER_PRICING_GROUP_ERROR];
+    }
+
+    /**
+     * @param \App\Model\Product\Product $product
+     */
+    private function hideProduct(Product $product): void
+    {
+        $productData = $this->productDataFactory->createFromProduct($product);
+        $productData->sellingDenied = true;
+
+        $this->productFacade->edit($product->getId(), $productData);
+        $this->dispatchFakeKernelResponseEventToTriggerImmediateRecalculations();
+    }
+}
