d5a9787ba0976431917ec28b779fd0019fe35999
Luboš Svoboda <lubos.svoboda@shopsys.com>
2022-04-26 13:11:10 +0200

    i18n-translator.js: use env variable (INTERNAL_GRAPHQL_ENDPOINT) to get the webserver URL instead of hardcoding it

--- storefront/i18n-translator.js
+++ storefront/i18n-translator.js
@@ -5,9 +5,14 @@ export async function getLocalTranslates(locale, namespace) {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function getFreshTranslates(locale, namespace) {
+    const ENDPOINT_URL = process.env.INTERNAL_GRAPHQL_ENDPOINT.substring(
+        0,
+        process.env.INTERNAL_GRAPHQL_ENDPOINT.indexOf('/graphql'),
+    );
+
     const [localTranslatesResponse, userTranslatesResponse] = await Promise.all([
-        fetch(`http://webserver:8080/locales/${locale}/${namespace}.json`),
-        fetch(`http://webserver:8080/content/locales/${locale}/${namespace}.json`),
+        fetch(`${ENDPOINT_URL}/locales/${locale}/${namespace}.json`),
+        fetch(`${ENDPOINT_URL}/content/locales/${locale}/${namespace}.json`),
     ]);
 
     const localTranslates = await localTranslatesResponse.json();
