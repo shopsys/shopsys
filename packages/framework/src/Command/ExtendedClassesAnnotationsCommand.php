be6d847d0347e5f32819c436af15889d3f6edcaa
Robin Dvořák <dvorak@uxf.cz>
2022-04-26 11:11:29 +0200

    Type properties in ExtendedClassesAnnotationsCommand

--- packages/framework/src/Command/ExtendedClassesAnnotationsCommand.php
+++ packages/framework/src/Command/ExtendedClassesAnnotationsCommand.php
@@ -30,37 +30,37 @@ class ExtendedClassesAnnotationsCommand extends Command
     /**
      * @var string
      */
-    protected $projectRootDirectory;
+    protected string $projectRootDirectory;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\ClassExtensionRegistry
      */
-    protected $classExtensionRegistry;
+    protected ClassExtensionRegistry $classExtensionRegistry;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\AnnotationsReplacer
      */
-    protected $annotationsReplacer;
+    protected AnnotationsReplacer $annotationsReplacer;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\AnnotationsReplacementsMap
      */
-    protected $annotationsReplacementsMap;
+    protected AnnotationsReplacementsMap $annotationsReplacementsMap;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\PropertyAnnotationsFactory
      */
-    protected $propertyAnnotationsFactory;
+    protected PropertyAnnotationsFactory $propertyAnnotationsFactory;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\MethodAnnotationsFactory
      */
-    protected $methodAnnotationsAdder;
+    protected MethodAnnotationsFactory $methodAnnotationsAdder;
 
     /**
      * @var \Shopsys\FrameworkBundle\Component\ClassExtension\AnnotationsAdder
      */
-    protected $annotationsAdder;
+    protected AnnotationsAdder $annotationsAdder;
 
     /**
      * {@inheritdoc}
