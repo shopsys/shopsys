1496a03136a29c60efda01915b9e13cc14bea88c
Rostislav Vitek <rostislav.vitek@shopsys.com>
2022-04-27 10:41:59 +0200

    i18n.js: set translations redis cache lifetime to 5 minutes

--- storefront/i18n.js
+++ storefront/i18n.js
@@ -4,7 +4,7 @@ const Sentry = require('@sentry/nextjs');
 const REDIS_URL = `redis://${process.env.REDIS_HOST}`;
 const REDIS_PREFIX = `${process.env.REDIS_PREFIX}:fe:translates:`;
 const REDIS_IS_UPDATE_JOB_TIMEOUT = 5; // seconds (default: 30)
-const REDIS_IS_CACHED_TIMEOUT = 30; // seconds (default: 5 * 60)
+const REDIS_IS_CACHED_TIMEOUT = 300; // seconds (default: 5 * 60)
 
 module.exports = {
     pages: {
