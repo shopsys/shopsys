b283cc4a48b290143dbbb4c133ed9a43f1b824a2
Jakub Honíšek <honisek@uxf.cz>
2022-05-05 13:39:17 +0200

    add new helpers

---
+++ storefront/helpers/canUseDom.ts
@@ -0,0 +1,7 @@
+export const canUseDom = (): boolean =>
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    !!(
+        typeof window !== 'undefined' &&
+        typeof window.document === 'object' &&
+        typeof window.document.createElement === 'function'
+    );
