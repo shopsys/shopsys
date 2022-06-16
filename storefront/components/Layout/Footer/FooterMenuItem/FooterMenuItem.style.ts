e56576d6b9abbb27aa5dbdaa9c1122f3b69ad51f
Jakub Honíšek <honisek@uxf.cz>
2022-06-16 11:21:57 +0200

    [FWCC-1051] footer menu SSR fix

--- storefront/components/Layout/Footer/FooterMenuItem/FooterMenuItem.style.ts
+++ storefront/components/Layout/Footer/FooterMenuItem/FooterMenuItem.style.ts
@@ -1,75 +1,31 @@
 import Heading from 'components/Basic/Heading';
-import Icon from 'components/Basic/Icon';
 import { styled } from 'components/Theme/main';
 import { css } from 'styled-components';
-
-type FooterMenuItemStyledProps = {
-    contentElementHeight: number;
-};
-
-type FooterMenuHeadingStyledProps = {
-    isContentVisible: boolean;
-};
 
 const localVariables = {
     footerMenuitemBorderColor: '#606476',
     footerMenuItemGap: '20px',
 };
 
-export const FooterMenuItemStyled = styled.div<FooterMenuItemStyledProps>`
-    ${({ theme, contentElementHeight }) => css`
+export const FooterMenuItemStyled = styled.div(
+    ({ theme }) => css`
         padding: 0 ${theme.layout.padding};
-
-        border-top: 2px solid ${localVariables.footerMenuitemBorderColor};
-
-        &:last-child {
-            border-bottom: 2px solid ${localVariables.footerMenuitemBorderColor};
-
-            @media ${theme.mediaQueries.queryLg} {
-                border-bottom: 0;
-            }
-        }
 
         @media ${theme.mediaQueries.queryLg} {
             width: 25%;
             padding-left: ${localVariables.footerMenuItemGap};
-
-            border-top: 0;
         }
+    `,
+);
 
-        @media ${theme.mediaQueries.queryTablet} {
-            .footer-menu-item-enter {
-                height: 0;
-                overflow: hidden;
-            }
-
-            .footer-menu-item-enter-active {
-                height: ${contentElementHeight}px;
-                transition: 0.3s all ease;
-            }
-
-            .footer-menu-item-exit {
-                height: ${contentElementHeight}px;
-            }
-
-            .footer-menu-item-exit-active {
-                height: 0;
-                overflow: hidden;
-                transition: 0.3s all ease;
-            }
-        }
-    `}
-`;
-
-export const FooterMenuHeadingStyled = styled(Heading)<FooterMenuHeadingStyledProps>`
-    ${({ theme, isContentVisible }) => css`
+export const FooterMenuHeadingStyled = styled(Heading)(
+    ({ theme }) => css`
         display: flex;
         align-items: center;
         justify-content: space-between;
         margin-bottom: 0;
         padding: 20px 0;
 
-        cursor: pointer;
         color: ${theme.color.white};
         font-weight: 700;
         text-transform: uppercase;
@@ -79,41 +35,21 @@ export const FooterMenuHeadingStyled = styled(Heading)<FooterMenuHeadingStyledPr
             margin-bottom: 24px;
             pointer-events: none;
         }
+    `,
+);
 
-        ${isContentVisible &&
-        css`
-            ${FooterMenuHeadingIconStyled} {
-                transform: rotate(180deg);
-            }
-        `}
-    `}
-`;
-
-export const FooterMenuHeadingIconStyled = styled(Icon)`
-    ${({ theme }) => css`
-        width: 20px;
-        height: 20px;
-
-        color: ${theme.color.creamWhite};
-
-        @media ${theme.mediaQueries.queryLg} {
-            display: none;
-        }
-    `}
-`;
-
-export const FooterMenuListStyled = styled.ul`
-    ${({ theme }) => css`
+export const FooterMenuListStyled = styled.ul(
+    ({ theme }) => css`
         padding-bottom: 20px;
 
         @media ${theme.mediaQueries.queryLg} {
             padding-bottom: 0;
         }
-    `}
-`;
+    `,
+);
 
-export const FooterMenuListItemStyled = styled.li`
-    ${({ theme }) => css`
+export const FooterMenuListItemStyled = styled.li(
+    ({ theme }) => css`
         margin-bottom: 5px;
 
         @media ${theme.mediaQueries.queryLg} {
@@ -123,11 +59,11 @@ export const FooterMenuListItemStyled = styled.li`
         &:last-child {
             margin-bottom: 0;
         }
-    `}
-`;
+    `,
+);
 
-export const FooterMenuListItemLinkStyled = styled.a`
-    ${({ theme }) => css`
+export const FooterMenuListItemLinkStyled = styled.a(
+    ({ theme }) => css`
         display: block;
 
         font-size: ${theme.fontSize.small};
@@ -137,5 +73,5 @@ export const FooterMenuListItemLinkStyled = styled.a`
         &:hover {
             color: ${theme.color.greyLight};
         }
-    `}
-`;
+    `,
+);
--- storefront/components/Layout/Footer/FooterMenuItem/FooterMenuItem.tsx
+++ storefront/components/Layout/Footer/FooterMenuItem/FooterMenuItem.tsx
@@ -1,18 +1,13 @@
 import {
-    FooterMenuHeadingIconStyled,
     FooterMenuHeadingStyled,
     FooterMenuItemStyled,
     FooterMenuListItemLinkStyled,
     FooterMenuListItemStyled,
     FooterMenuListStyled,
 } from './FooterMenuItem.style';
-import { desktopFirstSizes } from 'components/Theme/mediaQueries';
 import { SimpleArticleFragmentApi } from 'graphql/generated';
-import { useGetWindowSize } from 'hooks/ui/UseGetWindowSize';
-import { useResizeWidthEffect } from 'hooks/ui/UseResizeWidthEffect';
 import NextLink from 'next/link';
-import { FC, useCallback, useRef, useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { FC } from 'react';
 
 type FooterMenuItemProps = {
     title: string;
@@ -22,62 +17,23 @@ type FooterMenuItemProps = {
 const TEST_IDENTIFIER = 'layout-footer-footermenuitem';
 
 const FooterMenuItem: FC<FooterMenuItemProps> = ({ items, title }) => {
-    const [isContentVisible, setIsContentVisible] = useState(false);
-    const [contentElementHeight, setContentElementHeight] = useState(0);
-    const contentElement = useRef<HTMLUListElement>(null);
-    const wrapperElement = useRef<HTMLDivElement>(null);
-    const { width } = useGetWindowSize();
-
-    const calcHeight = useCallback(() => {
-        if (contentElement.current) {
-            setContentElementHeight(contentElement.current.clientHeight);
-        }
-    }, []);
-
-    useResizeWidthEffect(
-        width,
-        desktopFirstSizes.tablet,
-        () => setIsContentVisible(true),
-        () => setIsContentVisible(false),
-        () => setIsContentVisible(false),
-    );
-
     return (
-        <FooterMenuItemStyled contentElementHeight={contentElementHeight} data-testid={TEST_IDENTIFIER}>
-            <FooterMenuHeadingStyled
-                type="h4"
-                onClick={() => setIsContentVisible(!isContentVisible)}
-                isContentVisible={isContentVisible}
-            >
-                {title}
-                <FooterMenuHeadingIconStyled iconType="icon" icon="Arrow" />
-            </FooterMenuHeadingStyled>
-            <CSSTransition
-                in={isContentVisible}
-                timeout={300}
-                classNames="footer-menu-item"
-                onEnter={calcHeight}
-                onExit={calcHeight}
-                nodeRef={wrapperElement}
-                unmountOnExit
-            >
-                <div ref={wrapperElement}>
-                    <FooterMenuListStyled ref={contentElement}>
-                        {items.map((item) => (
-                            <FooterMenuListItemStyled key={item.uuid}>
-                                <NextLink href={item.slug} passHref>
-                                    <FooterMenuListItemLinkStyled
-                                        target={item.external ? '_blank' : undefined}
-                                        rel={item.external ? 'nofollow noreferrer noopener' : undefined}
-                                    >
-                                        {item.name}
-                                    </FooterMenuListItemLinkStyled>
-                                </NextLink>
-                            </FooterMenuListItemStyled>
-                        ))}
-                    </FooterMenuListStyled>
-                </div>
-            </CSSTransition>
+        <FooterMenuItemStyled data-testid={TEST_IDENTIFIER}>
+            <FooterMenuHeadingStyled type="h4">{title}</FooterMenuHeadingStyled>
+            <FooterMenuListStyled>
+                {items.map((item) => (
+                    <FooterMenuListItemStyled key={item.uuid}>
+                        <NextLink href={item.slug} passHref>
+                            <FooterMenuListItemLinkStyled
+                                target={item.external ? '_blank' : undefined}
+                                rel={item.external ? 'nofollow noreferrer noopener' : undefined}
+                            >
+                                {item.name}
+                            </FooterMenuListItemLinkStyled>
+                        </NextLink>
+                    </FooterMenuListItemStyled>
+                ))}
+            </FooterMenuListStyled>
         </FooterMenuItemStyled>
     );
 };
