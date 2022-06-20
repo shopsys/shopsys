64ec0d5cbbfd2dfd1c9e955d1397738e798533b0
Sebastian Holesz <sebastian.holesz@shopsys.com>
2022-06-20 12:45:34 +0200

    added optimistic updates for ChangePaymentInCart

--- storefront/urql/cacheExchange.ts
+++ storefront/urql/cacheExchange.ts
@@ -4,11 +4,14 @@ import {
     CartQueryApi,
     CartQueryDocumentApi,
     CartQueryVariablesApi,
+    ChangePaymentInCartInputApi,
+    ChangePaymentInCartMutationApi,
     ChangeTransportInCartInputApi,
     ChangeTransportInCartMutationApi,
     TransportsQueryApi,
     TransportsQueryDocumentApi,
     TransportsQueryVariablesApi,
+    TransportWithAvailablePaymentsAndStoresFragmentApi,
 } from 'graphql/generated';
 import schema from 'schema.graphql.json';
 
@@ -122,6 +125,20 @@ const cache = cacheExchange({
 
             return getOptimisticChangeTransportInCartResult(cartQueryResult, transportsQueryResult, input);
         },
+        ChangePaymentInCart: ({ input }: { input: ChangePaymentInCartInputApi }, cache) => {
+            const cartQueryResult: CartQueryApi | null = cache.readQuery<CartQueryApi>({
+                query: CartQueryDocumentApi,
+                variables: {
+                    cartUuid: input.cartUuid ?? null,
+                } as CartQueryVariablesApi,
+            });
+
+            if (cartQueryResult === null) {
+                return null;
+            }
+
+            return getOptimisticChangePaymentInCartResult(cartQueryResult, input);
+        },
     },
 });
 
@@ -155,5 +172,33 @@ const getOptimisticChangeTransportInCartResult = (
         transport:
             transportsQueryResult?.transports.find((transport) => transport.uuid === input.transportUuid) ?? null,
     } as ChangeTransportInCartMutationApi['ChangeTransportInCart']);
+
+const getOptimisticChangePaymentInCartResult = (cartQueryResult: CartQueryApi, input: ChangePaymentInCartInputApi) =>
+    ({
+        __typename: 'Cart',
+        items: cartQueryResult.cart?.items ?? null,
+        modifications: cartQueryResult.cart?.modifications ?? null,
+        payment: getPaymentFromTransport(cartQueryResult.cart?.transport, input.paymentUuid),
+        paymentGoPayBankSwift: cartQueryResult.cart?.paymentGoPayBankSwift ?? null,
+        promoCode: cartQueryResult.cart?.promoCode ?? null,
+        remainingAmountWithVatForFreeTransport: cartQueryResult.cart?.remainingAmountWithVatForFreeTransport ?? null,
+        selectedPickupPlaceIdentifier: cartQueryResult.cart?.selectedPickupPlaceIdentifier ?? null,
+        totalDiscountPrice: cartQueryResult.cart?.totalDiscountPrice ?? null,
+        totalItemsPrice: cartQueryResult.cart?.totalItemsPrice ?? null,
+        totalPrice: cartQueryResult.cart?.totalPrice ?? null,
+        uuid: cartQueryResult.cart?.uuid ?? null,
+        transport: cartQueryResult.cart?.transport,
+    } as ChangePaymentInCartMutationApi['ChangePaymentInCart']);
+
+const getPaymentFromTransport = (
+    transport: TransportWithAvailablePaymentsAndStoresFragmentApi | null | undefined,
+    paymentUuid: string | null,
+) => {
+    if (!transport || paymentUuid === null) {
+        return null;
+    }
+
+    return transport.payments.find((payment) => payment.uuid === paymentUuid) ?? null;
+};
 
 export default cache;
