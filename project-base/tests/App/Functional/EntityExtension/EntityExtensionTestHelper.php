631871c620ae8f23c4616794efdfa8b1e53ba67e
Rostislav Vítek <rostislav.vitek@shopsys.com>
2022-07-12 16:46:22 +0200

    ORM: it is now possible to overwrite association mappings (#2473)
    - if mapping of child class property is not equal to mapping of parent class property and child class property overrides parent class property, there will be used mapping of child class property

---
+++ project-base/tests/App/Functional/EntityExtension/EntityExtensionTestHelper.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Functional\EntityExtension;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
+use PHPUnit\Framework\Assert;
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityManagerDecorator;
+
+class EntityExtensionTestHelper
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\EntityExtension\EntityManagerDecorator
+     */
+    private EntityManagerDecorator $em;
+
+    /**
+     * @var \Tests\App\Functional\EntityExtension\OverwritableEntityNameResolver
+     */
+    private OverwritableEntityNameResolver $overwritableEntityNameResolver;
+
+    /**
+     * @var \Tests\App\Functional\EntityExtension\OverwritableEntityExtensionSubscriber
+     */
+    private OverwritableEntityExtensionSubscriber $overwritableEntityExtensionSubscriber;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityManagerDecorator $em
+     * @param \Tests\App\Functional\EntityExtension\OverwritableEntityNameResolver $overwritableEntityNameResolver
+     * @param \Tests\App\Functional\EntityExtension\OverwritableEntityExtensionSubscriber $overwritableEntityExtensionSubscriber
+     */
+    public function __construct(
+        EntityManagerDecorator $em,
+        OverwritableEntityNameResolver $overwritableEntityNameResolver,
+        OverwritableEntityExtensionSubscriber $overwritableEntityExtensionSubscriber
+    ) {
+        $this->em = $em;
+        $this->overwritableEntityNameResolver = $overwritableEntityNameResolver;
+        $this->overwritableEntityExtensionSubscriber = $overwritableEntityExtensionSubscriber;
+    }
+
+    /**
+     * @param string[] $entityExtensionMap
+     */
+    public function overwriteEntityExtensionMapInServicesInContainer(array $entityExtensionMap): void
+    {
+        $this->overwritableEntityExtensionSubscriber->overwriteEntityExtensionMap($entityExtensionMap);
+        $this->overwritableEntityNameResolver->overwriteEntityExtensionMap($entityExtensionMap);
+    }
+
+    public function registerTestEntities(): void
+    {
+        $driver = new AnnotationDriver(new AnnotationReader(), __DIR__ . '/Model');
+
+        $configuration = $this->em->getConfiguration();
+        $metadataDriverChain = $configuration->getMetadataDriverImpl();
+        if ($metadataDriverChain instanceof MappingDriverChain) {
+            $metadataDriverChain->addDriver($driver, 'Tests\\App\\Functional\\EntityExtension');
+        } else {
+            Assert::fail(sprintf('Metadata driver must be type of %s', MappingDriverChain::class));
+        }
+    }
+}
