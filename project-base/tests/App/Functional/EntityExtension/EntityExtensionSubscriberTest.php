990bc88a9940e16366191702a3c159e59a640860
Rostislav Vítek <rostislav.vitek@shopsys.com>
2022-07-12 16:46:22 +0200

    ORM: it is now possible to overwrite association mappings (#2473)
    - if mapping of child class property is not equal to mapping of parent class property and child class property overrides parent class property, there will be used mapping of child class property

---
+++ project-base/tests/App/Functional/EntityExtension/EntityExtensionSubscriberTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Functional\EntityExtension;
+
+use Tests\App\Functional\EntityExtension\Model\DummyEntity;
+use Tests\App\Functional\EntityExtension\Model\ExtendedDummyEntity;
+use Tests\App\Test\TransactionFunctionalTestCase;
+
+class EntityExtensionSubscriberTest extends TransactionFunctionalTestCase
+{
+    /**
+     * @var \Tests\App\Functional\EntityExtension\EntityExtensionTestHelper
+     * @inject
+     */
+    private EntityExtensionTestHelper $entityExtensionTestHelper;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityExtensionTestHelper->registerTestEntities();
+        $entityExtensionMap = [
+            DummyEntity::class => ExtendedDummyEntity::class,
+        ];
+        $this->entityExtensionTestHelper->overwriteEntityExtensionMapInServicesInContainer($entityExtensionMap);
+    }
+
+    public function testLoadClassMetadataForEntityWithOverriddenAssociation(): void
+    {
+        $classMetadata = $this->em->getClassMetadata(ExtendedDummyEntity::class);
+
+        $expectedOrderByValue = ['id' => 'DESC'];
+        $this->assertEquals($expectedOrderByValue, $classMetadata->associationMappings['flags']['orderBy']);
+    }
+}
