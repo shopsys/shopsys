cbd398cc0dee005201b65c2e9aca4f77655f5bfa
Roman Kuna <romankuna@Roman-MacBook-Air.local>
2022-07-13 11:09:05 +0200

    fix format code

--- storefront/cypress/integration/Functions/CartPopupWindow.js
+++ storefront/cypress/integration/Functions/CartPopupWindow.js
@@ -1,5 +1,5 @@
 export function checkProductAndGoToCartFromCartPopupWindow(productname) {
-    cy.get('[data-testid="layout-popup"]')
+    cy.get('[data-testid="layout-popup"]');
     cy.get('[data-testid="blocks-product-addtocartpopup-product-name"]').contains(productname);
     cy.get('[data-testid="basic-link-button"]').contains('Do košíku').click();
 }
