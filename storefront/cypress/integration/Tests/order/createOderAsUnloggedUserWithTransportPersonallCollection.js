92d5fa0a0873338865938bd52743f3b534492558
Roman Kuna <romankuna@Roman-MacBook-Air.local>
2022-07-13 11:09:03 +0200

    created order test as Unlogged user with transport personall collection

---
+++ storefront/cypress/integration/Tests/order/createOderAsUnloggedUserWithTransportPersonallCollection.js
@@ -0,0 +1,42 @@
+/// <reference types="Cypress" />
+
+it('Creating an order as unlogged user with one item, Personall collection and Cash', () => {
+    cy.visit('/');
+    addProductToCartFromPromotedProductsOnHomepage(product1_catnum);
+    checkProductAndGoToCartFromCartPopupWindow(product1_name_prefix_suffix);
+    checkProductInCart(product1_catnum, product1_name_prefix_suffix);
+    cy.url().should('contain', url_cart);
+    continueToSecondStep();
+    cy.url().should('contain', url_order_transport_and_payment);
+    checkTransportPrice('2', free_price);
+    chooseTransportPersonalCollection(store1_name); // selector doplnit do fce
+    checkSelectedStoreInTransportList(store1_name);
+    choosePayment(payment1_name);
+    checkOrderSummaryWithOneItem(
+        product1_name_prefix_suffix,
+        '1',
+        product1_price,
+        transport3_name,
+        free_price,
+        payment1_name,
+        free_price,
+        cart_total_price1,
+    );
+    continueToThirdStep();
+    cy.url().should('contain', url_order_contact_data);
+    fillEmailInThirdStep(customer1_email);
+    fillCustomerInformationInThirdStep(customer1_phone, customer1_first_name, customer1_last_name);
+    fillBillingAdressInThirdStep(customer1_billing_street, customer1_billing_city, customer1_billing_zip);
+    checkOrderSummaryWithOneItem(
+        product1_name_prefix_suffix,
+        '1',
+        product1_price,
+        transport3_name,
+        free_price,
+        payment1_name,
+        free_price,
+        cart_total_price1,
+    );
+    clickOnSendOrderButton();
+    checkFinishOrderPageAsUnregistredCustomer(customer1_email);
+});
