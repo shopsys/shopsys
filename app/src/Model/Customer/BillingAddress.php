51164d8e11cc8a4ef40e7a2c60e7bc071aa42589
Martin Grossmann <martin@vyvoj.net>
2022-07-13 09:43:55 +0200

    billing address now has required address fields

--- app/src/Model/Customer/BillingAddress.php
+++ app/src/Model/Customer/BillingAddress.php
@@ -17,6 +17,31 @@ use Shopsys\FrameworkBundle\Model\Customer\BillingAddressData;
 class BillingAddress extends BaseBillingAddress
 {
     /**
+     * @var string|null
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $street;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    protected $city;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=30, nullable=false)
+     */
+    protected $postcode;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Country\Country|null
+     * @ORM\ManyToOne(targetEntity="Shopsys\FrameworkBundle\Model\Country\Country")
+     * @ORM\JoinColumn(name="country_id", referencedColumnName="id", nullable=false)
+     */
+    protected $country;
+
+    /**
      * @var bool
      * @ORM\Column(type="boolean")
      */
