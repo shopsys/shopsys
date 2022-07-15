eeb8fea9f61109457af19da386b6200a848cd0fd
Sebastian Holesz <sebastian.holesz@shopsys.com>
2022-07-15 07:51:55 +0200

    added SSR loading for stores page

--- storefront/pages/stores.tsx
+++ storefront/pages/stores.tsx
@@ -1,6 +1,8 @@
 import StaticUrlGuard from 'components/Helpers/StaticUrlGuard';
 import CommonLayout from 'components/Layout/CommonLayout';
 import Stores from 'components/Pages/Stores';
+import { useStores } from 'connectors/stores/Stores';
+import { StoresQueryDocumentApi } from 'graphql/generated';
 import { initDomainConfig } from 'helpers/InitDomainConfig';
 import { initServerSideProps, ServerSidePropsType } from 'helpers/InitServerSideProps';
 import { useGtmStaticPageView } from 'hooks/gtm/useGtmStaticPageView';
@@ -12,13 +14,14 @@ import { useGtmStaticPageViewEvent } from 'utils/Gtm/EventFactories';
 const Index: FC<ServerSidePropsType> = () => {
     const t = useTypedTranslationFunction();
     const domainUrl = useShopsysSelector((state) => state.domain.url);
+    const stores = useStores();
     const gtmStaticPageViewEvent = useGtmStaticPageViewEvent('stores');
     useGtmStaticPageView(gtmStaticPageViewEvent);
 
     return (
         <StaticUrlGuard domainUrl={domainUrl}>
             <CommonLayout title={t('Stores')}>
-                <Stores />
+                <Stores stores={stores} />
             </CommonLayout>
         </StaticUrlGuard>
     );
@@ -26,7 +29,11 @@ const Index: FC<ServerSidePropsType> = () => {
 
 export const getServerSideProps = nextReduxWrapper.getServerSideProps((store) => async (context) => {
     initDomainConfig(context, store);
-    return initServerSideProps(context, store);
+    return initServerSideProps(context, store, false, [
+        {
+            query: StoresQueryDocumentApi,
+        },
+    ]);
 });
 
 export default Index;
