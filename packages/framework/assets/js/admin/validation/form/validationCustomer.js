53a09c8293652cbca113e3b538be098da507cfd3
Peter Turek <peter.turek@shopsys.com>
2022-07-25 09:08:37 +0200

    changed form id for BillingAddressFormType to right value for right work with validation group
    - fixed missing defined validation callback

--- packages/framework/assets/js/admin/validation/form/validationCustomer.js
+++ packages/framework/assets/js/admin/validation/form/validationCustomer.js
@@ -2,12 +2,20 @@ import Register from '../../../common/utils/Register';
 import { VALIDATION_GROUP_DEFAULT } from './validation';
 
 export default function validationCustomer () {
-    const $customerBillingAddressForm = $('#customer_form_billingAddressData');
+    const $customerUserUpdateFormType = $('#customer_user_update_form_customerUserData_personalData');
+    const $emailField = $customerUserUpdateFormType.find('#customer_user_update_form_customerUserData_personalData_email');
+    $emailField.jsFormValidator({
+        callbacks: {
+            validateUniqueEmail: function () {
+            }
+        }
+    });
+
+    const $customerBillingAddressForm = $('#customer_user_update_form_billingAddressData');
     $customerBillingAddressForm.jsFormValidator({
         'groups': function () {
-
             var groups = [VALIDATION_GROUP_DEFAULT];
-            if ($customerBillingAddressForm.find('#customer_form_billingAddressData_companyData_companyCustomer').is(':checked')) {
+            if ($customerBillingAddressForm.find('#customer_user_update_form_billingAddressData_companyData_companyCustomer').is(':checked')) {
                 groups.push('companyCustomer');
             }
 
