89cd3ccb8385923c26f0affa2b91d2cbb594bd2d
Peter Turek <peter.turek@shopsys.com>
2022-07-25 09:06:45 +0200

    deleted unused DeliveryAddressFormType

--- packages/framework/assets/js/admin/validation/form/validationCustomer.js
+++ packages/framework/assets/js/admin/validation/form/validationCustomer.js
@@ -2,17 +2,6 @@ import Register from '../../../common/utils/Register';
 import { VALIDATION_GROUP_DEFAULT } from './validation';
 
 export default function validationCustomer () {
-    const $customerDeliveryAddressForm = $('#customer_form_deliveryAddressData');
-    $customerDeliveryAddressForm.jsFormValidator({
-        'groups': function () {
-            const groups = [VALIDATION_GROUP_DEFAULT];
-            if ($customerDeliveryAddressForm.find('#customer_form_deliveryAddressData_deliveryAddress_addressFilled').is(':checked')) {
-                groups.push('differentDeliveryAddress');
-            }
-
-            return groups;
-        }
-    });
     const $customerBillingAddressForm = $('#customer_form_billingAddressData');
     $customerBillingAddressForm.jsFormValidator({
         'groups': function () {
