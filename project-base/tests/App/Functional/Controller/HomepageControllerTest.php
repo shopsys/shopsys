fba1cd9940c9228b15029a5f6874bd2aa900f800
Robin Dvořák <dvorak@uxf.cz>
2022-07-26 14:52:20 +0200

    [project-base] fix HomepageControllerTest

--- project-base/tests/App/Functional/Controller/HomepageControllerTest.php
+++ project-base/tests/App/Functional/Controller/HomepageControllerTest.php
@@ -8,21 +8,21 @@ use Tests\App\Test\FunctionalTestCase;
 
 class HomepageControllerTest extends FunctionalTestCase
 {
-    public function testHomepageHttpStatus200()
+    public function testHomepageHttpStatus200(): void
     {
         $client = $this->findClient();
 
-        $client->request('GET', '/');
+        $client->get($this->domain->getUrl());
         $code = $client->getResponse()->getStatusCode();
 
         $this->assertSame(200, $code);
     }
 
-    public function testHomepageHasBodyEnd()
+    public function testHomepageHasBodyEnd(): void
     {
         $client = $this->findClient();
 
-        $client->request('GET', '/');
+        $client->get($this->domain->getUrl());
         $content = $client->getResponse()->getContent();
 
         $this->assertMatchesRegularExpression('/<\/body>/ui', $content);
