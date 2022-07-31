b00ec82feb34577b95c503ed87dd9a44237118a8
Petr Kadlec <petr.kadlec@shopsys.com>
2022-07-31 18:05:49 +0200

    fixup! add parameter and return types in product feed zbozi package

--- packages/product-feed-zbozi/src/Form/ZboziProductFormType.php
+++ packages/product-feed-zbozi/src/Form/ZboziProductFormType.php
@@ -68,7 +68,7 @@ class ZboziProductFormType extends AbstractType
                     ],
                 ],
             ])
-            ->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+            ->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event): void {
                 // Setting default value of multidomain form "show" to true via event because of dynamic form count
                 $multidomainShowForm = $event->getForm()->get('show');
                 /** @var \Symfony\Component\Form\FormInterface $showForm */
