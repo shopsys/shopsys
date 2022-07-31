82ca417a7bf073dad460e217732ae1ed24baa7fd
Petr Kadlec <petr.kadlec@shopsys.com>
2022-07-31 06:44:26 +0200

    add return types in google cloud bundle package

--- packages/google-cloud-bundle/src/DependencyInjection/ShopsysGoogleCloudExtension.php
+++ packages/google-cloud-bundle/src/DependencyInjection/ShopsysGoogleCloudExtension.php
@@ -12,7 +12,7 @@ class ShopsysGoogleCloudExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
