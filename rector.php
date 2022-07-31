8d67b16ba2ad91db6978fb3f8683540346a850eb
Petr Kadlec <petr.kadlec@shopsys.com>
2022-07-31 17:38:18 +0200

    WIP - add rector + increase phpstan level to 6

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        //__DIR__ . '/project-base/app/src',
+        __DIR__ . '/packages/framework/src',
+        __DIR__ . '/packages/framework/tests',
+        __DIR__ . '/project-base/app/src',
+        __DIR__ . '/project-base/app/tests',
+    ]);
+
+    $rectorConfig->rules([
+        \Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictBoolReturnExprRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector::class,
+        //\Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector::class,
+        //\Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\AddMethodCallBasedStrictParamTypeRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector::class,
+    ]);
+};
