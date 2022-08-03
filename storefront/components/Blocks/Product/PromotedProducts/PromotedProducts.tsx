fb924a09283d1baf7fccd18911bc4b3a9a45ea1f
Sebastian Holesz <sebastian.holesz@shopsys.com>
2022-08-03 16:06:48 +0200

    removed duplicit product list view from HP promo products

--- storefront/components/Blocks/Product/PromotedProducts/PromotedProducts.tsx
+++ storefront/components/Blocks/Product/PromotedProducts/PromotedProducts.tsx
@@ -1,13 +1,11 @@
 import ProductsSlider from 'components/Blocks/Product/ProductsSlider';
 import { usePromotedProducts } from 'connectors/products/Products';
-import { useGtmSliderProductListView } from 'hooks/gtm/useGtmSliderProductListView';
 import { FC } from 'react';
 
 const GTM_LIST_NAME = 'homepage promo products' as const;
 
 const PromotedProducts: FC = () => {
     const promotedProducts = usePromotedProducts();
-    useGtmSliderProductListView(promotedProducts, GTM_LIST_NAME);
 
     if (promotedProducts !== undefined) {
         return <ProductsSlider products={promotedProducts} gtmListName={GTM_LIST_NAME} />;
