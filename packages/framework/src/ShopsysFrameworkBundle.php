31dd905d2cef3e135defc3054e69e497998f6127
Martin Grossmann <martin@vyvoj.net>
2022-08-12 08:43:29 +0200

    ShopsysFrameworkBundle: version updated to "10.0.4-dev"

--- packages/framework/src/ShopsysFrameworkBundle.php
+++ packages/framework/src/ShopsysFrameworkBundle.php
@@ -21,7 +21,7 @@ class ShopsysFrameworkBundle extends Bundle
     /**
      * @var string
      */
-    public const VERSION = '10.0.3';
+    public const VERSION = '10.0.4-dev';
 
     /**
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
