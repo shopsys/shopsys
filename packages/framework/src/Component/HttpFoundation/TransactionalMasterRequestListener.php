1aa9e2a307f7349437a7b0de132efccb08ec6137
Milan Staňo <milan.stano@shopsys.com>
2022-11-20 09:51:39 +0100

    Graphql query is not in transaction

--- packages/framework/src/Component/HttpFoundation/TransactionalMasterRequestListener.php
+++ packages/framework/src/Component/HttpFoundation/TransactionalMasterRequestListener.php
@@ -5,20 +5,33 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Component\HttpFoundation;
 
 use Doctrine\ORM\EntityManagerInterface;
+use GraphQL\Error\SyntaxError;
+use GraphQL\Language\AST\OperationDefinitionNode;
+use GraphQL\Language\Parser;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class TransactionalMasterRequestListener
 {
-    protected bool $inTransaction;
+    protected bool $inTransaction = false;
+
+    protected static bool $isManuallyRollbacked = false;
+
+    protected const GRAPHQL_ENDPOINT_ROUTE = 'overblog_graphql_endpoint';
+    protected const QUERY_TYPE = 'query';
 
     /**
      * @param \Doctrine\ORM\EntityManagerInterface $em
      */
-    public function __construct(protected readonly EntityManagerInterface $em)
+    public function __construct(
+        protected readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    public static function setTransactionManuallyRollbacked(): void
     {
-        $this->inTransaction = false;
+        static::$isManuallyRollbacked = true;
     }
 
     /**
@@ -26,7 +39,7 @@ class TransactionalMasterRequestListener
      */
     public function onKernelRequest(RequestEvent $event): void
     {
-        if ($event->isMainRequest() && !$this->inTransaction) {
+        if ($event->isMainRequest() && !$this->inTransaction && !$this->isRequestGraphQlQuery($event)) {
             $this->em->beginTransaction();
             $this->inTransaction = true;
         }
@@ -37,7 +50,7 @@ class TransactionalMasterRequestListener
      */
     public function onKernelResponse(ResponseEvent $event): void
     {
-        if ($event->isMainRequest() && $this->inTransaction) {
+        if ($event->isMainRequest() && $this->inTransaction && !static::$isManuallyRollbacked) {
             $this->em->commit();
             $this->inTransaction = false;
         }
@@ -52,5 +65,44 @@ class TransactionalMasterRequestListener
             $this->em->rollback();
             $this->inTransaction = false;
         }
+    }
+
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\RequestEvent $requestEvent
+     * @return bool
+     */
+    protected function isRequestGraphQlQuery(RequestEvent $requestEvent): bool
+    {
+        if ($requestEvent->getRequest()->attributes->get('_route') !== static::GRAPHQL_ENDPOINT_ROUTE) {
+            return false;
+        }
+
+        $requestContent = $requestEvent->getRequest()->getContent();
+
+        if ($requestContent === null || $requestContent === '') {
+            return false;
+        }
+
+        $source = json_decode($requestContent, true);
+
+        if (!array_key_exists(static::QUERY_TYPE, $source)) {
+            return false;
+        }
+
+        $queryString = $source[static::QUERY_TYPE];
+
+        try {
+            $parsed = Parser::parse($queryString);
+
+            foreach ($parsed->definitions as $definition) {
+                if ($definition instanceof OperationDefinitionNode) {
+                    return $definition->operation === static::QUERY_TYPE;
+                }
+            }
+        } catch (SyntaxError) {
+            return false;
+        }
+
+        return false;
     }
 }
