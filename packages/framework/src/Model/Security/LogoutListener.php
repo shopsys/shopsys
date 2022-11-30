170d538702019d9b3fc28030b0131796ee658c46
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2022-11-30 12:16:12 +0100

    resolved deprecation of Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface

---
+++ packages/framework/src/Model/Security/LogoutListener.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Security;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutListener implements EventSubscriberInterface
+{
+    protected const ADMINISTRATION_TOKEN = 'administration';
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Security\FrontLogoutHandler $frontLogoutHandler
+     * @param \Shopsys\FrameworkBundle\Model\Security\AdminLogoutHandler $adminLogoutHandler
+     */
+    public function __construct(
+        protected readonly FrontLogoutHandler $frontLogoutHandler,
+        protected readonly AdminLogoutHandler $adminLogoutHandler,
+    ) {
+    }
+
+    /**
+     * @param \Symfony\Component\Security\Http\Event\LogoutEvent $event
+     */
+    public function onLogout(LogoutEvent $event): void
+    {
+        if (!($event->getToken() instanceof UsernamePasswordToken)) {
+            return;
+        }
+
+        /** @var \Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken $token */
+        $token = $event->getToken();
+
+        if ($token->getFirewallName() === static::ADMINISTRATION_TOKEN) {
+            $response = $this->adminLogoutHandler->onLogoutSuccess($event->getRequest());
+        } else {
+            $response = $this->frontLogoutHandler->onLogoutSuccess($event->getRequest());
+        }
+
+        $event->setResponse($response);
+    }
+
+    /**
+     * @return array[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            LogoutEvent::class => ['onLogout', 64],
+        ];
+    }
+}
