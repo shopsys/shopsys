2fa9885c4f72eba218bba2e5dd8d123d0ace0e42
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2023-01-23 13:11:56 +0100

    current customer user is now cached for whole request, so it is not requested multiple times from database

--- packages/framework/src/Model/Customer/User/CurrentCustomerUser.php
+++ packages/framework/src/Model/Customer/User/CurrentCustomerUser.php
@@ -9,6 +9,11 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 class CurrentCustomerUser
 {
     /**
+     * @var \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser[]
+     */
+    protected array $customerUserCache = [];
+
+    /**
      * @var \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
      */
     protected $tokenStorage;
@@ -61,16 +66,25 @@ class CurrentCustomerUser
             return null;
         }
 
+        if (array_key_exists($token->getUserIdentifier(), $this->customerUserCache) === true) {
+            return $this->customerUserCache[$token->getUserIdentifier()];
+        }
+
         $user = $token->getUser();
 
         if (
             class_exists('\Shopsys\FrontendApiBundle\Model\User\FrontendApiUser')
             && $user instanceof FrontendApiUser
         ) {
-            return $this->customerUserFacade->getByUuid($user->getUuid());
+            $customerUser = $this->customerUserFacade->getByUuid($user->getUuid());
+            $this->customerUserCache[$token->getUserIdentifier()] = $customerUser;
+
+            return $customerUser;
         }
 
         if ($user instanceof CustomerUser) {
+            $this->customerUserCache[$token->getUserIdentifier()] = $user;
+
             return $user;
         }
 
---
+++ packages/framework/tests/Unit/Model/Customer/Mock/TokenMock.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FrameworkBundle\Unit\Model\Customer\Mock;
+
+use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class TokenMock implements TokenInterface
+{
+    private CustomerUser $customerUser;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser $customerUser
+     */
+    public function __construct(CustomerUser $customerUser)
+    {
+        $this->customerUser = $customerUser;
+    }
+
+    public function serialize()
+    {
+        return null;
+    }
+
+    /**
+     * @param string $data
+     */
+    public function unserialize(string $data)
+    {
+    }
+
+    public function __toString()
+    {
+        return '';
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoleNames(): array
+    {
+        return [];
+    }
+
+    public function getCredentials()
+    {
+    }
+
+    public function getUser()
+    {
+        return $this->customerUser;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+    }
+
+    public function isAuthenticated()
+    {
+        return true;
+    }
+
+    /**
+     * @param bool $isAuthenticated
+     */
+    public function setAuthenticated(bool $isAuthenticated)
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getAttributes()
+    {
+        return [];
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function setAttributes(array $attributes)
+    {
+    }
+
+    /**
+     * @param string $name
+     */
+    public function hasAttribute(string $name)
+    {
+        return true;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function getAttribute(string $name)
+    {
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function setAttribute(string $name, $value)
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function __serialize(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param array $data
+     */
+    public function __unserialize(array $data): void
+    {
+    }
+
+    public function getUsername()
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserIdentifier(): string
+    {
+        return $this->customerUser->getEmail();
+    }
+}
