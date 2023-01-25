19344e40cc6f4c9c1eaa49ab301062a9ba280325
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2023-01-25 09:22:53 +0100

    [releaser] lock files are now created in a separate folder

--- utils/releaser/src/ReleaseWorker/Release/CreateAndCommitLockFilesReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/Release/CreateAndCommitLockFilesReleaseWorker.php
@@ -16,7 +16,7 @@ final class CreateAndCommitLockFilesReleaseWorker extends AbstractShopsysRelease
      */
     public function getDescription(Version $version): string
     {
-        return 'Create and commit composer.lock, package-lock.json, and migrations-lock.yml and [Manually] push it';
+        return 'Create and commit composer.lock, symfony.lock, package-lock.json, and migrations-lock.yml and [Manually] push it';
     }
 
     /**
@@ -34,32 +34,41 @@ final class CreateAndCommitLockFilesReleaseWorker extends AbstractShopsysRelease
      */
     public function work(Version $version): void
     {
-        $this->symfonyStyle->note(
-            'Removing vendor/, node_modules/, composer.lock, migrations-lock.yml, and package-lock.json'
+        $currentDir = trim($this->processRunner->run('pwd'));
+
+        $packageName = 'project-base';
+        $tempDirectory = trim($this->processRunner->run('mktemp -d -t shopsys-release-XXXX'));
+
+        $this->symfonyStyle->note(sprintf('Cloning shopsys/%s. This can take a while.', $packageName));
+        $this->processRunner->run(
+            sprintf('cd %s && git clone --branch=%s https://github.com/shopsys/%s.git', $tempDirectory, $this->initialBranchName, $packageName)
         );
-        $this->processRunner->run('rm -rf project-base/vendor');
-        $this->processRunner->run('rm -rf project-base/node_modules');
-        $this->processRunner->run('rm -f project-base/composer.lock');
-        $this->processRunner->run('rm -f project-base/package-lock.json');
-        $this->processRunner->run('rm -f project-base/migrations-lock.yml');
-        $this->processRunner->run('cd project-base && composer install && npm install');
+
+        $this->symfonyStyle->note('Installing dependencies');
+        $this->processRunner->run(sprintf('cd %s/project-base && composer update && npm install', $tempDirectory));
+        $this->processRunner->run(sprintf('cd %s/project-base && php phing db-rebuild', $tempDirectory));
+
+        $this->symfonyStyle->note('Committing changes in composer.lock, symfony.lock, package-lock.json, and migrations-lock.yml');
+        $this->processRunner->run(sprintf('cp %s/project-base/composer.lock %s/project-base/composer.lock', $tempDirectory, $currentDir));
+        $this->processRunner->run(sprintf('cp %s/project-base/symfony.lock %s/project-base/symfony.lock', $tempDirectory, $currentDir));
+        $this->processRunner->run(sprintf('cp %s/project-base/package-lock.json %s/project-base/package-lock.json', $tempDirectory, $currentDir));
+        $this->processRunner->run(sprintf('cp %s/project-base/migrations-lock.yml %s/project-base/migrations-lock.yml', $tempDirectory, $currentDir));
 
         $this->processRunner->run('git add -f project-base/composer.lock');
+        $this->processRunner->run('git add project-base/symfony.lock');
         $this->processRunner->run('git add -f project-base/package-lock.json');
-
-        $this->processRunner->run('rm -rf project-base/vendor');
-        $this->processRunner->run('composer install');
-
-        $this->processRunner->run('php phing db-rebuild');
         $this->processRunner->run('git add -f project-base/migrations-lock.yml');
 
         $message = sprintf('locked versions of dependencies for %s release', $version->getVersionString());
         $this->commit($message);
 
-        $this->symfonyStyle->note('push last commit with composer, package, and migration locks');
+        $this->symfonyStyle->note([
+            'Push last commit with generated lock files',
+            'You have to allow push to the protected branch here https://github.com/shopsys/shopsys/settings/branches first and turn of "status checks to pass before merging"',
+        ]);
         $this->symfonyStyle->confirm(
             sprintf(
-                'confirm that composer.lock, package-lock.json, and migrations-lock.yml are pushed to "%s" branch',
+                'confirm that composer.lock, symfony.lock, package-lock.json, and migrations-lock.yml are pushed to "%s" branch',
                 $this->initialBranchName
             )
         );
