dbb6abe35e9c2bff3a778ecc48e9ed5162f64a9f
Rostislav Vítek <rostislav.vitek@shopsys.com>
2023-01-30 10:52:36 +0100

    [framework] fix annotations for CategoryTranslation::$name (#2525)
    Co-authored-by: Tomas Ludvik <tomas.ludvik@shopsys.com>

--- packages/framework/src/Model/Category/Category.php
+++ packages/framework/src/Model/Category/Category.php
@@ -136,7 +136,7 @@ class Category extends AbstractTranslatableEntity
 
     /**
      * @param string|null $locale
-     * @return string
+     * @return string|null
      */
     public function getName($locale = null)
     {
