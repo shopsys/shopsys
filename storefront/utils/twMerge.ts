5c332faa11772dc91f8d185cf0a67e6090bb25bf
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-02-06 16:20:48 +0100

    !fixup add Tailwind and config

--- storefront/utils/twMerge.ts
+++ storefront/utils/twMerge.ts
@@ -1,62 +1,13 @@
 // eslint-disable-next-line no-restricted-imports
 import { extendTailwindMerge } from 'tailwind-merge';
+import tailwindConfig from 'tailwind.config';
 
 const twMerge = extendTailwindMerge({
     classGroups: {
-        'font-size': [{ text: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'description', 'body', 'small', 'tiny', 'footer'] }],
-        'border-radius': [{ rounded: ['none', 'full', 'round', '2xl', 'xl', 'lg', 'md', 'sm'] }],
-        'z-index': [
-            {
-                z: [
-                    'hidden',
-                    'above',
-                    'two',
-                    'productSlider',
-                    'stickyBar',
-                    'overlay',
-                    'menu',
-                    'aboveMenu',
-                    'consentPopup',
-                    'cart',
-                    'aboveOverlay',
-                    'maximum',
-                    'auto',
-                ],
-            },
-        ],
+        'font-size': [{ text: Object.keys(tailwindConfig.theme?.fontSize || {}) }],
     },
     theme: {
-        color: [
-            'primary',
-            'white',
-            'blue',
-            'blueVeryLight',
-            'black',
-            'green',
-            'greenLight',
-            'greenVeryLight',
-            'greenDark',
-            'red',
-            'redLight',
-            'redDark',
-            'orange',
-            'orangeLight',
-            'gray',
-            'grayVeryLight',
-            'grayLight',
-            'grayLighter',
-            'grayMidDarker',
-            'grayDarker',
-            'grayDark',
-            'grayVeryDark',
-            'border',
-            'borderLight',
-            'outlineFocus',
-            'borderFocus',
-            'inherit',
-            'transparent',
-            'currentColor',
-        ],
+        color: Object.keys(tailwindConfig.theme?.colors || {}),
     },
 });
 
