193af45ecd49ff91b0916bb8bc42a6ca2c971ac6
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-02-14 14:37:56 +0100

    fixed generation of translations by adding support for domains to be set as constants

--- packages/framework/src/Component/Translation/PhpParserNodeHelper.php
+++ packages/framework/src/Component/Translation/PhpParserNodeHelper.php
@@ -4,12 +4,15 @@ namespace Shopsys\FrameworkBundle\Component\Translation;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\BinaryOp\Concat;
+use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Scalar\String_;
 use Shopsys\FrameworkBundle\Component\Translation\Exception\StringValueUnextractableException;
 use SplFileInfo;
 
 class PhpParserNodeHelper
 {
+    protected const TRANSLATOR_CLASS_FQN = '\Shopsys\FrameworkBundle\Component\Translation\Translator';
+
     /**
      * @param \PhpParser\Node $node
      * @param \SplFileInfo $fileInfo
@@ -28,10 +31,15 @@ class PhpParserNodeHelper
             );
         }
 
+        if ($node instanceof ClassConstFetch && $node->class->parts[0] === 'Translator') {
+            return constant(static::TRANSLATOR_CLASS_FQN . '::' . $node->name->name);
+        }
+
         throw new StringValueUnextractableException(
             sprintf(
-                'Can only extract the message ID or message domain from a scalar or concatenated string,'
+                'Can only extract the message ID or message domain from a scalar, concatenated string or "%s" class constant,'
                 . ' but got "%s". Please refactor your code to make it extractable (in %s on line %d).',
+                static::TRANSLATOR_CLASS_FQN,
                 get_class($node),
                 $fileInfo,
                 $node->getLine()
