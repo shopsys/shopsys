213f395957d3b5063f85bb9681bc3428c0a6ffd7
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-04-17 14:35:39 +0200

    returned symfony.lock to project-base as it caused problems with symfony flex installing packages again

--- utils/releaser/src/ReleaseWorker/AfterRelease/RemoveLockFilesReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/AfterRelease/RemoveLockFilesReleaseWorker.php
@@ -29,12 +29,12 @@ final class RemoveLockFilesReleaseWorker extends AbstractShopsysReleaseWorker
         $this->processRunner->run('git rm project-base/composer.lock --ignore-unmatch');
         $this->processRunner->run('git rm project-base/package-lock.json --ignore-unmatch');
         $this->processRunner->run('git rm project-base/migrations-lock.yml --ignore-unmatch');
-        $this->processRunner->run('git rm project-base/symfony.lock --ignore-unmatch');
+        // symfony.lock is not deleted as its removal would lead to reset of Symfony Flex
         $this->commit('removed locked versions of dependencies for unreleased version');
 
         if ($this->currentBranchName === AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME) {
             $this->symfonyStyle->note(
-                'You need to push the master branch manually, however, you have to wait until the previous (tagged) master build is finished on Heimdall. Otherwise, master-project-base would have never been built from the source codes where there are dependencies on the tagged versions of shopsys packages.'
+                'You need to push the master branch manually, however, you have to wait until the previous (tagged) master build is finished on Heimdall. Otherwise, master-project-base would have never been built from the source codes where there are dependencies on the tagged versions of Shopsys packages.'
             );
             $this->confirm('Confirm you have waited long enough and then pushed the master branch.');
         } else {
