e705a1b13ee99951548299bdf36af104d3495b17
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-05-11 20:08:14 +0200

    ProductTest now correctly sets all necessary data for a correct run of the tests

---
+++ packages/framework/tests/Unit/Model/Product/TestProductProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\FrameworkBundle\Unit\Model\Product;
+
+use Shopsys\FrameworkBundle\Model\Product\Availability\Availability;
+use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityData;
+use Shopsys\FrameworkBundle\Model\Product\ProductData;
+use Shopsys\FrameworkBundle\Model\Product\Unit\Unit;
+use Shopsys\FrameworkBundle\Model\Product\Unit\UnitData;
+
+class TestProductProvider
+{
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Product\ProductData
+     */
+    public static function getTestProductData(): ProductData
+    {
+        $availabilityData = new AvailabilityData();
+        $availabilityData->name = [
+            'en' => 'availability name',
+        ];
+
+        $unitData = new UnitData();
+        $unitData->name = [
+            'en' => 'unit name',
+        ];
+
+        $productData = new ProductData();
+        $productData->availability = new Availability($availabilityData);
+        $productData->unit = new Unit($unitData);
+
+        return $productData;
+    }
+}
