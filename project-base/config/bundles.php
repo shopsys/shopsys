63ce2c4dfd7b7427ef2b59bccdc29280ba5de3f3
Martin Grossmann <martin@vyvoj.net>
2023-05-29 10:14:57 +0200

    fixed project-base codeception versions

--- project-base/config/bundles.php
+++ project-base/config/bundles.php
@@ -37,4 +37,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Shopsys\S3Bridge\S3BridgeBundle::class => ['all' => true],
 ];
