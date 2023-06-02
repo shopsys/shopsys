96c6971b0d3ccf8ac2529137b074475e2de7f016
Martin Grossmann <martin@vyvoj.net>
2023-06-02 22:03:24 +0200

    removed no longer needed acceptance tests

--- project-base/app/tests/App/Acceptance/acceptance/ErrorHandlingCest.php
+++ project-base/app/tests/App/Acceptance/acceptance/ErrorHandlingCest.php
@@ -15,7 +15,7 @@ class ErrorHandlingCest
     public function testDisplayNotice(AcceptanceTester $me)
     {
         $me->wantTo('display notice error page');
-        $me->amOnPage('/test/error-handler/notice');
+        $me->amOnPage('/codeception-acceptance-test/error-handler/notice');
         $me->seeTranslationFrontend('Oops! Error occurred.');
         $me->dontSee('Notice');
     }
@@ -26,7 +26,7 @@ class ErrorHandlingCest
     public function testAccessUnknownDomain(AcceptanceTester $me)
     {
         $me->wantTo('display error when accessing an unknown domain');
-        $me->amOnPage('/test/error-handler/unknown-domain');
+        $me->amOnPage('/codeception-acceptance-test/error-handler/unknown-domain');
         $me->see('You are trying to access an unknown domain');
         $me->dontSeeTranslationFrontend('Page not found!');
     }
@@ -37,13 +37,13 @@ class ErrorHandlingCest
     public function test500ErrorPage(AcceptanceTester $me)
     {
         $me->wantTo('display 500 error and check error ID uniqueness');
-        $me->amOnPage('/test/error-handler/exception');
-        $me->seeTranslationFrontend('Oops! Error occurred');
+        $me->amOnPage('/codeception-acceptance-test/error-handler/exception');
+        $me->seeTranslationFrontend('Oops! Error occurred.');
 
         $cssIdentifier = ['css' => '#js-error-id'];
         $errorIdFirstAccess = $me->grabTextFrom($cssIdentifier);
 
-        $me->amOnPage('/test/error-handler/exception');
+        $me->amOnPage('/codeception-acceptance-test/error-handler/exception');
         $errorIdSecondAccess = $me->grabTextFrom($cssIdentifier);
 
         Assert::assertNotSame($errorIdFirstAccess, $errorIdSecondAccess);
