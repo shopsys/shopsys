e8790be8eca353649a37d757a4e63175ed7fd685
Martin Grossmann <martin@vyvoj.net>
2023-06-14 12:19:09 +0200

    fixed storefront mounted volume permissions in dev mode

--- project-base/scripts/configure.sh
+++ project-base/scripts/configure.sh
@@ -47,6 +47,8 @@ case "$operatingSystem" in
 
         sed -i -r "s#www_data_uid: [0-9]+#www_data_uid: $(id -u)#" ./docker-compose.yml
         sed -i -r "s#www_data_gid: [0-9]+#www_data_gid: $(id -g)#" ./docker-compose.yml
+        sed -i -r "s#node_uid: [0-9]+#node_uid: $(id -u)#" ./docker-compose.yml
+        sed -i -r "s#node_gid: [0-9]+#node_gid: $(id -g)#" ./docker-compose.yml
 
         echo "Starting docker-compose"
         docker-compose up -d --build --force-recreate
