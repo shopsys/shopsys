9ae8c7eec0e48aab633793a0db7db87ba4934f98
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-06-16 09:17:28 +0200

    enabling storefront in review stage

--- .github/set-urls-for-review.sh
+++ .github/set-urls-for-review.sh
@@ -10,6 +10,18 @@ setUrlsToDomainsUrls () {
     done
 }
 
+setDomainsToDockerCompose() {
+    DOMAINS=${HOSTS}
+    ITERATOR=1
+
+    for DOMAIN in ${DOMAINS//,/ } ; do
+        yq -i ".services.storefront.environment.DOMAIN_HOSTNAME_${ITERATOR} = \"https://${DOMAIN}/\"" ./docker-compose.yml
+        yq -i ".services.storefront.environment.PUBLIC_GRAPHQL_ENDPOINT_HOSTNAME_${ITERATOR} = \"https://${DOMAIN}/graphql/\"" ./docker-compose.yml
+
+        ITERATOR=$(expr $ITERATOR + 1)
+    done
+}
+
 printDomains() {
     DOMAINS=${HOSTS}
     echo "Available hosts"
@@ -22,4 +34,5 @@ printDomains() {
 case $1 in
     setUrlsToDomainsUrls) "$@"; exit;;
     printDomains) "$@"; exit;;
+    setDomainsToDockerCompose) "$@"; exit;;
 esac
