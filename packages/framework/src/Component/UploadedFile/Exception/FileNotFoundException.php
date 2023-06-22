11123d9ae3e2e71dc9b73acf4a0119317d086dfc
Michal Tomka <michal.tomka@shopsys.com>
2023-06-22 14:20:59 +0200

    FileNotFoundException extends NotFoundHttpException

--- packages/framework/src/Component/UploadedFile/Exception/FileNotFoundException.php
+++ packages/framework/src/Component/UploadedFile/Exception/FileNotFoundException.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Component\UploadedFile\Exception;
 
 use Exception;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class FileNotFoundException extends Exception
+class FileNotFoundException extends NotFoundHttpException
 {
     public function __construct(string $message = '', ?Exception $previous = null)
     {
-        parent::__construct($message, 0, $previous);
+        parent::__construct($message, $previous);
     }
 }
