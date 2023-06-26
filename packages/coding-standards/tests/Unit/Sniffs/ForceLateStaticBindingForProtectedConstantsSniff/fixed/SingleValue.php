0ff139c745c011aca7a6da001ca260e04420778d
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-06-26 12:57:00 +0200

    fixed ForceLateStaticBindingForProtectedConstantsSniff after changes in SlevomatCodingStandards 8.13.0

--- packages/coding-standards/tests/Unit/Sniffs/ForceLateStaticBindingForProtectedConstantsSniff/fixed/SingleValue.php
+++ packages/coding-standards/tests/Unit/Sniffs/ForceLateStaticBindingForProtectedConstantsSniff/fixed/SingleValue.php
@@ -9,15 +9,7 @@ class SingleValue
     public const A = 'value';
     protected const B = 'value';
     private const C = 'value';
-    /** @access public */
     const D = 'value';
-    /**
-     * @access protected
-     */
-    const E = 'value';
-    /** @access private */
-    const F = 'value';
-    const G = 'value';
 
     public function method()
     {
@@ -25,16 +17,10 @@ class SingleValue
         echo static::B;
         echo self::C;
         echo self::D;
-        echo static::E;
-        echo self::F;
-        echo self::G;
 
         echo static::A;
         echo static::B;
         echo static::C;
         echo static::D;
-        echo static::E;
-        echo static::F;
-        echo static::G;
     }
 }
