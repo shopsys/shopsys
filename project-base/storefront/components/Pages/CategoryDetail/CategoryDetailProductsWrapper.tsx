90fd397334943a4db0f7be1191b2967746067e8c
sebaholesz <sebastianholesz@seznam.cz>
2023-07-03 13:44:57 +0200

    optimised the number of requests regarding SEO categories
    - no new requests are sent to the API after a transition to a SEO category
    as the result would still be the same
    - in order to achieve this, a 'wasRedirectedToSeoCategory' boolean pointer
    is now set after each redirect to a SEO category and unset after the last
    category query (CategoryProductsQuery) is returned
    - CategoryDetailQuery and CategoryProductsQuery do not use
    the pre-generated hooks, but depend on a custom implementation
    which makes sure to call the API only when conditions are met
    - the body of the useQueryError hook was extracted in a separate handler
    so it can be used as a normal function
    - more granular skeletons were added on the category detail page
    so only specific parts (title and description) are loading when
    transitioning between classic and SEO categories

--- project-base/storefront/components/Pages/CategoryDetail/CategoryDetailProductsWrapper.tsx
+++ project-base/storefront/components/Pages/CategoryDetail/CategoryDetailProductsWrapper.tsx
@@ -1,20 +1,26 @@
 import { DEFAULT_PAGE_SIZE, Pagination } from 'components/Blocks/Pagination/Pagination';
 import { getEndCursor } from 'components/Blocks/Product/Filter/helpers/getEndCursor';
 import { ProductsList } from 'components/Blocks/Product/ProductsList/ProductsList';
-import { CategoryDetailFragmentApi, useCategoryProductsQueryApi } from 'graphql/generated';
-import { getFilterOptions } from 'helpers/filterOptions/getFilterOptions';
+import {
+    CategoryDetailFragmentApi,
+    CategoryProductsQueryApi,
+    CategoryProductsQueryDocumentApi,
+    CategoryProductsQueryVariablesApi,
+} from 'graphql/generated';
 import { mapParametersFilter } from 'helpers/filterOptions/mapParametersFilter';
-import { parseFilterOptionsFromQuery } from 'helpers/filterOptions/parseFilterOptionsFromQuery';
 import { getCategoryOrSeoCategoryGtmProductListName } from 'helpers/gtm/gtm';
 import { getMappedProducts } from 'helpers/mappers/products';
-import { getProductListSort } from 'helpers/sorting/getProductListSort';
-import { parseProductListSortFromQuery } from 'helpers/sorting/parseProductListSortFromQuery';
-import { useQueryError } from 'hooks/graphQl/useQueryError';
+import { getUrlWithoutGetParameters } from 'helpers/parsing/getUrlWithoutGetParameters';
+import { handleQueryError } from 'hooks/graphQl/useQueryError';
 import { useGtmPaginatedProductListViewEvent } from 'hooks/gtm/productList/useGtmPaginatedProductListViewEvent';
+import { useTypedTranslationFunction } from 'hooks/typescript/useTypedTranslationFunction';
 import { useQueryParams } from 'hooks/useQueryParams';
 import { useRouter } from 'next/router';
-import { RefObject, useMemo } from 'react';
+import { RefObject, useEffect, useMemo, useState } from 'react';
+import { useSessionStore } from 'store/zustand/useSessionStore';
 import { GtmMessageOriginType } from 'types/gtm/enums';
+import { useClient } from 'urql';
+import { getSlugFromUrl } from 'utils/getSlugFromUrl';
 
 type CategoryDetailProps = {
     category: CategoryDetailFragmentApi;
@@ -22,29 +28,14 @@ type CategoryDetailProps = {
 };
 
 export const CategoryDetailProductsWrapper: FC<CategoryDetailProps> = ({ category, paginationScrollTargetRef }) => {
-    const { query } = useRouter();
-    const { currentPage } = useQueryParams();
-    const orderingMode = getProductListSort(parseProductListSortFromQuery(query.sort));
-    const parametersFilter = getFilterOptions(parseFilterOptionsFromQuery(query.filter));
-
-    const [{ data: categoryProductsData, fetching }] = useQueryError(
-        useCategoryProductsQueryApi({
-            variables: {
-                endCursor: getEndCursor(currentPage),
-                filter: mapParametersFilter(parametersFilter),
-                orderingMode,
-                urlSlug: category.slug,
-                pageSize: DEFAULT_PAGE_SIZE,
-            },
-        }),
-    );
+    const [categoryProductsData, fetching] = useCategoryProductsData();
 
     const gtmProductListName = useMemo(
         () => getCategoryOrSeoCategoryGtmProductListName(category.originalCategorySlug),
         [category],
     );
 
-    const categoryListedProducts = getMappedProducts(categoryProductsData?.category?.products.edges);
+    const categoryListedProducts = getMappedProducts(categoryProductsData?.products.edges);
 
     useGtmPaginatedProductListViewEvent(categoryListedProducts, gtmProductListName);
 
@@ -63,4 +54,45 @@ export const CategoryDetailProductsWrapper: FC<CategoryDetailProps> = ({ categor
             />
         </>
     );
+};
+
+const useCategoryProductsData = (): [undefined | CategoryProductsQueryApi['category'], boolean] => {
+    const client = useClient();
+    const { asPath } = useRouter();
+    const { filter, sort, currentPage } = useQueryParams();
+    const t = useTypedTranslationFunction();
+
+    const endCursor = getEndCursor(currentPage);
+    const urlSlug = getSlugFromUrl(getUrlWithoutGetParameters(asPath));
+
+    const wasRedirectedToSeoCategory = useSessionStore((s) => s.wasRedirectedToSeoCategory);
+    const setWasRedirectedToSeoCategory = useSessionStore((s) => s.setWasRedirectedToSeoCategory);
+    const [categoryDetailData, setCategoryDetailData] = useState<CategoryProductsQueryApi>();
+    const [fetching, setFetching] = useState(false);
+
+    useEffect(() => {
+        if (wasRedirectedToSeoCategory) {
+            setWasRedirectedToSeoCategory(false);
+
+            return;
+        }
+        setFetching(true);
+
+        client
+            .query<CategoryProductsQueryApi, CategoryProductsQueryVariablesApi>(CategoryProductsQueryDocumentApi, {
+                endCursor,
+                filter: mapParametersFilter(filter),
+                orderingMode: sort ?? null,
+                urlSlug,
+                pageSize: DEFAULT_PAGE_SIZE,
+            })
+            .toPromise()
+            .then((response) => {
+                handleQueryError(response.error, t);
+                setCategoryDetailData(response.data?.category ?? undefined);
+            })
+            .finally(() => setFetching(false));
+    }, [urlSlug, sort, JSON.stringify(filter), endCursor]);
+
+    return [categoryDetailData, fetching];
 };
