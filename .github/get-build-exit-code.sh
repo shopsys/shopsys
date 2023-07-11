0dd15d69bc3a50461d4825881b29e14000b62854
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-07-11 11:48:25 +0200

    build will no longer be successful if the storefront standards or review will fail

--- .github/get-build-exit-code.sh
+++ .github/get-build-exit-code.sh
@@ -5,12 +5,14 @@ getBuildExitCodeBasedOnJobResults() {
     STANDARDS_RESULT=$2
     TESTS_RESULT=$3
     TESTS_ACCEPTANCE_RESULT=$4
+    STANDARDS_STOREFRONT_RESULT=$5
+    REVIEW_RESULT=$6
 
     if [[ "$BUILD_FORK_RESULT" == "success" ]]; then
         return 0
     fi
 
-    if [[ "$STANDARDS_RESULT" == "success" && "$TESTS_RESULT" == "success" && "$TESTS_ACCEPTANCE_RESULT" == "success" ]]; then
+    if [[ "$STANDARDS_RESULT" == "success" && "$TESTS_RESULT" == "success" && "$TESTS_ACCEPTANCE_RESULT" == "success" && "$STANDARDS_STOREFRONT_RESULT" == "success" && "$REVIEW_RESULT" == "success" ]]; then
         return 0
     else
         return 1
