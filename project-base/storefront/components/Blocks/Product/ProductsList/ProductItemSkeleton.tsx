0b492fc8bcd1bf90ebf5c7fdad7b02b17c20634c
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-07-13 16:39:25 +0200

    adjust ProductItem skeleton

--- project-base/storefront/components/Blocks/Product/ProductsList/ProductItemSkeleton.tsx
+++ project-base/storefront/components/Blocks/Product/ProductsList/ProductItemSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from 'react-loading-skeleton';
 
 export const ProductItemSkeleton: FC = () => (
     <div className="flex w-full flex-col border-b border-greyLighter pb-5 lg:py-4 lg:px-5">
-        <Skeleton className="mb-24 h-full rounded-none" containerClassName="h-[150px] mb-24 max-w-[160px] w-full" />
+        <Skeleton className="mb-24 h-full rounded-none" containerClassName="h-[179px] mb-24 max-w-[160px] w-full" />
         <Skeleton className="mb-4 h-4 w-5/6" containerClassName="w-full" />
         <Skeleton className="h-4 w-16" />
         <Skeleton className="mt-2 mb-4 h-5  w-4/6" />
