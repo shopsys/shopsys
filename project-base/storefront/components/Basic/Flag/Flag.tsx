21ea19123fd0d33f41db3f700c751d64667570f5
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-07-24 14:48:17 +0200

    refactor Flag component
    - there was unhandled case when href was not send, now in such a case we send div instead anchor

--- project-base/storefront/components/Basic/Flag/Flag.tsx
+++ project-base/storefront/components/Basic/Flag/Flag.tsx
@@ -1,26 +1,32 @@
-import { AnchorHTMLAttributes } from 'react';
-import { ExtractNativePropsFromDefault } from 'typeHelpers/ExtractNativePropsFromDefault';
 import { twMergeCustom } from 'utils/twMerge';
 import { ExtendedNextLink } from '../ExtendedNextLink/ExtendedNextLink';
 
-type NativeProps = ExtractNativePropsFromDefault<AnchorHTMLAttributes<HTMLAnchorElement>, 'href', never>;
-
-type FlagProps = NativeProps & {
-    classNames?: string;
-};
-
 const getDataTestId = (dataTestId?: string) => dataTestId ?? 'basic-flag';
 
-export const Flag: FC<FlagProps> = ({ children, dataTestId, href, classNames }) => (
-    <ExtendedNextLink
-        type="blogCategory"
-        href={href}
-        className={twMergeCustom(
-            'mb-2 mr-3 inline-flex rounded-sm bg-primaryLight py-1 px-2 text-xs uppercase text-dark no-underline hover:text-dark hover:no-underline',
-            classNames,
-        )}
-        data-testid={getDataTestId(dataTestId)}
-    >
-        <>{children}</>
-    </ExtendedNextLink>
-);
+type FlagProps = { href?: string };
+
+export const Flag: FC<FlagProps> = ({ children, dataTestId, href, className }) => {
+    const flagTwClass = twMergeCustom(
+        'inline-flex rounded-sm bg-primaryLight py-1 px-2 text-xs uppercase text-dark no-underline hover:text-dark hover:no-underline',
+        className,
+    );
+
+    if (href) {
+        return (
+            <ExtendedNextLink
+                type="blogCategory"
+                href={href}
+                className={flagTwClass}
+                data-testid={getDataTestId(dataTestId)}
+            >
+                {children}
+            </ExtendedNextLink>
+        );
+    }
+
+    return (
+        <div className={flagTwClass} data-testid={getDataTestId(dataTestId)}>
+            {children}
+        </div>
+    );
+};
