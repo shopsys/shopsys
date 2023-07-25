3be18cf2eaa2e288783c839b13eefc50da7aa098
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-07-25 14:55:26 +0200

    fixed canceling of reviews

--- .github/cancel-review.sh
+++ .github/cancel-review.sh
@@ -1,9 +1,17 @@
-echo "Trying to cancel review for branch $BRANCH_NAME"
+BRANCH_NAME=$1
 
-if [ ! -z "$BRANCH_NAME" ]; then
-    cd "../$BRANCH_NAME"
-    docker compose down -v --remove-orphans
-    docker system prune -a -f
-    cd ..
-    rm -rf "$BRANCH_NAME"
+if [ -n "$BRANCH_NAME" ]; then
+    echo "Info: Trying to cancel review for branch $BRANCH_NAME"
+
+    if [ -d "../$BRANCH_NAME" ]; then
+        cd "../$BRANCH_NAME"
+        docker compose down -v --remove-orphans
+        docker system prune -a -f
+        cd ..
+        rm -rf "$BRANCH_NAME"
+    else
+        echo "Info: Branch directory not found - review has already been cancelled."
+    fi
+else
+    echo "Error: Branch name not provided."
 fi
