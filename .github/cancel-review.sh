efcd5954822db170693a05e8c9163c863ac71bce
Tomas Ludvik <tomas.ludvik@shopsys.com>
2023-07-25 11:09:55 +0200

    fixed canceling old reviews (#2678)

--- .github/cancel-review.sh
+++ .github/cancel-review.sh
@@ -1,6 +1,8 @@
+BRANCH_NAME=$1
+
 echo "Trying to cancel review for branch $BRANCH_NAME"
 
-if [ ! -z "$BRANCH_NAME" ]; then
+if [ -d "../$BRANCH_NAME" ]; then
     cd "../$BRANCH_NAME"
     docker compose down -v --remove-orphans
     docker system prune -a -f
