99e29831947c0752c2b60818f2754d44ab8822f5
Martin Grossmann <martin@vyvoj.net>
2023-08-03 09:41:04 +0200

    project can be mirrored to multiple branches now

--- project-base/gitlab/mirror-repository/mirror.sh
+++ project-base/gitlab/mirror-repository/mirror.sh
@@ -7,7 +7,7 @@ SOURCE_URL=$2
 SOURCE_BRANCH=$3
 
 TARGET_URL=$4
-TARGET_BRANCH=$5
+TARGET_BRANCHES="$5"
 
 if [[ -d "$WORKING_DIR_NAME" ]]; then
     rm -rf "$WORKING_DIR_NAME"
@@ -16,5 +16,6 @@ fi
 git clone --bare --single-branch --branch="$SOURCE_BRANCH" "$SOURCE_URL" "$WORKING_DIR_NAME"
 cd "$WORKING_DIR_NAME"
 
-
-git push --force "$TARGET_URL" "$SOURCE_BRANCH":"$TARGET_BRANCH"
+for TARGET_BRANCH in $TARGET_BRANCHES; do
+    git push --force "$TARGET_URL" "$SOURCE_BRANCH":"$TARGET_BRANCH"
+done
