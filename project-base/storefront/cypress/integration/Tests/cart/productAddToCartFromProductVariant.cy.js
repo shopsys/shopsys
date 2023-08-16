a61e9049e0de03a51827ede624b7d5b843be30f5
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-08-16 17:06:33 +0200

    add delay to Cypress test in order to properly load

--- project-base/storefront/cypress/integration/Tests/cart/productAddToCartFromProductVariant.cy.js
+++ project-base/storefront/cypress/integration/Tests/cart/productAddToCartFromProductVariant.cy.js
@@ -1,13 +1,13 @@
 import { products, totalPrice, url } from '../../../fixtures/demodata';
 import { checkProductInCart, checkTotalPriceInCart } from '../../Functions/cart';
 import { checkProductAndGoToCartFromCartPopupWindow } from '../../Functions/cartPopupWindow';
-import { productClickFromPromotedProductsOnHomepage } from '../../Functions/homepage';
 import { addProductVariantToCartFromProductDetail } from '../../Functions/productDetail';
 
 it('Product variant - Adding variant product to cart from product detail and check product in cart', () => {
     cy.visit(products.philips32PFL4308.url);
     cy.url().should('contain', products.philips32PFL4308.url);
     addProductVariantToCartFromProductDetail(products.philips54CRT.catnum);
+    cy.wait(3000);
     checkProductAndGoToCartFromCartPopupWindow(products.philips54CRT.name);
     checkProductInCart(products.philips54CRT.catnum, products.philips54CRT.name);
     checkTotalPriceInCart(totalPrice.cart2);
