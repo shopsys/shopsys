ed000e4949b9d707082113dbbea70aabc380d62c
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-08-21 17:53:54 +0200

    FlagsTest is now multilangual

--- project-base/app/tests/FrontendApiBundle/Functional/Product/Flag/FlagsTest.php
+++ project-base/app/tests/FrontendApiBundle/Functional/Product/Flag/FlagsTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\FrontendApiBundle\Functional\Product\Flag;
 
+use Shopsys\FrameworkBundle\Component\ArrayUtils\ArraySorter;
 use Shopsys\FrameworkBundle\Component\Translation\Translator;
 use Tests\FrontendApiBundle\Test\GraphQlTestCase;
 
@@ -19,34 +20,38 @@ class FlagsTest extends GraphQlTestCase
             }
         ';
 
-        $jsonExpected = '{
-    "data": {
-        "flags": [
-            {
-                "name": "' . t('Action', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('Made in CZ', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('Made in DE', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('Made in SK', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('New', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('Price hit', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            },
-            {
-                "name": "' . t('Sale', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()) . '"
-            }
-        ]
-    }
-}';
+        $flags = [
+            [
+                'name' => t('Action', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('Made in CZ', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('Made in DE', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('Made in SK', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('New', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('Price hit', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+            [
+                'name' => t('Sale', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
+            ],
+        ];
 
-        $this->assertQueryWithExpectedJson($query, $jsonExpected);
+        ArraySorter::sortArrayAlphabeticallyByValue('name', $flags, $this->getLocaleForFirstDomain());
+
+        $arrayExpected = [
+            'data' => [
+                'flags' => $flags,
+            ],
+        ];
+
+        $this->assertQueryWithExpectedArray($query, $arrayExpected);
     }
 }
