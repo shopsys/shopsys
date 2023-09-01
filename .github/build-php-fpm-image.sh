4fd379aaf4faac2de1987b71e84dbbdaba414a48
Martin Grossmann <martin@vyvoj.net>
2023-09-01 11:56:21 +0200

    base image is now used to build application

--- .github/build-php-fpm-image.sh
+++ .github/build-php-fpm-image.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 DOCKER_PHP_FPM_REPOSITORY_TAG=$1
+TARGET_STAGE=${2:-base}
 
 docker image build \
     --build-arg project_root=project-base/app \
     --build-arg www_data_uid=$(id -u) \
     --build-arg www_data_gid=$(id -g) \
     --tag ${DOCKER_PHP_FPM_REPOSITORY_TAG} \
-    --target base \
+    --target ${TARGET_STAGE} \
     --no-cache \
     --compress \
     -f project-base/app/docker/php-fpm/Dockerfile \
