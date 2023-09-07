3c32ff2aaa8e489df81703cded98a5b1ef2e001b
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-09-07 10:00:05 +0200

    remove unused variables from mediaQueries.ts

--- project-base/storefront/helpers/mediaQueries.ts
+++ project-base/storefront/helpers/mediaQueries.ts
@@ -13,24 +13,3 @@ export const desktopFirstSizes = {
     tablet: 768,
     notLargeDesktop: 1023,
 } as const;
-
-const mobileFirst = {
-    queryXs: `(min-width: ${mobileFirstSizes.xs}px)`,
-    querySm: `(min-width: ${mobileFirstSizes.sm}px)`,
-    queryMd: `(min-width: ${mobileFirstSizes.md}px)`,
-    queryLg: `(min-width: ${mobileFirstSizes.lg}px)`,
-    queryVl: `(min-width: ${mobileFirstSizes.vl}px)`,
-    queryXl: `(min-width: ${mobileFirstSizes.xl}px)`,
-} as const;
-
-const desktopFirst = {
-    queryMobileXs: `(max-width: ${desktopFirstSizes.mobileXs}px)`,
-    queryMobile: `(max-width: ${desktopFirstSizes.mobile}px)`,
-    queryTablet: `(max-width: ${desktopFirstSizes.tablet}px)`,
-    queryNotLargeDesktop: `(max-width: ${desktopFirstSizes.notLargeDesktop}px)`,
-} as const;
-
-export const mediaQueries = {
-    ...mobileFirst,
-    ...desktopFirst,
-};
