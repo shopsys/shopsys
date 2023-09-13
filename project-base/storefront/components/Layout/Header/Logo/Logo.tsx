30cf77b4d73501b689830eb181f47b132c5f6c40
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-09-13 16:19:39 +0200

    refactor HeaderContact
    - remove useResizeWidthEffect

--- project-base/storefront/components/Layout/Header/Logo/Logo.tsx
+++ project-base/storefront/components/Layout/Header/Logo/Logo.tsx
@@ -5,10 +5,10 @@ import NextImage from 'next/image';
 
 const TEST_IDENTIFIER = 'layout-header-logo';
 
-export const Logo: FC = ({ className }) => (
+export const Logo: FC = () => (
     <>
         <LogoMetadata />
-        <ExtendedNextLink href="/" type="static" className={className}>
+        <ExtendedNextLink href="/" type="static" className="max-lg:flex-1">
             <NextImage
                 src="/images/logo.svg"
                 width={163}
