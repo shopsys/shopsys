db71976af76c09eff6f252cebcc1a99034c78de3
Martin Grossmann <martin@vyvoj.net>
2023-09-21 16:26:33 +0200

    added test for multiple image flush

---
+++ project-base/app/tests/App/Functional/Component/Image/ImageMultipleFlushTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Functional\Component\Image;
+
+use App\Component\FileUpload\FileUpload;
+use App\Component\Image\Image;
+use App\Component\Image\ImageLocator;
+use League\Flysystem\MountManager;
+use Shopsys\FrameworkBundle\Component\Image\Config\ImageConfig;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Tests\App\Test\TransactionFunctionalTestCase;
+
+class ImageMultipleFlushTest extends TransactionFunctionalTestCase
+{
+    /**
+     * @inject
+     */
+    private FileUpload $fileUpload;
+
+    /**
+     * @inject
+     */
+    private MountManager $mountManager;
+
+    /**
+     * @inject
+     */
+    private ImageLocator $imageLocator;
+
+    /**
+     * @inject
+     */
+    private ParameterBagInterface $parameterBag;
+
+    public function testImageCanBeUpdatedAfterFlush(): void
+    {
+        $testImageName = 'image.jpg';
+        $localImagePath = 'local://' . __DIR__ . '/Resources/' . $testImageName;
+        $abstractImagePath = 'main://' . $this->fileUpload->getTemporaryDirectory() . '/' . $testImageName;
+        $this->mountManager->copy($localImagePath, $abstractImagePath);
+
+        $image = new Image(
+            'product',
+            856,
+            [
+                'cs' => 'produkt',
+                'en' => 'the product',
+            ],
+            $testImageName,
+            null,
+        );
+
+        $this->em->persist($image);
+        $this->em->flush();
+
+        $image->setPosition(5);
+        $this->em->flush();
+
+        $this->assertFileExists(
+            $this->parameterBag->get('kernel.project_dir') .
+            $this->imageLocator->getAbsoluteImageFilepath($image, ImageConfig::ORIGINAL_SIZE_NAME),
+        );
+    }
+}
