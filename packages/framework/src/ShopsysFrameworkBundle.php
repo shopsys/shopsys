b8523692e852887e0501f7998550c0b0e46ac735
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-09-25 09:05:46 +0000

    ShopsysFrameworkBundle: version updated to "12.1.0-dev"

--- packages/framework/src/ShopsysFrameworkBundle.php
+++ packages/framework/src/ShopsysFrameworkBundle.php
@@ -23,7 +23,7 @@ class ShopsysFrameworkBundle extends Bundle
     /**
      * @var string
      */
-    public const VERSION = '12.0.0';
+    public const VERSION = '12.1.0-dev';
 
     /**
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
