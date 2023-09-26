08767854b7ca05c5ab274d97bd289a2662d571e6
Martin Grossmann <martin@vyvoj.net>
2023-09-26 11:48:15 +0200

    WIP

--- .github/cancel-review.sh
+++ .github/cancel-review.sh
@@ -1,10 +1,15 @@
 BRANCH_NAME=${1,,}
+BRANCH_NAME_ESCAPED="${BRANCH_NAME//./-}"
 
 if [ -n "$BRANCH_NAME" ]; then
     echo "Info: Trying to cancel review for branch $BRANCH_NAME"
 
     if [ -d "../$BRANCH_NAME" ]; then
         cd "../$BRANCH_NAME"
+
+        docker compose exec php-fpm php phing elasticsearch-index-delete clean-redis
+        docker compose exec php-fpm php ./bin/console doctrine:database:drop --force
+
         docker compose down -v --remove-orphans
         docker system prune -a -f
         cd ..
@@ -12,6 +17,13 @@ if [ -n "$BRANCH_NAME" ]; then
     else
         echo "Info: Branch directory not found - review has already been cancelled."
     fi
+
+    echo "Cleaning up Elasticsearch"
+    docker exec 6938e9798f9f curl -X DELETE "http://localhost:9200/${BRANCH_NAME_ESCAPED}_*"
+
+    echo "Cleaning up Redis"
+    docker exec 9468e1494b30 redis-cli keys "${BRANCH_NAME_ESCAPED}:*" | xargs -L1 redis-cli del
+
 else
     echo "Error: Branch name not provided."
 fi
