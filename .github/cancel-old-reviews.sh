963f18871bc21921c03f2c8934d2b39da56d3423
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2023-10-04 15:35:32 +0200

    fixed canceling of old reviews
    - filtering of branches has not been working

--- .github/cancel-old-reviews.sh
+++ .github/cancel-old-reviews.sh
@@ -15,14 +15,26 @@ api_url_last_seven_days="https://api.github.com/repos/shopsys/shopsys/actions/ru
 response_last_seven_days=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $ACTIONS_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$api_url_last_seven_days")
 branches_last_seven_days=$(echo "$response_last_seven_days" | jq -r '.workflow_runs[].head_branch' | sort -u)
 
+# convert strings to arrays
+SAVEIFS=$IFS
+IFS=$'\n'
+
+branches_last_seven_days=($branches_last_seven_days)
+branches_between_fourteen_and_seven_days_ago=($branches_between_fourteen_and_seven_days_ago)
+
+IFS=$SAVEIFS
+
+# add main branch to branches between 14 and 7 days ago in order to prevent its cancellation
+branches_between_fourteen_and_seven_days_ago+=($MAIN_BRANCH_NAME)
+
 filtered_branches=()
-for branch in "${branches_between_fourteen_and_seven_days_ago[@]}"; do
-    if [[ ! " ${branches_last_seven_days[@]} " =~ " ${branch} " ]]; then
-        filtered_branches+=("$branch")
+
+# check if branch from 14-7 days ago has not been built in the last 7 days
+for (( i=0; i<${#branches_between_fourteen_and_seven_days_ago[@]}; i++ )) do
+    if [[ ! " ${branches_last_seven_days[*]} " =~ " ${branches_between_fourteen_and_seven_days_ago[$i]} " ]]; then
+        filtered_branches+=("${branches_between_fourteen_and_seven_days_ago[$i]}")
     fi
 done
-
-filtered_branches=($(echo "${filtered_branches[@]}" | tr ' ' '\n' | grep -v "${MAIN_BRANCH_NAME}"))
 
 for BRANCH_NAME in "${filtered_branches[@]}"; do
     /bin/bash ./.github/cancel-review.sh "$BRANCH_NAME"
