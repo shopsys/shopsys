84ee10070c485baa53e13ce4cbef4c94d405493d
Martin Grossmann <martin@vyvoj.net>
2023-10-06 13:15:38 +0200

    [github-actions] improved removing the branches from packages (#2862)

--- .github/monorepo/remove_branch.sh
+++ .github/monorepo/remove_branch.sh
@@ -17,21 +17,13 @@ assert_split_branch_is_not_protected
 
 echo -e "${BLUE}Removing branch '$SPLIT_BRANCH'...${NC}"
 
-EXIT_STATUS=0
 for PACKAGE in $(get_all_packages); do
-
     if git push --delete "${REMOTE_TEMPLATE}${PACKAGE}.git" ${SPLIT_BRANCH}; then
-        echo -e "${BLUE}Branch ${GREEN}\"${SPLIT_BRANCH}\" ${BLUE}was removed from the package ${GREEN}\"${PACKAGE}\"${NC}"
+        echo -e "${GREEN}Branch \"${SPLIT_BRANCH}\" was removed from the package ${GREEN}\"${PACKAGE}\"${NC}"
     else
-        echo -e "${RED}Branch \"${SPLIT_BRANCH}\" could not be removed from the package \"${PACKAGE}\"!${NC}"
-        EXIT_STATUS=1
+        echo -e "${BLUE}Branch \"${SPLIT_BRANCH}\" does not exist on the package \"${PACKAGE}\"!${NC}"
+
     fi
 done
 
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    echo -e "${GREEN}Branches from all repositories were removed!${NC}"
-else
-    echo -e "${RED}Some branches were not removed from their remotes due to an error${NC}"
-fi
-
-exit $EXIT_STATUS
+echo -e "${GREEN}Branches from all repositories are removed!${NC}"
