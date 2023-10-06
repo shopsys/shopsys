9e6085112d0911f63c33b19fa5887ac2c3208ef1
Martin Grossmann <martin@vyvoj.net>
2023-10-06 08:37:56 +0200

    updated info in merge RC branch step

--- utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/Release/MergeReleaseCandidateBranchReleaseWorker.php
@@ -47,18 +47,10 @@ final class MergeReleaseCandidateBranchReleaseWorker extends AbstractShopsysRele
             ),
         );
 
-        if ($this->currentBranchName === AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME) {
-            $this->symfonyStyle->note(
-                'Rest assured, after the master branch is built on Heimdall, it is split automatically (using http://heimdall:8080/view/Tools/job/tool-monorepo-split/)',
-            );
-        } else {
-            $this->symfonyStyle->note(
-                sprintf(
-                    'You need split the "%s" branch it using "tool-monorepo-force-split-branch" on Heimdall (http://heimdall:8080/view/Tools/job/tool-monorepo-force-split-branch/)',
-                    $initialBranchName,
-                ),
-            );
-        }
+        $this->symfonyStyle->note(
+            'Rest assured, the branch is split automatically (thanks to https://github.com/shopsys/shopsys/actions/workflows/monorepo-split.yaml)',
+        );
+
         $this->confirm('Confirm the branch is split.');
     }
 
