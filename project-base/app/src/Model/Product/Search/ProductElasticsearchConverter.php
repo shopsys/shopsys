3dc74b1bf94a1877ca1eca1deab49677e8d90284
Martin Grossmann <martin@vyvoj.net>
2023-10-15 15:49:17 +0200

    parameters are now autofilled in product elasticsearch converter

--- project-base/app/src/Model/Product/Search/ProductElasticsearchConverter.php
+++ project-base/app/src/Model/Product/Search/ProductElasticsearchConverter.php
@@ -39,7 +39,7 @@ class ProductElasticsearchConverter extends BaseProductElasticsearchConverter
         $results = [];
 
         foreach ($parameters as $parameter) {
-            $filledParameter = [];
+            $filledParameter = $parameter;
             $filledParameter['parameter_id'] = $parameter['parameter_id'] ?? '';
             $filledParameter['parameter_uuid'] = $parameter['parameter_uuid'] ?? '';
             $filledParameter['parameter_name'] = $parameter['parameter_name'] ?? '';
