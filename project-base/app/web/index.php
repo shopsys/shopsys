92f667b8afbb7071f5ec1020930a9e2d259204ec
Martin Grossmann <martin@vyvoj.net>
2023-11-10 16:41:37 +0100

    [shopsys] replaced custom application bootstraping with symfony/runtime (#2914)

--- project-base/app/web/index.php
+++ project-base/app/web/index.php
@@ -3,32 +3,17 @@
 use App\Environment;
 use App\Kernel;
 use Shopsys\FrameworkBundle\Component\Environment\EnvironmentType;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__) . '/app/autoload.php';
+require_once dirname(__DIR__) . '/app/autoload.php';
 
-$_SERVER['APP_ENV'] = Environment::getEnvironment(false);
+$_SERVER['APP_ENV'] = Environment::getEnvironment();
 $_SERVER['APP_DEBUG'] = EnvironmentType::isDebug($_SERVER['APP_ENV']);
 
-require dirname(__DIR__).'/config/bootstrap.php';
+setlocale(LC_CTYPE, 'en_US.utf8');
+setlocale(LC_NUMERIC, 'en_US.utf8');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+return static function (array $context) {
+    ini_set('display_errors', 0);
 
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
