70bba069637c6481973037ce1fed2cf12ae680d9
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-01-20 16:10:37 +0100

    closed days and store opening hours are persisted and calculated in UTC

--- packages/framework/src/Component/DateTimeHelper/DateTimeHelper.php
+++ packages/framework/src/Component/DateTimeHelper/DateTimeHelper.php
@@ -7,6 +7,7 @@ namespace Shopsys\FrameworkBundle\Component\DateTimeHelper;
 use DateTime;
 use DateTimeImmutable;
 use DateTimeZone;
+use InvalidArgumentException;
 use Shopsys\FrameworkBundle\Component\DateTimeHelper\Exception\CannotParseDateTimeException;
 use Shopsys\FrameworkBundle\Component\Localization\DisplayTimeZoneProviderInterface;
 
@@ -50,5 +51,68 @@ class DateTimeHelper
         $time->modify('-' . ($time->format('i') % $intervalInMinutes) . ' min');
 
         return DateTimeImmutable::createFromMutable($time);
+    }
+
+    /**
+     * @param string $dateTime
+     * @param \DateTimeZone $inputTimeZone
+     * @return \DateTimeImmutable
+     */
+    public static function convertDateTimeFromTimezoneToUtc(
+        string $dateTime,
+        DateTimeZone $inputTimeZone,
+    ): DateTimeImmutable {
+        $utcTimeZone = new DateTimeZone('UTC');
+
+        return (new DateTimeImmutable($dateTime, $inputTimeZone))->setTimezone($utcTimeZone);
+    }
+
+    /**
+     * @param int $dayNumber
+     * @param \DateTimeZone $inputTimeZone
+     * @return \DateTimeImmutable
+     */
+    public static function getUtcDateForDayInCurrentWeek(
+        int $dayNumber,
+        DateTimeZone $inputTimeZone,
+    ): DateTimeImmutable {
+        $dayName = match ($dayNumber) {
+            1 => 'monday',
+            2 => 'tuesday',
+            3 => 'wednesday',
+            4 => 'thursday',
+            5 => 'friday',
+            6 => 'saturday',
+            7 => 'sunday',
+            default => throw new InvalidArgumentException(sprintf('Day number "%s" is not valid. (expected a value in range 1-7)', $dayNumber)),
+        };
+
+        return self::convertDateTimeFromTimezoneToUtc('this week ' . $dayName, $inputTimeZone);
+    }
+
+    /**
+     * @param string $hoursAndMinutes
+     * @return \DateTimeImmutable
+     */
+    public static function createDateTimeFromTime(string $hoursAndMinutes): DateTimeImmutable
+    {
+        return new DateTimeImmutable(sprintf('1970-01-01 %s:00', $hoursAndMinutes));
+    }
+
+    /**
+     * @param string|null $hoursAndMinutes
+     * @param int $domainId
+     * @return string|null
+     */
+    public function convertHoursAndMinutesFromUtcToDisplayTimezone(?string $hoursAndMinutes, int $domainId): ?string
+    {
+        if ($hoursAndMinutes === null) {
+            return null;
+        }
+
+        $displayTimeZone = $this->displayTimeZoneProvider->getDisplayTimeZoneByDomainId($domainId);
+        $dateTime = self::createDateTimeFromTime($hoursAndMinutes);
+
+        return $dateTime->setTimezone($displayTimeZone)->format('H:i');
     }
 }
--- project-base/app/src/DataFixtures/Demo/ClosedDayDataFixture.php
+++ project-base/app/src/DataFixtures/Demo/ClosedDayDataFixture.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\DataFixtures\Demo;
 
-use DateTimeImmutable;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Shopsys\FrameworkBundle\Component\DataFixture\AbstractReferenceFixture;
+use Shopsys\FrameworkBundle\Component\DateTimeHelper\DateTimeHelper;
+use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Component\Localization\DisplayTimeZoneProviderInterface;
 use Shopsys\FrameworkBundle\Component\Translation\Translator;
 use Shopsys\FrameworkBundle\Model\Store\ClosedDay\ClosedDayDataFactory;
 use Shopsys\FrameworkBundle\Model\Store\ClosedDay\ClosedDayFacade;
@@ -21,12 +23,14 @@ class ClosedDayDataFixture extends AbstractReferenceFixture implements Dependent
      * @param \Shopsys\FrameworkBundle\Model\Store\StoreFacade $storeFacade
      * @param \Shopsys\FrameworkBundle\Model\Store\ClosedDay\ClosedDayFacade $closedDayFacade
      * @param \Shopsys\FrameworkBundle\Model\Store\ClosedDay\ClosedDayDataFactory $closedDayDataFactory
+     * @param \Shopsys\FrameworkBundle\Component\Localization\DisplayTimeZoneProvider $displayTimeZoneProvider
      */
     public function __construct(
         private readonly Domain $domain,
         private readonly StoreFacade $storeFacade,
         private readonly ClosedDayFacade $closedDayFacade,
         private readonly ClosedDayDataFactory $closedDayDataFactory,
+        private readonly DisplayTimeZoneProviderInterface $displayTimeZoneProvider,
     ) {
     }
 
@@ -37,10 +41,9 @@ class ClosedDayDataFixture extends AbstractReferenceFixture implements Dependent
     {
         foreach ($this->domain->getAll() as $domainConfig) {
             $domainId = $domainConfig->getId();
-            $locale = $domainConfig->getLocale();
             $store = $this->storeFacade->getStoresListEnabledOnDomain($domainId)[0];
 
-            foreach ($this->getClosedDays($locale) as [$date, $name]) {
+            foreach ($this->getClosedDays($domainConfig) as [$date, $name]) {
                 $closedDayData = $this->closedDayDataFactory->create();
                 $closedDayData->domainId = $domainId;
                 $closedDayData->excludedStores = [$store];
@@ -52,23 +55,26 @@ class ClosedDayDataFixture extends AbstractReferenceFixture implements Dependent
     }
 
     /**
-     * @param string $locale
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig $domainConfig
      * @return iterable<array{\DateTimeImmutable, string}>
      */
-    private function getClosedDays(string $locale): iterable
+    private function getClosedDays(DomainConfig $domainConfig): iterable
     {
+        $locale = $domainConfig->getLocale();
+        $domainTimeZone = $this->displayTimeZoneProvider->getDisplayTimeZoneByDomainId($domainConfig->getId());
+
         yield [
-            new DateTimeImmutable('24.12.' . date('Y')),
+            DateTimeHelper::convertDateTimeFromTimezoneToUtc('24.12.' . date('Y'), $domainTimeZone),
             t('Christmas Eve', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale),
         ];
 
         yield [
-            new DateTimeImmutable('25.12.' . date('Y')),
+            DateTimeHelper::convertDateTimeFromTimezoneToUtc('25.12.' . date('Y'), $domainTimeZone),
             t('Christmas Day', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale),
         ];
 
         yield [
-            new DateTimeImmutable('26.12.' . date('Y')),
+            DateTimeHelper::convertDateTimeFromTimezoneToUtc('26.12.' . date('Y'), $domainTimeZone),
             t(' Second Christmas Day', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale),
         ];
     }
