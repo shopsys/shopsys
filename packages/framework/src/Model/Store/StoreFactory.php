f1f42fe44cb22276f46059374bac902677cfdf2e
Martin Grossmann <martin@vyvoj.net>
2024-01-27 00:50:38 +0100

    StoreFactory now uses entity name resolver

--- packages/framework/src/Model/Store/StoreFactory.php
+++ packages/framework/src/Model/Store/StoreFactory.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Model\Store;
 
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+
 class StoreFactory
 {
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     */
+    public function __construct(
+        protected readonly EntityNameResolver $entityNameResolver,
+    ) {
+    }
+
     /**
      * @param \Shopsys\FrameworkBundle\Model\Store\StoreData $storeData
      * @return \Shopsys\FrameworkBundle\Model\Store\Store
      */
     public function create(StoreData $storeData): Store
     {
-        return new Store($storeData);
+        $classData = $this->entityNameResolver->resolve(Store::class);
+
+        return new $classData($storeData);
     }
 }
