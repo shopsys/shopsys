2f58cc65ddbad60830eccfca837537b63a06947c
Martin Grossmann <martin@vyvoj.net>
2024-01-27 01:01:43 +0100

    stock entity is now created with factory

---
+++ packages/framework/src/Model/Stock/StockFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Stock;
+
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+
+class StockFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     */
+    public function __construct(
+        protected readonly EntityNameResolver $entityNameResolver,
+    ) {
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Stock\StockData $stockData
+     * @return \Shopsys\FrameworkBundle\Model\Stock\Stock
+     */
+    public function create(StockData $stockData): Stock
+    {
+        $classData = $this->entityNameResolver->resolve(Stock::class);
+
+        return new $classData($stockData);
+    }
+}
