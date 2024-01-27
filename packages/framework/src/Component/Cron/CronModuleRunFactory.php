2c8846e2de67ab0a0cfd4b7d146a3c062fc70253
Martin Grossmann <martin@vyvoj.net>
2024-01-27 01:18:10 +0100

    cron module run entity is now created with entity name resolver in its factory

--- packages/framework/src/Component/Cron/CronModuleRunFactory.php
+++ packages/framework/src/Component/Cron/CronModuleRunFactory.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Component\Cron;
 
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+
 class CronModuleRunFactory
 {
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     */
+    public function __construct(
+        protected readonly EntityNameResolver $entityNameResolver,
+    ) {
+    }
+
     /**
      * @param \Shopsys\FrameworkBundle\Component\Cron\CronModule $cronModule
      * @return \Shopsys\FrameworkBundle\Component\Cron\CronModuleRun
      */
     public function createFromFinishedCronModule(CronModule $cronModule): CronModuleRun
     {
-        return new CronModuleRun(
+        $classData = $this->entityNameResolver->resolve(CronModuleRun::class);
+
+        return new $classData(
             $cronModule,
             $cronModule->getStatus(),
             $cronModule->getLastStartedAt(),
