9d92ca72beca268f3fce67f51849e9756043ff70
Martin Grossmann <martin@vyvoj.net>
2024-02-15 14:22:25 +0100

    fixup! added unique constraint to cart

--- packages/framework/src/Migrations/Version20240209114704.php
+++ packages/framework/src/Migrations/Version20240209114704.php
@@ -15,7 +15,7 @@ class Version20240209114704 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->sql('CREATE UNIQUE INDEX cart_identifier ON carts (cart_identifier) WHERE cart_identifier <> \'\';');
-        $this->sql('CREATE UNIQUE INDEX customer_user_id ON carts (customer_user_id) WHERE customer_user_id is not null;');
+        $this->sql('CREATE UNIQUE INDEX customer_user_id ON carts (customer_user_id);');
     }
 
     /**
