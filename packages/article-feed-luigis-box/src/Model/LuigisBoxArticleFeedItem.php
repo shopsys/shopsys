a6bde3d1a8e1e189b9543d460dc20c36e3230941
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-02-21 12:50:36 +0100

    added images to Luigi's Box article feed

--- packages/article-feed-luigis-box/src/Model/LuigisBoxArticleFeedItem.php
+++ packages/article-feed-luigis-box/src/Model/LuigisBoxArticleFeedItem.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Shopsys\ArticleFeed\LuigisBoxBundle\Model;
 
+use Shopsys\FrameworkBundle\Component\Image\ImageUrlWithSizeHelper;
 use Shopsys\FrameworkBundle\Model\Feed\FeedItemInterface;
 
 class LuigisBoxArticleFeedItem implements FeedItemInterface
 {
     public const UNIQUE_BLOG_ARTICLE_IDENTIFIER_PREFIX = 'blog_article-';
     public const UNIQUE_ARTICLE_IDENTIFIER_PREFIX = 'article-';
+    protected const SMALL_IMAGE_SIZE = 100;
+    protected const MEDIUM_IMAGE_SIZE = 200;
+    protected const LARGE_IMAGE_SIZE = 600;
 
     /**
      * @param int $id
@@ -18,6 +22,7 @@ class LuigisBoxArticleFeedItem implements FeedItemInterface
      * @param string $link
      * @param string|null $description
      * @param string|null $perex
+     * @param string|null $imageUrl
      */
     public function __construct(
         protected readonly int $id,
@@ -26,6 +31,7 @@ class LuigisBoxArticleFeedItem implements FeedItemInterface
         protected readonly string $link,
         protected readonly ?string $description,
         protected readonly ?string $perex,
+        protected readonly ?string $imageUrl = null,
     ) {
     }
 
@@ -75,5 +81,41 @@ class LuigisBoxArticleFeedItem implements FeedItemInterface
     public function getAnnotation(): ?string
     {
         return $this->perex;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageLinkS(): ?string
+    {
+        if ($this->imageUrl === null) {
+            return null;
+        }
+
+        return ImageUrlWithSizeHelper::limitSizeInImageUrl($this->imageUrl, static::SMALL_IMAGE_SIZE, static::SMALL_IMAGE_SIZE);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageLinkM(): ?string
+    {
+        if ($this->imageUrl === null) {
+            return null;
+        }
+
+        return ImageUrlWithSizeHelper::limitSizeInImageUrl($this->imageUrl, static::MEDIUM_IMAGE_SIZE, static::MEDIUM_IMAGE_SIZE);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getImageLinkL(): ?string
+    {
+        if ($this->imageUrl === null) {
+            return null;
+        }
+
+        return ImageUrlWithSizeHelper::limitSizeInImageUrl($this->imageUrl, static::LARGE_IMAGE_SIZE, static::LARGE_IMAGE_SIZE);
     }
 }
