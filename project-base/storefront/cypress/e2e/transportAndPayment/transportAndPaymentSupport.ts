240f225af9f5f1dfc4c8d3797c5da3797335f2f3
Sebastian Holesz <sebastianholesz@seznam.cz>
2024-02-22 11:26:49 +0100

    fixed transport API response modification in cypress after rebase
    - this was needed because of how opening hours have
    changed while refactoring timezone on BE

--- project-base/storefront/cypress/e2e/transportAndPayment/transportAndPaymentSupport.ts
+++ project-base/storefront/cypress/e2e/transportAndPayment/transportAndPaymentSupport.ts
@@ -24,9 +24,6 @@ export const changeSelectionOfPaymentByName = (paymentName: string) => {
     cy.getByTID([TIDs.pages_order_payment, TIDs.pages_order_selectitem_label_name]).contains(paymentName).click('left');
 };
 
-export type Transport = {
-    stores: { edges: ({ node: { openingHours: { dayOfWeek: number } } | null } | null)[] | null } | null;
-};
 export const changeDayOfWeekInApiResponses = (dayOfWeek: number) => {
     cy.intercept('POST', '/graphql/', (req) => {
         req.reply((response) => {
@@ -40,10 +37,12 @@ const tryChangingDayOfWeekInTransportsApiResponse = (
     response: CyHttpMessages.IncomingHttpResponse,
     dayOfWeek: number,
 ) => {
-    response?.body?.data?.transports?.forEach((transport: Transport) => {
+    response?.body?.data?.transports?.forEach((transport: TransportWithAvailablePaymentsAndStoresFragmentApi) => {
         transport?.stores?.edges?.forEach((edge) => {
             if (edge?.node?.openingHours) {
+                edge.node.openingHours.isOpen = true;
                 edge.node.openingHours.dayOfWeek = dayOfWeek;
+                edge.node.openingHours.openingHoursOfDays = getStaticOpeningHoursOfDays();
             }
         });
     });
@@ -53,9 +52,125 @@ const tryChangingDayOfWeekInChangeTransportMutationApiResponse = (
     response: CyHttpMessages.IncomingHttpResponse,
     dayOfWeek: number,
 ) => {
-    (response?.body?.data?.ChangeTransportInCart?.transport as Transport)?.stores?.edges?.forEach((edge) => {
+    (
+        response?.body?.data?.ChangeTransportInCart?.transport as TransportWithAvailablePaymentsAndStoresFragmentApi
+    )?.stores?.edges?.forEach((edge) => {
         if (edge?.node?.openingHours) {
+            edge.node.openingHours.isOpen = true;
             edge.node.openingHours.dayOfWeek = dayOfWeek;
+            edge.node.openingHours.openingHoursOfDays = getStaticOpeningHoursOfDays();
         }
     });
 };
+
+const getStaticOpeningHoursOfDays = (): OpeningHoursOfDayApi[] => [
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-19T00:00:00+01:00',
+        dayOfWeek: 1,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '06:00',
+                closingTime: '11:00',
+            },
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '13:00',
+                closingTime: '15:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-20T00:00:00+01:00',
+        dayOfWeek: 2,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '07:00',
+                closingTime: '11:00',
+            },
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '13:00',
+                closingTime: '15:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-21T00:00:00+01:00',
+        dayOfWeek: 3,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '08:00',
+                closingTime: '11:00',
+            },
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '13:00',
+                closingTime: '15:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-22T00:00:00+01:00',
+        dayOfWeek: 4,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '09:00',
+                closingTime: '11:00',
+            },
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '13:00',
+                closingTime: '15:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-23T00:00:00+01:00',
+        dayOfWeek: 5,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '10:00',
+                closingTime: '11:00',
+            },
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '13:00',
+                closingTime: '15:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-24T00:00:00+01:00',
+        dayOfWeek: 6,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '08:00',
+                closingTime: '11:00',
+            },
+        ],
+    },
+    {
+        __typename: 'OpeningHoursOfDay',
+        date: '2024-02-25T00:00:00+01:00',
+        dayOfWeek: 7,
+        openingHoursRanges: [
+            {
+                __typename: 'OpeningHoursRange',
+                openingTime: '09:00',
+                closingTime: '11:00',
+            },
+        ],
+    },
+];
