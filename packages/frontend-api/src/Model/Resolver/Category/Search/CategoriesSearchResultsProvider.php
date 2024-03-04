061c8a983894717cb40c8b1ec8c6b113d6ad84ae
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-03-04 13:38:22 +0100

    CategoriesSearchQuery now uses SearchResultsProvider system

--- packages/frontend-api/src/Model/Resolver/Category/Search/CategoriesSearchResultsProvider.php
+++ packages/frontend-api/src/Model/Resolver/Category/Search/CategoriesSearchResultsProvider.php
@@ -2,36 +2,34 @@
 
 declare(strict_types=1);
 
-namespace Shopsys\FrontendApiBundle\Model\Resolver\Category;
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Category\Search;
 
+use GraphQL\Executor\Promise\Promise;
 use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface;
 use Overblog\GraphQLBundle\Relay\Connection\Paginator;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
-use Shopsys\FrontendApiBundle\Component\Validation\PageSizeValidator;
 use Shopsys\FrontendApiBundle\Model\Category\CategoryFacade;
-use Shopsys\FrontendApiBundle\Model\Resolver\AbstractQuery;
 
-class CategoriesSearchQuery extends AbstractQuery
+class CategoriesSearchResultsProvider implements CategoriesSearchResultsProviderInterface
 {
     /**
-     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\FrontendApiBundle\Model\Category\CategoryFacade $categoryFacade
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      */
     public function __construct(
-        protected readonly Domain $domain,
         protected readonly CategoryFacade $categoryFacade,
+        protected readonly Domain $domain,
     ) {
     }
 
     /**
      * @param \Overblog\GraphQLBundle\Definition\Argument $argument
-     * @return \Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface|object
+     * @return \GraphQL\Executor\Promise\Promise|\Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface
      */
-    public function categoriesSearchQuery(Argument $argument)
-    {
-        PageSizeValidator::checkMaxPageSize($argument);
-        $this->setDefaultFirstOffsetIfNecessary($argument);
-
+    public function getCategoriesSearchResults(
+        Argument $argument,
+    ): Promise|ConnectionInterface {
         $searchText = $argument['search'] ?? '';
 
         $paginator = new Paginator(function ($offset, $limit) use ($searchText) {
@@ -52,5 +50,14 @@ class CategoriesSearchQuery extends AbstractQuery
                 $this->domain->getId(),
             ),
         );
+    }
+
+    /**
+     * @param int $domainId
+     * @return bool
+     */
+    public function isEnabledOnDomain(int $domainId): bool
+    {
+        return true;
     }
 }
