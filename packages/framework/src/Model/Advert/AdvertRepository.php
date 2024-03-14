aff698a83a5951f930c1be0da71a028e1b0ca379
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-03-14 13:47:15 +0100

    [shopsys] adverts restricted by their display dates are now correctly displayed at specified dates

--- packages/framework/src/Model/Advert/AdvertRepository.php
+++ packages/framework/src/Model/Advert/AdvertRepository.php
@@ -6,8 +6,11 @@ namespace Shopsys\FrameworkBundle\Model\Advert;
 
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use LogicException;
 use Shopsys\FrameworkBundle\Model\Advert\Exception\AdvertNotFoundException;
+use Shopsys\FrameworkBundle\Model\Category\Category;
 
 class AdvertRepository
 {
@@ -21,16 +24,16 @@ class AdvertRepository
     /**
      * @return \Doctrine\ORM\EntityRepository
      */
-    protected function getAdvertRepository()
+    protected function getAdvertRepository(): EntityRepository
     {
         return $this->em->getRepository(Advert::class);
     }
 
     /**
-     * @param string $advertId
+     * @param int $advertId
      * @return \Shopsys\FrameworkBundle\Model\Advert\Advert|null
      */
-    public function findById($advertId)
+    public function findById(int $advertId): ?Advert
     {
         return $this->getAdvertRepository()->find($advertId);
     }
@@ -41,27 +44,18 @@ class AdvertRepository
      * @param \Shopsys\FrameworkBundle\Model\Category\Category|null $category
      * @return \Doctrine\ORM\QueryBuilder
      */
-    protected function getAdvertByPositionQueryBuilder($positionName, $domainId, $category = null)
-    {
-        if (AdvertPositionRegistry::isCategoryPosition($positionName) && $category === null) {
+    public function getVisibleAdvertByPositionQueryBuilder(
+        string $positionName,
+        int $domainId,
+        ? Category $category = null,
+    ): QueryBuilder {
+        if ($category === null && AdvertPositionRegistry::isCategoryPosition($positionName)) {
             throw new LogicException('Cannot retrieve advert on product list page without setting category.');
         }
 
-        $dateToday = (new DateTimeImmutable())->format('Y-m-d 00:00:00');
-
-        $queryBuilder = $this->em->createQueryBuilder()
-            ->select('a')
-            ->from(Advert::class, 'a')
-            ->where('a.positionName = :positionName')
-            ->andWhere('a.hidden = FALSE')
-            ->andWhere('a.domainId = :domainId')
-            ->andWhere('a.datetimeVisibleFrom is NULL or a.datetimeVisibleFrom <= :now')
-            ->andWhere('a.datetimeVisibleTo is NULL or a.datetimeVisibleTo >= :now')
-            ->setParameters([
-                'domainId' => $domainId,
-                'positionName' => $positionName,
-                'now' => $dateToday,
-            ]);
+        $queryBuilder = $this->getVisibleAdvertsQueryBuilder($domainId)
+            ->andWhere('a.positionName = :positionName')
+            ->setParameter('positionName', $positionName);
 
         if ($category !== null) {
             $queryBuilder
@@ -74,14 +68,36 @@ class AdvertRepository
     }
 
     /**
+     * @param int $domainId
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function getVisibleAdvertsQueryBuilder(int $domainId): QueryBuilder
+    {
+        $dateToday = (new DateTimeImmutable())->format('Y-m-d 00:00:00');
+
+        return $this->em->createQueryBuilder()
+            ->select('a')
+            ->from(Advert::class, 'a')
+            ->where('a.hidden = FALSE')
+            ->andWhere('a.datetimeVisibleFrom is NULL or a.datetimeVisibleFrom <= :now')
+            ->andWhere('a.datetimeVisibleTo is NULL or a.datetimeVisibleTo >= :now')
+            ->andWhere('a.domainId = :domainId')
+            ->setParameter('domainId', $domainId)
+            ->setParameter('now', $dateToday);
+    }
+
+    /**
      * @param string $positionName
      * @param int $domainId
      * @param \Shopsys\FrameworkBundle\Model\Category\Category|null $category
      * @return \Shopsys\FrameworkBundle\Model\Advert\Advert|null
      */
-    public function findRandomAdvertByPosition($positionName, $domainId, $category = null)
-    {
-        $count = $this->getAdvertByPositionQueryBuilder($positionName, $domainId, $category)
+    public function findRandomAdvertByPosition(
+        string $positionName,
+        int $domainId,
+        ?Category $category = null,
+    ): ?Advert {
+        $count = $this->getVisibleAdvertByPositionQueryBuilder($positionName, $domainId, $category)
             ->select('COUNT(a)')
             ->getQuery()->getSingleScalarResult();
 
@@ -90,7 +106,7 @@ class AdvertRepository
             return null;
         }
 
-        return $this->getAdvertByPositionQueryBuilder($positionName, $domainId, $category)
+        return $this->getVisibleAdvertByPositionQueryBuilder($positionName, $domainId, $category)
             ->setFirstResult(random_int(0, $count - 1))
             ->setMaxResults(1)
             ->getQuery()->getSingleResult();
@@ -100,7 +116,7 @@ class AdvertRepository
      * @param int $advertId
      * @return \Shopsys\FrameworkBundle\Model\Advert\Advert
      */
-    public function getById($advertId)
+    public function getById(int $advertId): Advert
     {
         $advert = $this->getAdvertRepository()->find($advertId);
 
