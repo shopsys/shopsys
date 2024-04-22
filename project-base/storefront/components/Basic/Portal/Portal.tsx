f02ce1a8edf1b8cca844f696833cd27f143c82f7
Sebastian Holesz <sebastianholesz@seznam.cz>
2024-04-22 21:19:34 +0200

    UI improvements to popup, overlay and portal components
    - popup and overlay now stop propagation of mouse events, which
    was necessary, because some scroll and drag events were propagated
    to the content behind the popup, which worsened the UX
    - portal component now removes any previous content before
    mounting new one, which is helpful if a popup is opened while
    another one is already open and visible
    - when overlay is active, we set overflow on the body element to
    hidden so that it is not possible to scroll the website while an
    overlay is active

--- project-base/storefront/components/Basic/Portal/Portal.tsx
+++ project-base/storefront/components/Basic/Portal/Portal.tsx
@@ -3,9 +3,15 @@ import { createPortal } from 'react-dom';
 
 export const Portal: FC = ({ children }) => {
     const portalElementRef = useRef(document.getElementById('portal'));
+    const hasMounted = useRef(false);
 
     if (portalElementRef.current === null) {
         return null;
+    }
+
+    if (!hasMounted.current) {
+        portalElementRef.current.innerHTML = '';
+        hasMounted.current = true;
     }
 
     return createPortal(children, portalElementRef.current);
