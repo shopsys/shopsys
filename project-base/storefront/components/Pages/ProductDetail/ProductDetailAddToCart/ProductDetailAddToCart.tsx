06f6c6b425b42914ae9600c0263b006e529fb0ee
Sebastian Holesz <sebastianholesz@seznam.cz>
2024-05-02 14:54:20 +0200

    removed duplicated price display on product detail page

--- project-base/storefront/components/Pages/ProductDetail/ProductDetailAddToCart/ProductDetailAddToCart.tsx
+++ project-base/storefront/components/Pages/ProductDetail/ProductDetailAddToCart/ProductDetailAddToCart.tsx
@@ -11,7 +11,6 @@ import dynamic from 'next/dynamic';
 import { useRef } from 'react';
 import { useSessionStore } from 'store/useSessionStore';
 import { useAddToCart } from 'utils/cart/useAddToCart';
-import { useFormatPrice } from 'utils/formatting/useFormatPrice';
 
 export type ProductDetailAddToCartProps = {
     product: TypeProductDetailFragment;
@@ -30,7 +29,6 @@ export const ProductDetailAddToCart: FC<ProductDetailAddToCartProps> = ({ produc
         GtmProductListNameType.product_detail,
     );
     const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
-    const formatPrice = useFormatPrice();
 
     const onAddToCartHandler = async () => {
         if (!spinboxRef.current) {
@@ -50,38 +48,35 @@ export const ProductDetailAddToCart: FC<ProductDetailAddToCartProps> = ({ produc
         }
     };
 
-    return (
-        <div className="flex flex-col gap-4 rounded bg-blueLight p-3">
-            <div className="text-2xl font-bold text-primary">{formatPrice(product.price.priceWithVat)}</div>
-            {product.isSellingDenied ? (
-                <p>{t('This item can no longer be purchased')}</p>
-            ) : (
-                <div className="text-sm vl:text-base">
-                    <div className="flex items-center justify-between">
-                        <Spinbox
-                            defaultValue={1}
-                            id={product.uuid}
-                            max={product.stockQuantity}
-                            min={1}
-                            ref={spinboxRef}
-                            step={1}
-                        />
-                        <div className="ml-2 flex-1">
-                            <Button
-                                className="w-full"
-                                isDisabled={fetching}
-                                tid={TIDs.pages_productdetail_addtocart_button}
-                                variant="primary"
-                                onClick={onAddToCartHandler}
-                            >
-                                {fetching ? <Loader className="w-[18px]" /> : <CartIcon className="w-[18px]" />}
+    if (product.isSellingDenied) {
+        return <p>{t('This item can no longer be purchased')}</p>;
+    }
 
-                                {t('Add to cart')}
-                            </Button>
-                        </div>
-                    </div>
+    return (
+        <div className="text-sm vl:text-base">
+            <div className="flex items-center justify-between">
+                <Spinbox
+                    defaultValue={1}
+                    id={product.uuid}
+                    max={product.stockQuantity}
+                    min={1}
+                    ref={spinboxRef}
+                    step={1}
+                />
+                <div className="ml-2 flex-1">
+                    <Button
+                        className="w-full"
+                        isDisabled={fetching}
+                        tid={TIDs.pages_productdetail_addtocart_button}
+                        variant="primary"
+                        onClick={onAddToCartHandler}
+                    >
+                        {fetching ? <Loader className="w-[18px]" /> : <CartIcon className="w-[18px]" />}
+
+                        {t('Add to cart')}
+                    </Button>
                 </div>
-            )}
+            </div>
         </div>
     );
 };
