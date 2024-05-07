4be047405926ac664469002c6c6fdd0a58bbbc55
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-05-07 15:51:50 +0200

    CheckShopsysInstallReleaseWorker: add instruction to run cypress tests
    - refactored to avoid DRY violations in the stages

--- utils/releaser/src/ReleaseWorker/AfterRelease/CheckShopsysInstallReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/AfterRelease/CheckShopsysInstallReleaseWorker.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Shopsys\Releaser\ReleaseWorker\AfterRelease;
 
+use Override;
 use PharIo\Version\Version;
 use Shopsys\Releaser\ReleaseWorker\AbstractCheckShopsysInstallReleaseWorker;
 use Shopsys\Releaser\ReleaseWorker\AbstractShopsysReleaseWorker;
@@ -33,28 +34,17 @@ final class CheckShopsysInstallReleaseWorker extends AbstractCheckShopsysInstall
 
     /**
      * @param \PharIo\Version\Version $version
-     * @param string $initialBranchName
      */
-    public function work(
-        Version $version,
-        string $initialBranchName = AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME,
-    ): void {
+    #[Override]
+    protected function writeInstructionsForProjectBasePreparation(Version $version): void
+    {
         $this->symfonyStyle->note(sprintf(
-            'Instructions for installation:
+            'Instructions for project base preparation:
 
 git clone https://github.com/shopsys/project-base.git
-git checkout %1$s
-
-# remove all docker containers
-docker rm $(docker ps -a -q)
-
-# remove all docker images
-docker rmi --force $(docker images -q)
-
-# install the application following the corresponding installation guide',
+git checkout v%s
+',
             $version->getVersionString(),
         ));
-
-        parent::work($version);
     }
 }
