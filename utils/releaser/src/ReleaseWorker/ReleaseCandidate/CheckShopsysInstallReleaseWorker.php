dbc26f96d44ebe144f2419fd21a21c1557a05db7
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-05-07 15:51:50 +0200

    CheckShopsysInstallReleaseWorker: add instruction to run cypress tests
    - refactored to avoid DRY violations in the stages

--- utils/releaser/src/ReleaseWorker/ReleaseCandidate/CheckShopsysInstallReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/ReleaseCandidate/CheckShopsysInstallReleaseWorker.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Shopsys\Releaser\ReleaseWorker\ReleaseCandidate;
 
+use Override;
 use PharIo\Version\Version;
 use Shopsys\Releaser\ReleaseWorker\AbstractCheckShopsysInstallReleaseWorker;
 use Shopsys\Releaser\ReleaseWorker\AbstractShopsysReleaseWorker;
@@ -33,38 +34,22 @@ final class CheckShopsysInstallReleaseWorker extends AbstractCheckShopsysInstall
 
     /**
      * @param \PharIo\Version\Version $version
-     * @param string $initialBranchName
      */
-    public function work(
-        Version $version,
-        string $initialBranchName = AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME,
-    ): void {
+    #[Override]
+    protected function writeInstructionsForProjectBasePreparation(Version $version): void
+    {
         $branchName = $this->createBranchName($version);
 
         $this->symfonyStyle->note(sprintf(
-            'Instructions for installation:
+            'Instructions for project base preparation:
 
 git clone https://github.com/shopsys/project-base.git
 git checkout %1$s
 
 # in composer.json, change a version of all shopsys/* packages from "%2$s" to "dev-%1$s as %2$s"
-
-# remove all docker containers
-docker rm $(docker ps -a -q)
-
-# remove all docker images
-docker rmi --force $(docker images -q)
-
-# install the application following the corresponding installation guide
-
-# run the test suites including acceptance tests:
-docker compose exec php-fpm php phing tests tests-acceptance',
+',
             $branchName,
             $version->getVersionString(),
         ));
-
-        $this->confirm('Confirm all tests passed from project-base');
-
-        parent::work($version);
     }
 }
