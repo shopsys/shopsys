57d7b8f392f2418c6f3ddc164ea5756c8fb63552
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-05-08 14:42:36 +0200

    move LocaleHelper from project-base to the framework package
    - add most common locales to the constants

---
+++ packages/framework/src/Component/Locale/LocaleHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Component\Locale;
+
+class LocaleHelper
+{
+    public const string LOCALE_CS = 'cs'; // Czech (Czech Republic)
+    public const string LOCALE_SK = 'sk'; // Slovak (Slovakia)
+    public const string LOCALE_EN = 'en'; // English (United Kingdom)
+    public const string LOCALE_DE = 'de'; // German (Germany)
+    public const string LOCALE_FR = 'fr'; // French (France)
+    public const string LOCALE_ES = 'es'; // Spanish (Spain)
+    public const string LOCALE_IT = 'it'; // Italian (Italy)
+    public const string LOCALE_PT = 'pt'; // Portuguese (Portugal)
+    public const string LOCALE_NL = 'nl'; // Dutch (Netherlands)
+    public const string LOCALE_PL = 'pl'; // Polish (Poland)
+    public const string LOCALE_RO = 'ro'; // Romanian (Romania)
+    public const string LOCALE_SV = 'sv'; // Swedish (Sweden)
+    public const string LOCALE_EL = 'el'; // Greek (Greece)
+    public const string LOCALE_HU = 'hu'; // Hungarian (Hungary)
+    public const string LOCALE_DA = 'da'; // Danish (Denmark)
+    public const string LOCALE_FI = 'fi'; // Finnish (Finland)
+    public const string LOCALE_BG = 'bg'; // Bulgarian (Bulgaria)
+    public const string LOCALE_LT = 'lt'; // Lithuanian (Lithuania)
+    public const string LOCALE_LV = 'lv'; // Latvian (Latvia)
+    public const string LOCALE_ET = 'et'; // Estonian (Estonia)
+    public const string LOCALE_SL = 'sl'; // Slovenian (Slovenia)
+    public const string LOCALE_HR = 'hr'; // Croatian (Croatia)
+    public const string LOCALE_UK = 'uk'; // Ukrainian (Ukraine)
+}
