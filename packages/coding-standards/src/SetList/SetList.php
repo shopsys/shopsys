ecdde20b58891fa34d0b56e5e42ddf8c1b3201a8
Martin Grossmann <martin@vyvoj.net>
2024-05-31 15:03:29 +0200

    upgraded symplify/easy-coding-standards to v12.2
    - configuration was changed
    - values in skip are not merged properly when the same key exists
      - created ecs-skip-rule.php that can be imported in monorepo and project-base
    - paths to check are now part of configuration

---
+++ packages/coding-standards/src/SetList/SetList.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\CodingStandards\SetList;
+
+class SetList
+{
+    public const string SHOPSYS_CODING_STANDARD = __DIR__ . '/../../ecs.php';
+}
