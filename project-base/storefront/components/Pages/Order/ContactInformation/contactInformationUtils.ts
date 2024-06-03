2d42fbd30da975cdcf4df990a6b654c0d9055b11
Sebastian Holesz <sebastianholesz@seznam.cz>
2024-06-03 11:18:29 +0200

    rewritten order utils to hooks
    - this was done to produce cleaner code without
    function with 15 params

--- project-base/storefront/components/Pages/Order/ContactInformation/contactInformationUtils.ts
+++ project-base/storefront/components/Pages/Order/ContactInformation/contactInformationUtils.ts
@@ -10,35 +10,28 @@ import {
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
 import { handleCartModifications } from 'connectors/cart/Cart';
 import { useCurrentCustomerData } from 'connectors/customer/CurrentCustomer';
-import { TypeCartFragment } from 'graphql/requests/cart/fragments/CartFragment.generated';
 import {
     TypeCreateOrderMutation,
     TypeCreateOrderMutationVariables,
     useCreateOrderMutation,
 } from 'graphql/requests/orders/mutations/CreateOrderMutation.generated';
-import { TypeSimplePaymentFragment } from 'graphql/requests/payments/fragments/SimplePaymentFragment.generated';
 import { TypeListedStoreFragment } from 'graphql/requests/stores/fragments/ListedStoreFragment.generated';
-import { Maybe } from 'graphql/types';
 import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
 import { getGtmCreateOrderEventOrderPart, getGtmCreateOrderEventUserPart } from 'gtm/factories/getGtmCreateOrderEvent';
 import { onGtmCreateOrderEventHandler } from 'gtm/handlers/onGtmCreateOrderEventHandler';
 import { getGtmReviewConsents } from 'gtm/utils/getGtmReviewConsents';
 import { saveGtmCreateOrderEventInLocalStorage } from 'gtm/utils/gtmCreateOrderEventLocalStorage';
-import { Translate } from 'next-translate';
 import useTranslation from 'next-translate/useTranslation';
-import { NextRouter, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { OrderConfirmationUrlQuery } from 'pages/order-confirmation';
 import { SubmitHandler, UseFormReturn, useWatch } from 'react-hook-form';
 import { ContactInformation } from 'store/slices/createContactInformationSlice';
-import { PageLoadingStateSlice } from 'store/slices/createPageLoadingStateSlice';
 import { usePersistStore } from 'store/usePersistStore';
 import { useSessionStore } from 'store/useSessionStore';
-import { CurrentCartType } from 'types/cart';
 import { CurrentCustomerType } from 'types/customer';
 import { OperationResult } from 'urql';
-import { ChangePaymentInCart, useChangePaymentInCart } from 'utils/cart/useChangePaymentInCart';
+import { useChangePaymentInCart } from 'utils/cart/useChangePaymentInCart';
 import { useCurrentCart } from 'utils/cart/useCurrentCart';
-import { DomainConfigType } from 'utils/domain/domainConfig';
 import { handleFormErrors } from 'utils/forms/handleFormErrors';
 import { getIsPaymentWithPaymentGate } from 'utils/mappers/payment';
 import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
@@ -72,19 +65,12 @@ export const useCreateOrder = (
     formProviderMethods: UseFormReturn<ContactInformation>,
     formMeta: ContactInformationFormMetaType,
 ) => {
-    const { t } = useTranslation();
     const [{ fetching: isCreatingOrder }, createOrderMutation] = useCreateOrderMutation();
     const cartUuid = usePersistStore((store) => store.cartUuid);
     const currentCart = useCurrentCart(false);
     const user = useCurrentCustomerData();
     const updatePageLoadingState = useSessionStore((s) => s.updatePageLoadingState);
-    const domainConfig = useDomainConfig();
-    const userContactInformation = useCurrentUserContactInformation();
-
-    const { changePaymentInCart } = useChangePaymentInCart();
-    const updateCartUuid = usePersistStore((store) => store.updateCartUuid);
-    const resetContactInformation = usePersistStore((store) => store.resetContactInformation);
-    const router = useRouter();
+    const handleCreateOrderResult = useHandleCreateOrderResult();
 
     const createOrder: SubmitHandler<ContactInformation> = async (formValues) => {
         updatePageLoadingState({ isPageLoading: true, redirectPageType: 'order-confirmation' });
@@ -93,23 +79,7 @@ export const useCreateOrder = (
             getCreateOrderMutationVariables(cartUuid, formValues, currentCart.pickupPlace, user),
         );
 
-        handleCreateOrderResult(
-            cartUuid,
-            formProviderMethods,
-            formMeta,
-            currentCart,
-            createOrderResult,
-            formValues,
-            user,
-            router,
-            domainConfig,
-            changePaymentInCart,
-            t,
-            userContactInformation,
-            updateCartUuid,
-            resetContactInformation,
-            updatePageLoadingState,
-        );
+        handleCreateOrderResult(formProviderMethods, formMeta, createOrderResult, formValues);
     };
 
     return { createOrder, isCreatingOrder };
@@ -152,117 +122,112 @@ const getCreateOrderMutationVariables = (
     };
 };
 
-const handleCreateOrderResult = (
-    cartUuid: string | null,
-    formProviderMethods: UseFormReturn<ContactInformation>,
-    formMeta: ContactInformationFormMetaType,
-    currentCart: CurrentCartType,
-    createOrderResult: OperationResult<TypeCreateOrderMutation, TypeCreateOrderMutationVariables>,
-    formValues: ContactInformation,
-    user: CurrentCustomerType | null | undefined,
-    router: NextRouter,
-    domainConfig: DomainConfigType,
-    changePaymentInCart: ChangePaymentInCart,
-    t: Translate,
-    userContactInformation: ContactInformation,
-    updateCartUuid: (value: string | null) => void,
-    resetContactInformation: () => void,
-    updatePageLoadingState: (value: Partial<PageLoadingStateSlice>) => void,
-) => {
-    const wasOrderCreated = createOrderResult.data?.CreateOrder.orderCreated;
-    const createdOrder = createOrderResult.data?.CreateOrder.order;
-    const modifiedCartAfterUnsuccessfulOrderCreation = createOrderResult.data?.CreateOrder.cart;
+const useHandleCreateOrderResult = () => {
+    const { t } = useTranslation();
+    const user = useCurrentCustomerData();
+    const updatePageLoadingState = useSessionStore((s) => s.updatePageLoadingState);
+    const domainConfig = useDomainConfig();
+    const { changePaymentInCart } = useChangePaymentInCart();
+    const router = useRouter();
+    const handleEventsAfterOrderCreation = useHandleEventsAfterOrderCreation();
 
-    if (wasOrderCreated && createdOrder) {
-        const orderConfirmationUrlQuery: OrderConfirmationUrlQuery = {
-            orderUuid: createdOrder.uuid,
-            orderEmail: formValues.email,
-            orderPaymentType: createdOrder.payment.type,
-        };
+    const handleCreateOrderResult = (
+        formProviderMethods: UseFormReturn<ContactInformation>,
+        formMeta: ContactInformationFormMetaType,
+        createOrderResult: OperationResult<TypeCreateOrderMutation, TypeCreateOrderMutationVariables>,
+        formValues: ContactInformation,
+    ) => {
+        const wasOrderCreated = createOrderResult.data?.CreateOrder.orderCreated;
+        const createdOrder = createOrderResult.data?.CreateOrder.order;
+        const modifiedCartAfterUnsuccessfulOrderCreation = createOrderResult.data?.CreateOrder.cart;
 
-        if (!user) {
-            orderConfirmationUrlQuery.registrationData = JSON.stringify(formValues);
+        if (wasOrderCreated && createdOrder) {
+            const orderConfirmationUrlQuery: OrderConfirmationUrlQuery = {
+                orderUuid: createdOrder.uuid,
+                orderEmail: formValues.email,
+                orderPaymentType: createdOrder.payment.type,
+            };
+
+            if (!user) {
+                orderConfirmationUrlQuery.registrationData = JSON.stringify(formValues);
+            }
+
+            const [orderConfirmationUrl] = getInternationalizedStaticUrls(['/order-confirmation'], domainConfig.url);
+
+            router
+                .replace(
+                    {
+                        pathname: orderConfirmationUrl,
+                        query: orderConfirmationUrlQuery,
+                    },
+                    orderConfirmationUrl,
+                )
+                .then(() => {
+                    handleEventsAfterOrderCreation(createdOrder.number);
+                });
+
+            return;
         }
 
-        const [orderConfirmationUrl] = getInternationalizedStaticUrls(['/order-confirmation'], domainConfig.url);
+        updatePageLoadingState({ isPageLoading: false });
+        if (!wasOrderCreated && modifiedCartAfterUnsuccessfulOrderCreation) {
+            handleCartModifications(modifiedCartAfterUnsuccessfulOrderCreation.modifications, t, changePaymentInCart);
+        }
 
-        router
-            .replace(
-                {
-                    pathname: orderConfirmationUrl,
-                    query: orderConfirmationUrlQuery,
-                },
-                orderConfirmationUrl,
-            )
-            .then(() => {
-                handleEventsAfterOrderCreation(
-                    cartUuid,
-                    currentCart.cart,
-                    currentCart.payment,
-                    currentCart.promoCode,
-                    createdOrder.number,
-                    user,
-                    domainConfig,
-                    userContactInformation,
-                    updateCartUuid,
-                    resetContactInformation,
-                );
-            });
+        handleFormErrors(
+            createOrderResult.error,
+            formProviderMethods,
+            t,
+            formMeta.messages.error,
+            undefined,
+            GtmMessageOriginType.contact_information_page,
+        );
+    };
 
-        return;
-    }
-
-    updatePageLoadingState({ isPageLoading: false });
-    if (!wasOrderCreated && modifiedCartAfterUnsuccessfulOrderCreation) {
-        handleCartModifications(modifiedCartAfterUnsuccessfulOrderCreation.modifications, t, changePaymentInCart);
-    }
-
-    handleFormErrors(
-        createOrderResult.error,
-        formProviderMethods,
-        t,
-        formMeta.messages.error,
-        undefined,
-        GtmMessageOriginType.contact_information_page,
-    );
+    return handleCreateOrderResult;
 };
 
-const handleEventsAfterOrderCreation = (
-    cartUuid: string | null,
-    cart: Maybe<TypeCartFragment> | undefined,
-    payment: Maybe<TypeSimplePaymentFragment> | undefined,
-    promoCode: string | null,
-    orderNumber: string,
-    user: CurrentCustomerType | undefined | null,
-    domainConfig: DomainConfigType,
-    userContactInformation: ContactInformation,
-    updateCartUuid: (value: string | null) => void,
-    resetContactInformation: () => void,
-) => {
-    if (cart && payment && promoCode) {
-        const gtmCreateOrderEventOrderPart = getGtmCreateOrderEventOrderPart(
-            cart,
-            payment,
-            promoCode,
-            orderNumber,
-            getGtmReviewConsents(),
-            domainConfig,
-        );
-        const gtmCreateOrderEventUserPart = getGtmCreateOrderEventUserPart(user, userContactInformation);
+const useHandleEventsAfterOrderCreation = () => {
+    const cartUuid = usePersistStore((store) => store.cartUuid);
+    const user = useCurrentCustomerData();
+    const domainConfig = useDomainConfig();
+    const userContactInformation = useCurrentUserContactInformation();
+    const { cart, payment, promoCode } = useCurrentCart();
+    const updateCartUuid = usePersistStore((store) => store.updateCartUuid);
+    const resetContactInformation = usePersistStore((store) => store.resetContactInformation);
 
-        const isPaymentWithPaymentGate = getIsPaymentWithPaymentGate(payment.type);
-        if (isPaymentWithPaymentGate) {
-            saveGtmCreateOrderEventInLocalStorage(gtmCreateOrderEventOrderPart, gtmCreateOrderEventUserPart);
+    const handleEventsAfterOrderCreation = (orderNumber: string) => {
+        if (cart && payment && promoCode) {
+            const gtmCreateOrderEventOrderPart = getGtmCreateOrderEventOrderPart(
+                cart,
+                payment,
+                promoCode,
+                orderNumber,
+                getGtmReviewConsents(),
+                domainConfig,
+            );
+            const gtmCreateOrderEventUserPart = getGtmCreateOrderEventUserPart(user, userContactInformation);
+
+            const isPaymentWithPaymentGate = getIsPaymentWithPaymentGate(payment.type);
+            if (isPaymentWithPaymentGate) {
+                saveGtmCreateOrderEventInLocalStorage(gtmCreateOrderEventOrderPart, gtmCreateOrderEventUserPart);
+            }
+
+            const isPaymentSuccessful = isPaymentWithPaymentGate ? undefined : true;
+
+            onGtmCreateOrderEventHandler(
+                gtmCreateOrderEventOrderPart,
+                gtmCreateOrderEventUserPart,
+                isPaymentSuccessful,
+            );
         }
 
-        const isPaymentSuccessful = isPaymentWithPaymentGate ? undefined : true;
+        if (cartUuid) {
+            updateCartUuid(null);
+        }
 
-        onGtmCreateOrderEventHandler(gtmCreateOrderEventOrderPart, gtmCreateOrderEventUserPart, isPaymentSuccessful);
-    }
+        resetContactInformation();
+    };
 
-    if (cartUuid) {
-        updateCartUuid(null);
-    }
-
-    resetContactInformation();
+    return handleEventsAfterOrderCreation;
 };
