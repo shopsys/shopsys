c62c5eff08b2956ebf7fa1d5b0063c00037e43a4
Martin Grossmann <martin@vyvoj.net>
2024-06-04 15:32:19 +0200

    removed unused advert_category table
    - for the relation is used advert_categories

---
+++ packages/framework/src/Migrations/Version20240604152553.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20240604152553 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->isAppMigrationNotInstalledRemoveIfExists('Version20200617101511');
+
+        $this->sql('DROP TABLE IF EXISTS advert_category');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
