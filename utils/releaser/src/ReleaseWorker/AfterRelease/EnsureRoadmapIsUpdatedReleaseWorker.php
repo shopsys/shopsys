639fe906bdacd6a84f0ce47b3d0d0cb7405235bb
Martin Grossmann <martin@vyvoj.net>
2024-06-26 21:25:53 +0200

    added update roadmap release step

---
+++ utils/releaser/src/ReleaseWorker/AfterRelease/EnsureRoadmapIsUpdatedReleaseWorker.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\Releaser\ReleaseWorker\AfterRelease;
+
+use PharIo\Version\Version;
+use Shopsys\Releaser\ReleaseWorker\AbstractShopsysReleaseWorker;
+use Shopsys\Releaser\Stage;
+
+final class EnsureRoadmapIsUpdatedReleaseWorker extends AbstractShopsysReleaseWorker
+{
+    /**
+     * @param \PharIo\Version\Version $version
+     * @param string $initialBranchName
+     * @return string
+     */
+    public function getDescription(
+        Version $version,
+        string $initialBranchName = AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME,
+    ): string {
+        return '[Manually] Ensure "Roadmap" is updated';
+    }
+
+    /**
+     * @param \PharIo\Version\Version $version
+     * @param string $initialBranchName
+     */
+    public function work(
+        Version $version,
+        string $initialBranchName = AbstractShopsysReleaseWorker::MAIN_BRANCH_NAME,
+    ): void {
+        $this->symfonyStyle->note([
+            'Notify Product owner of the necessity to update the "Roadmap" page on the website.',
+            'If the roadmap cannot be updated right now, the user story should be created and put into the current sprint.',
+            'The "Roadmap" page is located at https://www.shopsys.cz/product-roadmap/',
+        ]);
+
+        $this->confirm('Confirm the roadmap is updated or the user story is in the current sprint.');
+    }
+
+    /**
+     * @return string
+     */
+    public function getStage(): string
+    {
+        return Stage::AFTER_RELEASE;
+    }
+}
