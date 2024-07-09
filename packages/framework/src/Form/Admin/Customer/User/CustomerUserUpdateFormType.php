8f520c896722346354148aac865d02ee2cc4f453
Michal Tomka <michal.tomka@shopsys.com>
2024-07-09 23:30:26 +0200

    on B2B domain allow to assign customer user for single man company

--- packages/framework/src/Form/Admin/Customer/User/CustomerUserUpdateFormType.php
+++ packages/framework/src/Form/Admin/Customer/User/CustomerUserUpdateFormType.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Customer\User;
 
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Form\Admin\Customer\BillingAddressFormType;
 use Shopsys\FrameworkBundle\Form\Constraints\UniqueBillingAddress;
+use Shopsys\FrameworkBundle\Form\CustomerUserListType;
 use Shopsys\FrameworkBundle\Form\DeliveryAddressListType;
 use Shopsys\FrameworkBundle\Form\OrderListType;
 use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser;
@@ -20,9 +22,12 @@ class CustomerUserUpdateFormType extends AbstractType
 {
     /**
      * @param \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUserUpdateDataFactoryInterface $customerUserUpdateDataFactory
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      */
-    public function __construct(private readonly CustomerUserUpdateDataFactoryInterface $customerUserUpdateDataFactory)
-    {
+    public function __construct(
+        private readonly CustomerUserUpdateDataFactoryInterface $customerUserUpdateDataFactory,
+        private readonly Domain $domain,
+    ) {
     }
 
     /**
@@ -55,6 +60,7 @@ class CustomerUserUpdateFormType extends AbstractType
                 'label' => t('Send confirmation email about registration to customer'),
             ]);
         } else {
+            $this->addCustomerUserList($options, $builder);
             $builder->add('deliveryAddresses', DeliveryAddressListType::class, [
                 'customerUser' => $options['customerUser'],
             ]);
@@ -82,5 +88,24 @@ class CustomerUserUpdateFormType extends AbstractType
                     ]),
                 ],
             ]);
+    }
+
+    /**
+     * @param array $options
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @throws \Shopsys\FrameworkBundle\Component\Domain\Exception\InvalidDomainIdException
+     */
+    private function addCustomerUserList(array $options, FormBuilderInterface $builder): void
+    {
+        $domain = $this->domain->getDomainConfigById($options['domain_id']);
+        $customer = $options['customerUser']->getCustomer();
+        $isCompanyUser = $customer->getBillingAddress()->isCompanyCustomer();
+
+        if ($domain->isB2b() && $isCompanyUser) {
+            $builder->add('customerUsers', CustomerUserListType::class, [
+                'customer' => $customer,
+                'allowAdd' => true,
+            ]);
+        }
     }
 }
