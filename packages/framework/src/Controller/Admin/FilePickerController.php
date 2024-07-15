eb0516c4fd43d606f14e84d434e89ad1f2e24caa
Hynek Nerad <iam@kennydaren.me>
2024-07-15 15:55:09 +0200

    file picker created

---
+++ packages/framework/src/Controller/Admin/FilePickerController.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Controller\Admin;
+
+use Shopsys\FrameworkBundle\Component\Grid\GridFactory;
+use Shopsys\FrameworkBundle\Component\Grid\QueryBuilderWithRowManipulatorDataSource;
+use Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileAdminListFacade;
+use Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileFacade;
+use Shopsys\FrameworkBundle\Form\Admin\QuickSearch\QuickSearchFormData;
+use Shopsys\FrameworkBundle\Form\Admin\QuickSearch\QuickSearchFormType;
+use Shopsys\FrameworkBundle\Model\Administrator\AdministratorGridFacade;
+use Shopsys\FrameworkBundle\Model\Product\Listing\ProductListAdminFacade;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FilePickerController extends AdminBaseController
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Administrator\AdministratorGridFacade $administratorGridFacade
+     * @param \Shopsys\FrameworkBundle\Component\Grid\GridFactory $gridFactory
+     * @param \Shopsys\FrameworkBundle\Model\Product\Listing\ProductListAdminFacade $productListAdminFacade
+     * @param \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileFacade $uploadedFileFacade
+     * @param \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileAdminListFacade $uploadedFileAdminListFacade
+     */
+    public function __construct(
+        protected readonly AdministratorGridFacade $administratorGridFacade,
+        protected readonly GridFactory $gridFactory,
+        protected readonly ProductListAdminFacade $productListAdminFacade,
+        protected readonly UploadedFileFacade $uploadedFileFacade,
+        protected readonly UploadedFileAdminListFacade $uploadedFileAdminListFacade,
+    ) {
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param int|string $jsInstanceId
+     */
+    #[Route(path: '/file-picker/pick-multiple/{jsInstanceId}')]
+    public function pickMultipleAction(
+        Request $request,
+        int|string $jsInstanceId,
+    ) {
+        return $this->getPickerResponse(
+            $request,
+            [
+                'isMultiple' => true,
+            ],
+            [
+                'isMultiple' => true,
+                'jsInstanceId' => $jsInstanceId,
+            ],
+        );
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param array $viewParameters
+     * @param array $gridViewParameters
+     */
+    protected function getPickerResponse(Request $request, array $viewParameters, array $gridViewParameters)
+    {
+        $quickSearchData = new QuickSearchFormData();
+
+        $quickSearchForm = $this->createForm(QuickSearchFormType::class, $quickSearchData);
+
+        $quickSearchForm->handleRequest($request);
+
+        $queryBuilder = $this->uploadedFileAdminListFacade->getQueryBuilderByQuickSearchData($quickSearchData);
+        $dataSource = new QueryBuilderWithRowManipulatorDataSource(
+            $queryBuilder,
+            'u.id',
+            function ($row) {
+                $row['filename'] = sprintf('%s.%s', $row['u']['name'], $row['u']['extension']);
+
+                return $row;
+            },
+        );
+
+        $grid = $this->gridFactory->create('filePicker', $dataSource);
+        $grid->enablePaging();
+
+        $grid->addColumn('filename', 'filename', t('Filename'));
+        $grid->addColumn('translatedName', 'ut.name', t('Name'), true);
+        $grid->addColumn('extension', 'u.extension', t('Ext.'), true);
+
+        $grid->addColumn('select', 'u.id', '')
+            ->setClassAttribute('table-col table-col-15 text-center');
+
+        $grid->setTheme('@ShopsysFramework/Admin/Content/FilePicker/listGrid.html.twig', $gridViewParameters);
+
+        $this->administratorGridFacade->restoreAndRememberGridLimit($this->getCurrentAdministrator(), $grid);
+
+        $viewParameters['gridView'] = $grid->createView();
+        $viewParameters['quickSearchForm'] = $quickSearchForm->createView();
+
+        return $this->render('@ShopsysFramework/Admin/Content/FilePicker/list.html.twig', $viewParameters);
+    }
+}
