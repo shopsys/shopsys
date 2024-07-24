069ef23dca3089446cc8e69e2414c7a3947f4d63
Michal Tomka <michal.tomka@shopsys.com>
2024-07-24 16:53:22 +0200

    FE API: query for listing customer users

--- packages/frontend-api/src/Model/Resolver/Customer/User/CurrentCustomerUserQuery.php
+++ packages/frontend-api/src/Model/Resolver/Customer/User/CurrentCustomerUserQuery.php
@@ -5,17 +5,21 @@ declare(strict_types=1);
 namespace Shopsys\FrontendApiBundle\Model\Resolver\Customer\User;
 
 use Overblog\GraphQLBundle\Error\UserWarning;
+use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser;
 use Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser;
 use Shopsys\FrontendApiBundle\Model\Resolver\AbstractQuery;
+use Shopsys\FrontendApiBundle\Model\Token\Exception\InvalidTokenUserMessageException;
 
 class CurrentCustomerUserQuery extends AbstractQuery
 {
     /**
      * @param \Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
+     * @param \Shopsys\FrameworkBundle\Model\Customer\CustomerFacade $customerFacade
      */
     public function __construct(
         protected readonly CurrentCustomerUser $currentCustomerUser,
+        protected readonly CustomerFacade $customerFacade,
     ) {
     }
 
@@ -31,5 +35,19 @@ class CurrentCustomerUserQuery extends AbstractQuery
         }
 
         return $currentCustomerUser;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser[]
+     */
+    public function customerUsersQuery(): array
+    {
+        $currentCustomerUser = $this->currentCustomerUser->findCurrentCustomerUser();
+
+        if ($currentCustomerUser === null) {
+            throw new InvalidTokenUserMessageException('No customer user is currently logged in.');
+        }
+
+        return $this->customerFacade->getCustomerUsers($currentCustomerUser->getCustomer());
     }
 }
