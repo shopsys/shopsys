942e9bd86d1e3904348ceb0ccac44d26733e9ab6
Hynek Nerad <iam@kennydaren.me>
2024-07-31 16:52:40 +0200

    complaint data structure

---
+++ packages/framework/src/Model/Complaint/ComplaintData.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint;
+
+class ComplaintData
+{
+    /**
+     * @var string
+     */
+    public $number;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Order\Order|null
+     */
+    public $order;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Customer\User\CustomerUser|null
+     */
+    public $customerUser;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryFirstName;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryLastName;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryCompanyName;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryTelephone;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryStreet;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryCity;
+
+    /**
+     * @var string|null
+     */
+    public $deliveryPostcode;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Country\Country|null
+     */
+    public $deliveryCountry;
+
+    /**
+     * @var string|null
+     */
+    public $status;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Complaint\ComplaintItem[]
+     */
+    public $complaintItems = [];
+}
