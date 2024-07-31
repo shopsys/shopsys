8c26ef6bf766fdb5fd3c7493773cb24ffb870c30
Hynek Nerad <iam@kennydaren.me>
2024-07-31 16:52:40 +0200

    complaint data structure

---
+++ packages/framework/src/Model/Complaint/ComplaintItemData.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint;
+
+class ComplaintItemData
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Order\Item\OrderItem|null
+     */
+    public $orderItem;
+
+    /**
+     * @var int|null
+     */
+    public $quantity;
+
+    /**
+     * @var string|null
+     */
+    public $description;
+}
