1342b74f5990acd493306d1af33c0bcff4421dcf
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-08-02 12:21:31 +0200

    updated promocode button styles

--- project-base/storefront/components/Pages/Cart/CartSummary.tsx
+++ project-base/storefront/components/Pages/Cart/CartSummary.tsx
@@ -4,7 +4,7 @@ import { CartPreview } from 'components/Pages/Cart/CartPreview';
 
 export const CartSummary: FC = () => (
     <div className="mb-8 flex flex-col vl:flex-row vl:items-baseline">
-        <div className="pr-8 vl:pr-4">
+        <div className="pr-0 vl:pr-4">
             <PromoCode />
         </div>
         <div className="ml-auto text-center vl:pr-8">
