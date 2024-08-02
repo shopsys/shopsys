0eade67069e52ba5ae7bf6b5dc0dbf05ba7fcd6b
Michal Tomka <michal.tomka@shopsys.com>
2024-08-02 19:00:52 +0200

    ProductSellingDeniedRecalculator: move logic from project-base to framework package

--- packages/framework/src/Model/Product/ProductSellingDeniedRecalculator.php
+++ packages/framework/src/Model/Product/ProductSellingDeniedRecalculator.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Model\Product;
 
+use Doctrine\DBAL\ArrayParameterType;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\EntityManagerInterface;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 
 class ProductSellingDeniedRecalculator
 {
     /**
      * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      */
     public function __construct(
         protected readonly EntityManagerInterface $em,
+        protected readonly Domain $domain,
     ) {
     }
 
@@ -35,6 +40,7 @@ class ProductSellingDeniedRecalculator
     protected function calculate(array $productIds = []): void
     {
         $this->calculateIndependent($productIds);
+        $this->propagateCalculatedSaleExclusionToCalculatedSellingDenied($productIds);
         $this->propagateMainVariantSellingDeniedToVariants($productIds);
         $this->propagateVariantsSellingDeniedToMainVariant($productIds);
     }
@@ -52,6 +58,87 @@ class ProductSellingDeniedRecalculator
             $qb->andWhere('p IN (:productIds)')->setParameter('productIds', $productIds);
         }
         $qb->getQuery()->execute();
+
+        $this->calculatePerDomain($productIds);
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function calculatePerDomain(array $productIds): void
+    {
+        $query = 'UPDATE product_domains AS pd
+            SET calculated_sale_exclusion = CASE
+                    WHEN (
+                        p.calculated_selling_denied = TRUE
+                        OR
+                        pd.domain_hidden = TRUE
+                        OR (
+                            p.variant_type != :variantTypeMain
+                            AND
+                            NOT EXISTS(
+                                SELECT 1
+                                FROM product_stocks as ps
+                                JOIN stocks as s ON s.id = ps.stock_id
+                                JOIN stock_domains sd ON s.id = sd.stock_id AND sd.domain_id = :domainId
+                                WHERE ps.product_id = p.id AND sd.is_enabled = TRUE
+                                HAVING SUM(ps.product_quantity) > 0
+                            )
+                        )
+                        OR (
+                            pd.sale_exclusion = TRUE
+                            AND                        
+                            p.variant_type = :variantTypeMain
+                        )
+                    )
+                    THEN TRUE
+                    ELSE FALSE
+                END
+            FROM products AS p
+            WHERE p.id = pd.product_id
+                AND pd.domain_id = :domainId
+            ' . (count($productIds) > 0 ? ' AND p.id IN (:productIds)' : '');
+
+        $params = [];
+        $params['productIds'] = $productIds;
+        $params['variantTypeMain'] = Product::VARIANT_TYPE_MAIN;
+
+        foreach ($this->domain->getAll() as $domain) {
+            $params['domainId'] = $domain->getId();
+
+            $this->em->getConnection()->executeStatement(
+                $query,
+                $params,
+                [
+                    'productIds' => ArrayParameterType::INTEGER,
+                    'variantTypeMain' => Types::STRING,
+                    'domainId' => Types::INTEGER,
+                ],
+            );
+        }
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function propagateCalculatedSaleExclusionToCalculatedSellingDenied(array $productIds): void
+    {
+        $query = 'UPDATE products as p
+                SET calculated_selling_denied = TRUE
+                WHERE p.calculated_selling_denied = FALSE
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM product_domains as pd
+                    WHERE pd.product_id = p.id
+                        AND pd.calculated_sale_exclusion = FALSE
+                )
+            ' . (count($productIds) > 0 ? ' AND p.id IN (:productIds)' : '');
+
+        $this->em->getConnection()->executeStatement(
+            $query,
+            ['productIds' => $productIds],
+            ['productIds' => ArrayParameterType::INTEGER],
+        );
     }
 
     /**
@@ -78,6 +165,68 @@ class ProductSellingDeniedRecalculator
             $qb->andWhere('p IN (:productIds)')->setParameter('productIds', $productIds);
         }
         $qb->getQuery()->execute();
+
+        $this->propagateMainVariantSellingDeniedToVariantsCalculatedSaleExclusion($productIds);
+        $this->propagateMainVariantCalculateSaleExclusionToVariantsCalculatedSaleExclusion($productIds);
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function propagateMainVariantSellingDeniedToVariantsCalculatedSaleExclusion(array $productIds): void
+    {
+        $query = 'UPDATE product_domains as pd
+                SET calculated_sale_exclusion = TRUE
+                FROM products as p
+                JOIN products as m ON p.main_variant_id = m.id
+                WHERE m.variant_type = :variantTypeMain
+                AND pd.product_id = p.id
+                AND pd.calculated_sale_exclusion = FALSE
+                AND p.calculated_selling_denied = TRUE
+            ' . (count($productIds) > 0 ? ' AND m.id IN (:productIds)' : '');
+
+        $this->em->getConnection()->executeStatement(
+            $query,
+            [
+                'productIds' => $productIds,
+                'variantTypeMain' => Product::VARIANT_TYPE_MAIN,
+            ],
+            [
+                'productIds' => ArrayParameterType::INTEGER,
+                'variantTypeMain' => Types::STRING,
+            ],
+        );
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function propagateMainVariantCalculateSaleExclusionToVariantsCalculatedSaleExclusion(
+        array $productIds,
+    ): void {
+        $query = 'UPDATE product_domains as pd
+                SET calculated_sale_exclusion = TRUE
+                FROM products as p
+                JOIN products as m ON p.main_variant_id = m.id
+                JOIN product_domains as pdm ON pdm.product_id = m.id 
+                WHERE m.variant_type = :variantTypeMain
+                AND pd.product_id = p.id
+                AND pdm.domain_id = pd.domain_id
+                AND pd.calculated_sale_exclusion = FALSE
+                AND pdm.calculated_sale_exclusion = TRUE
+            ' . (count($productIds) > 0 ? ' AND m.id IN (:productIds)' : '');
+
+        $this->em->getConnection()->executeStatement(
+            $query,
+            [
+                'productIds' => $productIds,
+                'variantTypeMain' => Product::VARIANT_TYPE_MAIN,
+            ],
+            [
+                'productIds' => ArrayParameterType::INTEGER,
+                'variantTypeMain' => Types::STRING,
+            ],
+        );
     }
 
     /**
@@ -104,5 +253,72 @@ class ProductSellingDeniedRecalculator
             $qb->andWhere('p IN (:productIds)')->setParameter('productIds', $productIds);
         }
         $qb->getQuery()->execute();
+
+        $this->propagateVariantsSaleExclusionToMainVariantCalculateSaleExclusion($productIds);
+        $this->propagateVariantsSaleExclusionToMainVariantCalculateSellingDenied($productIds);
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function propagateVariantsSaleExclusionToMainVariantCalculateSaleExclusion(array $productIds): void
+    {
+        $query = 'UPDATE product_domains as pd
+                SET calculated_sale_exclusion = TRUE
+                FROM products as p 
+                WHERE p.variant_type = :variantTypeMain
+                AND pd.product_id = p.id
+                AND pd.calculated_sale_exclusion = FALSE
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM products as v
+                    JOIN product_domains as pdv ON pdv.product_id = v.id AND pdv.domain_id = pd.domain_id
+                    WHERE v.main_variant_id = p.id
+                        AND pdv.calculated_sale_exclusion = FALSE
+                )
+            ' . (count($productIds) > 0 ? ' AND p.id IN (:productIds)' : '');
+
+        $this->em->getConnection()->executeStatement(
+            $query,
+            [
+                'productIds' => $productIds,
+                'variantTypeMain' => Product::VARIANT_TYPE_MAIN,
+            ],
+            [
+                'productIds' => ArrayParameterType::INTEGER,
+                'variantTypeMain' => Types::STRING,
+            ],
+        );
+    }
+
+    /**
+     * @param int[] $productIds
+     */
+    protected function propagateVariantsSaleExclusionToMainVariantCalculateSellingDenied(array $productIds): void
+    {
+        $query = 'UPDATE products as p
+                SET calculated_selling_denied = TRUE
+                WHERE p.variant_type = :variantTypeMain
+                AND p.calculated_selling_denied = FALSE
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM products as v
+                    JOIN product_domains as pdv ON pdv.product_id = v.id
+                    WHERE v.main_variant_id = p.id
+                        AND pdv.calculated_sale_exclusion = FALSE
+                )
+            ' . (count($productIds) > 0 ? ' AND p.id IN (:productIds)' : '');
+
+        $this->em->getConnection()->executeStatement(
+            $query,
+            [
+                'productIds' => $productIds,
+                'variantTypeMain' => Product::VARIANT_TYPE_MAIN,
+            ],
+            [
+                'productIds' => ArrayParameterType::INTEGER,
+                'variantTypeMain' => Types::STRING,
+            ],
+        );
     }
 }
