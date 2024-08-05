8d9e6ba832f994f54236f585b509621474b46f50
Sebastian Holesz <sebastianholesz@seznam.cz>
2024-08-05 14:39:12 +0200

    regenerated cypress screenshots and removed redundant tests

--- project-base/storefront/cypress/e2e/cart/productAddToCart.cy.ts
+++ project-base/storefront/cypress/e2e/cart/productAddToCart.cy.ts
@@ -72,45 +72,6 @@ describe('Product add to cart tests', () => {
         checkPopupIsVisible(true);
     });
 
-    it('should add multiple products to cart from category page using spinbox buttons', function () {
-        cy.visitAndWaitForStableAndInteractiveDOM(url.categoryElectronics);
-
-        increaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        increaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        increaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        increaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        decreaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        decreaseProductListQuantityWithSpinbox(products.helloKitty.catnum);
-        addProductToCartFromProductList(products.helloKitty.catnum);
-        checkPopupIsVisible();
-        cy.waitForStableAndInteractiveDOM();
-        takeSnapshotAndCompare(this.test?.title, 'add to cart popup', {
-            capture: 'viewport',
-            blackout: [
-                { tid: TIDs.add_to_cart_popup_image, zIndex: 20000 },
-                { tid: TIDs.simple_navigation_image, zIndex: 9999 },
-            ],
-        });
-        checkPopupIsVisible(true);
-    });
-
-    it('should try to add more products to cart than allowed from category page using spinbox input, and the spinbox quantity should correct itself', function () {
-        cy.visitAndWaitForStableAndInteractiveDOM(url.categoryElectronics);
-
-        changeProductListItemQuantityWithSpinboxInput(3000, products.helloKitty.catnum);
-        addProductToCartFromProductList(products.helloKitty.catnum);
-        checkPopupIsVisible();
-        cy.waitForStableAndInteractiveDOM();
-        takeSnapshotAndCompare(this.test?.title, 'add to cart popup', {
-            capture: 'viewport',
-            blackout: [
-                { tid: TIDs.add_to_cart_popup_image, zIndex: 20000 },
-                { tid: TIDs.simple_navigation_image, zIndex: 9999 },
-            ],
-        });
-        checkPopupIsVisible(true);
-    });
-
     it('should add variant product to cart from product detail', function () {
         cy.visitAndWaitForStableAndInteractiveDOM(products.philips32PFL4308.url);
 
