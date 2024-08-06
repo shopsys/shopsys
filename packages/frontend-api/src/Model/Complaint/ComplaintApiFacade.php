fe7b85f0656df7b8a5e301d3b6e875f9d0f42da5
Hynek Nerad <iam@kennydaren.me>
2024-08-06 16:29:56 +0200

    complaint file upload support

--- packages/frontend-api/src/Model/Complaint/ComplaintApiFacade.php
+++ packages/frontend-api/src/Model/Complaint/ComplaintApiFacade.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace Shopsys\FrontendApiBundle\Model\Complaint;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFileFacade;
 use Shopsys\FrameworkBundle\Model\Complaint\Complaint;
 use Shopsys\FrameworkBundle\Model\Complaint\ComplaintData;
 use Shopsys\FrameworkBundle\Model\Complaint\ComplaintFactory;
+use Shopsys\FrameworkBundle\Model\Complaint\ComplaintItemFactory;
 
 class ComplaintApiFacade
 {
     /**
      * @param \Doctrine\ORM\EntityManagerInterface $em
      * @param \Shopsys\FrameworkBundle\Model\Complaint\ComplaintFactory $complaintFactory
+     * @param \Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFileFacade $customerUploadedFileFacade
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\ComplaintItemFactory $complaintItemFactory
      */
     public function __construct(
         protected readonly EntityManagerInterface $em,
         protected readonly ComplaintFactory $complaintFactory,
+        protected readonly CustomerUploadedFileFacade $customerUploadedFileFacade,
+        protected readonly ComplaintItemFactory $complaintItemFactory,
     ) {
     }
 
@@ -27,10 +33,22 @@ class ComplaintApiFacade
      */
     public function create(ComplaintData $complaintData): Complaint
     {
-        $complaint = $this->complaintFactory->create($complaintData);
+        $complaintItemsData = [];
+        $complaintItems = [];
+
+        foreach ($complaintData->complaintItems as $key => $complaintItem) {
+            $complaintItemsData[$key] = $complaintItem;
+            $complaintItems[$key] = $this->complaintItemFactory->create($complaintItem);
+        }
+
+        $complaint = $this->complaintFactory->create($complaintData, $complaintItems);
 
         $this->em->persist($complaint);
         $this->em->flush();
+
+        foreach ($complaintItems as $key => $item) {
+            $this->customerUploadedFileFacade->manageFiles($item, $complaintItemsData[$key]->files);
+        }
 
         return $complaint;
     }
