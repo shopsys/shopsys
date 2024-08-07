36027cd56c5b397c64e9c5836f6a6018d0adf6d4
Hynek Nerad <iam@kennydaren.me>
2024-08-07 16:22:28 +0200

    order query filter support

--- project-base/app/src/FrontendApi/Model/Order/OrderRepository.php
+++ project-base/app/src/FrontendApi/Model/Order/OrderRepository.php
@@ -9,8 +9,8 @@ use Shopsys\FrontendApiBundle\Model\Order\OrderRepository as BaseOrderRepository
 /**
  * @method \App\Model\Order\Order|null findByUuidAndCustomerUser(string $uuid, \App\Model\Customer\User\CustomerUser $customerUser)
  * @method \App\Model\Order\Order|null findByUuidAndUrlHash(string $uuid, string $urlHash)
- * @method \App\Model\Order\Order[] getCustomerUserOrderLimitedList(\App\Model\Customer\User\CustomerUser $customerUser, int $limit, int $offset)
- * @method int getCustomerUserOrderCount(\App\Model\Customer\User\CustomerUser $customerUser)
+ * @method \App\Model\Order\Order[] getCustomerUserOrderLimitedList(\App\Model\Customer\User\CustomerUser $customerUser, int $limit, int $offset, \Shopsys\FrontendApiBundle\Model\Order\OrderFilter $filter = null)
+ * @method int getCustomerUserOrderCount(\App\Model\Customer\User\CustomerUser $customerUser, \Shopsys\FrontendApiBundle\Model\Order\OrderFilter $filter)
  * @method \App\Model\Order\Order getByUuidAndCustomerUser(string $uuid, \App\Model\Customer\User\CustomerUser $customerUser)
  * @method \App\Model\Order\Order getByOrderNumberAndCustomerUser(string $orderNumber, \App\Model\Customer\User\CustomerUser $customerUser)
  * @method \App\Model\Order\Order|null findByOrderNumberAndCustomerUser(string $orderNumber, \App\Model\Customer\User\CustomerUser $customerUser)
