b9ba457e11bc3a766b0cb7b7b9b019fc8b614bb4
Hynek Nerad <iam@kennydaren.me>
2024-08-12 14:33:00 +0200

    order number sequence use abstract number sequence

--- packages/framework/src/Model/Order/OrderNumberSequenceRepository.php
+++ packages/framework/src/Model/Order/OrderNumberSequenceRepository.php
@@ -4,70 +4,16 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Model\Order;
 
-use Doctrine\DBAL\LockMode;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
-use Exception;
-use Shopsys\FrameworkBundle\Model\Order\Exception\OrderNumberSequenceNotFoundException;
+use Shopsys\FrameworkBundle\Model\NumberSequence\AbstractNumberSequenceRepository;
 
-class OrderNumberSequenceRepository
+class OrderNumberSequenceRepository extends AbstractNumberSequenceRepository
 {
-    protected const int ID = 1;
-
     /**
-     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @return \Doctrine\ORM\EntityRepository
      */
-    public function __construct(
-        protected readonly EntityManagerInterface $em,
-    ) {
-    }
-
-    /**
-     * @return \Doctrine\ORM\EntityRepository<\Shopsys\FrameworkBundle\Model\Order\OrderNumberSequence>
-     */
-    protected function getOrderNumberSequenceRepository(): EntityRepository
+    protected function getNumberSequenceRepository(): EntityRepository
     {
         return $this->em->getRepository(OrderNumberSequence::class);
-    }
-
-    /**
-     * @return string
-     */
-    public function getNextNumber(): string
-    {
-        try {
-            $this->em->beginTransaction();
-
-            $requestedNumber = time();
-
-            /** @var \Shopsys\FrameworkBundle\Model\Order\OrderNumberSequence|null $orderNumberSequence */
-            $orderNumberSequence = $this->getOrderNumberSequenceRepository()->find(
-                static::ID,
-                LockMode::PESSIMISTIC_WRITE,
-            );
-
-            if ($orderNumberSequence === null) {
-                throw new OrderNumberSequenceNotFoundException(
-                    'Order number sequence ID ' . static::ID . ' not found.',
-                );
-            }
-
-            $lastNumber = $orderNumberSequence->getNumber();
-
-            if ($requestedNumber <= $lastNumber) {
-                $requestedNumber = (int)$lastNumber + 1;
-            }
-
-            $orderNumberSequence->setNumber((string)$requestedNumber);
-
-            $this->em->flush();
-            $this->em->commit();
-        } catch (Exception $e) {
-            $this->em->rollback();
-
-            throw $e;
-        }
-
-        return $orderNumberSequence->getNumber();
     }
 }
