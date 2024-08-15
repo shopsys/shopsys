2a9e4e985e40aea6ee5193ebc43e406eae039999
Martin Grossmann <martin@vyvoj.net>
2024-08-15 22:50:57 +0200

    removed dependency on App from frontend-api package

--- packages/frontend-api/src/Model/Resolver/Products/BestsellingProductsQuery.php
+++ packages/frontend-api/src/Model/Resolver/Products/BestsellingProductsQuery.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Shopsys\FrontendApiBundle\Model\Resolver\Products;
 
-use App\Model\Category\Category;
-use App\Model\CategorySeo\ReadyCategorySeoMix;
 use GraphQL\Executor\Promise\Promise;
-use InvalidArgumentException;
 use Overblog\DataLoader\DataLoaderInterface;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Category\Category;
 use Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser;
 use Shopsys\FrameworkBundle\Model\Product\BestsellingProduct\CachedBestsellingProductFacade;
 use Shopsys\FrameworkBundle\Model\Product\ProductFrontendLimitProvider;
@@ -20,7 +18,7 @@ class BestsellingProductsQuery extends AbstractQuery
     /**
      * @param \Shopsys\FrameworkBundle\Model\Product\BestsellingProduct\CachedBestsellingProductFacade $cachedBestsellingProductFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
-     * @param \App\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
      * @param \Overblog\DataLoader\DataLoaderInterface $productsSellableByIdsBatchLoader
      * @param \Shopsys\FrameworkBundle\Model\Product\ProductFrontendLimitProvider $productFrontendLimitProvider
      */
@@ -34,26 +32,12 @@ class BestsellingProductsQuery extends AbstractQuery
     }
 
     /**
-     * @param \App\Model\Category\Category|\App\Model\CategorySeo\ReadyCategorySeoMix $categoryOrReadyCategorySeoMix
+     * @param \Shopsys\FrameworkBundle\Model\Category\Category $category
      * @return \GraphQL\Executor\Promise\Promise
      */
-    public function bestSellingProductsByCategoryOrReadyCategorySeoMixQuery(
-        Category|ReadyCategorySeoMix $categoryOrReadyCategorySeoMix,
+    public function bestSellingProductsByCategoryQuery(
+        Category $category,
     ): Promise {
-        if ($categoryOrReadyCategorySeoMix instanceof Category) {
-            $category = $categoryOrReadyCategorySeoMix;
-        } elseif ($categoryOrReadyCategorySeoMix instanceof ReadyCategorySeoMix) {
-            $category = $categoryOrReadyCategorySeoMix->getCategory();
-        } else {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'The "$categoryOrReadyCategorySeoMix" argument must be an instance of "%s" or "%s".',
-                    Category::class,
-                    ReadyCategorySeoMix::class,
-                ),
-            );
-        }
-
         $bestsellingProductsIds = $this->cachedBestsellingProductFacade->getOfferedBestsellingProductIds(
             $this->domain->getId(),
             $category,
