e0dbcab3d9fc35596da281b3c064a50959de8bb2
Martin Bača <martin.baca@shopsys.com>
2024-09-02 07:54:29 +0200

    Remove deprecated title, caption, geo_location, license + lastmod from image sitemap

--- packages/framework/src/Model/ImageSitemap/ImageSitemapItemImage.php
+++ packages/framework/src/Model/ImageSitemap/ImageSitemapItemImage.php
@@ -7,12 +7,4 @@ namespace Shopsys\FrameworkBundle\Model\ImageSitemap;
 class ImageSitemapItemImage
 {
     public string $loc;
-
-    public ?string $caption = null;
-
-    public ?string $geoLocation = null;
-
-    public string $title;
-
-    public ?string $license = null;
 }
