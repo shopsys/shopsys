f9c6f7688121b504fca3419d8ad2b5addafa6a7a
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-09-05 15:29:41 +0200

    admin: complaint form: an item description is now a textarea

--- packages/framework/src/Form/Admin/Complaint/ComplaintItemFormType.php
+++ packages/framework/src/Form/Admin/Complaint/ComplaintItemFormType.php
@@ -7,6 +7,7 @@ namespace Shopsys\FrameworkBundle\Form\Admin\Complaint;
 use Shopsys\FrameworkBundle\Model\Complaint\ComplaintItemData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,7 +27,7 @@ class ComplaintItemFormType extends AbstractType
             ->add('catnum', TextType::class, [
                 'disabled' => true,
             ])
-            ->add('description', TextType::class, [
+            ->add('description', TextareaType::class, [
                 'constraints' => [
                     new Constraints\NotBlank(['message' => 'Please enter description']),
                 ],
