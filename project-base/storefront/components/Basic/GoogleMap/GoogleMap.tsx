9f700ab295e9cb66ece0c02ecab5e8b6918c1c77
Jan Górecki <jan.gorecki@shopsys.com>
2024-09-11 23:38:09 +0200

    make google map interactive with store lists

--- project-base/storefront/components/Basic/GoogleMap/GoogleMap.tsx
+++ project-base/storefront/components/Basic/GoogleMap/GoogleMap.tsx
@@ -2,7 +2,7 @@ import { GoogleMapMarker } from './GoogleMapMarker';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
 import GoogleMapReact from 'google-map-react';
 import getConfig from 'next/config';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { PointFeature } from 'supercluster';
 import { MapMarker, MapMarkerNullable } from 'types/map';
 import useSupercluster from 'use-supercluster';
@@ -14,13 +14,14 @@ type GoogleMapProps = {
     markers?: MapMarkerNullable[];
     activeMarkerHandler?: (id: string) => void;
     isDetail?: boolean;
-    closeMarkers?: boolean;
 };
 
 type MarkerProperties = {
     cluster: boolean;
     markerIdentifier: string;
 };
+
+declare const google: any;
 
 const getMarkerIdentifier = (marker: MapMarker) => marker.identifier ?? `${marker.latitude}-${marker.longitude}`;
 
@@ -43,7 +44,6 @@ export const GoogleMap: FC<GoogleMapProps> = ({
     markers,
     activeMarkerHandler,
     isDetail,
-    closeMarkers,
 }) => {
     const { publicRuntimeConfig } = getConfig();
     const { mapSetting } = useDomainConfig();
@@ -52,6 +52,8 @@ export const GoogleMap: FC<GoogleMapProps> = ({
     const [activeMarkerIdentifier, setActiveMarkerIdentifier] = useState<string>('');
     const [zoom, setZoom] = useState<number>(defaultZoom ?? mapSetting.zoom);
     const [bounds, setBounds] = useState<GeoJSON.BBox>();
+    const mapRef = useRef<any>(null);
+    const [isGoogleApiLoaded, setIsGoogleApiLoaded] = useState(false);
 
     const markersClusterConfig: PointFeature<MarkerProperties>[] = useMemo(() => {
         const validMarkers = (markers?.filter((marker) => marker.latitude !== null && marker.longitude !== null) ??
@@ -60,11 +62,11 @@ export const GoogleMap: FC<GoogleMapProps> = ({
         return validMarkers.map(markerMapper);
     }, [markers]);
 
-    const { clusters } = useSupercluster({
+    const { clusters, supercluster } = useSupercluster({
         points: markersClusterConfig,
         zoom,
         bounds,
-        options: { radius: 75, maxZoom: 20 },
+        options: { radius: 75, minZoom: 0, maxZoom: 20 },
     });
 
     const selectMarkerHandler = (identifier: string) => {
@@ -75,15 +77,63 @@ export const GoogleMap: FC<GoogleMapProps> = ({
         }
     };
 
-    useEffect(() => {
-        if (closeMarkers) {
-            setActiveMarkerIdentifier('');
+    const handleClusterClick = (cluster: any) => {
+        const { cluster_id, point_count } = cluster.properties;
+
+        if (supercluster && cluster_id) {
+            const expansionZoom = Math.min(
+                supercluster.getClusterExpansionZoom(cluster_id),
+                20, // Max zoom level
+            );
+
+            const markersInCluster = supercluster.getLeaves(cluster_id, point_count); // Get all markers in this cluster
+
+            const latLngBounds = new google.maps.LatLngBounds();
+            markersInCluster.forEach((marker) => {
+                latLngBounds.extend({
+                    lat: marker.geometry.coordinates[1],
+                    lng: marker.geometry.coordinates[0],
+                });
+            });
+
+            setZoom(expansionZoom); // Set the zoom level to zoom into the cluster
+            mapRef.current?.fitBounds(latLngBounds); // Adjust map bounds to fit all markers
         }
-    }, [closeMarkers]);
+    };
+
+    useEffect(() => {
+        if (!isGoogleApiLoaded) {
+            return;
+        }
+
+        if (markersClusterConfig.length > 1 && mapRef.current !== null && google !== undefined) {
+            const newBounds = new google.maps.LatLngBounds();
+
+            markersClusterConfig.forEach((point) => {
+                newBounds.extend({
+                    lat: point.geometry.coordinates[1],
+                    lng: point.geometry.coordinates[0],
+                });
+            });
+
+            mapRef.current.fitBounds(newBounds);
+        } else if (markersClusterConfig.length === 1) {
+            if (mapRef.current !== null) {
+                mapRef.current.setZoom(15);
+                mapRef.current.panTo({
+                    lat: markersClusterConfig[0].geometry.coordinates[1],
+                    lng: markersClusterConfig[0].geometry.coordinates[0],
+                });
+            }
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [markersClusterConfig, isGoogleApiLoaded]);
 
     return (
         <div className="w-full">
             <GoogleMapReact
+                yesIWantToUseGoogleMapApiInternals
                 bootstrapURLKeys={{ key: publicRuntimeConfig.googleMapApiKey }}
                 defaultCenter={{ lat: defaultLatitude, lng: defaultLongitude }}
                 defaultZoom={defaultZoom ?? mapSetting.zoom}
@@ -95,6 +145,10 @@ export const GoogleMap: FC<GoogleMapProps> = ({
                 onChange={({ zoom, bounds }) => {
                     setZoom(zoom);
                     setBounds([bounds.nw.lng, bounds.se.lat, bounds.se.lng, bounds.nw.lat]);
+                }}
+                onGoogleApiLoaded={({ map }) => {
+                    mapRef.current = map;
+                    setIsGoogleApiLoaded(true);
                 }}
             >
                 {clusters.map((cluster) => {
@@ -111,6 +165,7 @@ export const GoogleMap: FC<GoogleMapProps> = ({
                             activeMarkerIdentifier={activeMarkerIdentifier}
                             cluster={cluster}
                             isDetail={isDetail}
+                            onClusterClicked={handleClusterClick}
                             onMarkerClicked={selectMarkerHandler}
                             {...coordinates}
                         />
