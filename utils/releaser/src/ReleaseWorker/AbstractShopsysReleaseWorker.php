07e5a75daf0faa38c55cc5f62536ec1695d00081
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-09-13 10:23:14 +0200

    releaser: administration package is not used in version 14.0

--- utils/releaser/src/ReleaseWorker/AbstractShopsysReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/AbstractShopsysReleaseWorker.php
@@ -38,6 +38,8 @@ abstract class AbstractShopsysReleaseWorker implements StageWorkerInterface
         'shopsys/monorepo-builder',
         'shopsys/backend-api',
         'shopsys/read-model',
+        // used in newer versions
+        'shopsys/administration',
         // forks
         'shopsys/postgres-search-bundle',
         'shopsys/doctrine-orm',
