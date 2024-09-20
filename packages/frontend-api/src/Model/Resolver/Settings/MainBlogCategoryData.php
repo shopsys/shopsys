55a4aee52722c081a2c1b7615d34705b930cf3bd
Martin Grossmann <martin@vyvoj.net>
2024-09-20 15:31:06 +0200

    setting query now can return main blog category image

---
+++ packages/frontend-api/src/Model/Resolver/Settings/MainBlogCategoryData.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Settings;
+
+class MainBlogCategoryData
+{
+    public $mainBlogCategoryUrl;
+
+    public $mainBlogCategoryMainImage;
+}
