2a365bd698e8bc01639dcc74499e64ef477bdfea
Martin Grossmann <martin@vyvoj.net>
2024-09-23 15:42:12 +0200

    fixup! setting query now can return main blog category image

--- packages/frontend-api/src/Model/Resolver/Settings/MainBlogCategoryData.php
+++ packages/frontend-api/src/Model/Resolver/Settings/MainBlogCategoryData.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Shopsys\FrontendApiBundle\Model\Resolver\Settings;
 
-use GraphQL\Executor\Promise\Promise;
-
 class MainBlogCategoryData
 {
-    public ?string $mainBlogCategoryUrl = null;
+    public $mainBlogCategoryUrl;
 
-    public ?Promise $mainBlogCategoryMainImage = null;
+    public $mainBlogCategoryMainImage;
 }
