04aa514e6a6344310b661c0f75432d65cceb7279
Martin Grossmann <martin@vyvoj.net>
2024-09-24 13:36:11 +0200

    product type can now be set in admin

--- packages/framework/src/Model/Product/ProductTypeEnum.php
+++ packages/framework/src/Model/Product/ProductTypeEnum.php
@@ -11,4 +11,15 @@ class ProductTypeEnum extends AbstractEnum
     public const string TYPE_BASIC = 'basic';
 
     public const string TYPE_INQUIRY = 'inquiry';
+
+    /**
+     * @return array<string, string>
+     */
+    public function getAllIndexedByTranslations(): array
+    {
+        return [
+            t('Basic') => self::TYPE_BASIC,
+            t('Upon inquiry') => self::TYPE_INQUIRY,
+        ];
+    }
 }
