1bc0081aa84585596fdfc3325377854a2f8d977f
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-09-27 13:46:09 +0200

    improved textation

--- project-base/app/check-composer-json.sh
+++ project-base/app/check-composer-json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f "../../parameters_monorepo.yaml" ]; then
-    echo 'Installing composer in monorepo project-base is not supported.'
+    echo 'Installing composer in the project-base subfolder in the monorepo is not supported. Install composer in the monorepo root instead.'
     exit 1
 fi
 
