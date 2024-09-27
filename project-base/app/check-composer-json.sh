0439acbd6d6b6609dcc82adcebc9b43548d4fb0d
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-09-27 13:22:35 +0200

    fixed downloading phing.phar in project named project-base

--- project-base/app/check-composer-json.sh
+++ project-base/app/check-composer-json.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f "../../parameters_monorepo.yaml" ]; then
+    echo 'Installing composer in monorepo project-base is not supported.'
+    exit 1
+fi
+
 if [ -h "composer.json" ]; then
     echo 'Please run composer install in the app/ subfolder.'
     exit 1
