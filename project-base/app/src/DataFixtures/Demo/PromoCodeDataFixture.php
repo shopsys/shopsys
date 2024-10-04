63a7e40553d86e067194dd06002686f9673813cf
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-10-04 11:52:26 +0200

    removed empty PromoCode classes from project-base

--- project-base/app/src/DataFixtures/Demo/PromoCodeDataFixture.php
+++ project-base/app/src/DataFixtures/Demo/PromoCodeDataFixture.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\DataFixtures\Demo;
 
-use App\Model\Order\PromoCode\PromoCode;
 use DateTime;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Shopsys\FrameworkBundle\Component\DataFixture\AbstractReferenceFixture;
+use Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCode;
 use Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeCategory\PromoCodeCategoryFactory;
 use Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeFacade;
@@ -32,8 +32,8 @@ class PromoCodeDataFixture extends AbstractReferenceFixture implements Dependent
     public const string PROMO_CODE_FOR_FREE_TRANSPORT_PAYMENT = 'promo_code_for_free_transport_and_payemnt';
 
     /**
-     * @param \App\Model\Order\PromoCode\PromoCodeFacade $promoCodeFacade
-     * @param \App\Model\Order\PromoCode\PromoCodeDataFactory $promoCodeDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeFacade $promoCodeFacade
+     * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeDataFactoryInterface $promoCodeDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeProduct\PromoCodeProductFactory $promoCodeProductFactory
      * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeCategory\PromoCodeCategoryFactory $promoCodeCategoryFactory
      * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeLimit\PromoCodeLimitFactory $promoCodeLimitFactory
@@ -182,7 +182,7 @@ class PromoCodeDataFixture extends AbstractReferenceFixture implements Dependent
     }
 
     /**
-     * @param \App\Model\Order\PromoCode\PromoCode $promoCode
+     * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCode $promoCode
      */
     private function setDefaultLimit(PromoCode $promoCode): void
     {
@@ -193,7 +193,7 @@ class PromoCodeDataFixture extends AbstractReferenceFixture implements Dependent
     }
 
     /**
-     * @param \App\Model\Order\PromoCode\PromoCode $promoCode
+     * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCode $promoCode
      */
     private function setDefaultNominalLimit(PromoCode $promoCode): void
     {
