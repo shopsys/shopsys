06a93768525630096e0a7518f436e9ca5ddeb751
MichalSrnicek <michal.srnicek@shopsys.com>
2024-10-07 15:58:04 +0200

    [SSP-2697] added load more button on the blog category page

--- project-base/storefront/utils/loadMore/mergeProductEdges.ts
+++
@@ -1,6 +0,0 @@
-import { TypeListedProductConnectionFragment } from 'graphql/requests/products/fragments/ListedProductConnectionFragment.generated';
-
-export const mergeProductEdges = (
-    previousProductEdges?: TypeListedProductConnectionFragment['edges'],
-    newProductEdges?: TypeListedProductConnectionFragment['edges'],
-) => [...(previousProductEdges || []), ...(newProductEdges || [])];
