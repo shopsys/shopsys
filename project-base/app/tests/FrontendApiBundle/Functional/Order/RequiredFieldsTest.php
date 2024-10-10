5e23f312f1ce1b87472b1fc3b84e511f758cf873
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-10-10 17:00:01 +0200

    FE API: order creation: email is always taken from the logged user

--- project-base/app/tests/FrontendApiBundle/Functional/Order/RequiredFieldsTest.php
+++ project-base/app/tests/FrontendApiBundle/Functional/Order/RequiredFieldsTest.php
@@ -13,27 +13,26 @@ class RequiredFieldsTest extends GraphQlTestCase
     public function testCreateMinimalOrderMutation(): void
     {
         $expectedViolationMessages = [
-            0 => 'Field OrderInput.firstName of required type String! was not provided.',
-            1 => 'Field OrderInput.lastName of required type String! was not provided.',
-            2 => 'Field OrderInput.email of required type String! was not provided.',
-            3 => 'Field OrderInput.telephone of required type String! was not provided.',
-            4 => 'Field OrderInput.onCompanyBehalf of required type Boolean! was not provided.',
-            5 => 'Field OrderInput.street of required type String! was not provided.',
-            6 => 'Field OrderInput.city of required type String! was not provided.',
-            7 => 'Field OrderInput.postcode of required type String! was not provided.',
-            8 => 'Field OrderInput.country of required type String! was not provided.',
-            9 => 'Field OrderInput.isDeliveryAddressDifferentFromBilling of required type Boolean! was not provided.',
-            10 => 'Field OrderInput.payment of required type PaymentInput! was not provided.',
-            11 => 'Field OrderInput.transport of required type TransportInput! was not provided.',
-            12 => 'Field OrderInput.products of required type [OrderProductInput!]! was not provided.',
+            'Field OrderInput.firstName of required type String! was not provided.',
+            'Field OrderInput.lastName of required type String! was not provided.',
+            'Field OrderInput.telephone of required type String! was not provided.',
+            'Field OrderInput.onCompanyBehalf of required type Boolean! was not provided.',
+            'Field OrderInput.street of required type String! was not provided.',
+            'Field OrderInput.city of required type String! was not provided.',
+            'Field OrderInput.postcode of required type String! was not provided.',
+            'Field OrderInput.country of required type String! was not provided.',
+            'Field OrderInput.isDeliveryAddressDifferentFromBilling of required type Boolean! was not provided.',
         ];
 
         $orderMutation = $this->getOrderMutation(__DIR__ . '/graphql/requiredFields.graphql');
 
         $response = $this->getResponseContentForQuery($orderMutation);
         $this->assertResponseContainsArrayOfErrors($response);
+        $errorsFromResponse = $this->getErrorsFromResponse($response);
 
-        foreach ($this->getErrorsFromResponse($response) as $key => $responseRow) {
+        $this->assertCount(count($expectedViolationMessages), $errorsFromResponse);
+
+        foreach ($errorsFromResponse as $key => $responseRow) {
             $this->assertArrayHasKey('message', $responseRow);
             $this->assertEquals($expectedViolationMessages[$key], $responseRow['message']);
         }
