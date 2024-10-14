f843211cfd1e7c51b090367fec6615b6de27f91f
Michal Vanek <michal.vanek@shopsys.com>
2024-10-14 11:31:05 +0200

    SSP-2751 - Set default ordering by first name in CustomerRepository::getCustomerUsersQueryBuilder

--- packages/framework/src/Model/Customer/CustomerRepository.php
+++ packages/framework/src/Model/Customer/CustomerRepository.php
@@ -77,6 +77,7 @@ class CustomerRepository
         return
             $this->getCustomerUsersQueryBuilder($customer)
             ->select('cu')
+            ->addOrderBy('cu.firstName, cu.lastName', 'ASC')
             ->getQuery()
             ->getResult();
     }
