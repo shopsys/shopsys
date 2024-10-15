b2c08e49509770cb16c36f536d54b9c10d64f8b5
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-10-15 15:01:58 +0200

    added brand and flag page filtration

--- project-base/storefront/store/useSessionStore.ts
+++ project-base/storefront/store/useSessionStore.ts
@@ -1,12 +1,14 @@
+import { createFilterPanelSlice, FilterPanelSlice } from './slices/createFilterPanelSlice';
 import { createPageLoadingStateSlice, PageLoadingStateSlice } from './slices/createPageLoadingStateSlice';
 import { createPortalSlice, PortalSlice } from './slices/createPortalSlice';
 import { createSeoCategorySlice, SeoCategorySlice } from './slices/createSeoCategorySlice';
 import { create } from 'zustand';
 
-type SessionStore = SeoCategorySlice & PageLoadingStateSlice & PortalSlice;
+type SessionStore = SeoCategorySlice & PageLoadingStateSlice & PortalSlice & FilterPanelSlice;
 
 export const useSessionStore = create<SessionStore>()((...store) => ({
     ...createSeoCategorySlice(...store),
     ...createPageLoadingStateSlice(...store),
     ...createPortalSlice(...store),
+    ...createFilterPanelSlice(...store),
 }));
