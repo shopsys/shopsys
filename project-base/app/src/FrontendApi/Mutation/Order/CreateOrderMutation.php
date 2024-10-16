f5b1194ddcbfc624e90b7b3e46ca6ba37eecbcdb
Michal Vanek <michal.vanek@shopsys.com>
2024-10-16 14:49:57 +0200

    fixup! SSP-2670 - FIX CS in CreateOrderMutation.php

--- project-base/app/src/FrontendApi/Mutation/Order/CreateOrderMutation.php
+++ project-base/app/src/FrontendApi/Mutation/Order/CreateOrderMutation.php
@@ -12,6 +12,7 @@ use Shopsys\FrontendApiBundle\Model\Mutation\Order\CreateOrderMutation as BaseCr
  * @property \App\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
  * @property \App\Model\Order\PlaceOrderFacade $placeOrderFacade
  * @method __construct(\App\FrontendApi\Model\Order\OrderDataFactory $orderDataFactory, \App\Model\Customer\User\CurrentCustomerUser $currentCustomerUser, \Shopsys\FrontendApiBundle\Model\Cart\CartApiFacade $cartApiFacade, \Shopsys\FrontendApiBundle\Model\Order\CreateOrderResultFactory $createOrderResultFactory, \Shopsys\FrontendApiBundle\Model\Cart\CartWatcherFacade $cartWatcherFacade, \Shopsys\FrameworkBundle\Component\Domain\Domain $domain, \Shopsys\FrameworkBundle\Model\Order\Processing\OrderProcessor $orderProcessor, \App\Model\Order\PlaceOrderFacade $placeOrderFacade, \Shopsys\FrameworkBundle\Model\Order\Processing\OrderInputFactory $orderInputFactory)
+ * @method string[] computeValidationGroups(\Overblog\GraphQLBundle\Definition\Argument $argument, \App\Model\Customer\User\CustomerUser|null $currentCustomerUser)
  */
 class CreateOrderMutation extends BaseCreateOrderMutation
 {
