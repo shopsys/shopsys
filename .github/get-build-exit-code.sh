457d05dc1ff59ac9e11892559c5f6d2770285212
Michal Vanek <michal.vanek@shopsys.com>
2024-10-17 16:48:07 +0200

    optimizing the cypress test and progressing to success build/success regenerate

--- .github/get-build-exit-code.sh
+++ .github/get-build-exit-code.sh
@@ -10,14 +10,13 @@ getBuildExitCodeBasedOnJobResults() {
     REVIEW_RESULT=${7}
     CHECK_CONSOLE_COMMANDS_RESULT=${8}
     TESTS_STOREFRONT_ACCEPTANCE_RESULT=${9}
-    TESTS_STOREFRONT_REGENERATE_RESULT=${10}
-    UNIT_TESTS_STOREFRONT_RESULT=${11}
+    UNIT_TESTS_STOREFRONT_RESULT=${10}
 
     if [[ "$BUILD_FORK_RESULT" == "success" ]]; then
         return 0
     fi
 
-    if [[ "$STANDARDS_RESULT" == "success" && "$TESTS_RESULT" == "success" && "$TESTS_ACCEPTANCE_RESULT" == "success" && "$STANDARDS_STOREFRONT_RESULT" == "success" && "$TRANSLATIONS_DUMP_RESULT" == "success" && "$REVIEW_RESULT" == "success" && "$CHECK_CONSOLE_COMMANDS_RESULT" == "success" && ("$TESTS_STOREFRONT_ACCEPTANCE_RESULT" == "success" || "$TESTS_STOREFRONT_REGENERATE_RESULT" == "success") && "$UNIT_TESTS_STOREFRONT_RESULT" == "success" ]]; then
+    if [[ "$STANDARDS_RESULT" == "success" && "$TESTS_RESULT" == "success" && "$TESTS_ACCEPTANCE_RESULT" == "success" && "$STANDARDS_STOREFRONT_RESULT" == "success" && "$TRANSLATIONS_DUMP_RESULT" == "success" && "$REVIEW_RESULT" == "success" && "$CHECK_CONSOLE_COMMANDS_RESULT" == "success" && "$TESTS_STOREFRONT_ACCEPTANCE_RESULT" == "success" && "$UNIT_TESTS_STOREFRONT_RESULT" == "success" ]]; then
         return 0
     else
         return 1
