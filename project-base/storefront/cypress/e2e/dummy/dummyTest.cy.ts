e7ec567e5bba009c520622c22a45c6aa9c0a91a1
Michal Vanek <michal.vanek@shopsys.com>
2024-10-18 12:35:38 +0200

    fixup! change PR label regenerate screenshots detector in build

--- project-base/storefront/cypress/e2e/dummy/dummyTest.cy.ts
+++ project-base/storefront/cypress/e2e/dummy/dummyTest.cy.ts
@@ -1,4 +1,10 @@
-describe('Dummy Test', () => {
+import { initializePersistStoreInLocalStorageToDefaultValues } from '../../support';
+
+describe('Dummy Test for blank others group', () => {
+    beforeEach(() => {
+        initializePersistStoreInLocalStorageToDefaultValues();
+    });
+
     it('should always pass', () => {
         expect(1).to.equal(1);
     });
