3996e211d227dda0c43bbeec8d8cdbcde1e3ca6b
Martin Grossmann <martin@vyvoj.net>
2024-10-21 15:06:10 +0200

    moved category graphql fields to frontend-api package

--- packages/frontend-api/src/Model/Resolver/Breadcrumb/Exception/UnableToGenerateBreadcrumbItemsUserError.php
+++ packages/frontend-api/src/Model/Resolver/Breadcrumb/Exception/UnableToGenerateBreadcrumbItemsUserError.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\FrontendApi\Component\Breadcrumb\Exception;
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Breadcrumb\Exception;
 
 use Overblog\GraphQLBundle\Error\UserError;
 use Shopsys\FrontendApiBundle\Model\Error\UserErrorWithCodeInterface;
 
 class UnableToGenerateBreadcrumbItemsUserError extends UserError implements UserErrorWithCodeInterface
 {
-    protected const CODE = 'unable-to-generate-breadcrumb-items';
+    protected const string CODE = 'unable-to-generate-breadcrumb-items';
 
     /**
      * {@inheritdoc}
