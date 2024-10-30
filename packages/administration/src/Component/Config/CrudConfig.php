244a93de8d1b0aa6223ebfd629dce9a01240615d
Jan Górecki <jan.gorecki@shopsys.com>
2024-10-31 00:54:55 +0100

    crud controller can be configured to be in menu

--- packages/administration/src/Component/Config/CrudConfig.php
+++ packages/administration/src/Component/Config/CrudConfig.php
@@ -18,30 +18,76 @@ final class CrudConfig
     /**
      * Sets a custom title for a given page type.
      *
-     * @param PageType $pageType
+     * @param \Shopsys\AdministrationBundle\Component\Config\PageType $pageType
      * @param string $title
      * @return $this
      */
     public function setTitle(PageType $pageType, string $title): self
     {
         $this->crudConfigData->customPageTitles[$pageType->value] = $title;
+
         return $this;
     }
 
+    /**
+     * Sets the title of the menu item that will be used.
+     *
+     * @param string $menuTitle
+     * @return $this
+     */
+    public function setMenuTitle(string $menuTitle): self
+    {
+        $this->crudConfigData->menuTitle = $menuTitle;
+
+        return $this;
+    }
 
     /**
      * Sets which default actions are enabled for the crud controller.
      *
-     * @param PageType[] $actions
+     * @param \Shopsys\AdministrationBundle\Component\Config\PageType[] $actions
      * @return $this
      */
     public function setActions(array $actions): self
     {
         Assert::allIsInstanceOfAny($actions, PageType::cases(), 'The given action is not a valid page type');
         $this->crudConfigData->defaultActions = $actions;
+
         return $this;
     }
 
+    /**
+     * TODO: Do something with $menuSection to be able to use some Enum instead of string
+     *
+     * Sets where the crud controller will be displayed in the side menu.
+     *
+     * @param string $menuSection Name of root level menu section
+     * @param string|null $submenuSection Name of submenu section
+     * @return self
+     */
+    public function setMenuSection(string $menuSection, ?string $submenuSection = null): self
+    {
+        $this->crudConfigData->menuSection = $menuSection;
+        $this->crudConfigData->submenuSection = $submenuSection;
+
+        return $this;
+    }
+
+    /**
+     * Hides the CRUD controller in the side menu.
+     *
+     * @return $this
+     */
+    public function hideInMenu(): self
+    {
+        $this->crudConfigData->visibleInMenu = false;
+
+        return $this;
+    }
+
+    /**
+     * @return \Shopsys\AdministrationBundle\Component\Config\CrudConfigData
+     */
     public function getConfig(): CrudConfigData
     {
         return $this->crudConfigData;
--- packages/administration/src/Component/Config/CrudConfigData.php
+++ packages/administration/src/Component/Config/CrudConfigData.php
@@ -10,21 +10,69 @@ final class CrudConfigData
         PageType::CREATE->value => null,
         PageType::EDIT->value => null,
         PageType::LIST->value => null,
-        PageType::DETAIL->value => null
+        PageType::DETAIL->value => null,
     ];
+
+    public ?string $menuTitle = null;
 
     public array $defaultActions = [PageType::LIST];
 
+    public string $menuSection = 'root';
+
+    public ?string $submenuSection = null;
+
+    public bool $visibleInMenu = true;
+
+    /**
+     * @param \Shopsys\AdministrationBundle\Component\Config\PageType $pageType
+     * @return string|null
+     */
     public function getTitle(PageType $pageType): ?string
     {
-        return $this->customPageTitles[$pageType->value] ?? null;
+        return $this->customPageTitles[$pageType->value] ?? 'test'; // TODO: hotfix for menu, rewrite to generate page titles based on entity class
     }
 
     /**
-     * @return PageType[]
+     * @return string|null
+     */
+    public function getMenuTitle(): ?string
+    {
+        if ($this->menuTitle !== null) {
+            return $this->menuTitle;
+        }
+
+        return $this->getTitle(PageType::LIST);
+    }
+
+    /**
+     * @return \Shopsys\AdministrationBundle\Component\Config\PageType[]
      */
     public function getDefaultActions(): array
     {
         return $this->defaultActions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMenuSection(): string
+    {
+        return $this->menuSection;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubmenuSection(): ?string
+    {
+        return $this->submenuSection;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVisibleInMenu(): bool
+    {
+        return $this->visibleInMenu;
     }
 }
