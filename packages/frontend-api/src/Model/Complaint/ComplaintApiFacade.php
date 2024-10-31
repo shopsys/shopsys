c467ad2e96b1d50bfc3388775adf9662ba6ac409
Michal Vanek <michal.vanek@shopsys.com>
2024-10-31 09:03:01 +0100

    fixup! FE API: role ROLE_ALL_API sees all customer user complaints

--- packages/frontend-api/src/Model/Complaint/ComplaintApiFacade.php
+++ packages/frontend-api/src/Model/Complaint/ComplaintApiFacade.php
@@ -184,7 +184,7 @@ class ComplaintApiFacade
      * @param int $limit
      * @param int $offset
      * @param string|null $search
-     * @return array
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Complaint[]
      */
     public function getCustomerUserComplaintsLimitedList(
         CustomerUser $customerUser,
