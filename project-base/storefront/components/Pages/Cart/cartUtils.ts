279ebfe61c81919a3a373b94c48935328be67e13
Jan Molcik <molcik.jan@gmail.com>
2024-11-13 16:40:15 +0000

    remove unused code

--- project-base/storefront/components/Pages/Cart/cartUtils.ts
+++ project-base/storefront/components/Pages/Cart/cartUtils.ts
@@ -4,12 +4,9 @@ import { useSessionStore } from 'store/useSessionStore';
 import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
 
 export const useCartPageNavigation = () => {
-    const { url, convertimProjectUuid } = useDomainConfig();
+    const { url } = useDomainConfig();
     const router = useRouter();
-    const [transportAndPaymentUrl, convertim] = getInternationalizedStaticUrls(
-        ['/order/transport-and-payment', '/order/convertim'],
-        url,
-    );
+    const [transportAndPaymentUrl] = getInternationalizedStaticUrls(['/order/transport-and-payment'], url);
     const updatePageLoadingState = useSessionStore((s) => s.updatePageLoadingState);
 
     const goToPreviousStepFromCartPage = () => {
@@ -19,10 +16,6 @@ export const useCartPageNavigation = () => {
 
     const goToNextStepFromCartPage = () => {
         updatePageLoadingState({ isPageLoading: true, redirectPageType: 'transport-and-payment' });
-        if (convertimProjectUuid) {
-            router.push(convertim);
-            return;
-        }
         router.push(transportAndPaymentUrl);
     };
 
