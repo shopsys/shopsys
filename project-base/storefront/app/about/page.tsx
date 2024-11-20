f555cf0f84ba3652d21648eae31cf832355bda8f
Jan Molcik <molcik.jan@gmail.com>
2024-11-20 16:07:43 +0000

    share urql ssrDate with client components using hooks

--- project-base/storefront/app/about/page.tsx
+++ project-base/storefront/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { getUrqlExchanges } from 'app/_urql/exchanges';
+import { getUrqlData } from 'app/_urql/urql-dto';
 import {
     BlogArticleDetailQueryDocument,
     TypeBlogArticleDetailQuery,
@@ -9,62 +9,10 @@ import {
     TypeCurrentCustomerUserQuery,
     TypeCurrentCustomerUserQueryVariables,
 } from 'graphql/requests/customer/queries/CurrentCustomerUserQuery.ssr';
-import getConfig from 'next/config';
-import { headers } from 'next/headers';
-// eslint-disable-next-line no-restricted-imports
-import { createClient, OperationResult, ssrExchange } from 'urql';
-import { fetcher } from 'urql/fetcher';
-import { getDomainConfig } from 'utils/domain/domainConfig';
+import { Client, OperationResult } from 'urql';
 import { getServerT } from 'utils/getServerTranslation';
 
-async function getRedis() {
-    const createRedisClient = (await import('redis')).createClient;
-
-    const redisClient = createRedisClient({
-        url: `redis://${process.env.REDIS_HOST}`,
-        socket: {
-            connectTimeout: 5000,
-        },
-    });
-
-    return redisClient;
-}
-
-async function getClient() {
-    const t = await getServerT();
-    const { serverRuntimeConfig } = getConfig();
-    const internalGraphqlEndpoint = serverRuntimeConfig?.internalGraphqlEndpoint ?? undefined;
-
-    const domainConfig = getDomainConfig(headers().get('host')!);
-
-    const publicGraphqlEndpoint = domainConfig.publicGraphqlEndpoint;
-    const publicGraphqlEndpointObject = new URL(publicGraphqlEndpoint);
-
-    const redisClient = await getRedis();
-
-    await redisClient.connect();
-
-    const client = createClient({
-        url: internalGraphqlEndpoint ?? publicGraphqlEndpoint,
-        exchanges: await getUrqlExchanges(ssrExchange({ isClient: false }), t),
-        fetchOptions: {
-            headers: {
-                OriginalHost: publicGraphqlEndpointObject.host,
-                'X-Forwarded-Proto': publicGraphqlEndpointObject.protocol === 'https:' ? 'on' : 'off',
-            },
-            cache: 'no-store',
-        },
-        fetch: fetcher(redisClient),
-    });
-
-    redisClient.disconnect();
-
-    return client;
-}
-
-async function getArticle() {
-    const client = await getClient();
-
+async function getArticle(client: Client) {
     const blogArticleResponse: OperationResult<TypeBlogArticleDetailQuery, TypeBlogArticleDetailQueryVariables> =
         await client.query(BlogArticleDetailQueryDocument, {
             urlSlug: 'blog-article-example-2-en',
@@ -73,9 +21,7 @@ async function getArticle() {
     return blogArticleResponse;
 }
 
-async function getCustomerUser() {
-    const client = await getClient();
-
+async function getCustomerUser(client: Client) {
     const customerUserResponse: OperationResult<TypeCurrentCustomerUserQuery, TypeCurrentCustomerUserQueryVariables> =
         await client.query(CurrentCustomerUserQueryDocument, {});
 
@@ -84,8 +30,13 @@ async function getCustomerUser() {
 
 export default async function IndexPage() {
     const t = await getServerT();
-    const { data } = await getArticle();
-    const { data: customerUserData } = await getCustomerUser();
+    const { client } = await getUrqlData();
+    const { data } = await getArticle(client);
+    const { data: customerUserData } = await getCustomerUser(client);
+    console.log('🚀 -> file: page.tsx:40 -> IndexPage -> customerUserData:', customerUserData);
+
+    /* const resp = await setTokensToCookiesServer('hello', 'world');
+    console.log("🚀 -> file: page.tsx:44 -> IndexPage -> resp:", resp) */
 
     return (
         <div>
---
+++ project-base/storefront/app/refresh-token/page.tsx
@@ -0,0 +1,9 @@
+import { CustomerInfo } from './CustomerInfo';
+
+export default async function RefreshTokenPage() {
+    return (
+        <div>
+            <CustomerInfo />
+        </div>
+    );
+}
