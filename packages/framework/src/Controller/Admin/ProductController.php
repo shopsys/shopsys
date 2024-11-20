dfb4a5d53b17cb129ced08740f68270b3e635e38
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-11-20 14:00:04 +0100

    fixed getting GET data for quick search form

--- packages/framework/src/Controller/Admin/ProductController.php
+++ packages/framework/src/Controller/Admin/ProductController.php
@@ -178,7 +178,7 @@ class ProductController extends AdminBaseController
 
         // Cannot call $form->handleRequest() because the GET forms are not handled in POST request.
         // See: https://github.com/symfony/symfony/issues/12244
-        $quickSearchForm->submit($request->query->get($quickSearchForm->getName()));
+        $quickSearchForm->submit($request->get($quickSearchForm->getName()));
 
         $massActionForm = $this->createForm(ProductMassActionFormType::class);
         $massActionForm->handleRequest($request);
