4b23de924545fc0fada5237d65bbfa3a3bf50bd9
Jan Molcik <molcik.jan@gmail.com>
2024-11-21 22:12:17 +0000

    remove unused client-side urql client provider

--- project-base/storefront/app/refresh-token/CustomerInfo.tsx
+++ project-base/storefront/app/refresh-token/CustomerInfo.tsx
@@ -1,22 +1,5 @@
 'use client';
 
-import { useCurrentCustomerData } from 'connectors/customer/CurrentCustomer';
-
 export const CustomerInfo: FC = () => {
-    const customer = useCurrentCustomerData();
-
-    return (
-        <div>
-            {customer ? (
-                <div className="flex flex-col gap-3">
-                    <span>
-                        {customer.firstName} {customer.lastName}
-                    </span>
-                    <span>{customer.email}</span>
-                </div>
-            ) : (
-                'No customer data'
-            )}
-        </div>
-    );
+    return <div>No customer client customer data yet!</div>;
 };
