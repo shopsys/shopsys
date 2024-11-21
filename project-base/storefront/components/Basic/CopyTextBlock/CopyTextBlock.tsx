7b5dba781f1136f739dc79f2d3148d01bd107b4f
Jan Molcik <molcik.jan@gmail.com>
2024-11-21 09:17:38 +0000

    add server action to set cookies
    remove logs and prettify

--- project-base/storefront/components/Basic/CopyTextBlock/CopyTextBlock.tsx
+++ project-base/storefront/components/Basic/CopyTextBlock/CopyTextBlock.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { Button } from 'components/Forms/Button/Button';
 import { useState } from 'react';
--- project-base/storefront/utils/toasts/showErrorMessage.tsx
+++ project-base/storefront/utils/toasts/showErrorMessage.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { showMessage } from './showMessage';
 import { GtmMessageDetailType } from 'gtm/enums/GtmMessageDetailType';
