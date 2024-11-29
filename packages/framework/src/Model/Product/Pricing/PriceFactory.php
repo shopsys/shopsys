2398578cd0a3c1aefc59fd8e68c150ae3f351308
Martin Grossmann <martin@vyvoj.net>
2024-11-29 14:57:43 +0100

    refactored PriceFactory for easier usage

--- packages/framework/src/Model/Product/Pricing/PriceFactory.php
+++ packages/framework/src/Model/Product/Pricing/PriceFactory.php
@@ -12,7 +12,7 @@ use Shopsys\FrameworkBundle\Model\Pricing\Price;
 class PriceFactory
 {
     /**
-     * @param array $pricesArray
+     * @param array<int, array{price_without_vat: mixed, price_with_vat: mixed, price_from: bool, pricing_group_id: int}> $pricesArray
      * @param \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup $pricingGroup
      * @return \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPrice
      */
@@ -22,14 +22,33 @@ class PriceFactory
     ): ProductPrice {
         foreach ($pricesArray as $priceArray) {
             if ($priceArray['pricing_group_id'] === $pricingGroup->getId()) {
-                $priceWithoutVat = Money::create((string)$priceArray['price_without_vat']);
-                $priceWithVat = Money::create((string)$priceArray['price_with_vat']);
-                $price = new Price($priceWithoutVat, $priceWithVat);
-
-                return new ProductPrice($price, $priceArray['price_from']);
+                return $this->createProductPriceFromArray($priceArray);
             }
         }
 
         throw new NoProductPriceForPricingGroupException(0, $pricingGroup->getId());
+    }
+
+    /**
+     * @param array{price_without_vat: mixed, price_with_vat: mixed, price_from: bool} $priceArray
+     * @return \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPrice
+     */
+    public function createProductPriceFromArray(array $priceArray): ProductPrice
+    {
+        $price = $this->createPriceFromArray($priceArray);
+
+        return new ProductPrice($price, $priceArray['price_from']);
+    }
+
+    /**
+     * @param array{price_without_vat: mixed, price_with_vat: mixed} $priceArray
+     * @return \Shopsys\FrameworkBundle\Model\Pricing\Price
+     */
+    public function createPriceFromArray(array $priceArray): Price
+    {
+        $priceWithoutVat = Money::create((string)$priceArray['price_without_vat']);
+        $priceWithVat = Money::create((string)$priceArray['price_with_vat']);
+
+        return new Price($priceWithoutVat, $priceWithVat);
     }
 }
