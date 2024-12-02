620a3b7ddff8bb0d830e3106de27bd4ea3b9c78d
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-12-02 14:19:59 +0100

    added server action reset password

---
+++ project-base/storefront/app/reset-password/loading.tsx
@@ -0,0 +1,5 @@
+import { SkeletonPageResetPassword } from 'components/Blocks/Skeleton/SkeletonPageResetPassword';
+
+export default function Loading() {
+    return <SkeletonPageResetPassword />;
+}
---
+++ project-base/storefront/components/Blocks/Skeleton/SkeletonPageResetPassword.tsx
@@ -0,0 +1,14 @@
+import { SkeletonModuleBreadcrumbs } from './SkeletonModuleBreadcrumbs';
+import { Webline } from 'components/Layout/Webline/Webline';
+import Skeleton from 'react-loading-skeleton';
+
+export const SkeletonPageResetPassword: FC = () => (
+    <Webline>
+        <SkeletonModuleBreadcrumbs count={2} />
+
+        <div className="mx-auto flex w-full max-w-3xl flex-col">
+            <Skeleton className="h-10 w-72" containerClassName="flex mb-5" />
+            <Skeleton className="h-[180px]" containerClassName="flex w-full" />
+        </div>
+    </Webline>
+);
