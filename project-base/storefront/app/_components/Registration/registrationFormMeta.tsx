c4e4fb15efdaf3f251db196f0ca575da693b8688
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-12-04 16:15:54 +0100

    added server action registration

---
+++ project-base/storefront/app/_components/Registration/registrationFormMeta.tsx
@@ -0,0 +1,258 @@
+'use client';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link, linkPlaceholderTwClass } from 'components/Basic/Link/Link';
+import {
+    validateCity,
+    validateCompanyNameRequired,
+    validateCompanyNumber,
+    validateCompanyTaxNumber,
+    validateCountry,
+    validateCustomer,
+    validateEmail,
+    validateFirstName,
+    validateLastName,
+    validatePassword,
+    validatePostcode,
+    validatePrivacyPolicy,
+    validatePasswordConfirm,
+    validateStreet,
+    validateTelephoneRequired,
+} from 'components/Forms/validationRules';
+import { useSettings } from 'components/providers/SettingsProvider';
+import Trans from 'next-translate/Trans';
+import useTranslation from 'next-translate/useTranslation';
+import { useMemo } from 'react';
+import { FieldError, UseFormReturn, useWatch } from 'react-hook-form';
+import { CustomerTypeEnum } from 'types/customer';
+import { RegistrationFormType } from 'types/form';
+import { SelectOptionType } from 'types/selectOptions';
+import { useShopsysForm } from 'utils/forms/useShopsysForm';
+import * as Yup from 'yup';
+
+type UseRegistrationFormProps = {
+    preSelectedCountry: SelectOptionType;
+};
+
+export const useRegistrationForm = ({
+    preSelectedCountry,
+}: UseRegistrationFormProps): [UseFormReturn<RegistrationFormType>, RegistrationFormType] => {
+    const { t } = useTranslation();
+    const resolver = yupResolver(
+        Yup.object().shape<Record<keyof RegistrationFormType, any>>({
+            email: validateEmail(t),
+            password: validatePassword(t),
+            passwordConfirm: validatePasswordConfirm(t),
+            customer: validateCustomer(),
+            telephone: validateTelephoneRequired(t),
+            firstName: validateFirstName(t),
+            lastName: validateLastName(t),
+            street: validateStreet(t),
+            city: validateCity(t),
+            postcode: validatePostcode(t),
+            country: validateCountry(t),
+            companyName: Yup.string().when('customer', {
+                is: (customer: string) => customer === 'companyCustomer',
+                then: () => validateCompanyNameRequired(t),
+                otherwise: (schema) => schema,
+            }),
+            companyNumber: Yup.string().when('customer', {
+                is: (customer: string) => customer === 'companyCustomer',
+                then: () => validateCompanyNumber(t),
+                otherwise: (schema) => schema,
+            }),
+            companyTaxNumber: Yup.string().when('customer', {
+                is: (customer: string) => customer === 'companyCustomer',
+                then: () => validateCompanyTaxNumber(t),
+                otherwise: (schema) => schema,
+            }),
+            gdprAgreement: validatePrivacyPolicy(t),
+            newsletterSubscription: Yup.boolean(),
+        }),
+    );
+    const defaultValues = {
+        email: '',
+        password: '',
+        passwordConfirm: '',
+        customer: CustomerTypeEnum.CommonCustomer,
+        telephone: '',
+        firstName: '',
+        lastName: '',
+        street: '',
+        city: '',
+        postcode: '',
+        country: preSelectedCountry,
+        companyName: '',
+        companyNumber: '',
+        companyTaxNumber: '',
+        gdprAgreement: false,
+        newsletterSubscription: false,
+    };
+
+    return [useShopsysForm(resolver, defaultValues), defaultValues];
+};
+
+type RegistrationFormMetaType = {
+    formName: string;
+    messages: {
+        error: string;
+        successAndLogged: string;
+    };
+    fields: {
+        [key in keyof RegistrationFormType]: {
+            name: key;
+            label: string | JSX.Element;
+            errorMessage: string | undefined;
+        };
+    };
+};
+
+export const useRegistrationFormMeta = (
+    formProviderMethods: UseFormReturn<RegistrationFormType>,
+): RegistrationFormMetaType => {
+    const { t } = useTranslation();
+    const isEmailValid = formProviderMethods.formState.errors.email === undefined;
+    const { privacyPolicyArticleUrl } = useSettings();
+
+    const customerFieldName = 'customer' as const;
+
+    const [customerValue] = useWatch({
+        name: [customerFieldName],
+        control: formProviderMethods.control,
+    });
+
+    const errors = formProviderMethods.formState.errors;
+
+    const formMeta = useMemo(
+        () => ({
+            formName: 'registration-form',
+            messages: {
+                error: t('Could not create account'),
+                successAndLogged: t('Your account has been created and you are logged in now'),
+            },
+            fields: {
+                email: {
+                    name: 'email' as const,
+                    label: t('Your email'),
+                    errorMessage: errors.email?.message,
+                },
+                password: {
+                    name: 'password' as const,
+                    label: t('Password'),
+                    errorMessage: errors.password?.message,
+                },
+                passwordConfirm: {
+                    name: 'passwordConfirm' as const,
+                    label: t('Password again'),
+                    errorMessage: errors.passwordConfirm?.message,
+                },
+                [customerFieldName]: {
+                    name: customerFieldName,
+                    label: t('You will shop with us as'),
+                    errorMessage: errors.customer?.message,
+                },
+                telephone: {
+                    name: 'telephone' as const,
+                    label: t('Phone'),
+                    errorMessage: errors.telephone?.message,
+                },
+                firstName: {
+                    name: 'firstName' as const,
+                    label: t('First name'),
+                    errorMessage: errors.firstName?.message,
+                },
+                lastName: {
+                    name: 'lastName' as const,
+                    label: t('Last name'),
+                    errorMessage: errors.lastName?.message,
+                },
+                companyName: {
+                    name: 'companyName' as const,
+                    label: t('Company name'),
+                    errorMessage:
+                        customerValue === CustomerTypeEnum.CompanyCustomer ? errors.companyName?.message : undefined,
+                },
+                companyNumber: {
+                    name: 'companyNumber' as const,
+                    label: t('Company number'),
+                    errorMessage:
+                        customerValue === CustomerTypeEnum.CompanyCustomer ? errors.companyNumber?.message : undefined,
+                },
+                companyTaxNumber: {
+                    name: 'companyTaxNumber' as const,
+                    label: t('Tax number'),
+                    errorMessage:
+                        customerValue === CustomerTypeEnum.CompanyCustomer
+                            ? errors.companyTaxNumber?.message
+                            : undefined,
+                },
+                street: {
+                    name: 'street' as const,
+                    label: t('Street and house no.'),
+                    errorMessage: errors.street?.message,
+                },
+                city: {
+                    name: 'city' as const,
+                    label: t('City'),
+                    errorMessage: errors.city?.message,
+                },
+                postcode: {
+                    name: 'postcode' as const,
+                    label: t('Postcode'),
+                    errorMessage: errors.postcode?.message,
+                },
+                country: {
+                    name: 'country' as const,
+                    label: t('Country'),
+                    errorMessage: (errors.country as FieldError | undefined)?.message,
+                },
+                gdprAgreement: {
+                    name: 'gdprAgreement' as const,
+                    label: (
+                        <Trans
+                            defaultTrans="I agree with <lnk1>processing of privacy policy</lnk1>."
+                            i18nKey="GdprAgreementCheckbox"
+                            components={{
+                                lnk1: privacyPolicyArticleUrl ? (
+                                    <Link isExternal href={privacyPolicyArticleUrl} target="_blank" />
+                                ) : (
+                                    <span className={linkPlaceholderTwClass} />
+                                ),
+                            }}
+                        />
+                    ),
+                    errorMessage: errors.gdprAgreement?.message,
+                },
+                newsletterSubscription: {
+                    name: 'newsletterSubscription' as const,
+                    label: t('I want to subscribe to the newsletter'),
+                    errorMessage: isEmailValid ? errors.newsletterSubscription?.message : undefined,
+                },
+            },
+        }),
+        [
+            t,
+            errors.email?.message,
+            errors.password?.message,
+            errors.passwordConfirm?.message,
+            errors.customer?.message,
+            errors.telephone?.message,
+            errors.firstName?.message,
+            errors.lastName?.message,
+            errors.companyName?.message,
+            errors.companyNumber?.message,
+            errors.companyTaxNumber?.message,
+            errors.street?.message,
+            errors.city?.message,
+            errors.postcode?.message,
+            errors.country,
+            errors.gdprAgreement?.message,
+            errors.newsletterSubscription?.message,
+            customerValue,
+            privacyPolicyArticleUrl,
+            isEmailValid,
+        ],
+    );
+
+    return formMeta;
+};
--- project-base/storefront/app/layout.tsx
+++ project-base/storefront/app/layout.tsx
@@ -1,12 +1,14 @@
 import { getIsUserLoggedInQuery } from './_queries/getIsUserLoggedInQuery';
 import { createQuery } from './_urql/urql-dto';
 import imageLogo from '/public/images/logo.svg';
+import { getSettingsQuery } from 'app/_queries/getSettingsQuery';
 import { ExtendedNextLink } from 'components/Basic/ExtendedNextLink/ExtendedNextLink';
 import { Image } from 'components/Basic/Image/Image';
 import { Navigation } from 'components/Layout/Header/Navigation/Navigation';
 import AuthProvider from 'components/providers/AuthProvider';
 import BroadcastChannelProvider from 'components/providers/BroadcastChannelProvider';
 import { DomainConfigProvider } from 'components/providers/DomainConfigProvider';
+import { SettingsProvider } from 'components/providers/SettingsProvider';
 import ToastifyProvider from 'components/providers/ToastifyProvider';
 import { TranslationProvider } from 'components/providers/TranslationProvider';
 import { DEFAULT_SKELETON_TYPE } from 'config/constants';
@@ -29,83 +31,129 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     const domainConfig = getDomainConfig(headers().get('host')!);
     const { defaultLocale: lang } = domainConfig;
     const dictionary = await getDictionary(lang);
-    const [t, navigationResponse, isUserLoggedIn] = await Promise.all([
+    const [t, navigationResponse, isUserLoggedIn, settings] = await Promise.all([
         getServerT({ defaultLang: lang, defaultDictionary: dictionary }),
         createQuery<TypeNavigationQuery, TypeNavigationQueryVariables>(NavigationQueryDocument, {}),
         getIsUserLoggedInQuery(),
+        getSettingsQuery(),
     ]);
     const { data: navigationData } = navigationResponse;
     const currentYear = new Date().getFullYear();
 
     return (
         <DomainConfigProvider domainConfig={domainConfig}>
-            <TranslationProvider dictionary={dictionary} lang={lang}>
-                <html lang={lang}>
-                    {/* suppressHydrationWarning for ignoring grammarly extension */}
-                    <body suppressHydrationWarning>
-                        <ToastifyProvider>
-                            <BroadcastChannelProvider>
-                                <AuthProvider isUserLoggedIn={isUserLoggedIn}>
-                                    <nav className="flex bg-gradient-to-tr from-backgroundBrand to-backgroundBrandLess px-4">
-                                        <div className="flex w-fit shrink-0">
-                                            <ExtendedNextLink
-                                                href="/app"
-                                                className={twJoin(
-                                                    'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
-                                                    'text-linkInverted no-underline',
-                                                    'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                    'active:text-linkInvertedHovered',
-                                                    'disabled:text-linkInvertedDisabled',
+            <SettingsProvider settings={settings}>
+                <TranslationProvider dictionary={dictionary} lang={lang}>
+                    <html lang={lang}>
+                        {/* suppressHydrationWarning for ignoring grammarly extension */}
+                        <body suppressHydrationWarning>
+                            <ToastifyProvider>
+                                <BroadcastChannelProvider>
+                                    <AuthProvider isUserLoggedIn={isUserLoggedIn}>
+                                        <nav className="flex bg-gradient-to-tr from-backgroundBrand to-backgroundBrandLess px-4">
+                                            <div className="flex w-fit shrink-0">
+                                                <ExtendedNextLink
+                                                    href="/"
+                                                    className={twJoin(
+                                                        'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                                        'text-linkInverted no-underline',
+                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                                        'active:text-linkInvertedHovered',
+                                                        'disabled:text-linkInvertedDisabled',
+                                                    )}
+                                                >
+                                                    HP
+                                                </ExtendedNextLink>
+                                                <ExtendedNextLink
+                                                    href="/app"
+                                                    className={twJoin(
+                                                        'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                                        'text-linkInverted no-underline',
+                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                                        'active:text-linkInvertedHovered',
+                                                        'disabled:text-linkInvertedDisabled',
+                                                    )}
+                                                >
+                                                    APP
+                                                </ExtendedNextLink>
+                                                {!isUserLoggedIn && (
+                                                    <>
+                                                        <ExtendedNextLink
+                                                            href="/login"
+                                                            className={twJoin(
+                                                                'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                                                'text-linkInverted no-underline',
+                                                                'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                                                'active:text-linkInvertedHovered',
+                                                                'disabled:text-linkInvertedDisabled',
+                                                            )}
+                                                        >
+                                                            {t('Login')}
+                                                        </ExtendedNextLink>
+                                                        <ExtendedNextLink
+                                                            href="/registration"
+                                                            className={twJoin(
+                                                                'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                                                'text-linkInverted no-underline',
+                                                                'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                                                'active:text-linkInvertedHovered',
+                                                                'disabled:text-linkInvertedDisabled',
+                                                            )}
+                                                        >
+                                                            {t('Registration')}
+                                                        </ExtendedNextLink>
+                                                    </>
                                                 )}
-                                            >
-                                                {t('Home page')}
-                                            </ExtendedNextLink>
-                                            <ExtendedNextLink
-                                                href="/about"
-                                                className={twJoin(
-                                                    'relative m-0 flex items-center p-5 pr-10 font-secondary text-sm font-bold vl:text-base',
-                                                    'text-linkInverted no-underline',
-                                                    'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                    'active:text-linkInvertedHovered',
-                                                    'disabled:text-linkInvertedDisabled',
+                                                <ExtendedNextLink
+                                                    href="/about"
+                                                    className={twJoin(
+                                                        'relative m-0 flex items-center p-5 pr-10 font-secondary text-sm font-bold vl:text-base',
+                                                        'text-linkInverted no-underline',
+                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                                        'active:text-linkInvertedHovered',
+                                                        'disabled:text-linkInvertedDisabled',
+                                                    )}
+                                                >
+                                                    {t('About Shopsys')}
+                                                </ExtendedNextLink>
+                                            </div>
+                                            {navigationData && (
+                                                <Navigation
+                                                    navigation={navigationData.navigation}
+                                                    skeletonType={DEFAULT_SKELETON_TYPE}
+                                                />
+                                            )}
+                                        </nav>
+                                        <div>Language in layout is {lang}</div>
+                                        {children}
+                                        <div className="flex flex-col items-center text-center">
+                                            <div className="flex items-center text-sm text-textDisabled">
+                                                {t(
+                                                    'Copyright © {{ currentYear }}, Shopsys s.r.o. All rights reserved.',
+                                                    {
+                                                        currentYear,
+                                                    },
                                                 )}
-                                            >
-                                                {t('About Shopsys')}
-                                            </ExtendedNextLink>
+                                            </div>
+                                            <div className="flex items-center text-sm text-textDisabled">
+                                                {t('Customized E-shop by')}
+                                                <a
+                                                    className="ml-2 flex w-20"
+                                                    href="https://www.shopsys.com"
+                                                    rel="noreferrer"
+                                                    target="_blank"
+                                                >
+                                                    <Image alt="footer logo" src={imageLogo} />
+                                                </a>
+                                            </div>
                                         </div>
-                                        {navigationData && (
-                                            <Navigation
-                                                navigation={navigationData.navigation}
-                                                skeletonType={DEFAULT_SKELETON_TYPE}
-                                            />
-                                        )}
-                                    </nav>
-                                    <div>Language in layout is {lang}</div>
-                                    {children}
-                                    <div className="flex flex-col items-center text-center">
-                                        <div className="flex items-center text-sm text-textDisabled">
-                                            {t('Copyright © {{ currentYear }}, Shopsys s.r.o. All rights reserved.', {
-                                                currentYear,
-                                            })}
-                                        </div>
-                                        <div className="flex items-center text-sm text-textDisabled">
-                                            {t('Customized E-shop by')}
-                                            <a
-                                                className="ml-2 flex w-20"
-                                                href="https://www.shopsys.com"
-                                                rel="noreferrer"
-                                                target="_blank"
-                                            >
-                                                <Image alt="footer logo" src={imageLogo} />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </AuthProvider>
-                            </BroadcastChannelProvider>
-                        </ToastifyProvider>
-                    </body>
-                </html>
-            </TranslationProvider>
+                                    </AuthProvider>
+                                </BroadcastChannelProvider>
+                            </ToastifyProvider>
+                        </body>
+                    </html>
+                </TranslationProvider>
+            </SettingsProvider>
         </DomainConfigProvider>
     );
 }
