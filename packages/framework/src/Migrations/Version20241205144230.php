edd1999bcc5c287d2824e93bc964810d75cfee17
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2024-12-05 16:15:52 +0100

    add trigram indexes on columns used for quick search with like condition

---
+++ packages/framework/src/Migrations/Version20241205144230.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20241205144230 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        //product indexes
+        $this->sql('CREATE INDEX products_catnum_trgm_idx ON products USING gin (normalized(catnum) gin_trgm_ops);');
+        $this->sql('CREATE INDEX products_partno_trgm_idx ON products USING gin (normalized(partno) gin_trgm_ops);');
+        $this->sql('CREATE INDEX product_translations_name_trgm_idx ON product_translations USING gin (normalized(name) gin_trgm_ops);');
+
+        //order indexes
+        $this->sql('CREATE INDEX orders_email_trgm_idx ON orders USING gin (normalized(email) gin_trgm_ops);');
+        $this->sql('CREATE INDEX orders_last_name_trgm_idx ON orders USING gin (normalized(last_name) gin_trgm_ops);');
+        $this->sql('CREATE INDEX orders_company_name_trgm_idx ON orders USING gin (normalized(company_name) gin_trgm_ops);');
+
+        //user indexes
+        $this->sql('CREATE INDEX customer_users_email_trgm_idx ON customer_users USING gin (normalized(email) gin_trgm_ops);');
+        $this->sql('CREATE INDEX customer_users_last_name_trgm_idx ON customer_users USING gin (normalized(last_name) gin_trgm_ops);');
+        $this->sql('CREATE INDEX customer_users_telephone_trgm_idx ON customer_users USING gin (normalized(telephone) gin_trgm_ops);');
+
+        $this->sql('CREATE INDEX billing_addresses_company_name_trgm_idx ON billing_addresses USING gin (normalized(company_name) gin_trgm_ops);');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
