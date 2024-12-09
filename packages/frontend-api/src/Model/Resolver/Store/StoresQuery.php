f498c209f12b1db9898bfd92158e7a7cdd3e90ec
Jan Górecki <jan.gorecki@shopsys.com>
2024-12-09 13:53:46 +0100

    implement searching stores by city or postcode

--- packages/frontend-api/src/Model/Resolver/Store/StoresQuery.php
+++ packages/frontend-api/src/Model/Resolver/Store/StoresQuery.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace Shopsys\FrontendApiBundle\Model\Resolver\Store;
 
-use App\Model\Transport\Transport;
 use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Relay\Connection\Paginator;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
-use Shopsys\FrameworkBundle\Model\Store\StoreFacade;
+use Shopsys\FrameworkBundle\Model\Transport\Transport;
 use Shopsys\FrontendApiBundle\Component\Validation\PageSizeValidator;
 use Shopsys\FrontendApiBundle\Model\Resolver\AbstractQuery;
+use Shopsys\FrontendApiBundle\Model\Store\StoreFacade;
+use Shopsys\FrontendApiBundle\Model\Store\StoresFilterOptions;
 
 class StoresQuery extends AbstractQuery
 {
     /**
-     * @param \Shopsys\FrameworkBundle\Model\Store\StoreFacade $storeFacade
+     * @param \Shopsys\FrontendApiBundle\Model\Store\StoreFacade $storeFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      */
     public function __construct(
@@ -33,17 +34,24 @@ class StoresQuery extends AbstractQuery
         PageSizeValidator::checkMaxPageSize($argument);
         $domainId = $this->domain->getId();
 
-        $paginator = new Paginator(function ($offset, $limit) use ($domainId) {
-            return $this->storeFacade->getStoresByDomainId($domainId, $limit, $offset);
+        /** @var string|null $searchText */
+        $searchText = $argument->offsetGet('searchText');
+
+        $filterOptions = new StoresFilterOptions(
+            searchText: $searchText,
+        );
+
+        $paginator = new Paginator(function ($offset, $limit) use ($domainId, $filterOptions) {
+            return $this->storeFacade->getFilteredStores($domainId, $filterOptions, $limit, $offset);
         });
 
-        $storesCount = $this->storeFacade->getStoresCountByDomainId($domainId);
+        $storesCount = $this->storeFacade->getFilteredStoresCount($domainId, $filterOptions);
 
         return $paginator->auto($argument, $storesCount);
     }
 
     /**
-     * @param \App\Model\Transport\Transport $transport
+     * @param \Shopsys\FrameworkBundle\Model\Transport\Transport $transport
      * @param \Overblog\GraphQLBundle\Definition\Argument $argument
      * @return \Overblog\GraphQLBundle\Relay\Connection\ConnectionInterface|object|null
      */
--- project-base/storefront/components/Blocks/StoreList/StoresWrapper.tsx
+++ project-base/storefront/components/Blocks/StoreList/StoresWrapper.tsx
@@ -1,24 +1,31 @@
 import { GoogleMap } from 'components/Basic/GoogleMap/GoogleMap';
 import { StoreList } from 'components/Blocks/StoreList/StoreList';
+import { SearchInput } from 'components/Forms/TextInput/SearchInput';
 import { TIDs } from 'cypress/tids';
 import { useStoresQuery } from 'graphql/requests/stores/queries/StoresQuery.generated';
+import useTranslation from 'next-translate/useTranslation';
 import { useCallback, useMemo, useState } from 'react';
 import { mapConnectionEdges } from 'utils/mappers/connection';
 import { StoreOrPacketeryPoint } from 'utils/packetery/types';
+import { useDebounce } from 'utils/useDebounce';
 
 export const StoresWrapper: FC = () => {
-    const [{ data: storesData }] = useStoresQuery();
+    const [searchTextValue, setSearchTextValue] = useState<string>('');
+    const debouncedSearchTextValue = useDebounce(searchTextValue, 700);
+    const { t } = useTranslation();
+    const [{ data: storesData, fetching: isFetching }] = useStoresQuery({
+        variables: {
+            searchText: debouncedSearchTextValue || null,
+        },
+    });
     const edges = storesData?.stores.edges || [];
     const mappedStores = useMemo(() => mapConnectionEdges<StoreOrPacketeryPoint>(edges), [edges]);
 
     const [selectedStore, setSelectedStore] = useState<string | null>(null);
 
-    const clickOnMarkerHandler = useCallback(
-        (uuid: string) => {
-            setSelectedStore(uuid);
-        },
-        [mappedStores],
-    );
+    const clickOnMarkerHandler = useCallback((uuid: string) => {
+        setSelectedStore(uuid);
+    }, []);
 
     if (!mappedStores) {
         return null;
@@ -27,6 +34,15 @@ export const StoresWrapper: FC = () => {
     return (
         <div className="flex w-full flex-col lg:flex-row lg:gap-5">
             <div className="w-full max-lg:order-2 max-lg:mt-5 lg:basis-1/2">
+                <div className="mb-2.5">
+                    <SearchInput
+                        label={t('City or postcode')}
+                        shouldShowSpinnerInInput={isFetching}
+                        value={searchTextValue}
+                        onChange={(e) => setSearchTextValue(e.currentTarget.value)}
+                        onClear={() => setSearchTextValue('')}
+                    />
+                </div>
                 <StoreList selectedStoreUuid={selectedStore} stores={mappedStores} />
             </div>
             <div className="w-full max-lg:order-1 lg:basis-1/2" tid={TIDs.stores_map}>
