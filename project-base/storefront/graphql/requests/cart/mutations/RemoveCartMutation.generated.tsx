5b65bc31db3cc2070b25bcce547a1a5c67648918
Jan Molcik <molcik.jan@gmail.com>
2024-12-09 15:30:02 +0100

    fix removing of cart with mutation

---
+++ project-base/storefront/graphql/requests/cart/mutations/RemoveCartMutation.generated.tsx
@@ -0,0 +1,99 @@
+import * as Types from '../../../types';
+
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type TypeRemoveCartMutationVariables = Types.Exact<{
+  cartUuid?: Types.InputMaybe<Types.Scalars['Uuid']['input']>;
+}>;
+
+
+export type TypeRemoveCartMutation = { __typename?: 'Mutation', RemoveCart: boolean };
+
+
+      export interface PossibleTypesResultData {
+        possibleTypes: {
+          [key: string]: string[]
+        }
+      }
+      const result: PossibleTypesResultData = {
+  "possibleTypes": {
+    "Advert": [
+      "AdvertCode",
+      "AdvertImage"
+    ],
+    "ArticleInterface": [
+      "ArticleSite",
+      "BlogArticle"
+    ],
+    "Breadcrumb": [
+      "ArticleSite",
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Category",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "Store",
+      "Variant"
+    ],
+    "CustomerUser": [
+      "CompanyCustomerUser",
+      "RegularCustomerUser"
+    ],
+    "Hreflang": [
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "SeoPage",
+      "Variant"
+    ],
+    "NotBlogArticleInterface": [
+      "ArticleLink",
+      "ArticleSite"
+    ],
+    "ParameterFilterOptionInterface": [
+      "ParameterCheckboxFilterOption",
+      "ParameterColorFilterOption",
+      "ParameterSliderFilterOption"
+    ],
+    "Product": [
+      "MainVariant",
+      "RegularProduct",
+      "Variant"
+    ],
+    "ProductListable": [
+      "Brand",
+      "Category",
+      "Flag"
+    ],
+    "Slug": [
+      "ArticleSite",
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Category",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "Store",
+      "Variant"
+    ]
+  }
+};
+      export default result;
+    
+
+export const RemoveCartMutationDocument = gql`
+    mutation RemoveCartMutation($cartUuid: Uuid) {
+  RemoveCart(input: {cartUuid: $cartUuid})
+}
+    `;
+
+export function useRemoveCartMutation() {
+  return Urql.useMutation<TypeRemoveCartMutation, TypeRemoveCartMutationVariables>(RemoveCartMutationDocument);
+};
--- project-base/storefront/graphql/types.ts
+++ project-base/storefront/graphql/types.ts
@@ -1353,6 +1353,8 @@ export type TypeMutation = {
   Register: TypeLoginResult;
   /** Register new customer user using an order data */
   RegisterByOrder: TypeLoginResult;
+  /** Remove cart */
+  RemoveCart: Scalars['Boolean']['output'];
   /** delete customer user */
   RemoveCustomerUser: Scalars['Boolean']['output'];
   /** Remove product from cart */
@@ -1506,6 +1508,11 @@ export type TypeMutationRegisterArgs = {
 
 export type TypeMutationRegisterByOrderArgs = {
   input: TypeRegistrationByOrderInput;
+};
+
+
+export type TypeMutationRemoveCartArgs = {
+  input: TypeRemoveCartInput;
 };
 
 
@@ -3020,6 +3027,11 @@ export type TypeRegularProductImagesArgs = {
 /** Represents a product */
 export type TypeRegularProductMainImageArgs = {
   type?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type TypeRemoveCartInput = {
+  /** Cart UUID to be removed or null for logged customer user */
+  cartUuid: InputMaybe<Scalars['Uuid']['input']>;
 };
 
 export type TypeRemoveCustomerUserDataInput = {
