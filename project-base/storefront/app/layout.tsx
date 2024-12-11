d5258a9f386d5fa33f11054bbd3b89ae2e2f0c08
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-12-11 12:32:49 +0100

    added cookies store

--- project-base/storefront/app/layout.tsx
+++ project-base/storefront/app/layout.tsx
@@ -1,16 +1,10 @@
 import { getIsUserLoggedInQuery } from './_queries/getIsUserLoggedInQuery';
 import { createQuery } from './_urql/urql-dto';
 import imageLogo from '/public/images/logo.svg';
-import { getSettingsQuery } from 'app/_queries/getSettingsQuery';
 import { ExtendedNextLink } from 'components/Basic/ExtendedNextLink/ExtendedNextLink';
 import { Image } from 'components/Basic/Image/Image';
 import { Navigation } from 'components/Layout/Header/Navigation/Navigation';
-import AuthProvider from 'components/providers/AuthProvider';
-import BroadcastChannelProvider from 'components/providers/BroadcastChannelProvider';
-import { DomainConfigProvider } from 'components/providers/DomainConfigProvider';
-import { SettingsProvider } from 'components/providers/SettingsProvider';
-import ToastifyProvider from 'components/providers/ToastifyProvider';
-import { TranslationProvider } from 'components/providers/TranslationProvider';
+import Providers from 'components/providers/Providers';
 import { DEFAULT_SKELETON_TYPE } from 'config/constants';
 import {
     NavigationQueryDocument,
@@ -31,130 +25,103 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     const domainConfig = getDomainConfig(headers().get('host')!);
     const { defaultLocale: lang } = domainConfig;
     const dictionary = await getDictionary(lang);
-    const [t, navigationResponse, isUserLoggedIn, settings] = await Promise.all([
+    const [t, navigationResponse, isUserLoggedIn] = await Promise.all([
         getServerT({ defaultLang: lang, defaultDictionary: dictionary }),
         createQuery<TypeNavigationQuery, TypeNavigationQueryVariables>(NavigationQueryDocument, {}),
         getIsUserLoggedInQuery(),
-        getSettingsQuery(),
     ]);
     const { data: navigationData } = navigationResponse;
     const currentYear = new Date().getFullYear();
 
     return (
-        <DomainConfigProvider domainConfig={domainConfig}>
-            <SettingsProvider settings={settings}>
-                <TranslationProvider dictionary={dictionary} lang={lang}>
-                    <html lang={lang}>
-                        {/* suppressHydrationWarning for ignoring grammarly extension */}
-                        <body suppressHydrationWarning>
-                            <ToastifyProvider>
-                                <BroadcastChannelProvider>
-                                    <AuthProvider isUserLoggedIn={isUserLoggedIn}>
-                                        <nav className="flex bg-gradient-to-tr from-backgroundBrand to-backgroundBrandLess px-4">
-                                            <div className="flex w-fit shrink-0">
-                                                <ExtendedNextLink
-                                                    href="/"
-                                                    className={twJoin(
-                                                        'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
-                                                        'text-linkInverted no-underline',
-                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                        'active:text-linkInvertedHovered',
-                                                        'disabled:text-linkInvertedDisabled',
-                                                    )}
-                                                >
-                                                    HP
-                                                </ExtendedNextLink>
-                                                <ExtendedNextLink
-                                                    href="/app"
-                                                    className={twJoin(
-                                                        'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
-                                                        'text-linkInverted no-underline',
-                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                        'active:text-linkInvertedHovered',
-                                                        'disabled:text-linkInvertedDisabled',
-                                                    )}
-                                                >
-                                                    APP
-                                                </ExtendedNextLink>
-                                                {!isUserLoggedIn && (
-                                                    <>
-                                                        <ExtendedNextLink
-                                                            href="/login"
-                                                            className={twJoin(
-                                                                'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
-                                                                'text-linkInverted no-underline',
-                                                                'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                                'active:text-linkInvertedHovered',
-                                                                'disabled:text-linkInvertedDisabled',
-                                                            )}
-                                                        >
-                                                            {t('Login')}
-                                                        </ExtendedNextLink>
-                                                        <ExtendedNextLink
-                                                            href="/registration"
-                                                            className={twJoin(
-                                                                'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
-                                                                'text-linkInverted no-underline',
-                                                                'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                                'active:text-linkInvertedHovered',
-                                                                'disabled:text-linkInvertedDisabled',
-                                                            )}
-                                                        >
-                                                            {t('Registration')}
-                                                        </ExtendedNextLink>
-                                                    </>
-                                                )}
-                                                <ExtendedNextLink
-                                                    href="/about"
-                                                    className={twJoin(
-                                                        'relative m-0 flex items-center p-5 pr-10 font-secondary text-sm font-bold vl:text-base',
-                                                        'text-linkInverted no-underline',
-                                                        'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
-                                                        'active:text-linkInvertedHovered',
-                                                        'disabled:text-linkInvertedDisabled',
-                                                    )}
-                                                >
-                                                    {t('About Shopsys')}
-                                                </ExtendedNextLink>
-                                            </div>
-                                            {navigationData && (
-                                                <Navigation
-                                                    navigation={navigationData.navigation}
-                                                    skeletonType={DEFAULT_SKELETON_TYPE}
-                                                />
-                                            )}
-                                        </nav>
-                                        <div>Language in layout is {lang}</div>
-                                        {children}
-                                        <div className="flex flex-col items-center text-center">
-                                            <div className="flex items-center text-sm text-textDisabled">
-                                                {t(
-                                                    'Copyright © {{ currentYear }}, Shopsys s.r.o. All rights reserved.',
-                                                    {
-                                                        currentYear,
-                                                    },
-                                                )}
-                                            </div>
-                                            <div className="flex items-center text-sm text-textDisabled">
-                                                {t('Customized E-shop by')}
-                                                <a
-                                                    className="ml-2 flex w-20"
-                                                    href="https://www.shopsys.com"
-                                                    rel="noreferrer"
-                                                    target="_blank"
-                                                >
-                                                    <Image alt="footer logo" src={imageLogo} />
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </AuthProvider>
-                                </BroadcastChannelProvider>
-                            </ToastifyProvider>
-                        </body>
-                    </html>
-                </TranslationProvider>
-            </SettingsProvider>
-        </DomainConfigProvider>
+        <Providers>
+            <nav className="flex bg-gradient-to-tr from-backgroundBrand to-backgroundBrandLess px-4">
+                <div className="flex w-fit shrink-0">
+                    <ExtendedNextLink
+                        href="/"
+                        className={twJoin(
+                            'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                            'text-linkInverted no-underline',
+                            'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                            'active:text-linkInvertedHovered',
+                            'disabled:text-linkInvertedDisabled',
+                        )}
+                    >
+                        HP
+                    </ExtendedNextLink>
+                    <ExtendedNextLink
+                        href="/app"
+                        className={twJoin(
+                            'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                            'text-linkInverted no-underline',
+                            'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                            'active:text-linkInvertedHovered',
+                            'disabled:text-linkInvertedDisabled',
+                        )}
+                    >
+                        APP
+                    </ExtendedNextLink>
+                    {!isUserLoggedIn && (
+                        <>
+                            <ExtendedNextLink
+                                href="/login"
+                                className={twJoin(
+                                    'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                    'text-linkInverted no-underline',
+                                    'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                    'active:text-linkInvertedHovered',
+                                    'disabled:text-linkInvertedDisabled',
+                                )}
+                            >
+                                {t('Login')}
+                            </ExtendedNextLink>
+                            <ExtendedNextLink
+                                href="/registration"
+                                className={twJoin(
+                                    'relative m-0 flex w-fit items-center p-5 pl-0 font-secondary text-sm font-bold vl:text-base',
+                                    'text-linkInverted no-underline',
+                                    'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                                    'active:text-linkInvertedHovered',
+                                    'disabled:text-linkInvertedDisabled',
+                                )}
+                            >
+                                {t('Registration')}
+                            </ExtendedNextLink>
+                        </>
+                    )}
+                    <ExtendedNextLink
+                        href="/about"
+                        className={twJoin(
+                            'relative m-0 flex items-center p-5 pr-10 font-secondary text-sm font-bold vl:text-base',
+                            'text-linkInverted no-underline',
+                            'hover:text-linkInvertedHovered hover:no-underline group-hover:text-linkInvertedHovered group-hover:no-underline',
+                            'active:text-linkInvertedHovered',
+                            'disabled:text-linkInvertedDisabled',
+                        )}
+                    >
+                        {t('About Shopsys')}
+                    </ExtendedNextLink>
+                </div>
+                {navigationData && (
+                    <Navigation navigation={navigationData.navigation} skeletonType={DEFAULT_SKELETON_TYPE} />
+                )}
+            </nav>
+            <div>Language in layout is {lang}</div>
+            {children}
+            <div className="flex flex-col items-center text-center">
+                <div className="flex items-center text-sm text-textDisabled">
+                    {t('Copyright © {{ currentYear }}, Shopsys s.r.o. All rights reserved.', {
+                        currentYear,
+                    })}
+                </div>
+                <div className="flex items-center text-sm text-textDisabled">
+                    {t('Customized E-shop by')}
+                    <a className="ml-2 flex w-20" href="https://www.shopsys.com" rel="noreferrer" target="_blank">
+                        <Image alt="footer logo" src={imageLogo} />
+                    </a>
+                </div>
+            </div>
+        </Providers>
     );
 }
 
