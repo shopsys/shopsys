e4e808127cf4aecea43a6590f6bc6f982652a11c
Tomas Chlebek <chlebek.tomas@seznam.cz>
2024-12-11 12:32:49 +0100

    added cookies store

--- project-base/storefront/components/providers/ToastifyProvider.tsx
+++ project-base/storefront/components/providers/ToastifyProvider.tsx
@@ -3,13 +3,8 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const ToastifyProvider: FC = ({ children }) => {
-    return (
-        <>
-            {children}
-            <ToastContainer autoClose={6000} position="top-center" theme="colored" />
-        </>
-    );
+export const ToastifyProvider: FC = () => {
+    return <ToastContainer autoClose={6000} position="top-center" theme="colored" />;
 };
 
 export default ToastifyProvider;
