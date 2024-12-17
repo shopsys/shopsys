b387e9cc0b3c72f0c46e3e167c8e53e9668b955d
Rostislav Kreisinger <rostislav.kreisinger@shopsys.com>
2024-12-17 14:44:04 +0100

    [SSP-2904] Search in administration - in the header in the administration it is now possible to search by the name of the sections that the given administrator has allowed according to his rights

---
+++ packages/framework/src/Controller/Admin/SearchAdminController.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Controller\Admin;
+
+use Knp\Menu\ItemInterface;
+use Normalizer;
+use Shopsys\FrameworkBundle\Model\AdminNavigation\SideMenuBuilder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchAdminController extends AdminBaseController
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\AdminNavigation\SideMenuBuilder $sideMenuBuilder
+     */
+    public function __construct(
+        protected readonly SideMenuBuilder $sideMenuBuilder,
+    ) {
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    #[Route(path: '/search/')]
+    public function searchAction(Request $request): Response
+    {
+        $searchString = $request->get('search');
+        $menu = $this->sideMenuBuilder->createMenu();
+
+        $results = [];
+        $this->buildResultsList($results, $menu, $searchString);
+
+        return $this->render(
+            '@ShopsysFramework/Admin/Content/Default/searchResult.html.twig',
+            [
+                'results' => $results,
+            ],
+        );
+    }
+
+    /**
+     * @param array $results
+     * @param \Knp\Menu\ItemInterface $item
+     * @param string $searchString
+     */
+    protected function buildResultsList(array &$results, ItemInterface $item, string $searchString): void
+    {
+        if ($item->getLabel() && $item->getUri() && $this->containsLabelSearchString($item->getLabel(), $searchString)) {
+            $results[] = [
+                'uri' => $item->getUri(),
+                'pathLabels' => $this->buildPathByItem($item),
+                'label' => $item->getLabel(),
+            ];
+        }
+
+        foreach ($item->getChildren() as $child) {
+            $this->buildResultsList($results, $child, $searchString);
+        }
+    }
+
+    /**
+     * @param string $label
+     * @param string $searchString
+     * @return bool
+     */
+    protected function containsLabelSearchString(string $label, string $searchString): bool
+    {
+        return str_contains($this->convertStringWithDiacritics($label), $this->convertStringWithDiacritics($searchString));
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function convertStringWithDiacritics(string $string): string
+    {
+        return strtolower(preg_replace('~[\p{M}-]+~u', '', Normalizer::normalize($string, Normalizer::FORM_D)));
+    }
+
+    /**
+     * @param \Knp\Menu\ItemInterface $item
+     * @return array
+     */
+    protected function buildPathByItem(ItemInterface $item): array
+    {
+        $itemLabels = [$item->getLabel()];
+
+        while ($item->getParent()) {
+            $item = $item->getParent();
+
+            if ($item->getLabel() !== 'root') {
+                $itemLabels = array_merge([$item->getLabel()], $itemLabels);
+            }
+        }
+
+        return $itemLabels;
+    }
+}
