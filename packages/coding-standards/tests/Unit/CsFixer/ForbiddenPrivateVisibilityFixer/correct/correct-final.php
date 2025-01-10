de973413fedafad62392c0b1a14475d72541ddda
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-01-10 14:49:53 +0100

    ForbiddenPrivateVisibilityFixer now ignores final classes

---
+++ packages/coding-standards/tests/Unit/CsFixer/ForbiddenPrivateVisibilityFixer/correct/correct-final.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TestNamespace\TestSubNamespace;
+
+final class SomeClass
+{
+    /**
+     * @var int
+     */
+    private $field;
+
+    /**
+     * @return bool
+     */
+    private function method()
+    {
+        return true;
+    }
+}
