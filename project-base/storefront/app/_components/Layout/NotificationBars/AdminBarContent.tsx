8d24ebce5490d9b7c548a2b46119ac792ae0c9f1
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-17 16:00:32 +0100

    added notification bar

---
+++ project-base/storefront/app/_components/Layout/NotificationBars/AdminBarContent.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useLogout } from 'app/_hooks/useLogout';
+import { Button } from 'components/Forms/Button/Button';
+import Trans from 'next-translate/Trans';
+
+type LoggedAsUserBarProps = {
+    userEmail: string | undefined;
+};
+
+export const AdminBarContent = ({ userEmail }: LoggedAsUserBarProps) => {
+    const logout = useLogout();
+
+    if (!userEmail) {
+        return null;
+    }
+
+    return (
+        <div className="bg-backgroundError py-2">
+            <div className="flex items-center justify-center text-center text-sm font-bold text-text">
+                <Trans
+                    defaultTrans="Warning! You are logged in as a customer with the email {{ email }} <button>Log out</button>"
+                    i18nKey="adminLoggedInAsCustomerWarning"
+                    values={{ email: userEmail }}
+                    components={{
+                        button: (
+                            <Button
+                                size="small"
+                                style={{ marginLeft: '10px' }}
+                                type="button"
+                                variant="inverted"
+                                onClick={logout}
+                            />
+                        ),
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
