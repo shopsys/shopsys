e121a8f2fcd8748433bec3ca71d97a00db409ee2
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-17 09:28:45 +0100

    updated product detail availability

--- project-base/storefront/components/Pages/ProductDetail/ProductDetailContent.tsx
+++ project-base/storefront/components/Pages/ProductDetail/ProductDetailContent.tsx
@@ -1,15 +1,21 @@
 'use client';
 
+import { ProductDetailAvailabilityList } from './ProductDetailAvailabilityList';
 import { ProductDetailPrefix, ProductDetailHeading } from './ProductDetailElements';
 import { ProductDetailGallery } from './ProductDetailGallery';
 import { ProductDetailPrice } from './ProductDetailPrice';
 import { ProductDetailTabs } from './ProductDetailTabs/ProductDetailTabs';
 import { ProductDetailUsps } from './ProductDetailUsps';
 import { ExtendedNextLink } from 'components/Basic/ExtendedNextLink/ExtendedNextLink';
+import { ProductAvailability } from 'components/Blocks/Product/ProductAvailability';
 import { WatchDogButton } from 'components/Blocks/Product/Watchdog/WatchDogButton';
+import { Popup } from 'components/Layout/Popup/Popup';
 import { Webline } from 'components/Layout/Webline/Webline';
 import { useTranslation } from 'components/providers/TranslationProvider';
 import { TypeProductDetailFragment } from 'graphql/requests/products/fragments/ProductDetailFragment.generated';
+import { TypeAvailabilityStatusEnum } from 'graphql/types';
+import { useSessionStore } from 'store/useSessionStore';
+import { twJoin } from 'tailwind-merge';
 
 type ProductDetailContentProps = {
     product: TypeProductDetailFragment;
@@ -18,7 +24,7 @@ type ProductDetailContentProps = {
 export const ProductDetailContent: FC<ProductDetailContentProps> = ({ product }) => {
     const { t } = useTranslation();
     // const router = useRouter();
-    // const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
 
     // const { isLuigisBoxActive } = useDomainConfig();
 
@@ -72,7 +78,7 @@ export const ProductDetailContent: FC<ProductDetailContentProps> = ({ product })
                         <div className="bg-backgroundMore flex flex-col gap-4 rounded-xl p-3 sm:p-6">
                             <ProductDetailPrice productPrice={product.price} />
 
-                            {/* {!product.isSellingDenied && (
+                            {!product.isSellingDenied && (
                                 <ProductAvailability
                                     availability={product.availability}
                                     availableStoresCount={product.availableStoresCount}
@@ -93,7 +99,7 @@ export const ProductDetailContent: FC<ProductDetailContentProps> = ({ product })
                                         )
                                     }
                                 />
-                            )} */}
+                            )}
 
                             <WatchDogButton
                                 availability={product.availability}
