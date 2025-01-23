e41db71279e6806cdcab2ec20bd574fa07148bcd
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-23 13:46:25 +0100

    added product list provider

---
+++ project-base/storefront/app/_components/Blocks/Product/Watchdog/WatchDogButton.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { WatchdogIcon } from 'components/Basic/Icon/Watchdog';
+import { Button } from 'components/Forms/Button/Button';
+import { useTranslation } from 'components/providers/TranslationProvider';
+import { TypeAvailability, TypeAvailabilityStatusEnum } from 'graphql/types';
+import dynamic from 'next/dynamic';
+import { useSessionStore } from 'store/useSessionStore';
+import { twJoin } from 'tailwind-merge';
+
+const WatchdogPopup = dynamic(
+    () => import('components/Blocks/Popup/WatchdogPopup').then((component) => component.WatchdogPopup),
+    {
+        ssr: false,
+    },
+);
+
+type WatchDogButtonProps = {
+    productUuid: string | undefined;
+    isInquiryType: boolean;
+    availability: TypeAvailability;
+    productIsSellingDenied: boolean | undefined;
+};
+
+export const WatchDogButton: FC<WatchDogButtonProps> = ({
+    productUuid,
+    isInquiryType,
+    availability,
+    productIsSellingDenied,
+    className,
+}) => {
+    const { t } = useTranslation();
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
+
+    const showWatchdogButton =
+        productUuid &&
+        !isInquiryType &&
+        (availability.status === TypeAvailabilityStatusEnum.OutOfStock || productIsSellingDenied);
+
+    if (!showWatchdogButton) {
+        return null;
+    }
+
+    const openWatchDogPopup = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation();
+
+        if (productUuid) {
+            updatePortalContent(<WatchdogPopup productUuid={productUuid} />);
+        }
+    };
+
+    return (
+        <Button
+            className={twJoin('whitespace-nowrap', className)}
+            size="large"
+            variant="primary"
+            onClick={openWatchDogPopup}
+        >
+            <WatchdogIcon className="size-6" />
+            {t('Watch the goods')}
+        </Button>
+    );
+};
---
+++ project-base/storefront/app/_components/Blocks/Product/Watchdog/watchdogFormMeta.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link, linkPlaceholderTwClass } from 'components/Basic/Link/Link';
+import { validateEmail, validatePrivacyPolicy } from 'components/Forms/validationRules';
+import { useSettings } from 'components/providers/SettingsProvider';
+import { useTranslation } from 'components/providers/TranslationProvider';
+import Trans from 'next-translate/Trans';
+import { useMemo } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import { WatchdogFormType } from 'types/form';
+import { useShopsysForm } from 'utils/forms/useShopsysForm';
+import * as Yup from 'yup';
+
+export const useWatchdogForm = (
+    defaultValues: WatchdogFormType,
+): [UseFormReturn<WatchdogFormType>, WatchdogFormType | undefined] => {
+    const { t } = useTranslation();
+
+    const resolver = yupResolver(
+        Yup.object().shape<Record<keyof WatchdogFormType, any>>({
+            email: validateEmail(t),
+            productUuid: Yup.string().required(),
+            gdprAgreement: validatePrivacyPolicy(t),
+        }),
+    );
+
+    return [useShopsysForm(resolver, defaultValues), defaultValues];
+};
+
+type WatchdogFormMetaType = {
+    formName: string;
+    messages: {
+        error: string;
+    };
+    fields: {
+        [key in keyof WatchdogFormType]: {
+            name: key;
+            label: string | JSX.Element;
+            errorMessage?: string;
+        };
+    };
+};
+
+export const useWatchdogFormMeta = (formProviderMethods: UseFormReturn<WatchdogFormType>): WatchdogFormMetaType => {
+    const { t } = useTranslation();
+    const { privacyPolicyArticleUrl } = useSettings();
+
+    const errors = formProviderMethods.formState.errors;
+
+    const formMeta = useMemo(
+        () => ({
+            formName: 'watchdog-form',
+            messages: {
+                error: t('An error occurred while creating your watchdog'),
+            },
+            fields: {
+                email: {
+                    name: 'email' as const,
+                    label: t('Your email'),
+                    errorMessage: errors.email?.message,
+                },
+                productUuid: {
+                    name: 'productUuid' as const,
+                    label: t('Product'),
+                    errorMessage: errors.productUuid?.message,
+                },
+                gdprAgreement: {
+                    name: 'gdprAgreement' as const,
+                    label: (
+                        <Trans
+                            defaultTrans="I agree with <lnk1>processing of privacy policy</lnk1>."
+                            i18nKey="GdprAgreementCheckbox"
+                            components={{
+                                lnk1: privacyPolicyArticleUrl ? (
+                                    <Link
+                                        isExternal
+                                        className="inline"
+                                        href={privacyPolicyArticleUrl}
+                                        target="_blank"
+                                    />
+                                ) : (
+                                    <span className={linkPlaceholderTwClass} />
+                                ),
+                            }}
+                        />
+                    ),
+                    errorMessage: errors.gdprAgreement?.message,
+                },
+            },
+        }),
+        [errors.gdprAgreement?.message, errors.productUuid?.message, t],
+    );
+
+    return formMeta;
+};
