ebaaba2330390c7b6de16ca5802eb06c75b996e2
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-01-27 14:59:28 +0100

    move lastOrderQuery from project-base to the frontend-api package

--- packages/frontend-api/src/Model/Resolver/Order/LastOrderQuery.php
+++ packages/frontend-api/src/Model/Resolver/Order/LastOrderQuery.php
@@ -2,32 +2,31 @@
 
 declare(strict_types=1);
 
-namespace App\FrontendApi\Resolver\Order;
+namespace Shopsys\FrontendApiBundle\Model\Resolver\Order;
 
-use App\FrontendApi\Model\Order\OrderApiFacade;
-use App\Model\Order\Order;
 use Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser;
+use Shopsys\FrameworkBundle\Model\Order\Order;
 use Shopsys\FrontendApiBundle\Model\Mutation\Customer\User\Exception\InvalidCredentialsUserError;
+use Shopsys\FrontendApiBundle\Model\Order\OrderApiFacade;
 use Shopsys\FrontendApiBundle\Model\Resolver\AbstractQuery;
 
 class LastOrderQuery extends AbstractQuery
 {
     /**
-     * @param \App\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
-     * @param \App\FrontendApi\Model\Order\OrderApiFacade $orderApiFacade
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User\CurrentCustomerUser $currentCustomerUser
+     * @param \Shopsys\FrontendApiBundle\Model\Order\OrderApiFacade $orderApiFacade
      */
     public function __construct(
-        private readonly CurrentCustomerUser $currentCustomerUser,
-        private readonly OrderApiFacade $orderApiFacade,
+        protected readonly CurrentCustomerUser $currentCustomerUser,
+        protected readonly OrderApiFacade $orderApiFacade,
     ) {
     }
 
     /**
-     * @return \App\Model\Order\Order|null
+     * @return \Shopsys\FrameworkBundle\Model\Order\Order|null
      */
     public function lastOrderQuery(): ?Order
     {
-        /** @var \App\Model\Customer\User\CustomerUser|null $customerUser */
         $customerUser = $this->currentCustomerUser->findCurrentCustomerUser();
 
         if ($customerUser === null) {
