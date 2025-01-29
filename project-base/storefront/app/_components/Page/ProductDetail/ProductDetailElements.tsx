543d9784778393c84073839bc29aea1c5dcb1f47
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-29 14:18:54 +0100

    added product detail

---
+++ project-base/storefront/app/_components/Page/ProductDetail/ProductDetailElements.tsx
@@ -0,0 +1,5 @@
+export const ProductDetailPrefix: FC = ({ children }) => (
+    <div className="mb-1 font-secondary text-textDisabled">{children}</div>
+);
+
+export const ProductDetailHeading: FC = ({ children }) => <h1>{children}</h1>;
