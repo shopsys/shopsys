7283fbb63ba90c3243559b6227bde96149de1434
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-29 14:18:54 +0100

    added product detail

---
+++ project-base/storefront/app/_components/Basic/Flag/Flag.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { twMergeCustom } from 'utils/twMerge';
+
+export type FlagTypes = 'blog' | 'dynamic' | 'custom' | 'discount';
+
+type FlagProps = {
+    href?: string;
+    type?: FlagTypes;
+};
+
+export const Flag: FC<FlagProps> = ({ children, href, className, type = 'custom' }) => {
+    const router = useRouter();
+
+    const flagTwClass = twMergeCustom(
+        'inline-flex rounded px-1.5 py-0.5 text-xs transition-all',
+        className,
+        type === 'blog' &&
+            'bg-textSubtle font-secondary font-semiBold text-xs !text-textInverted no-underline hover:bg-backgroundAccentMore hover:text-textInverted hover:!no-underline',
+        type === 'dynamic' && '!text-textInverted text-zero py-1.5 rounded-full vl:text-xs vl:py-0.5 vl:rounded',
+        type === 'discount' && 'bg-priceDiscounted text-textInverted font-semiBold py-1',
+    );
+
+    return (
+        <div className={flagTwClass} onClick={() => href && router.push(href)}>
+            {children}
+        </div>
+    );
+};
