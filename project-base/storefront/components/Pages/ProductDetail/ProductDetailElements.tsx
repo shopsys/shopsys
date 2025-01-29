93256e491451ea3a39e94899c5dd0f7288283cd3
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-01-29 14:27:43 +0100

    removed unused component for product detail

--- project-base/storefront/components/Pages/ProductDetail/ProductDetailElements.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-export const ProductDetailPrefix: FC = ({ children }) => (
-    <div className="mb-1 font-secondary text-textDisabled">{children}</div>
-);
-
-export const ProductDetailHeading: FC = ({ children }) => <h1>{children}</h1>;
