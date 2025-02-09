f9e05e2c99dbc138d7e847bd9d0a0a8cc38d03e9
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-02-09 13:35:41 +0100

    add DB trigram inexes for complaint fields that are used for search in admin

---
+++ packages/framework/src/Migrations/Version20250209123001.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20250209123001 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('CREATE INDEX complaint_email_trgm_idx ON complaints USING gin (normalized(email) gin_trgm_ops);');
+        $this->sql('CREATE INDEX complaint_manual_document_number_trgm_idx ON complaints USING gin (normalized(manual_document_number) gin_trgm_ops);');
+        $this->sql('CREATE INDEX complaint_delivery_last_name_trgm_idx ON complaints USING gin (normalized(delivery_last_name) gin_trgm_ops);');
+        $this->sql('CREATE INDEX complaint_delivery_company_name_trgm_idx ON complaints USING gin (normalized(delivery_company_name) gin_trgm_ops);');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
