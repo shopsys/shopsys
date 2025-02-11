088e87ac86377ff3c8a2f4d36e48a6eaf2385d18
Martin Grossmann <martin@vyvoj.net>
2025-02-11 14:29:05 +0100

    added new form type - action bar
    - wraps the save button with link to back to overview
    - allows adding more buttons, links

---
+++ packages/form-types-bundle/src/LinkType.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FormTypesBundle;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class LinkType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver
+            ->setRequired(['link', 'label'])
+            ->setAllowedTypes('link', ['string'])
+            ->setAllowedTypes('label', ['string'])
+            ->setDefaults([
+                'mapped' => false,
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['link'] = $options['link'];
+        $view->vars['label'] = $options['label'];
+    }
+}
