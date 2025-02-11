12a56bd6ec6a13e90267ed8af8b60e18ee62acce
Jan Molcik <molcik.jan@gmail.com>
2025-02-11 09:40:27 +0100

    regenerate gql schema

---
+++ project-base/storefront/graphql/requests/complaints/queries/ComplaintResolutionQuery.generated.tsx
@@ -0,0 +1,106 @@
+import * as Types from '../../../types';
+
+import gql from 'graphql-tag';
+import { ComplaintResolutionFragment } from '../fragments/ComplaintResolutionFragment.generated';
+import * as Urql from 'urql';
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type TypeComplaintResolutionQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type TypeComplaintResolutionQuery = { __typename?: 'Query', complaintResolution: Array<{ __typename?: 'ComplaintResolution', name: string, value: string }> };
+
+
+      export interface PossibleTypesResultData {
+        possibleTypes: {
+          [key: string]: string[]
+        }
+      }
+      const result: PossibleTypesResultData = {
+  "possibleTypes": {
+    "Advert": [
+      "AdvertCode",
+      "AdvertImage"
+    ],
+    "ArticleInterface": [
+      "ArticleSite",
+      "BlogArticle"
+    ],
+    "BaseCustomerUser": [
+      "CompanyCustomerUser",
+      "CurrentCompanyCustomerUser",
+      "CurrentRegularCustomerUser",
+      "RegularCustomerUser"
+    ],
+    "Breadcrumb": [
+      "ArticleSite",
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Category",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "Store",
+      "Variant"
+    ],
+    "CurrentCustomerUser": [
+      "CurrentCompanyCustomerUser",
+      "CurrentRegularCustomerUser"
+    ],
+    "Hreflang": [
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "SeoPage",
+      "Variant"
+    ],
+    "NotBlogArticleInterface": [
+      "ArticleLink",
+      "ArticleSite"
+    ],
+    "ParameterFilterOptionInterface": [
+      "ParameterCheckboxFilterOption",
+      "ParameterColorFilterOption",
+      "ParameterSliderFilterOption"
+    ],
+    "Product": [
+      "MainVariant",
+      "RegularProduct",
+      "Variant"
+    ],
+    "ProductListable": [
+      "Brand",
+      "Category",
+      "Flag"
+    ],
+    "Slug": [
+      "ArticleSite",
+      "BlogArticle",
+      "BlogCategory",
+      "Brand",
+      "Category",
+      "Flag",
+      "MainVariant",
+      "RegularProduct",
+      "Store",
+      "Variant"
+    ]
+  }
+};
+      export default result;
+    
+
+export const ComplaintResolutionQueryDocument = gql`
+    query ComplaintResolutionQuery {
+  complaintResolution {
+    ...ComplaintResolutionFragment
+  }
+}
+    ${ComplaintResolutionFragment}`;
+
+export function useComplaintResolutionQuery(options?: Omit<Urql.UseQueryArgs<TypeComplaintResolutionQueryVariables>, 'query'>) {
+  return Urql.useQuery<TypeComplaintResolutionQuery, TypeComplaintResolutionQueryVariables>({ query: ComplaintResolutionQueryDocument, ...options });
+};
