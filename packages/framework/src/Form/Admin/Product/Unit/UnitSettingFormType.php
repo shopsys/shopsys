fb50c9c1b37412e28f6f08166f54d4025f14b809
Martin Grossmann <martin@vyvoj.net>
2025-02-12 14:12:19 +0100

    ActionBar is used in Unit

--- packages/framework/src/Form/Admin/Product/Unit/UnitSettingFormType.php
+++ packages/framework/src/Form/Admin/Product/Unit/UnitSettingFormType.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Product\Unit;
 
+use Shopsys\FormTypesBundle\ActionBarType;
 use Shopsys\FrameworkBundle\Model\Product\Unit\UnitFacade;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
@@ -38,7 +38,9 @@ class UnitSettingFormType extends AbstractType
                     new Constraints\NotBlank(['message' => 'Please choose default unit']),
                 ],
             ])
-            ->add('save', SubmitType::class);
+            ->add('actionBar', ActionBarType::class, [
+                'save_label' => t('Save changes'),
+            ]);
     }
 
     /**
