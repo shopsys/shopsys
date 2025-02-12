d8b1163698b9d519749f332756e28509f7689642
Martin Grossmann <martin@vyvoj.net>
2025-02-12 14:12:27 +0100

    ActionBar is used in TopProd

--- packages/framework/src/Form/Admin/Product/TopProduct/TopProductsFormType.php
+++ packages/framework/src/Form/Admin/Product/TopProduct/TopProductsFormType.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Product\TopProduct;
 
+use Shopsys\FormTypesBundle\ActionBarType;
 use Shopsys\FrameworkBundle\Form\ProductsType;
 use Shopsys\FrameworkBundle\Form\Transformers\RemoveDuplicatesFromArrayTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -30,12 +30,15 @@ class TopProductsFormType extends AbstractType
             ->add(
                 $builder
                     ->create('products', ProductsType::class, [
+                        'label' => t('Products'),
                         'required' => false,
                         'sortable' => true,
                     ])
                     ->addViewTransformer($this->removeDuplicatesTransformer),
             )
-            ->add('save', SubmitType::class);
+            ->add('actionBar', ActionBarType::class, [
+                'save_label' => t('Save changes'),
+            ]);
     }
 
     /**
