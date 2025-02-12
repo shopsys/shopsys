1f852e5ebb690984e330ac8f36aea516d3b86def
Martin Grossmann <martin@vyvoj.net>
2025-02-12 14:17:19 +0100

    ActionBar is used in Vat

--- packages/framework/src/Form/Admin/Vat/VatSettingsFormType.php
+++ packages/framework/src/Form/Admin/Vat/VatSettingsFormType.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Vat;
 
+use Shopsys\FormTypesBundle\ActionBarType;
 use Shopsys\FrameworkBundle\Component\Domain\AdminDomainTabsFacade;
 use Shopsys\FrameworkBundle\Form\GroupType;
 use Shopsys\FrameworkBundle\Model\Pricing\Vat\VatFacade;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
@@ -53,7 +53,9 @@ class VatSettingsFormType extends AbstractType
 
         $builder
             ->add($builderSettingsGroup)
-            ->add('save', SubmitType::class);
+            ->add('actionBar', ActionBarType::class, [
+                'save_label' => t('Save changes'),
+            ]);
     }
 
     /**
