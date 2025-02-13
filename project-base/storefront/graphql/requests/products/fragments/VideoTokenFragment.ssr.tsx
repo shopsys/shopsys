85a9d727318f6d186557d3b69507915e2bf95f4f
Jan Molcik <molcik.jan@gmail.com>
2025-02-13 15:28:34 +0100

    regenerate gql schema

--- project-base/storefront/graphql/requests/products/fragments/VideoTokenFragment.ssr.tsx
+++ project-base/storefront/graphql/requests/products/fragments/VideoTokenFragment.ssr.tsx
@@ -1,7 +1,7 @@
 import * as Types from '../../../types';
 
 import gql from 'graphql-tag';
-export type TypeVideoTokenFragment = { __typename: 'VideoToken', description: string, token: string };
+export type TypeVideoTokenFragment = { __typename: 'VideoToken', description: string | null, token: string };
 
 
       export interface PossibleTypesResultData {
