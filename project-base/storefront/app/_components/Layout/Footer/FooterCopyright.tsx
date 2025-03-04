7eab76b7cef43fb6e8bf14efa74f4bd8e7d4051c
Jan Molcik <molcik.jan@gmail.com>
2025-03-04 19:35:46 +0100

    fix standards

--- project-base/storefront/app/_components/Layout/Footer/FooterCopyright.tsx
+++ project-base/storefront/app/_components/Layout/Footer/FooterCopyright.tsx
@@ -2,6 +2,7 @@ import imageLogo from '/public/images/logo.svg';
 import { getDomainConfig } from 'app/_utils/getDomainConfig';
 import { getTranslation } from 'app/_utils/translation/getTranslation';
 import { Image } from 'components/Basic/Image/Image';
+import { TIDs } from 'cypress/tids';
 import { headers } from 'next/headers';
 import { getDictionary } from 'utils/getDictionary';
 
@@ -12,17 +13,20 @@ export const FooterCopyright = async () => {
 
     const currentYear = new Date().getFullYear();
 
-    // TODO: add translation
-
     return (
-        <div className="flex flex-col items-center py-4 text-center">
-            <div className="text-textDisabled flex items-center text-sm">{currentYear}</div>
-            <div className="text-textDisabled flex items-center text-sm">
+        <>
+            <div
+                className="text-textDisabled flex items-center justify-center text-center text-sm"
+                tid={TIDs.footer_copyright}
+            >
+                {t('footerCopyright', { currentYear })}
+            </div>
+            <div className="text-textDisabled flex items-center justify-center text-sm">
                 {t('Customized E-shop by')}
                 <a className="ml-2 flex w-20" href="https://www.shopsys.com" rel="noreferrer" target="_blank">
                     <Image alt="footer logo" src={imageLogo} />
                 </a>
             </div>
-        </div>
+        </>
     );
 };
