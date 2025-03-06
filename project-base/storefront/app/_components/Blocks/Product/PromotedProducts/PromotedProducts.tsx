c0e087b3e34a78bbc3034d0ef68242b22db7e80d
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-03-06 12:42:13 +0700

    added product sliders to homepage

---
+++ project-base/storefront/app/_components/Blocks/Product/PromotedProducts/PromotedProducts.tsx
@@ -0,0 +1,14 @@
+import { PromotedProductsContent } from './PromotedProductsContent';
+import { SkeletonModulePromotedProducts } from 'components/Blocks/Skeleton/SkeletonModulePromotedProducts';
+import { Webline } from 'components/Layout/Webline/Webline';
+import { Suspense } from 'react';
+
+export const PromotedProducts = async () => {
+    return (
+        <Webline>
+            <Suspense fallback={<SkeletonModulePromotedProducts />}>
+                <PromotedProductsContent />
+            </Suspense>
+        </Webline>
+    );
+};
---
+++ project-base/storefront/app/_components/Blocks/Product/PromotedProducts/PromotedProductsContent.tsx
@@ -0,0 +1,33 @@
+import { ProductSlider } from 'app/_components/Blocks/Product/ProductSlider';
+import { ProductListItem } from 'app/_components/Blocks/Product/ProductsList/ProductListItem';
+import { getPromotedProductsQuery } from 'app/_queries/getPromotedProductsQuery';
+import { getTranslation } from 'app/_utils/translation/getTranslation';
+import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
+import { GtmProductListNameType } from 'gtm/enums/GtmProductListNameType';
+
+export const PromotedProductsContent = async () => {
+    const [t, promotedProductsResult] = await Promise.all([getTranslation(), getPromotedProductsQuery()]);
+
+    if (!promotedProductsResult) {
+        return null;
+    }
+
+    return (
+        <section>
+            <h5 className="mb-3">{t('News on offer')}</h5>
+
+            <ProductSlider totalItems={promotedProductsResult.promotedProducts.length} variant="default">
+                {promotedProductsResult.promotedProducts.map((product, index) => (
+                    <ProductListItem
+                        key={product.uuid}
+                        isShownInSlider
+                        gtmMessageOrigin={GtmMessageOriginType.product_detail_page}
+                        gtmProductListName={GtmProductListNameType.last_visited_products}
+                        listIndex={index}
+                        product={product}
+                    />
+                ))}
+            </ProductSlider>
+        </section>
+    );
+};
