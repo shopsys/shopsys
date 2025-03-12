7eb8f6031ccc7b4e98e6662e326a6d3d4bfb4c11
Jan Molcik <molcik.jan@gmail.com>
2025-03-12 17:38:21 +0100

    move personal-data-export page to app

---
+++ project-base/storefront/app/_components/Page/PersonalData/Export/PersonalDataExportContent.tsx
@@ -0,0 +1,24 @@
+import { PersonalDataExportForm } from './PersonalDataExportForm';
+import { getTranslation } from 'app/_utils/translation/getTranslation';
+import { UserText } from 'components/Basic/UserText/UserText';
+import { Webline } from 'components/Layout/Webline/Webline';
+
+type PersonalDataExportContentProps = {
+    contentSiteText: string | undefined;
+};
+
+export const PersonalDataExportContent = async ({ contentSiteText }: PersonalDataExportContentProps) => {
+    const t = await getTranslation();
+
+    return (
+        <Webline className="flex flex-col items-center">
+            <h1 className="w-full max-w-3xl">{t('Personal data export')}</h1>
+            {!!contentSiteText && (
+                <div className="mb-5 block max-w-3xl text-justify">
+                    <UserText htmlContent={contentSiteText} />
+                </div>
+            )}
+            <PersonalDataExportForm />
+        </Webline>
+    );
+};
