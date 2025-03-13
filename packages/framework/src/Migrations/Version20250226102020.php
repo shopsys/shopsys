a32d3cb502d67bbbf50981359e278e62856f9cfd
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2025-03-13 11:58:40 +0100

    applied new coding standards

--- packages/framework/src/Migrations/Version20250226102020.php
+++ packages/framework/src/Migrations/Version20250226102020.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Override;
 use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 class Version20250226102020 extends AbstractMigration
@@ -12,6 +13,7 @@ class Version20250226102020 extends AbstractMigration
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
+    #[Override]
     public function up(Schema $schema): void
     {
         $this->sql('ALTER TABLE currencies ADD rounding_places_price_without_vat INT NOT NULL DEFAULT 2');
@@ -21,6 +23,7 @@ class Version20250226102020 extends AbstractMigration
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
+    #[Override]
     public function down(Schema $schema): void
     {
     }
--- packages/framework/src/Migrations/Version20250303130234.php
+++ packages/framework/src/Migrations/Version20250303130234.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Override;
 use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 class Version20250303130234 extends AbstractMigration
@@ -12,6 +13,7 @@ class Version20250303130234 extends AbstractMigration
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
+    #[Override]
     public function up(Schema $schema): void
     {
         $this->sql('ALTER TABLE promo_code_limit DROP CONSTRAINT promo_code_limit_pkey');
@@ -22,6 +24,7 @@ class Version20250303130234 extends AbstractMigration
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
+    #[Override]
     public function down(Schema $schema): void
     {
     }
