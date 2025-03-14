94a0edb8048833a40f759c1905b9c7f202cd8090
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-03-14 11:33:30 +0700

    added autocomplete search to the header

---
+++ project-base/storefront/app/_components/Layout/Header/AutocompleteSearch/AutocompleteSkeleton.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { SearchResultSectionGroup } from './AutocompleteSearchPopup';
+import Skeleton from 'react-loading-skeleton';
+import { createEmptyArray } from 'utils/arrays/createEmptyArray';
+
+export const AutocompleteSkeleton: FC = () => {
+    return (
+        <div className="vl:gap-6 flex flex-col gap-5">
+            <div>
+                <Skeleton className="mb-2 h-5 w-[100px]" />
+                <div className="flex gap-4 overflow-hidden lg:grid lg:grid-cols-5 lg:gap-5">
+                    {createEmptyArray(4).map((_, index) => (
+                        <Skeleton key={index} className="h-full min-h-[250px] min-w-[138px]" />
+                    ))}
+                </div>
+            </div>
+            <div>
+                <Skeleton className="mb-2 h-5 w-[80px]" />
+                <SearchResultSectionGroup>
+                    {createEmptyArray(3).map((_, index) => (
+                        <Skeleton key={index} className="h-[32px] w-[70px] !rounded-full" />
+                    ))}
+                </SearchResultSectionGroup>
+            </div>
+
+            <div className="flex justify-center">
+                <Skeleton className="vl:w-[186px] h-[52px] w-full" containerClassName="w-full vl:w-fit" />
+            </div>
+        </div>
+    );
+};
