deafa8f41ca412d8d8e327667bb6581423f3b46b
Martin Grossmann <martin@vyvoj.net>
2025-03-14 12:39:59 +0100

    fixed code style

--- packages/framework/src/Model/Mail/Setting/MailSetting.php
+++ packages/framework/src/Model/Mail/Setting/MailSetting.php
@@ -6,14 +6,14 @@ namespace Shopsys\FrameworkBundle\Model\Mail\Setting;
 
 class MailSetting
 {
-    public const MAIN_ADMIN_MAIL = 'mainAdminMail';
-    public const MAIN_ADMIN_MAIL_NAME = 'mainAdminMailName';
-    public const MAIL_WHITELIST = 'mailWhitelist';
-    public const MAIL_WHITELIST_ENABLED = 'mailWhitelistEnabled';
-    public const MAIL_FACEBOOK_URL = 'mailFacebookUrl';
-    public const MAIL_INSTAGRAM_URL = 'mailInstagramUrl';
-    public const MAIL_YOUTUBE_URL = 'mailYoutubeUrl';
-    public const MAIL_LINKEDIN_URL = 'mailLinkedinUrl';
-    public const MAIL_TIKTOK_URL = 'mailTiktokUrl';
-    public const MAIL_FOOTER_TEXT = 'mailFooterText';
+    public const string MAIN_ADMIN_MAIL = 'mainAdminMail';
+    public const string MAIN_ADMIN_MAIL_NAME = 'mainAdminMailName';
+    public const string MAIL_WHITELIST = 'mailWhitelist';
+    public const string MAIL_WHITELIST_ENABLED = 'mailWhitelistEnabled';
+    public const string MAIL_FACEBOOK_URL = 'mailFacebookUrl';
+    public const string MAIL_INSTAGRAM_URL = 'mailInstagramUrl';
+    public const string MAIL_YOUTUBE_URL = 'mailYoutubeUrl';
+    public const string MAIL_LINKEDIN_URL = 'mailLinkedinUrl';
+    public const string MAIL_TIKTOK_URL = 'mailTiktokUrl';
+    public const string MAIL_FOOTER_TEXT = 'mailFooterText';
 }
