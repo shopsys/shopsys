622f4bf9c5931b280c93f69ab85b11cdedce30c0
František Heča <frantisek.heca@shopsys.com>
2025-03-15 11:25:14 +0100

    fixed weird behaviour around product caching - in middleware we must rewrite to `/product/product-name` not to `product/[productSlug]`

--- project-base/storefront/types/friendlyUrl.ts
+++ project-base/storefront/types/friendlyUrl.ts
@@ -37,14 +37,15 @@ export const FriendlyPagesTypes = {
     account: 'front_customer_account',
 } as const;
 
+// TODO: refactorovat ostatní /[whatever] routy po vzoru '/products' ?
 export const FriendlyPagesDestinations: Record<FriendlyPagesTypesKey, string> = {
     article: '/articles/[articleSlug]',
     blogArticle: '/blogArticles/[blogArticleSlug]',
     blogCategory: '/blogCategories/[blogCategorySlug]',
     brand: '/brands/[brandSlug]',
     brandsOverview: '/brands-overview',
-    category: '/categories/[categorySlug]',
-    product: '/products/[productSlug]',
+    category: '/categories',
+    product: '/products',
     stores: '/stores',
     store: '/stores/[storeSlug]',
     flag: '/flags/[flagSlug]',
