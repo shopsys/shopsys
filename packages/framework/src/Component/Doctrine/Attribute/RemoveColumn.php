08d323fceabb41a768d79727945682b66c93f978
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2025-03-19 16:53:26 +0100

    Doctrine: Migration: added functionality for ignore column from entity by annotation

---
+++ packages/framework/src/Component/Doctrine/Attribute/RemoveColumn.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Component\Doctrine\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class RemoveColumn
+{
+}
