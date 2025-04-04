4998bb8f172c772e52a181e647dc93b1eaa91996
Jan Górecki <jan.gorecki@shopsys.com>
2025-04-04 23:58:26 +0200

    use device and image sizes configuration and synchronize it with imageResizer

--- project-base/storefront/next.config.js
+++ project-base/storefront/next.config.js
@@ -17,6 +17,8 @@ const nextConfig = {
     assetPrefix: process.env.CDN_DOMAIN ?? undefined,
     images: {
         loader: 'custom',
+        deviceSizes: [480, 768, 1024, 1440], // Do not forget to update the same values in the `app/web/imageResizer.php` file
+        imageSizes: [16, 24, 32, 48, 64, 96, 128, 256],
         remotePatterns: [
             {
                 hostname: process.env.DOMAIN_HOSTNAME_1,
