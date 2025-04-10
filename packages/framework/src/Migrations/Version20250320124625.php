1e79fc4924f75698cdce8c401da6148f00afa5df
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2025-04-10 14:12:31 +0200

    fixed migration for store coordinates

--- packages/framework/src/Migrations/Version20250320124625.php
+++ packages/framework/src/Migrations/Version20250320124625.php
@@ -16,6 +16,16 @@ class Version20250320124625 extends AbstractMigration
     #[Override]
     public function up(Schema $schema): void
     {
+        $this->sql('UPDATE stores SET latitude = CASE
+            WHEN latitude ~ \'[0-9.]+[NnEe]\' THEN regexp_replace(latitude, \'[^0-9.-]\', \'\', \'g\')
+            WHEN latitude ~ \'[0-9.]+[SsWw]\' THEN \'-\' || regexp_replace(latitude, \'[^0-9.-]\', \'\', \'g\')
+            ELSE latitude
+        END');
+        $this->sql('UPDATE stores SET longitude = CASE
+            WHEN longitude ~ \'[0-9.]+[NnEe]\' THEN regexp_replace(longitude, \'[^0-9.-]\', \'\', \'g\')
+            WHEN longitude ~ \'[0-9.]+[SsWw]\' THEN \'-\' || regexp_replace(longitude, \'[^0-9.-]\', \'\', \'g\')
+            ELSE longitude
+        END');
         $this->sql('ALTER TABLE stores ALTER latitude TYPE NUMERIC(20, 10) USING latitude::NUMERIC(20,10)');
         $this->sql('ALTER TABLE stores ALTER longitude TYPE NUMERIC(20, 10) USING longitude::NUMERIC(20,10)');
     }
