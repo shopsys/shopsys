2b6bfdd61c016458a427d36fc8f3bbd0c19783ba
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-04-18 07:25:24 +0200

    fixed long product detail name

--- project-base/storefront/components/Pages/ProductDetail/ProductDetailElements.tsx
+++ project-base/storefront/components/Pages/ProductDetail/ProductDetailElements.tsx
@@ -2,4 +2,4 @@ export const ProductDetailPrefix: FC = ({ children }) => (
     <div className="font-secondary text-textDisabled mb-1">{children}</div>
 );
 
-export const ProductDetailHeading: FC = ({ children }) => <h1 className="mb-4">{children}</h1>;
+export const ProductDetailHeading: FC = ({ children }) => <h1 className="mb-4 wrap-anywhere">{children}</h1>;
