8655826ec71643e28ce36e34667d17bc3e244840
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-04-23 13:12:15 +0200

    tweak data fixture maker template for multidomain and multilocale entities

--- packages/maker/templates/DataFixture.tpl.php
+++ packages/maker/templates/DataFixture.tpl.php
@@ -20,22 +20,26 @@ class <?= $class_name; ?> extends AbstractReferenceFixture implements DependentF
 
     public function load(ObjectManager $manager): void
     {
+        $<?= lcfirst($entity_config->entityName); ?>Data = $this-><?= lcfirst($entity_config->entityName); ?>DataFactory->create();
+
+        // $<?= lcfirst($entity_config->entityName); ?>Data-> // set common data object properties here
+
         foreach ($this->domainsForDataFixtureProvider->getAllowedDemoDataDomains() as $domainConfig) {
-            $<?= lcfirst($entity_config->entityName); ?>Data = $this-><?= lcfirst($entity_config->entityName); ?>DataFactory->create();
             $domainId = $domainConfig->getId();
+            $locale = $domainConfig->getLocale();
 
-            // $<?= lcfirst($entity_config->entityName); ?>Data-> // set data object properties here
-
-            $this->create<?= $entity_config->entityName; ?>($<?= lcfirst($entity_config->entityName); ?>Data, $domainId, self::<?= strtoupper($entity_config->entityName); ?>_REFERENCE);
+            // $<?= lcfirst($entity_config->entityName); ?>Data-> // set domain and locale data object properties here
         }
+
+        $this->create<?= $entity_config->entityName; ?>($<?= lcfirst($entity_config->entityName); ?>Data, self::<?= strtoupper($entity_config->entityName); ?>_REFERENCE);
     }
 
-    private function create<?= $entity_config->entityName; ?>(<?= $entity_config->entityName; ?>Data $<?= lcfirst($entity_config->entityName); ?>Data, int $domainId, ?string $referenceName = null): void
+    private function create<?= $entity_config->entityName; ?>(<?= $entity_config->entityName; ?>Data $<?= lcfirst($entity_config->entityName); ?>Data, ?string $referenceName = null): void
     {
         $<?= lcfirst($entity_config->entityName); ?> = $this-><?= lcfirst($entity_config->entityName); ?>Facade->create($<?= lcfirst($entity_config->entityName); ?>Data);
 
         if ($referenceName !== null) {
-            $this->addReferenceForDomain($referenceName, $<?= lcfirst($entity_config->entityName); ?>, $domainId);
+            $this->addReference($referenceName, $<?= lcfirst($entity_config->entityName); ?>);
         }
     }
 
