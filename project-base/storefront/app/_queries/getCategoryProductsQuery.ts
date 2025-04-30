6b3ecf655cd593ce34dc27b46db6d0acd708f9bd
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-04-30 14:49:04 +0200

    added category page

---
+++ project-base/storefront/app/_queries/getCategoryProductsQuery.ts
@@ -0,0 +1,38 @@
+'use server';
+
+import { createQuery } from 'app/_urql/urql-dto';
+import {
+    CategoryProductsQueryDocument,
+    TypeCategoryProductsQuery,
+    TypeCategoryProductsQueryVariables,
+} from 'graphql/requests/products/queries/CategoryProductsQuery.ssr';
+import { TypeProductOrderingModeEnum, TypeProductFilter } from 'graphql/types';
+
+export async function getCategoryProductsQuery(
+    urlSlug: string,
+    endCursor: string,
+    orderingMode: TypeProductOrderingModeEnum,
+    filter: TypeProductFilter,
+    pageSize: number,
+) {
+    const result = await createQuery<TypeCategoryProductsQuery, TypeCategoryProductsQueryVariables>(
+        CategoryProductsQueryDocument,
+        {
+            urlSlug,
+            endCursor,
+            orderingMode,
+            filter,
+            pageSize,
+        },
+    );
+
+    if (!result.data?.products.edges) {
+        return null;
+    }
+
+    const productsData = result.data.products.edges.map((edge) => edge?.node);
+
+    const products = productsData.filter((product) => product !== null && product !== undefined);
+
+    return products;
+}
