81c2e67cb0472de9f389a04ca7263b53a7065c16
František Heča <frantisek.heca@shopsys.com>
2025-04-30 10:48:35 +0200

    promotedCategoriesData fix - was it removed by mistake?

--- project-base/storefront/app/_components/Blocks/Categories/PromotedCategories.tsx
+++ project-base/storefront/app/_components/Blocks/Categories/PromotedCategories.tsx
@@ -1,8 +1,20 @@
 import { PromotedCategoriesContent } from './PromotedCategoriesContent';
+import { getTranslation } from 'app/_utils/translation/getTranslation';
 import { SkeletonModulePromotedCategories } from 'components/Blocks/Skeleton/SkeletonModulePromotedCategories';
+import { TypePromotedCategoriesQuery } from 'graphql/requests/categories/queries/PromotedCategoriesQuery.generated';
 import { Suspense } from 'react';
 
-export async function PromotedCategories() {
+export type PromotedCategoriesProps = {
+    promotedCategoriesData: TypePromotedCategoriesQuery | null | undefined;
+};
+
+export async function PromotedCategories({ promotedCategoriesData }: PromotedCategoriesProps) {
+    const t = await getTranslation();
+
+    if (!promotedCategoriesData?.promotedCategories.length) {
+        return null;
+    }
+
     return (
         <Suspense
             fallback={
@@ -11,7 +23,7 @@ export async function PromotedCategories() {
                 </section>
             }
         >
-            <PromotedCategoriesContent />
+            <PromotedCategoriesContent promotedCategoriesData={promotedCategoriesData} />
         </Suspense>
     );
 }
