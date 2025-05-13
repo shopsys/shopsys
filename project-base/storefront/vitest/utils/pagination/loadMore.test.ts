640ae8849c9085461cff138bd9ef63b8ed3d59cc
Jan Molcik <molcik.jan@gmail.com>
2025-05-13 15:55:59 +0200

    fix loadMore tests

--- project-base/storefront/vitest/utils/pagination/loadMore.test.ts
+++ project-base/storefront/vitest/utils/pagination/loadMore.test.ts
@@ -2,6 +2,8 @@ import { DEFAULT_PAGE_SIZE } from 'config/constants';
 import { CategoryProductsQueryDocument } from 'graphql/requests/products/queries/CategoryProductsQuery.generated';
 import { TypeProductOrderingModeEnum } from 'graphql/types';
 import { stringify } from 'querystring';
+import { UrlQueries } from 'types/urlQueries';
+import { getOffsetPage } from 'utils/loadMore/getOffsetPage';
 import { getOffsetPageAndLoadMore } from 'utils/loadMore/getOffsetPageAndLoadMore';
 import { getPreviousProductsFromCache } from 'utils/loadMore/getPreviousProductsFromCache';
 import { getRedirectWithOffsetPage } from 'utils/loadMore/getRedirectWithOffsetPage';
@@ -106,29 +108,88 @@ describe('getPreviouslyQueriedProductsFromCache tests', () => {
 
 describe('getOffsetPageAndLoadMore tests', () => {
     test('loading a valid number of products does not offset the page', () => {
-        expect(getOffsetPageAndLoadMore(2, 10)).toBe(undefined);
-
-        expect(getOffsetPageAndLoadMore(1, 10)).toBe(undefined);
-
         expect(getOffsetPageAndLoadMore(1, 0)).toBe(undefined);
 
+        expect(getOffsetPageAndLoadMore(1, 1)).toBe(undefined);
+
+        expect(getOffsetPageAndLoadMore(1, 2)).toBe(undefined);
+
         expect(getOffsetPageAndLoadMore(1, 1, 50)).toBe(undefined);
+
+        expect(getOffsetPageAndLoadMore(2, 10, undefined, 310)).toBe(undefined);
+
+        expect(getOffsetPageAndLoadMore(2, 10, undefined, 310)).toBe(undefined);
+
+        expect(getOffsetPageAndLoadMore(1, 10, undefined, 310)).toBe(undefined);
     });
 
     test('loading too many products offsets the page', () => {
-        expect(getOffsetPageAndLoadMore(2, 11)).toStrictEqual({
-            updatedPage: 3,
-            updatedLoadMore: 10,
+        expect(getOffsetPageAndLoadMore(1, 3)).toStrictEqual({
+            updatedPage: 2,
+            updatedLoadMore: 2,
         });
 
-        expect(getOffsetPageAndLoadMore(1, 11)).toStrictEqual({
+        expect(getOffsetPageAndLoadMore(1, 10)).toStrictEqual({
+            updatedPage: 9,
+            updatedLoadMore: 2,
+        });
+
+        expect(getOffsetPageAndLoadMore(2, 10, 50)).toStrictEqual({
+            updatedPage: 11,
+            updatedLoadMore: 1,
+        });
+
+        expect(getOffsetPageAndLoadMore(2, 10, 50, 310)).toStrictEqual({
+            updatedPage: 7,
+            updatedLoadMore: 5,
+        });
+
+        expect(getOffsetPageAndLoadMore(1, 11, undefined, 310)).toStrictEqual({
             updatedPage: 2,
             updatedLoadMore: 10,
         });
 
-        expect(getOffsetPageAndLoadMore(2, 10, 50)).toStrictEqual({
-            updatedPage: 7,
-            updatedLoadMore: 5,
+        expect(getOffsetPageAndLoadMore(2, 11, undefined, 310)).toStrictEqual({
+            updatedPage: 3,
+            updatedLoadMore: 10,
+        });
+    });
+});
+
+describe('getOffsetPage tests', () => {
+    test('loading a valid number of products does not offset the page', () => {
+        expect(getOffsetPage(1, 0)).toBe(undefined);
+    });
+
+    test('loading too many products offsets the page', () => {
+        expect(getOffsetPage(1, 1)).toStrictEqual({
+            updatedPage: 2,
+            updatedLoadMore: 0,
+        });
+
+        expect(getOffsetPage(1, 2)).toStrictEqual({
+            updatedPage: 3,
+            updatedLoadMore: 0,
+        });
+
+        expect(getOffsetPage(2, 2)).toStrictEqual({
+            updatedPage: 4,
+            updatedLoadMore: 0,
+        });
+
+        expect(getOffsetPage(2, 10)).toStrictEqual({
+            updatedPage: 12,
+            updatedLoadMore: 0,
+        });
+
+        expect(getOffsetPage(1, 11)).toStrictEqual({
+            updatedPage: 12,
+            updatedLoadMore: 0,
+        });
+
+        expect(getOffsetPage(2, 11)).toStrictEqual({
+            updatedPage: 13,
+            updatedLoadMore: 0,
         });
     });
 });
@@ -136,30 +197,10 @@ describe('getOffsetPageAndLoadMore tests', () => {
 describe('getRedirectWithOffsetPage tests', () => {
     test('loading a valid number of products does not redirect the page', () => {
         expect(
-            getRedirectWithOffsetPage(2, 10, '/my-url', {
-                [PAGE_QUERY_PARAMETER_NAME]: '2',
-                [LOAD_MORE_QUERY_PARAMETER_NAME]: '10',
+            getRedirectWithOffsetPage(1, 0, '/my-url', {
+                [PAGE_QUERY_PARAMETER_NAME]: '1',
+                [LOAD_MORE_QUERY_PARAMETER_NAME]: '0',
             }),
-        ).toBe(undefined);
-
-        expect(
-            getRedirectWithOffsetPage(1, 10, '/my-url', {
-                [LOAD_MORE_QUERY_PARAMETER_NAME]: '10',
-            }),
-        ).toBe(undefined);
-
-        expect(getRedirectWithOffsetPage(1, 0, '/my-url', {})).toBe(undefined);
-
-        expect(
-            getRedirectWithOffsetPage(
-                1,
-                1,
-                '/my-url',
-                {
-                    [LOAD_MORE_QUERY_PARAMETER_NAME]: '1',
-                },
-                50,
-            ),
         ).toBe(undefined);
     });
 
@@ -171,7 +212,7 @@ describe('getRedirectWithOffsetPage tests', () => {
             }),
         ).toStrictEqual({
             redirect: {
-                destination: '/my-url?page=3&lm=10',
+                destination: '/my-url?page=13',
                 permanent: false,
             },
         });
@@ -182,38 +223,26 @@ describe('getRedirectWithOffsetPage tests', () => {
             }),
         ).toStrictEqual({
             redirect: {
-                destination: '/my-url?lm=10&page=2',
+                destination: '/my-url?page=12',
                 permanent: false,
             },
         });
 
         expect(
-            getRedirectWithOffsetPage(
-                2,
-                10,
-                '/my-url',
-                {
-                    [LOAD_MORE_QUERY_PARAMETER_NAME]: '11',
-                },
-                50,
-            ),
+            getRedirectWithOffsetPage(2, 10, '/my-url', {
+                [LOAD_MORE_QUERY_PARAMETER_NAME]: '11',
+            }),
         ).toStrictEqual({
             redirect: {
-                destination: '/my-url?lm=5&page=7',
+                destination: '/my-url?page=12',
                 permanent: false,
             },
         });
 
         expect(
-            getRedirectWithOffsetPage(
-                2,
-                10,
-                '/my-url',
-                {
-                    [LOAD_MORE_QUERY_PARAMETER_NAME]: '11',
-                },
-                310,
-            ),
+            getRedirectWithOffsetPage(2, 10, '/my-url', {
+                [LOAD_MORE_QUERY_PARAMETER_NAME]: '11',
+            }),
         ).toStrictEqual({
             redirect: {
                 destination: '/my-url?page=12',
@@ -223,7 +252,7 @@ describe('getRedirectWithOffsetPage tests', () => {
     });
 
     test('redirecting correctly keeps other query parameters if both page and load more are kept', () => {
-        const currentQueryWithoutDynamicPageQuery = {
+        const currentQueryWithoutDynamicPageQuery: UrlQueries = {
             [PAGE_QUERY_PARAMETER_NAME]: '2',
             [LOAD_MORE_QUERY_PARAMETER_NAME]: '11',
             [FILTER_QUERY_PARAMETER_NAME]: JSON.stringify({
@@ -251,8 +280,8 @@ describe('getRedirectWithOffsetPage tests', () => {
             categorySlug: '/category-url',
         };
 
-        currentQueryWithoutDynamicPageQuery[PAGE_QUERY_PARAMETER_NAME] = '3';
-        currentQueryWithoutDynamicPageQuery[LOAD_MORE_QUERY_PARAMETER_NAME] = '10';
+        currentQueryWithoutDynamicPageQuery[PAGE_QUERY_PARAMETER_NAME] = '13';
+        delete currentQueryWithoutDynamicPageQuery[LOAD_MORE_QUERY_PARAMETER_NAME];
 
         expect(getRedirectWithOffsetPage(2, 11, '/my-url', currentQuery)).toStrictEqual({
             redirect: {
@@ -292,7 +321,7 @@ describe('getRedirectWithOffsetPage tests', () => {
 
         currentQueryWithoutDynamicPageAndLoadMoreQuery[PAGE_QUERY_PARAMETER_NAME] = '13';
 
-        expect(getRedirectWithOffsetPage(2, 11, '/my-url', currentQuery, 310)).toStrictEqual({
+        expect(getRedirectWithOffsetPage(2, 11, '/my-url', currentQuery)).toStrictEqual({
             redirect: {
                 destination: `/my-url?${stringify(currentQueryWithoutDynamicPageAndLoadMoreQuery)}`,
                 permanent: false,
