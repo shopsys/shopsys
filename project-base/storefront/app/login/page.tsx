f85638a2d0fd28638cee8c56d36bba591f594283
Jan Molcik <molcik.jan@gmail.com>
2025-06-05 12:16:52 +0200

    fix static pages build failed

--- project-base/storefront/app/login/page.tsx
+++ project-base/storefront/app/login/page.tsx
@@ -2,6 +2,8 @@ import { LoginForm } from 'app/_components/Blocks/LoginForm/LoginForm';
 import { getTranslation } from 'app/_utils/translation/getTranslation';
 import { Webline } from 'components/Layout/Webline/Webline';
 
+export const dynamic = 'force-dynamic';
+
 const LoginPage = async () => {
     const t = await getTranslation();
 
