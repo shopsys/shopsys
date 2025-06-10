fa743fdf7f477c2721f9c8c7d972b0d5ddb704e7
Jan Molcik <molcik.jan@gmail.com>
2025-06-10 15:47:06 +0200

    fix loading pages

---
+++ project-base/storefront/app/user-consent/loading.tsx
@@ -0,0 +1,7 @@
+import { SkeletonPageArticle } from 'components/Blocks/Skeleton/SkeletonPageArticle';
+
+const Loading = () => {
+    return <SkeletonPageArticle />;
+};
+
+export default Loading;
--- project-base/storefront/components/Blocks/Skeleton/SkeletonPageArticle.tsx
+++ project-base/storefront/components/Blocks/Skeleton/SkeletonPageArticle.tsx
@@ -1,20 +1,15 @@
 'use client';
 
-import { SkeletonModuleBreadcrumbs } from './SkeletonModuleBreadcrumbs';
 import { Skeleton } from 'components/Basic/Skeleton/Skeleton';
 import { VerticalStack } from 'components/Layout/VerticalStack/VerticalStack';
 import { Webline } from 'components/Layout/Webline/Webline';
 
 export const SkeletonPageArticle: FC = () => (
-    <>
-        <SkeletonModuleBreadcrumbs count={2} />
-
-        <Webline width="md">
-            <VerticalStack gap="sm">
-                <Skeleton className="h-10 w-1/2" />
-                <Skeleton className="h-5 w-28 rounded-sm" />
-                <Skeleton className="h-96" />
-            </VerticalStack>
-        </Webline>
-    </>
+    <Webline width="md">
+        <VerticalStack gap="sm">
+            <Skeleton className="h-10 w-1/2" />
+            <Skeleton className="h-5 w-28 rounded-sm" />
+            <Skeleton className="h-96" />
+        </VerticalStack>
+    </Webline>
 );
