d785be3a677406173ded3571d31f231020645259
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-06-13 16:03:04 +0200

    admin: remove ProductVisibility.js
    - instead, ajaxPopover component is used in product list and detail

--- packages/framework/assets/js/admin/components/index.js
+++ packages/framework/assets/js/admin/components/index.js
@@ -40,7 +40,6 @@ import './OrderTransportAndPaymentPrefiller';
 import './Parameters';
 import './Payment';
 import './Product';
-import './ProductVisibility';
 import './PriceListProductPicker';
 import './ProductPicker';
 import './PriceListProductPickerWindow';
