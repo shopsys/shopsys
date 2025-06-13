588b417bf78b79b59cad8195ac39ea1534c5c62c
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-06-13 16:24:25 +0200

    admin: add caching to ajaxPopover component
    - the cache ensures that for the given URL, only one AJAX call is performed, even if the popover is triggered multiple times

--- packages/administration/assets/src/ajaxPopover.js
+++ packages/administration/assets/src/ajaxPopover.js
@@ -1,17 +1,29 @@
 import { Popover } from '@tabler/core';
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"][data-content-url]'));
+let contentCache = {};
 
 popoverTriggerList.map(function (popoverTriggerEl) {
     popoverTriggerEl.addEventListener('shown.bs.popover', function () {
-        fetch(this.getAttribute('data-content-url'))
-            .then((response) => response.text())
-            .then((data) => {
-                const popover = Popover.getInstance(popoverTriggerEl);
-                if (popover) {
-                    popover.tip.querySelector('.popover-body').innerHTML = data;
-                    popover.update();
-                }
-            });
+        const contentUrl = this.getAttribute('data-content-url');
+
+        if (contentCache[contentUrl]) {
+            updatePopoverContent(contentCache[contentUrl], popoverTriggerEl);
+        } else {
+            fetch(contentUrl)
+                .then((response) => response.text())
+                .then((data) => {
+                    contentCache[contentUrl] = data;
+                    updatePopoverContent(data, popoverTriggerEl);
+                });
+        }
     });
 });
+
+const updatePopoverContent = function (content, popoverTriggerEl) {
+    const popover = Popover.getInstance(popoverTriggerEl);
+    if (popover) {
+        popover.tip.querySelector('.popover-body').innerHTML = content;
+        popover.update();
+    }
+}
