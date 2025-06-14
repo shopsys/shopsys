0f255e41edd7783297052b2c513e3d840ae3f7ed
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-06-14 17:05:09 +0200

    admin: ensure popover is closed when clicked anywhere on the page
    - popover remains opened when user clicks on its content

--- packages/administration/assets/src/ajaxPopover.js
+++ packages/administration/assets/src/ajaxPopover.js
@@ -3,6 +3,19 @@ import { Popover } from '@tabler/core';
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"][data-content-url]'));
 let contentCache = {};
 
+$('body').on('click', function (e) {
+    if ($(e.target).closest('.popover').length > 0) {
+        return;
+    }
+
+    document.querySelectorAll('[data-bs-toggle="popover"]').forEach(trigger => {
+        const popover = Popover.getInstance(trigger);
+        if (popover) {
+            popover.hide();
+        }
+    });
+});
+
 popoverTriggerList.map(function (popoverTriggerEl) {
     popoverTriggerEl.addEventListener('shown.bs.popover', function () {
         const contentUrl = this.getAttribute('data-content-url');
