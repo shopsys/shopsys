81a11d99ffecc7329ae531f2bfb150d803d9cb49
Martin Grossmann <martin@vyvoj.net>
2025-06-16 16:01:13 +0200

    replaced jquery-ui/slider with nouislider

--- packages/framework/assets/js/admin/components/NumberSlider.js
+++ packages/framework/assets/js/admin/components/NumberSlider.js
@@ -1,21 +1,28 @@
-import 'jquery-ui/ui/widgets/slider';
 import Register from '../../common/utils/Register';
+import noUiSlider from 'nouislider';
 
 export default class NumberSlider {
     constructor ($sliderContainer) {
-        $sliderContainer.children('.js-number-slider__slider').slider({
-            min: 0,
-            max: 1,
+        const sliderElement = $sliderContainer.children('.js-number-slider__slider')[0];
+        const inputElement = $sliderContainer.find('.js-number-slider__input')[0];
+
+        const initialValue = parseFloat(inputElement.value.replace(',', '.')) || 0;
+
+        noUiSlider.create(sliderElement, {
+            start: [initialValue],
             step: 0.01,
-            slide: function (event, ui) {
-                $(this).next('.js-number-slider__input').val(ui.value);
-            },
-            create: function (event, ui) {
-                $(this).slider(
-                    'value',
-                    $(this).next('.js-number-slider__input').val().replace(',', '.')
-                );
+            range: {
+                min: 0,
+                max: 1
             }
+        });
+
+        sliderElement.noUiSlider.on('update', (values, handle) => {
+            inputElement.value = values[handle];
+        });
+
+        inputElement.addEventListener('change', () => {
+            sliderElement.noUiSlider.set([parseFloat(inputElement.value.replace(',', '.')) || 0]);
         });
     }
 
