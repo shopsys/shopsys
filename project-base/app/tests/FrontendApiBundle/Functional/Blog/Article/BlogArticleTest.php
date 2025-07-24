25d2658db9717aca79f340e25ef0aac6aa123172
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-07-24 17:04:23 +0200

    added articles demo data

--- project-base/app/tests/FrontendApiBundle/Functional/Blog/Article/BlogArticleTest.php
+++ project-base/app/tests/FrontendApiBundle/Functional/Blog/Article/BlogArticleTest.php
@@ -214,36 +214,20 @@ class BlogArticleTest extends GraphQlTestCase
         $firstBlogCategory = $this->getReference(BlogArticleDataFixture::FIRST_DEMO_BLOG_CATEGORY, BlogCategory::class);
         $firstBlogCategorySlug = $this->urlGenerator->generate('front_blogcategory_detail', ['id' => $firstBlogCategory->getId()]);
 
-        $description = t(
-            '<div class="gjs-text-ckeditor">
-                    description - Lorem ipsum dolor sit amet,
-                </div>
-                %productsFirstRow%
-                <div class="gjs-text-ckeditor">
-                    consectetur
-                </div>
-                %productsSecondRow%
-                <div class="gjs-text-ckeditor">adipiscing elit. Vivamus felis nisi, tincidunt sollicitudin augue eu,
-                    laoreet blandit sem. Donec rutrum augue a elit imperdiet, eu vehicula tortor porta.
-                    Vivamus pulvinar sem non auctor dictum.
-                    Morbi eleifend semper enim, eu faucibus tortor posuere vitae.
-                    Donec tincidunt ipsum ullamcorper nisi accumsan tincidunt.
-                    Aenean sed velit massa. Nullam interdum eget est ut convallis.
-                    Vestibulum et mauris condimentum, rutrum sem congue, suscipit arcu.
-                    \nSed tristique vehicula ipsum, ut vulputate tortor feugiat eu.
-                    Vivamus convallis quam vulputate faucibus facilisis.
-                    Curabitur tincidunt pulvinar leo, eu dapibus augue lacinia a.
-                    Fusce sed tincidunt nunc. Morbi a nisi a odio pharetra laoreet nec eget quam.
-                    In in nisl tortor. Ut fringilla vitae lectus eu venenatis. Nullam interdum sed odio a posuere.
-                    Fusce pellentesque dui vel tortor blandit, a dictum nunc congue.
-                </div>',
-            [
-                '%productsFirstRow%' => '<div class="gjs-products" data-products="9177759,7700768,9146508"><div class="gjs-product" data-product="9177759"></div><div class="gjs-product" data-product="7700768"></div><div class="gjs-product" data-product="9146508"></div></div>',
-                '%productsSecondRow%' => '<div class="gjs-products" data-products="9177759,9176508"><div class="gjs-product" data-product="9177759"></div><div class="gjs-product" data-product="9176508"></div></div>',
-            ],
-            Translator::DATA_FIXTURES_TRANSLATION_DOMAIN,
-            $locale,
-        );
+        $description = str_replace(['    ', PHP_EOL], '', trim(<<<EOT
+            <div class="gjs-text-ckeditor">
+                <p>
+                    Blog Article Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quos doloribus accusantium, aliquam commodi molestiae atque laudantium in dolorem esse error blanditiis, debitis facere id voluptate. Accusantium mollitia placeat consequatur.
+                </p>
+            </div>
+            <div class="gjs-text-ckeditor">
+                <h2>Heading H2</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quos doloribus accusantium, aliquam commodi molestiae atque laudantium in dolorem esse error blanditiis, debitis facere id voluptate. Accusantium mollitia placeat consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quos doloribus accusantium, aliquam commodi molestiae atque laudantium in dolorem esse error blanditiis, debitis facere id voluptate. Accusantium mollitia placeat consequatur.
+                </p>
+            </div>
+            <div class="gjs-products" data-products="9177759,5965879P,9184449,9176544M,7700768"><div class="gjs-product" data-product="9177759"></div><div class="gjs-product" data-product="5965879P"></div><div class="gjs-product" data-product="9184449"></div><div class="gjs-product" data-product="9176544M"></div><div class="gjs-product" data-product="7700768"></div></div>
+        EOT));
         $description = $this->grapesJsParser->parse($description);
 
         return [
