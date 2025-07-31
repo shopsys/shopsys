335a08fa16cc2deabac5bd5abf3a0d1b6d4fc2ea
Martin Grossmann <martin@vyvoj.net>
2025-07-31 10:23:16 +0200

    chore: removed unused imports

--- packages/frontend-api/src/Model/Resolver/Price/SpecialPriceApiFactory.php
+++ packages/frontend-api/src/Model/Resolver/Price/SpecialPriceApiFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Shopsys\FrontendApiBundle\Model\Resolver\Price;
 
 use DateTimeImmutable;
-use Shopsys\FrameworkBundle\Model\Pricing\Price;
 use Shopsys\FrameworkBundle\Model\Pricing\PriceInterface;
 use Shopsys\FrameworkBundle\Model\Pricing\SpecialPrice\SpecialPrice;
 use Shopsys\FrameworkBundle\Model\Pricing\SpecialPrice\SpecialPriceFactory;
