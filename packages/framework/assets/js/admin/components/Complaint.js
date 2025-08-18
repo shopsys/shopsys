9a2801b85ec776dfff5349e68af27aace2819944
Martin Grossmann <martin@vyvoj.net>
2025-08-18 14:03:38 +0200

    fixed complaint edit JS

--- packages/framework/assets/js/admin/components/Complaint.js
+++ packages/framework/assets/js/admin/components/Complaint.js
@@ -1,19 +1,29 @@
 import Register from '../../common/utils/Register';
 
 export default class Complaint {
-    static init() {
-        const $complaintResolutionInput = $('.js-complaint-resolution');
-        const $bankAccountNumberInput = $('.js-complaint-bank-account-number');
+    constructor($container) {
+        this.$complaintResolutionInput = $container.find('.js-complaint-resolution');
+        this.$bankAccountNumberRow = $container.find('[data-js-complaint-bank-account-number]');
 
-        $complaintResolutionInput.on('change', function () {
-            if ($(this).val() === 'money_return') {
-                $bankAccountNumberInput.closest('.form-line').show();
-            } else {
-                $bankAccountNumberInput.closest('.form-line').hide();
-            }
-        });
+        this.$complaintResolutionInput.on('change', () => this.handleResolutionChange());
+        this.$complaintResolutionInput.trigger('change');
+    }
 
-        $complaintResolutionInput.trigger('change');
+    handleResolutionChange() {
+        if (this.$complaintResolutionInput.val() === 'money_return') {
+            this.$bankAccountNumberRow.show();
+        } else {
+            this.$bankAccountNumberRow.hide();
+        }
+    }
+
+    static init($container) {
+        const $complaintForm = $container.filterAllNodes('.js-complaint-resolution');
+
+        if ($complaintForm.length > 0) {
+            // eslint-disable-next-line no-new
+            new Complaint($container);
+        }
     }
 }
 
