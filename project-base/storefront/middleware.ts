4caefce912e4d7c37ba3dea2200f9129c2aa16ab
Martin Grossmann <martin@vyvoj.net>
2025-09-30 01:03:22 +0200

    optimized nginx config
    - internal redirects don't make unnecessary stat file due to try_files hack. used error_page instead
    - split locations into meaningful blocks. each render corresponding error page
    - simplified image rendering location regex
    - internal locations are marked internal (for consistency)

--- project-base/storefront/middleware.ts
+++ project-base/storefront/middleware.ts
@@ -123,7 +123,7 @@ export const middleware: NextMiddleware = async (request) => {
 export const config = {
     matcher: [
         '/', // Explicitly match the homepage
-        '/((?!api|_next|favicon.ico|fonts|svg|images|locales|icons|grapesjs-template|grapesjs-homepage-article-template|grapesjs-article-template|tailwind-for-admin|robots).*)',
+        '/((?!api|_next|favicon.ico|fonts|svg|images|locales|content/locales|icons|grapesjs-template|grapesjs-homepage-article-template|grapesjs-article-template|tailwind-for-admin|robots).*)',
     ],
 };
 
