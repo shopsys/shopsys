34a3e9d11341a54d88721a1b066d772f3c98703f
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-10-10 14:21:04 +0200

    SF product filter choices: ensure parameter values are sorted alphabetically for the given locale

---
+++ packages/framework/src/Model/Product/Parameter/ParameterSortingHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Product\Parameter;
+
+use Collator;
+
+class ParameterSortingHelper
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterValue[] $parameterValues
+     * @param string $locale
+     * @return \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterValue[]
+     */
+    public function sortParameterValuesAlphabetically(array $parameterValues, string $locale): array
+    {
+        $collator = new Collator($locale);
+
+        uasort($parameterValues, static function (ParameterValue $first, ParameterValue $second) use ($collator) {
+            return $collator->compare($first->getText(), $second->getText());
+        });
+
+        return $parameterValues;
+    }
+}
