10389ac2eae567f6558ce3e102bcdb90fae3a6fa
Jan Górecki <jan.gorecki@shopsys.com>
2025-10-27 15:47:13 +0100

    make CrudConfigData immutable

--- packages/administration/src/Component/Config/CrudConfig.php
+++ packages/administration/src/Component/Config/CrudConfig.php
@@ -4,19 +4,50 @@ declare(strict_types=1);
 
 namespace Shopsys\AdministrationBundle\Component\Config;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Webmozart\Assert\Assert;
 
 final class CrudConfig
 {
-    private CrudConfigData $crudConfigData;
+    /**
+     * @var array<value-of<\Shopsys\AdministrationBundle\Component\Config\ActionType>, string>
+     */
+    private array $customPageTitles;
+
+    public ?string $menuTitle = null;
+
+    private bool $fullDisabled = false;
 
     /**
-     * @param class-string<\Shopsys\AdministrationBundle\Controller\AbstractCrudController> $crudController
+     * @var \Doctrine\Common\Collections\ArrayCollection<\Shopsys\AdministrationBundle\Component\Config\ActionType>
+     */
+    private ArrayCollection $enabledActions;
+
+    private string $menuSection = 'root';
+
+    private ?string $submenuSection = null;
+
+    private bool $visibleInMenu = true;
+
+    private ?string $routePrefix = null;
+
+    /**
      * @param string $entityName
      */
-    public function __construct(string $crudController, string $entityName)
+    public function __construct(string $entityName)
     {
-        $this->crudConfigData = new CrudConfigData($crudController, $entityName);
+        $this->customPageTitles = [
+            ActionType::CREATE->value => t('Creating new %entity_name%', ['%entity_name%' => $entityName]),
+            ActionType::EDIT->value => t('Editing %entity_name%', ['%entity_name%' => $entityName]),
+            ActionType::LIST->value => t('%entity_name% Overview', ['%entity_name%' => $entityName]),
+            ActionType::DETAIL->value => t('Viewing %entity_name%', ['%entity_name%' => $entityName]),
+        ];
+
+        $this->enabledActions = new ArrayCollection([
+            ActionType::LIST,
+        ]);
+
+        $this->menuTitle = t('%entity_name% Overview', ['%entity_name%' => $entityName]);
     }
 
     /**
@@ -28,7 +59,7 @@ final class CrudConfig
      */
     public function setTitle(ActionType $actionType, string $title): self
     {
-        $this->crudConfigData->customPageTitles[$actionType->value] = $title;
+        $this->customPageTitles[$actionType->value] = $title;
 
         return $this;
     }
@@ -41,7 +72,7 @@ final class CrudConfig
      */
     public function setMenuTitle(string $menuTitle): self
     {
-        $this->crudConfigData->menuTitle = $menuTitle;
+        $this->menuTitle = $menuTitle;
 
         return $this;
     }
@@ -61,7 +92,11 @@ final class CrudConfig
         Assert::allIsInstanceOf($actions, ActionType::class, 'The given action is not a valid action type');
 
         foreach ($actions as $action) {
-            $this->crudConfigData->enableAction($action);
+            if ($this->enabledActions->contains($action)) {
+                continue;
+            }
+
+            $this->enabledActions->add($action);
         }
 
         return $this;
@@ -82,7 +117,7 @@ final class CrudConfig
         Assert::allIsInstanceOf($actions, ActionType::class, 'The given action is not a valid action type');
 
         foreach ($actions as $action) {
-            $this->crudConfigData->disableAction($action);
+            $this->enabledActions->removeElement($action);
         }
 
         return $this;
@@ -97,8 +132,8 @@ final class CrudConfig
      */
     public function setMenuSection(string $menuSection, ?string $submenuSection = null): self
     {
-        $this->crudConfigData->menuSection = $menuSection;
-        $this->crudConfigData->submenuSection = $submenuSection;
+        $this->menuSection = $menuSection;
+        $this->submenuSection = $submenuSection;
 
         return $this;
     }
@@ -111,7 +146,7 @@ final class CrudConfig
      */
     public function visibleInMenu(bool $visible): self
     {
-        $this->crudConfigData->visibleInMenu = $visible;
+        $this->visibleInMenu = $visible;
 
         return $this;
     }
@@ -124,7 +159,7 @@ final class CrudConfig
      */
     public function disable(bool $disabled): self
     {
-        $this->crudConfigData->fullDisabled = $disabled;
+        $this->fullDisabled = $disabled;
 
         return $this;
     }
@@ -139,7 +174,7 @@ final class CrudConfig
      */
     public function setRoutePrefix(?string $routePrefix): self
     {
-        $this->crudConfigData->routePrefix = $routePrefix;
+        $this->routePrefix = $routePrefix;
 
         return $this;
     }
@@ -149,6 +184,15 @@ final class CrudConfig
      */
     public function getConfig(): CrudConfigData
     {
-        return $this->crudConfigData;
+        return new CrudConfigData(
+            $this->customPageTitles,
+            $this->menuTitle,
+            $this->fullDisabled,
+            $this->enabledActions->toArray(),
+            $this->menuSection,
+            $this->submenuSection,
+            $this->visibleInMenu,
+            $this->routePrefix,
+        );
     }
 }
--- packages/administration/src/Component/Config/CrudConfigData.php
+++ packages/administration/src/Component/Config/CrudConfigData.php
@@ -4,56 +4,28 @@ declare(strict_types=1);
 
 namespace Shopsys\AdministrationBundle\Component\Config;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
-final class CrudConfigData
+final readonly class CrudConfigData
 {
-    public array $customPageTitles = [
-        ActionType::CREATE->value => null,
-        ActionType::EDIT->value => null,
-        ActionType::LIST->value => null,
-        ActionType::DETAIL->value => null,
-    ];
-
-    public ?string $menuTitle = null;
-
-    public bool $fullDisabled = false;
-
     /**
-     * @var \Doctrine\Common\Collections\ArrayCollection<\Shopsys\AdministrationBundle\Component\Config\ActionType>
+     * @param array<value-of<\Shopsys\AdministrationBundle\Component\Config\ActionType>, string> $customPageTitles
+     * @param string|null $menuTitle
+     * @param bool $fullDisabled
+     * @param \Shopsys\AdministrationBundle\Component\Config\ActionType[] $enabledActions
+     * @param string $menuSection
+     * @param string|null $submenuSection
+     * @param bool $visibleInMenu
+     * @param string|null $routePrefix
      */
-    private ArrayCollection $enabledActions;
-
-    public string $menuSection = 'root';
-
-    public ?string $submenuSection = null;
-
-    public bool $visibleInMenu = true;
-
-    public ?string $routePrefix = null;
-
-    /**
-     * @param class-string<\Shopsys\AdministrationBundle\Controller\AbstractCrudController> $crudController
-     * @param string $entityName
-     */
-    public function __construct(private string $crudController, private string $entityName)
-    {
-        $this->enabledActions = new ArrayCollection([
-            ActionType::LIST,
-            ActionType::CREATE,
-            ActionType::EDIT,
-            ActionType::DETAIL,
-            ActionType::DELETE,
-        ]);
-
-        $this->customPageTitles = [
-            ActionType::CREATE->value => t('Creating new %entity_name%', ['%entity_name%' => $this->entityName]),
-            ActionType::EDIT->value => t('Editing %entity_name%', ['%entity_name%' => $this->entityName]),
-            ActionType::LIST->value => t('%entity_name% Overview', ['%entity_name%' => $this->entityName]),
-            ActionType::DETAIL->value => t('Viewing %entity_name%', ['%entity_name%' => $this->entityName]),
-        ];
-
-        $this->menuTitle = t('%entity_name% Overview', ['%entity_name%' => $this->entityName]);
+    public function __construct(
+        private array $customPageTitles,
+        private ?string $menuTitle,
+        private bool $fullDisabled,
+        private array $enabledActions,
+        private string $menuSection,
+        private ?string $submenuSection,
+        private bool $visibleInMenu,
+        private ?string $routePrefix,
+    ) {
     }
 
     /**
@@ -61,7 +33,7 @@ final class CrudConfigData
      */
     public function getCrudController(): string
     {
-        return $this->crudController;
+        return '';
     }
 
     /**
@@ -69,7 +41,7 @@ final class CrudConfigData
      */
     public function getEntityName(): string
     {
-        return $this->entityName;
+        return '';
     }
 
     /**
@@ -78,7 +50,7 @@ final class CrudConfigData
      */
     public function getTitle(ActionType $pageType): string
     {
-        return $this->customPageTitles[$pageType->value];
+        return $this->customPageTitles[$pageType->value] ?? '';
     }
 
     /**
@@ -98,19 +70,7 @@ final class CrudConfigData
             return [];
         }
 
-        return $this->enabledActions->getValues();
-    }
-
-    /**
-     * @param \Shopsys\AdministrationBundle\Component\Config\ActionType $actionType
-     */
-    public function enableAction(ActionType $actionType): void
-    {
-        if ($this->enabledActions->contains($actionType)) {
-            return;
-        }
-
-        $this->enabledActions->add($actionType);
+        return $this->enabledActions;
     }
 
     /**
@@ -119,19 +79,7 @@ final class CrudConfigData
      */
     public function isActionEnabled(ActionType $actionType): bool
     {
-        return $this->enabledActions->contains($actionType);
-    }
-
-    /**
-     * @param \Shopsys\AdministrationBundle\Component\Config\ActionType $actionType
-     */
-    public function disableAction(ActionType $actionType): void
-    {
-        if (!$this->enabledActions->contains($actionType)) {
-            return;
-        }
-
-        $this->enabledActions->removeElement($actionType);
+        return in_array($actionType, $this->enabledActions, true);
     }
 
     /**
@@ -163,7 +111,7 @@ final class CrudConfigData
      */
     public function isVisibleInMenu(): bool
     {
-        return $this->visibleInMenu && $this->enabledActions->contains(ActionType::LIST);
+        return $this->visibleInMenu && $this->isActionEnabled(ActionType::LIST);
     }
 
     /**
