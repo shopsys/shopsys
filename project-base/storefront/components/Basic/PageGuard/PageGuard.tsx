231e038baa926281e9012f8d71b95d95c9762240
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-10-28 15:19:01 +0100

    SF: add error flash message to page guard

--- project-base/storefront/components/Basic/PageGuard/PageGuard.tsx
+++ project-base/storefront/components/Basic/PageGuard/PageGuard.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
+import useTranslation from 'utils/i18n/useTranslationWrapper';
 import { isClient } from 'utils/isClient';
+import { showErrorMessage } from 'utils/toasts/showErrorMessage';
 
 type PageGuardProps = {
     isWithAccess: boolean;
@@ -8,9 +10,11 @@ type PageGuardProps = {
 
 export const PageGuard: FC<PageGuardProps> = ({ isWithAccess, errorRedirectUrl, children }) => {
     const router = useRouter();
+    const { t } = useTranslation();
 
     if (!isWithAccess) {
         if (isClient) {
+            showErrorMessage(t('Requested page cannot be accessed.'));
             router.replace(errorRedirectUrl);
         }
 
