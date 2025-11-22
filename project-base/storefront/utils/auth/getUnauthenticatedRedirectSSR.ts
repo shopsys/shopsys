5323288ab00583bf438ba37b07fec8f660303a57
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-11-22 20:40:50 +0100

    SF: getUnauthenticatedRedirectSSR now always includes redirect to referer

--- project-base/storefront/utils/auth/getUnauthenticatedRedirectSSR.ts
+++ project-base/storefront/utils/auth/getUnauthenticatedRedirectSSR.ts
@@ -1,26 +1,15 @@
 import { getLoginUrlWithRedirect } from './getLoginUrlWithRedirect';
-import { STATIC_REWRITE_PATHS, StaticRewritePathKeyType } from 'config/staticRewritePaths';
 import { GetServerSidePropsContext, Redirect } from 'next';
-import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
 
 export const getUnauthenticatedRedirectSSR = (
     resolvedUrl: string,
     domainUrl: string,
     context: GetServerSidePropsContext,
 ): { redirect: Redirect } => {
-    let redirectTargetUrlWithLeadingSlash = getInternationalizedStaticUrls(['/login'], domainUrl)[0];
-
-    if (resolvedUrl in STATIC_REWRITE_PATHS[domainUrl]) {
-        redirectTargetUrlWithLeadingSlash = getInternationalizedStaticUrls(
-            [resolvedUrl as StaticRewritePathKeyType],
-            domainUrl,
-        )[0];
-    }
-
     return {
         redirect: {
             statusCode: 302,
-            destination: getLoginUrlWithRedirect(redirectTargetUrlWithLeadingSlash, domainUrl, context),
+            destination: getLoginUrlWithRedirect(resolvedUrl, domainUrl, context),
         },
     };
 };
