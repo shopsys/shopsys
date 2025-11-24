ce86bdfd4ba57c4d8430a72457291ad46f34b19c
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-11-24 15:09:43 +0100

    clean up mail_templates DB table
    - there were orphaned entries for an order status that is already removed
    - see Version20200921071900 and Version20220729131426

---
+++ packages/framework/src/Migrations/Version20251124135037.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Override;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20251124135037 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    #[Override]
+    public function up(Schema $schema): void
+    {
+        $this->sql('DELETE FROM mail_templates WHERE name LIKE \'order_status_%\' AND order_status_id IS NULL AND subject IS NULL AND body IS NULL');
+    }
+}
