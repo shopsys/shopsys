9ad899cccd948b6d17383be7defa3e152423fb20
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-12-05 14:17:47 +0100

    fixed grapesjs format helper

--- packages/framework/src/Component/GrapesJs/EnsureCorrectGrapesJsFormatHelper.php
+++ packages/framework/src/Component/GrapesJs/EnsureCorrectGrapesJsFormatHelper.php
@@ -19,10 +19,22 @@ class EnsureCorrectGrapesJsFormatHelper
             return '<div class="gjs-text-ckeditor">' . t('Please replace this text with your own content.', locale: $locale) . '</div>';
         }
 
-        if (str_starts_with(trim($string), '<div')) {
+        if ($this->containsGrapesJsComponents($string)) {
             return $string;
         }
 
         return '<div class="gjs-text-ckeditor">' . $string . '</div>';
+    }
+
+    /**
+     * @param string $string
+     * @return bool
+     */
+    protected function containsGrapesJsComponents(string $string): bool
+    {
+        // Check for any GrapesJS component class (gjs-*) or typed component (data-gjs-type)
+        return (bool)preg_match('/class=["\'][^"\']*\bgjs-/', $string)
+            || str_contains($string, 'data-gjs-type')
+            || str_contains($string, 'data-gjs-droppable');
     }
 }
