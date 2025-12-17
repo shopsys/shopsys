fa2b67a69045a1f9c996e6049738151703c0a41b
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-12-17 13:52:29 +0100

    regenerated schema

--- project-base/storefront/graphql/requests/sliderItems/fragments/SliderItemFragment.generated.tsx
+++ project-base/storefront/graphql/requests/sliderItems/fragments/SliderItemFragment.generated.tsx
@@ -2,7 +2,7 @@ import * as Types from '../../../types';
 
 import gql from 'graphql-tag';
 import { ImageFragment } from '../../images/fragments/ImageFragment.generated';
-export type TypeSliderItemFragment = { __typename: 'SliderItem', uuid: string, name: string, link: string, description: string | null, rgbBackgroundColor: string, opacity: number, webMainImage: { __typename: 'Image', name: string | null, url: string }, mobileMainImage: { __typename: 'Image', name: string | null, url: string } };
+export type TypeSliderItemFragment = { __typename: 'SliderItem', uuid: string, name: string, link: string, routeName: Types.TypeFriendlyUrlRouteEnum | null, description: string | null, rgbBackgroundColor: string, opacity: number, webMainImage: { __typename: 'Image', name: string | null, url: string }, mobileMainImage: { __typename: 'Image', name: string | null, url: string } };
 
 export const SliderItemFragment = gql`
     fragment SliderItemFragment on SliderItem {
@@ -10,6 +10,7 @@ export const SliderItemFragment = gql`
   uuid
   name
   link
+  routeName
   description
   rgbBackgroundColor
   opacity
