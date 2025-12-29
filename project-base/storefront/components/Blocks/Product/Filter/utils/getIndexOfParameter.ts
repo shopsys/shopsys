5f1e1b18a54c138216f4290891c8dfe3c1ed9441
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-12-29 13:50:49 +0100

    removed unused utils and hooks

--- project-base/storefront/components/Blocks/Product/Filter/utils/getIndexOfParameter.ts
+++
@@ -1,4 +0,0 @@
-import { ParametersType } from 'types/productFilter';
-
-export const getIndexOfParameter = (parametersValue: ParametersType[], parameterUuid: string): number =>
-    parametersValue.findIndex((item) => item.uuid === parameterUuid);
