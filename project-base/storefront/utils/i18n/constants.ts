6aa2253333516c366d4f6cfc9cea2d95b74ad9c4
Tomas Chlebek <chlebek.tomas@seznam.cz>
2025-12-29 13:50:40 +0100

    removed unused types and constants

--- project-base/storefront/utils/i18n/constants.ts
+++ project-base/storefront/utils/i18n/constants.ts
@@ -2,7 +2,3 @@ export const I18N_NAMESPACES = {
     COMMON: 'common',
     ACCESSIBILITY: 'accessibility',
 } as const;
-
-export type I18nNamespace = (typeof I18N_NAMESPACES)[keyof typeof I18N_NAMESPACES];
-
-export const AVAILABLE_NAMESPACES = Object.values(I18N_NAMESPACES);
