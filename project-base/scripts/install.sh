0853025aca1e9e6cd4699a89e0b83469d39d6a44
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-12-30 13:57:30 +0100

    install script now prints the domains from domains.yaml config

--- project-base/scripts/install.sh
+++ project-base/scripts/install.sh
@@ -9,5 +9,13 @@ echo "Installing application inside a php-fpm container"
 docker compose exec -T php-fpm composer install
 docker compose exec -T php-fpm ./phing db-create test-db-create frontend-api-generate-new-keys build-demo-dev-quick
 
-echo "Your application is now ready under http://127.0.0.1:8000 and second domain under http://127.0.0.2:8000"
-echo "Administration is ready under http://127.0.0.1:8000/admin, you can log in using username 'admin' and password 'admin123'"
+DOMAINS_URLS_FILE="$DIR/../app/config/domains_urls.yaml"
+DOMAIN_URLS=($(grep -oP 'url:\s*\K\S+' "$DOMAINS_URLS_FILE"))
+
+FIRST_DOMAIN_URL="${DOMAIN_URLS[0]}"
+
+echo "Your application is now ready under the following domains:"
+for url in "${DOMAIN_URLS[@]}"; do
+    echo "  - $url"
+done
+echo "Administration is ready under ${FIRST_DOMAIN_URL}/admin, you can log in using username 'admin' and password 'admin123'"
