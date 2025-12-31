687886b20d93ffba1103a82e7af622a56b537d67
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-12-31 13:50:48 +0100

    upgrade symfony/doctrine-messenger to ^6.4.31
    - DB schema had to be updated for this version
    - see https://github.com/symfony/symfony/pull/61963

---
+++ packages/framework/src/Migrations/Version20251231124034.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Override;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20251231124034 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    #[Override]
+    public function up(Schema $schema): void
+    {
+        $this->sql('
+            CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (
+                queue_name, available_at, delivered_at,
+                id
+            )');
+        $this->sql('DROP INDEX IF EXISTS IDX_75EA56E016BA31DB');
+        $this->sql('DROP INDEX IF EXISTS IDX_75EA56E0E3BD61CE');
+        $this->sql('DROP INDEX IF EXISTS IDX_75EA56E0FB7336F0');
+    }
+}
