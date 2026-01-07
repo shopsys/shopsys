4ef77fd438d3379a9134ad4cc5e598c579f45d47
Tomas Chlebek <chlebek.tomas@seznam.cz>
2026-01-07 14:50:58 +0100

    moved productListTwClass to shared import

--- project-base/storefront/components/Pages/CategoryDetail/CategoryDetailProductsWrapper/CategoryDetailProductsWrapperPlaceholder.tsx
+++ project-base/storefront/components/Pages/CategoryDetail/CategoryDetailProductsWrapper/CategoryDetailProductsWrapperPlaceholder.tsx
@@ -1,16 +1,8 @@
 import { CategoryDetailProductsWrapperProps } from './CategoryDetailProductsWrapper';
 import { Adverts } from 'components/Blocks/Adverts/Adverts';
 import { ProductListItemPlaceholder } from 'components/Blocks/Product/ProductsList/ProductListItemPlaceholder';
+import { productListTwClass } from 'components/Blocks/Product/ProductsList/ProductsList';
 import { CategoryDetailContentMessage } from 'components/Pages/CategoryDetail/CategoryDetailContentMessage';
-import { twJoin } from 'tailwind-merge';
-
-export const productListTwClass = twJoin(
-    'relative grid gap-2.5 sm:gap-x-5 sm:gap-y-6',
-    'grid-cols-1',
-    'xs:grid-cols-2',
-    'lg:grid-cols-3',
-    'xl:grid-cols-4',
-);
 
 type CategoryDetailProductsWrapperPlaceholderProps = Pick<CategoryDetailProductsWrapperProps, 'category' | 'products'>;
 
