0ff41727be44db140ed5e9492b7ee21bd45fce1b
Martin Grossmann <martin@vyvoj.net>
2026-01-07 13:04:49 +0100

    init base of bootstrapping app (Cli)

---
+++ packages/cli/src/Application.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\Cli;
+
+use Override;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Command\HelpCommand;
+use Symfony\Component\Console\Command\ListCommand;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class Application extends BaseApplication
+{
+    public const string NAME = 'Shopsys CLI';
+
+    public const string VERSION = '1.0.0';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME, self::VERSION);
+
+        $this->buildContainer();
+    }
+
+    /**
+     * @return \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    private function buildContainer(): ContainerBuilder
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.project_dir', dirname(__DIR__));
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../config'),
+        );
+        $loader->load('services.yaml');
+
+        $container->compile();
+
+        return $container;
+    }
+
+    /**
+     * @return \Symfony\Component\Console\Command\Command[]
+     */
+    #[Override]
+    protected function getDefaultCommands(): array
+    {
+        return [
+            new HelpCommand(),
+            new ListCommand(),
+        ];
+    }
+}
