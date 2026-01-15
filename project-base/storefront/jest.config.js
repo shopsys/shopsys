7dd446f4ebf7c36ff569aa2520dd961b3f4fd06d
Tomas Chlebek <chlebek.tomas@seznam.cz>
2026-01-15 14:36:17 +0100

    removed unused test configuration files

--- project-base/storefront/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    moduleDirectories: ['node_modules', '.'],
-    moduleNameMapper: {
-        '^react(.*)$': '<rootDir>/node_modules/react$1',
-    },
-};
--- project-base/storefront/vitest/utils/accessibility/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './keyboard-navigation';
-export * from './a11y-testing';
