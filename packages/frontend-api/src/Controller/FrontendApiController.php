289f81bf6d00dae93c0fb4cb4755ac184833294c
Martin Grossmann <martin@vyvoj.net>
2026-01-29 16:46:14 +0100

    frontend api is now always enabled (#4423)

--- packages/frontend-api/src/Controller/FrontendApiController.php
+++ packages/frontend-api/src/Controller/FrontendApiController.php
@@ -6,9 +6,7 @@ namespace Shopsys\FrontendApiBundle\Controller;
 
 use Overblog\GraphQLBundle\Controller\GraphController;
 use Shopsys\FrameworkBundle\Component\EntityLog\Detection\DetectionFacade;
-use Shopsys\FrontendApiBundle\Component\Domain\EnabledOnDomainChecker;
 use Shopsys\FrontendApiBundle\Model\GraphqlConfigurator;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,13 +14,11 @@ class FrontendApiController
 {
     /**
      * @param \Overblog\GraphQLBundle\Controller\GraphController $graphController
-     * @param \Shopsys\FrontendApiBundle\Component\Domain\EnabledOnDomainChecker $enabledOnDomainChecker
      * @param \Shopsys\FrontendApiBundle\Model\GraphqlConfigurator $graphqlConfigurator
      * @param \Shopsys\FrameworkBundle\Component\EntityLog\Detection\DetectionFacade $detectionFacade
      */
     public function __construct(
         protected readonly GraphController $graphController,
-        protected readonly EnabledOnDomainChecker $enabledOnDomainChecker,
         protected readonly GraphqlConfigurator $graphqlConfigurator,
         protected readonly DetectionFacade $detectionFacade,
     ) {
@@ -36,10 +32,6 @@ class FrontendApiController
     public function endpointAction(Request $request, ?string $schemaName = null): Response
     {
         $this->detectionFacade->setFrontendApiSourceAndUserIdentifier();
-
-        if (!$this->enabledOnDomainChecker->isEnabledOnCurrentDomain()) {
-            return $this->createApiNotEnabledResponse();
-        }
 
         $this->graphqlConfigurator->applyExtraConfiguration();
 
@@ -55,22 +47,8 @@ class FrontendApiController
     {
         $this->detectionFacade->setFrontendApiSourceAndUserIdentifier();
 
-        if (!$this->enabledOnDomainChecker->isEnabledOnCurrentDomain()) {
-            return $this->createApiNotEnabledResponse();
-        }
-
         $this->graphqlConfigurator->applyExtraConfiguration();
 
         return $this->graphController->batchEndpointAction($request, $schemaName);
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function createApiNotEnabledResponse(): Response
-    {
-        return new JsonResponse([
-            'errors' => [['message' => 'Frontend API is not enabled on current domain']],
-        ], 404);
     }
 }
