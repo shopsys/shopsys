a0d76cab334380f4ee0b378b29ca2fcbbeb336ec
Tomáš Ludvik <ludviktomas@gmail.com>
2026-02-05 02:06:42 +0100

    replaced deprecated HttpKernel Extension with DependencyInjection Extension for Symfony 7.4

--- packages/article-feed-luigis-box/src/DependencyInjection/ShopsysArticleFeedLuigisBoxExtension.php
+++ packages/article-feed-luigis-box/src/DependencyInjection/ShopsysArticleFeedLuigisBoxExtension.php
@@ -7,9 +7,9 @@ namespace Shopsys\ArticleFeed\LuigisBoxBundle\DependencyInjection;
 use Override;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ShopsysArticleFeedLuigisBoxExtension extends Extension implements PrependExtensionInterface
 {
