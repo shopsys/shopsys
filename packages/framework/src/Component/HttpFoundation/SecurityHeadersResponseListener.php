0f5d9186e0c15dbeb747063f1068856f96de62f1
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-02-18 14:16:31 +0100

    add "localhost:35729" to PHP CSP header in dev mode
    - it is necessary for livereload in administration

--- packages/framework/src/Component/HttpFoundation/SecurityHeadersResponseListener.php
+++ packages/framework/src/Component/HttpFoundation/SecurityHeadersResponseListener.php
@@ -6,7 +6,9 @@ namespace Shopsys\FrameworkBundle\Component\HttpFoundation;
 
 use Shopsys\FrameworkBundle\Component\Context\ContextResolverInterface;
 use Shopsys\FrameworkBundle\Component\Context\FrontendApiContext;
+use Shopsys\FrameworkBundle\Component\Environment\EnvironmentType;
 use Shopsys\FrameworkBundle\Component\Setting\Setting;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
@@ -15,6 +17,7 @@ class SecurityHeadersResponseListener
     public function __construct(
         protected readonly Setting $setting,
         protected readonly ContextResolverInterface $contextResolver,
+        protected readonly ParameterBagInterface $parameterBag,
     ) {
     }
 
@@ -29,6 +32,12 @@ class SecurityHeadersResponseListener
             return;
         }
 
-        $event->getResponse()->headers->set('Content-Security-Policy', $this->setting->get(Setting::CSP_HEADER));
+        $cspHeader = $this->setting->get(Setting::CSP_HEADER);
+
+        if ($this->parameterBag->get('kernel.environment') === EnvironmentType::DEVELOPMENT) {
+            $cspHeader .= ' http://localhost:35729';
+        }
+
+        $event->getResponse()->headers->set('Content-Security-Policy', $cspHeader);
     }
 }
