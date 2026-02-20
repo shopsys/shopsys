13d7c4a75accde90f704b2ec49782a46998023d6
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-02-20 12:43:34 +0100

    FriendlyUrlType now provides the proper normalized encoded slug in the validation message

--- packages/framework/src/Form/FriendlyUrlType.php
+++ packages/framework/src/Form/FriendlyUrlType.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Form;
 
 use Override;
+use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlSlugNormalizer;
 use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\UrlListData;
+use Shopsys\FrameworkBundle\Component\Translation\Translator;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 final class FriendlyUrlType extends AbstractType
 {
@@ -33,10 +36,7 @@ final class FriendlyUrlType extends AbstractType
             'help' => t('If the slug contains non-ASCII characters (e.g. "café"), encode it as URL-encoded form (e.g. "caf%C3%A9"). You can use https://www.urlencoder.org/.'),
             'constraints' => [
                 new Constraints\NotBlank(),
-                new Constraints\Regex(
-                    pattern: static::SLUG_REGEX,
-                    message: 'Slug containing non-ASCII characters must be URL-encoded. You can use https://www.urlencoder.org/.',
-                ),
+                new Constraints\Callback(callback: [$this, 'validateSlugEncoding']),
             ],
         ]);
     }
@@ -49,5 +49,24 @@ final class FriendlyUrlType extends AbstractType
                 'limit_domains_by_ids' => [],
             ])
             ->setAllowedTypes('limit_domains_by_ids', 'array');
+    }
+
+    public function validateSlugEncoding(mixed $slug, ExecutionContextInterface $context): void
+    {
+        if (!is_string($slug) || $slug === '') {
+            return;
+        }
+
+        if (preg_match(static::SLUG_REGEX, $slug) === 1) {
+            return;
+        }
+
+        $context->buildViolation(
+            t('Slug containing non-ASCII characters must be URL-encoded. The encoded value of "%enteredValue%" is "%normalizedValue%".', [
+                '%enteredValue%' => $slug,
+                '%normalizedValue%' => FriendlyUrlSlugNormalizer::normalize($slug),
+            ], Translator::VALIDATOR_TRANSLATION_DOMAIN),
+        )
+            ->addViolation();
     }
 }
