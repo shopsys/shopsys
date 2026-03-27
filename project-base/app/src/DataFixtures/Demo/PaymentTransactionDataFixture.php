02bca829b5d70012d00a391809f3d043ebf2a109
Martin Grossmann <martin@vyvoj.net>
2026-03-27 21:41:53 +0100

    demo payment transactions now use final statuses to prevent cron errors
    - cron was trying to update non-existing GoPay payment transactions with CREATED status

--- project-base/app/src/DataFixtures/Demo/PaymentTransactionDataFixture.php
+++ project-base/app/src/DataFixtures/Demo/PaymentTransactionDataFixture.php
@@ -25,11 +25,11 @@ class PaymentTransactionDataFixture extends AbstractReferenceFixture implements 
     public function load(ObjectManager $manager): void
     {
         $order = $this->getReference(OrderDataFixture::ORDER_WITH_GOPAY_PAYMENT_1, Order::class);
-        $this->createPaymentTransaction($order, 'TR-123456', PaymentStatus::CREATED);
+        $this->createPaymentTransaction($order, 'TR-123456', PaymentStatus::CANCELED);
 
         $order = $this->getReference(OrderDataFixture::ORDER_WITH_GOPAY_PAYMENT_14, Order::class);
-        $this->createPaymentTransaction($order, '12454321', PaymentStatus::CREATED);
-        $this->createPaymentTransaction($order, '52467431', PaymentStatus::CREATED);
+        $this->createPaymentTransaction($order, '12454321', PaymentStatus::TIMEOUTED);
+        $this->createPaymentTransaction($order, '52467431', PaymentStatus::CANCELED);
     }
 
     /**
