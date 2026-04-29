0fe77aed69c2c632ce648eb95874b10436013f2b
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-04-29 11:55:38 +0200

    CategoriesIdsToCategoriesTransformer now depends on CategoryFacade instead of CategoryRepository

--- packages/framework/src/Form/Transformers/CategoriesIdsToCategoriesTransformer.php
+++ packages/framework/src/Form/Transformers/CategoriesIdsToCategoriesTransformer.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Form\Transformers;
 
 use Override;
-use Shopsys\FrameworkBundle\Model\Category\CategoryRepository;
+use Shopsys\FrameworkBundle\Model\Category\CategoryFacade;
 use Shopsys\FrameworkBundle\Model\Category\Exception\CategoryNotFoundException;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class CategoriesIdsToCategoriesTransformer implements DataTransformerInterface
 {
-    public function __construct(protected readonly CategoryRepository $categoryRepository)
+    public function __construct(protected readonly CategoryFacade $categoryFacade)
     {
     }
 
@@ -46,7 +46,7 @@ class CategoriesIdsToCategoriesTransformer implements DataTransformerInterface
         if (is_array($categoriesIds)) {
             foreach ($categoriesIds as $categoryId) {
                 try {
-                    $categories[] = $this->categoryRepository->getById((int)$categoryId);
+                    $categories[] = $this->categoryFacade->getById((int)$categoryId);
                 } catch (CategoryNotFoundException $e) {
                     throw new TransformationFailedException('Category not found', 0, $e);
                 }
