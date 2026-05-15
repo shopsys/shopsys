d90dd5504ae57909a8cdb0c50b8dbde794f81adf
Martin Grossmann <martin@vyvoj.net>
2026-05-15 08:42:54 +0200

    admin: language constant can be saved with leading/trailing space
    - translations can contain space in specific cases

--- packages/framework/src/Form/Admin/LanguageConstant/LanguageConstantFormType.php
+++ packages/framework/src/Form/Admin/LanguageConstant/LanguageConstantFormType.php
@@ -6,6 +6,7 @@ namespace Shopsys\FrameworkBundle\Form\Admin\LanguageConstant;
 
 use Override;
 use Shopsys\FormTypesBundle\ActionBarType;
+use Shopsys\FrameworkBundle\Component\Translation\Translator;
 use Shopsys\FrameworkBundle\Form\DisplayOnlyType;
 use Shopsys\FrameworkBundle\Model\LanguageConstant\LanguageConstantData;
 use Symfony\Component\Form\AbstractType;
@@ -13,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 final class LanguageConstantFormType extends AbstractType
 {
@@ -37,8 +39,15 @@ final class LanguageConstantFormType extends AbstractType
             ->add('userTranslation', TextType::class, [
                 'required' => true,
                 'label' => 'User translation',
+                'trim' => false,
                 'constraints' => [
                     new Constraints\NotBlank(),
+                    new Constraints\Callback(static function ($value, ExecutionContextInterface $context): void {
+                        if (is_string($value) && preg_match('/[^\S ]/u', $value) === 1) {
+                            $context->buildViolation(t('Only regular spaces are allowed as whitespace characters.', domain: Translator::VALIDATOR_TRANSLATION_DOMAIN))
+                                ->addViolation();
+                        }
+                    }),
                 ],
             ])
             ->add('actionBar', ActionBarType::class, [
