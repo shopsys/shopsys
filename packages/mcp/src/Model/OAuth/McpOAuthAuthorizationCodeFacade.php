shopsys/mcp

mcp: admin server now supports browser-based client authorization
- adds OAuth-style client registration, consent, and token exchange for browser-auth MCP clients
- simplifies MCP token management around manual and connected-client tokens
- updates migrations, nginx wiring, and tests to cover the final flow

src/Model/OAuth/McpOAuthAuthorizationCodeFacade.php
@@ -1,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\McpBundle\Model\OAuth;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+class McpOAuthAuthorizationCodeFacade
+{
+    protected const int AUTHORIZATION_CODE_TTL_SECONDS = 300;
+    protected const string CACHE_KEY_PREFIX = 'shopsys_mcp_oauth_code_';
+
+    public function __construct(
+        protected readonly CacheItemPoolInterface $cacheItemPool,
+    ) {
+    }
+
+    public function createAuthorizationCode(
+        int $administratorId,
+        string $clientId,
+        string $redirectUri,
+        string $codeChallenge,
+    ): string {
+        $authorizationCode = bin2hex(random_bytes(32));
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($authorizationCode));
+        $cacheItem->set([
+            'administrator_id' => $administratorId,
+            'client_id' => $clientId,
+            'redirect_uri' => $redirectUri,
+            'code_challenge' => $codeChallenge,
+        ]);
+        $cacheItem->expiresAfter(self::AUTHORIZATION_CODE_TTL_SECONDS);
+        $this->cacheItemPool->save($cacheItem);
+
+        return $authorizationCode;
+    }
+
+    /**
+     * @return array{administrator_id: int, client_id: string, redirect_uri: string, code_challenge: string}|null
+     */
+    public function consumeAuthorizationCode(string $authorizationCode): ?array
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($authorizationCode));
+
+        if (!$cacheItem->isHit()) {
+            return null;
+        }
+
+        /** @var array{administrator_id: int, client_id: string, redirect_uri: string, code_challenge: string} $authorizationCodeData */
+        $authorizationCodeData = $cacheItem->get();
+        $this->cacheItemPool->deleteItem($this->getCacheKey($authorizationCode));
+
+        return $authorizationCodeData;
+    }
+
+    protected function getCacheKey(string $authorizationCode): string
+    {
+        return self::CACHE_KEY_PREFIX . $authorizationCode;
+    }
+}

src/Model/OAuth/McpOAuthClientRegistrationStorage.php
@@ -1,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\McpBundle\Model\OAuth;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+class McpOAuthClientRegistrationStorage
+{
+    protected const int REGISTRATION_TTL_SECONDS = 2592000;
+    protected const string CACHE_KEY_PREFIX = 'shopsys_mcp_oauth_client_';
+
+    public function __construct(
+        protected readonly CacheItemPoolInterface $cacheItemPool,
+    ) {
+    }
+
+    public function save(McpOAuthClientRegistrationData $clientRegistrationData): void
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($clientRegistrationData->clientId));
+        $cacheItem->set($clientRegistrationData->toArray());
+        $cacheItem->expiresAfter(static::REGISTRATION_TTL_SECONDS);
+        $this->cacheItemPool->save($cacheItem);
+    }
+
+    public function findByClientId(string $clientId): ?McpOAuthClientRegistrationData
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($clientId));
+
+        if (!$cacheItem->isHit()) {
+            return null;
+        }
+
+        /** @var array{client_id: string, client_name: string, redirect_uris: array<string>} $registrationData */
+        $registrationData = $cacheItem->get();
+
+        return McpOAuthClientRegistrationData::createFromArray($registrationData);
+    }
+
+    protected function getCacheKey(string $clientId): string
+    {
+        return self::CACHE_KEY_PREFIX . $clientId;
+    }
+}

shopsys/shopsys

packages/mcp/src/Model/OAuth/McpOAuthAuthorizationCodeFacade.php
@@ -1,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\McpBundle\Model\OAuth;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+class McpOAuthAuthorizationCodeFacade
+{
+    protected const int AUTHORIZATION_CODE_TTL_SECONDS = 300;
+    protected const string CACHE_KEY_PREFIX = 'mcp_oauth_code_';
+
+    public function __construct(
+        protected readonly CacheItemPoolInterface $cacheItemPool,
+    ) {
+    }
+
+    public function createAuthorizationCode(
+        int $administratorId,
+        string $clientId,
+        string $redirectUri,
+        string $codeChallenge,
+    ): string {
+        $authorizationCode = bin2hex(random_bytes(32));
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($authorizationCode));
+        $cacheItem->set([
+            'administrator_id' => $administratorId,
+            'client_id' => $clientId,
+            'redirect_uri' => $redirectUri,
+            'code_challenge' => $codeChallenge,
+        ]);
+        $cacheItem->expiresAfter(self::AUTHORIZATION_CODE_TTL_SECONDS);
+        $this->cacheItemPool->save($cacheItem);
+
+        return $authorizationCode;
+    }
+
+    /**
+     * @return array{administrator_id: int, client_id: string, redirect_uri: string, code_challenge: string}|null
+     */
+    public function consumeAuthorizationCode(string $authorizationCode): ?array
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($authorizationCode));
+
+        if (!$cacheItem->isHit()) {
+            return null;
+        }
+
+        /** @var array{administrator_id: int, client_id: string, redirect_uri: string, code_challenge: string} $authorizationCodeData */
+        $authorizationCodeData = $cacheItem->get();
+        $this->cacheItemPool->deleteItem($this->getCacheKey($authorizationCode));
+
+        return $authorizationCodeData;
+    }
+
+    protected function getCacheKey(string $authorizationCode): string
+    {
+        return self::CACHE_KEY_PREFIX . $authorizationCode;
+    }
+}

packages/mcp/src/Model/OAuth/McpOAuthClientRegistrationStorage.php
@@ -1,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\McpBundle\Model\OAuth;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+class McpOAuthClientRegistrationStorage
+{
+    protected const int REGISTRATION_TTL_SECONDS = 2592000;
+    protected const string CACHE_KEY_PREFIX = 'mcp_oauth_client_';
+
+    public function __construct(
+        protected readonly CacheItemPoolInterface $cacheItemPool,
+    ) {
+    }
+
+    public function save(McpOAuthClientRegistrationData $clientRegistrationData): void
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($clientRegistrationData->clientId));
+        $cacheItem->set($clientRegistrationData->toArray());
+        $cacheItem->expiresAfter(static::REGISTRATION_TTL_SECONDS);
+        $this->cacheItemPool->save($cacheItem);
+    }
+
+    public function findByClientId(string $clientId): ?McpOAuthClientRegistrationData
+    {
+        $cacheItem = $this->cacheItemPool->getItem($this->getCacheKey($clientId));
+
+        if (!$cacheItem->isHit()) {
+            return null;
+        }
+
+        /** @var array{client_id: string, client_name: string, redirect_uris: array<string>} $registrationData */
+        $registrationData = $cacheItem->get();
+
+        return McpOAuthClientRegistrationData::createFromArray($registrationData);
+    }
+
+    protected function getCacheKey(string $clientId): string
+    {
+        return self::CACHE_KEY_PREFIX . $clientId;
+    }
+}