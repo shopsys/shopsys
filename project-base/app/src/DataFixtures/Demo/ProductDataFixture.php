14b48180cf62fbed2a39db971ab32d31fa882272
Vojtěch Korduliak <korduliak.v@seznam.cz>
2026-08-18 06:38:42 +0200

    demo data: two Television Hyundai variants are now out of stock
    - no variant was really out of stock (zero quantity and negative stock not allowed), so the duplicated watchdog button could neither be reproduced nor covered by a test
    - one of them also gets an expected restocking date, to cover the second branch of the watchdog condition

--- project-base/app/src/DataFixtures/Demo/ProductDataFixture.php
+++ project-base/app/src/DataFixtures/Demo/ProductDataFixture.php
@@ -2234,7 +2234,8 @@ class ProductDataFixture extends AbstractReferenceFixture implements DependentFi
         $this->productDemoDataSetter->setOrderingPriority($productData, 8);
         $this->productDemoDataSetter->setPriceForAllPricingGroups($productData, '9173.5');
         $this->productDemoDataSetter->setSellingFrom($productData, '15.1.2000');
-        $this->productDemoDataSetter->setStocksQuantity($productData, 200);
+        $this->productDemoDataSetter->setStocksQuantity($productData, 0);
+        $productData->isAllowedNegativeStock = false;
         $this->productDemoDataSetter->setCategoriesForAllDomains($productData, [CategoryDataFixture::CATEGORY_TV, CategoryDataFixture::CATEGORY_PC]);
         $this->productDemoDataSetter->setFlags($productData, [FlagDataFixture::FLAG_PRODUCT_ACTION]);
         $this->productDemoDataSetter->setBrand($productData, BrandDataFixture::BRAND_HYUNDAI);
@@ -3997,7 +3998,9 @@ class ProductDataFixture extends AbstractReferenceFixture implements DependentFi
         $productData->ean = '8845781243205';
         $this->productDemoDataSetter->setPriceForAllPricingGroups($productData, '9173.5');
         $this->productDemoDataSetter->setSellingFrom($productData, '15.1.2000');
-        $this->productDemoDataSetter->setStocksQuantity($productData, 10);
+        $this->productDemoDataSetter->setStocksQuantity($productData, 0);
+        $this->productDemoDataSetter->setExpectedRestockingDate($productData, 'midnight +2 weeks');
+        $productData->isAllowedNegativeStock = false;
         $this->productDemoDataSetter->setCategoriesForAllDomains($productData, [CategoryDataFixture::CATEGORY_TV, CategoryDataFixture::CATEGORY_PC]);
         $this->productDemoDataSetter->setFlags($productData, [FlagDataFixture::FLAG_PRODUCT_ACTION]);
         $this->productDemoDataSetter->setBrand($productData, BrandDataFixture::BRAND_HYUNDAI);
