ca483c056f0b5f875781540706680bb6a6ac15e2
Vojtěch Korduliak <korduliak.v@seznam.cz>
2026-08-18 06:38:42 +0200

    SF cypress tests: added test for a single watchdog button on product variants

---
+++ project-base/storefront/cypress/e2e/watchdog/watchdog.cy.ts
@@ -0,0 +1,40 @@
+import { staticData } from 'fixtures/demodata';
+import { initializePersistStoreInLocalStorageToDefaultValues } from 'support';
+import { visitEntityByUuid } from 'support/navigation';
+import { TIDs } from 'tids';
+
+describe('Watchdog Button Tests (SSP-4259)', () => {
+    beforeEach(() => {
+        initializePersistStoreInLocalStorageToDefaultValues();
+    });
+
+    it('[Variants Table] should show exactly one watchdog button for each unavailable variant', () => {
+        visitEntityByUuid('product', staticData.products.televisionHyundaiM.uuid);
+
+        cy.getByTID([
+            [TIDs.pages_productdetail_variant_, staticData.products.televisionHyundaiM.outOfStockVariantCatnum],
+            TIDs.blocks_product_watchdog_button,
+        ])
+            .should('have.length', 1)
+            .find('svg')
+            .should('be.visible');
+        cy.getByTID([
+            [TIDs.pages_productdetail_variant_, staticData.products.televisionHyundaiM.outOfStockVariantCatnum],
+            TIDs.blocks_product_addtocart,
+        ]).should('not.exist');
+
+        cy.getByTID([
+            [TIDs.pages_productdetail_variant_, staticData.products.televisionHyundaiM.expectedRestockVariantCatnum],
+            TIDs.blocks_product_watchdog_button,
+        ]).should('have.length', 1);
+
+        cy.getByTID([
+            [TIDs.pages_productdetail_variant_, staticData.products.televisionHyundaiM.inStockVariantCatnum],
+            TIDs.blocks_product_watchdog_button,
+        ]).should('not.exist');
+        cy.getByTID([
+            [TIDs.pages_productdetail_variant_, staticData.products.televisionHyundaiM.inStockVariantCatnum],
+            TIDs.blocks_product_addtocart,
+        ]).should('have.length', 1);
+    });
+});
