28684238b28735907fe17fb1c6140cb1bd2330f6
Jan Górecki <jan.gorecki@shopsys.com>
2026-08-21 19:21:59 +0200

    admin: CRUD controller actions are final now
    - overriding an action silently disabled the extension hooks, which are dispatched from
      inside the action bodies, and in the case of `deleteAction()` also dropped the CSRF
      protection, because PHP attributes are not inherited by an overriding method
    - customization belongs to `configure*()`, `getAdditionalTemplateParameters()` and the hook
      interfaces; an action that fits none of them is disabled in the config and replaced by
      an own action with its own route

--- packages/administration/src/Controller/AbstractCrudController.php
+++ packages/administration/src/Controller/AbstractCrudController.php
@@ -203,7 +203,7 @@ abstract class AbstractCrudController extends AdminBaseController
     {
     }
 
-    public function listAction(): Response
+    final public function listAction(): Response
     {
         $listDomainControl = $this->definition->getConfig()->getListDomainControl();
         $adapter = $this->ormAdapterFactory->create($this->definition->entityClass, function (QueryBuilder $queryBuilder): void {
@@ -229,7 +229,7 @@ abstract class AbstractCrudController extends AdminBaseController
         ]);
     }
 
-    public function detailAction(int $id): Response
+    final public function detailAction(int $id): Response
     {
         return $this->renderAction(ActionType::DETAIL, [
             'title' => $this->definition->getConfig()->getTitle(ActionType::DETAIL),
@@ -237,7 +237,7 @@ abstract class AbstractCrudController extends AdminBaseController
         ]);
     }
 
-    public function editAction(Request $request, int $id): Response
+    final public function editAction(Request $request, int $id): Response
     {
         /** @var \Shopsys\AdministrationBundle\Component\Crud\Handler\EditHandlerInterface $handler */
         $handler = $this->definition->getHandlerForAction(ActionType::EDIT);
@@ -308,7 +308,7 @@ abstract class AbstractCrudController extends AdminBaseController
         ], $entity);
     }
 
-    public function createAction(Request $request): Response
+    final public function createAction(Request $request): Response
     {
         /** @var \Shopsys\AdministrationBundle\Component\Crud\Handler\CreateHandlerInterface $handler */
         $handler = $this->definition->getHandlerForAction(ActionType::CREATE);
@@ -367,7 +367,7 @@ abstract class AbstractCrudController extends AdminBaseController
     }
 
     #[CsrfProtection]
-    public function deleteAction(int $id): RedirectResponse
+    final public function deleteAction(int $id): RedirectResponse
     {
         /** @var \Shopsys\AdministrationBundle\Component\Crud\Handler\DeleteHandlerInterface $handler */
         $handler = $this->definition->getHandlerForAction(ActionType::DELETE);
