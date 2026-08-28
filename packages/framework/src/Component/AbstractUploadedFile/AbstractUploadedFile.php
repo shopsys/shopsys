1b467deca4c5a704dac2f7e71ae83a71b6c14a3b
Martin Grossmann <martin@vyvoj.net>
2026-08-28 15:38:53 +0200

    AbstractUploadedFile: file name is now webalized to avoid issues with special characters
    - uploading a complaint file named e.g. „Pozadí obrázku «generated-image» bylo odstraněno.png" caused problems, special characters in stored file names are now replaced via webalization

--- packages/framework/src/Component/AbstractUploadedFile/AbstractUploadedFile.php
+++ packages/framework/src/Component/AbstractUploadedFile/AbstractUploadedFile.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Component\AbstractUploadedFile;
 
 use Doctrine\ORM\Mapping as ORM;
+use Nette\Utils\Strings;
 use Override;
 use Shopsys\FrameworkBundle\Component\ClassExtension\ExtendedClassNameResolver;
 use Shopsys\FrameworkBundle\Component\FileUpload\EntityFileUploadInterface;
@@ -42,7 +43,12 @@ abstract class AbstractUploadedFile implements EntityFileUploadInterface, Upload
      */
     #[AsMcpColumn]
     #[ORM\Column(type: 'string', length: 255)]
-    protected $name;
+    protected $name {
+        set {
+            $webalizedName = Strings::webalize($value, lower: false);
+            $this->name = $webalizedName === '' ? 'file' : $webalizedName;
+        }
+    }
 
     /**
      * @var int
