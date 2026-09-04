1658508407366d8bc9a9002ced2a24c748ac3c8b
Tomáš Ludvik <ludviktomas@gmail.com>
2026-09-04 12:22:32 +0200

    fixup! added gift voucher generation with email delivery on order payment

--- packages/framework/src/Model/GiftVoucher/GiftVoucherPdfGenerator.php
+++ packages/framework/src/Model/GiftVoucher/GiftVoucherPdfGenerator.php
@@ -5,26 +5,28 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Model\GiftVoucher;
 
 use CommerceGuys\Intl\Currency\CurrencyRepositoryInterface;
-use Dompdf\Dompdf;
-use Dompdf\Options;
+use IntlDateFormatter;
 use Shopsys\FrameworkBundle\Component\CurrencyFormatter\CurrencyFormatterFactory;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Component\Localization\DateTimeFormatterInterface;
 use Shopsys\FrameworkBundle\Component\Router\DomainRouterFactory;
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Twig\Environment;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class GiftVoucherPdfGenerator
 {
     public function __construct(
-        protected readonly Environment $twig,
         protected readonly Domain $domain,
         protected readonly DomainRouterFactory $domainRouterFactory,
         protected readonly CurrencyFacade $currencyFacade,
         protected readonly CurrencyFormatterFactory $currencyFormatterFactory,
         protected readonly CurrencyRepositoryInterface $intlCurrencyRepository,
+        protected readonly DateTimeFormatterInterface $dateTimeFormatter,
+        protected readonly TranslatorInterface $translator,
         protected readonly string $logoFilepath,
         protected readonly string $backgroundFilepath,
+        protected readonly string $fontDir,
     ) {
     }
 
@@ -32,37 +34,51 @@ class GiftVoucherPdfGenerator
     {
         $domainId = $giftVoucher->getDomainId();
         $domainConfig = $this->domain->getDomainConfigById($domainId);
-        $formattedValueParts = $this->splitFormattedValue($this->formatValue($giftVoucher, $domainConfig->getLocale()));
+        $locale = $domainConfig->getLocale();
+        $formattedValueParts = $this->splitFormattedValue($this->formatValue($giftVoucher, $locale));
 
-        $html = $this->twig->render('@ShopsysFramework/Mail/GiftVoucher/giftVoucherPdf.html.twig', [
-            'giftVoucher' => $giftVoucher,
+        $document = $this->createDocument();
+        $document->renderVoucher([
+            'title' => $this->translator->trans('Gift voucher', [], 'messages', $locale),
+            'valueLabel' => $this->translator->trans('Voucher value', [], 'messages', $locale),
+            'valuePrefix' => $formattedValueParts['prefix'],
+            'valueMain' => $formattedValueParts['main'],
+            'valueSuffix' => $formattedValueParts['suffix'],
+            'backgroundFilepath' => $this->backgroundFilepath,
+            'logoFilepath' => is_file($this->logoFilepath) ? $this->logoFilepath : null,
             'shopName' => $domainConfig->getName(),
             'shopUrl' => $this->getShopUrl($domainId),
-            'logoDataUri' => $this->findLogoDataUri(),
-            'backgroundDataUri' => $this->getBackgroundDataUri(),
-            'domainLocale' => $domainConfig->getLocale(),
-            'formattedValuePrefix' => $formattedValueParts['prefix'],
-            'formattedValueMain' => $formattedValueParts['main'],
-            'formattedValueSuffix' => $formattedValueParts['suffix'],
+            'codeLabel' => $this->translator->trans('Code for redemption', [], 'messages', $locale),
+            'codeChunks' => str_split($giftVoucher->getCode(), 4),
+            'validityLabel' => $this->translator->trans('Valid until', [], 'messages', $locale),
+            'validityDate' => (string)$this->dateTimeFormatter->format(
+                $giftVoucher->getValidUntil(),
+                IntlDateFormatter::MEDIUM,
+                IntlDateFormatter::NONE,
+                $locale,
+            ),
+            'redemptionLabel' => $this->translator->trans('Redemption:', [], 'messages', $locale),
+            'redemptionText' => $this->translator->trans(
+                'enter the code in the cart into the field for discount coupons and gift vouchers.',
+                [],
+                'messages',
+                $locale,
+            ),
+            'conditionsTitle' => $this->translator->trans('Conditions', [], 'messages', $locale),
+            'conditionsText' => $this->translator->trans(
+                'The gift voucher applies to all goods and services. The gift voucher cannot be redeemed after the stated validity date; after the validity expires, its value is forfeited without any right to compensation. The voucher cannot be exchanged for cash. The goods or services must be of equal or higher value.',
+                [],
+                'messages',
+                $locale,
+            ),
         ]);
 
-        $dompdf = $this->createDompdf();
-        $dompdf->loadHtml($html, 'UTF-8');
-        $dompdf->render();
-
-        return (string)$dompdf->output();
+        return $document->Output('S');
     }
 
-    protected function createDompdf(): Dompdf
+    protected function createDocument(): GiftVoucherPdfDocument
     {
-        $dompdfOptions = new Options();
-        $dompdfOptions->setDefaultFont('DejaVu Sans');
-        $dompdfOptions->setIsRemoteEnabled(false);
-
-        $dompdf = new Dompdf($dompdfOptions);
-        $dompdf->setPaper('A5', 'landscape');
-
-        return $dompdf;
+        return new GiftVoucherPdfDocument($this->fontDir);
     }
 
     protected function formatValue(GiftVoucher $giftVoucher, string $locale): string
@@ -114,25 +130,5 @@ class GiftVoucherPdfGenerator
         );
 
         return rtrim((string)preg_replace('~^https?://~', '', $shopUrl), '/');
-    }
-
-    protected function getBackgroundDataUri(): string
-    {
-        return 'data:image/png;base64,' . base64_encode((string)file_get_contents($this->backgroundFilepath));
-    }
-
-    protected function findLogoDataUri(): ?string
-    {
-        if (!is_file($this->logoFilepath)) {
-            return null;
-        }
-
-        $logoContent = file_get_contents($this->logoFilepath);
-
-        if ($logoContent === false) {
-            return null;
-        }
-
-        return 'data:image/png;base64,' . base64_encode($logoContent);
     }
 }
