3505a7da33a6397182598e66d330f0eb44504092
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-09-08 14:13:24 +0200

    FE API: Flag now exposes SEO attributes through the seo field
    - Flag implements the Seo interface; there is no fallback for the meta description as flags have no description

--- project-base/app/tests/FrontendApiBundle/Functional/Product/Flag/FlagTest.php
+++ project-base/app/tests/FrontendApiBundle/Functional/Product/Flag/FlagTest.php
@@ -30,6 +30,13 @@ class FlagTest extends GraphQlTestCase
 
         $this->assertSame(t('Made in DE', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()), $responseData['name']);
         $this->assertSame('#000000', $responseData['rgbColor']);
+        $this->assertSame([
+            'title' => null,
+            'metaDescription' => null,
+            'h1' => null,
+            'metaRobots' => null,
+            'canonicalUrl' => null,
+        ], $responseData['seo']);
         $this->assertSame($this->urlGenerator->generate('front_flag_detail', ['id' => $flag->getId()]), $responseData['slug']);
         $this->assertSame([
             [
