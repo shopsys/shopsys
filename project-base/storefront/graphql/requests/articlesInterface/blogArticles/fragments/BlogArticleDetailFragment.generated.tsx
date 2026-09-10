23b2d39a86301bc35afe2a2526ada9d4e3aaa301
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-09-10 14:16:54 +0200

    storefront: regenerated GraphQL types

--- project-base/storefront/graphql/requests/articlesInterface/blogArticles/fragments/BlogArticleDetailFragment.generated.tsx
+++ project-base/storefront/graphql/requests/articlesInterface/blogArticles/fragments/BlogArticleDetailFragment.generated.tsx
@@ -6,10 +6,11 @@ import * as Types from '../../../../types';
 import gql from 'graphql-tag';
 import { ImageFragment } from '../../../images/fragments/ImageFragment.generated';
 import { BreadcrumbFragment } from '../../../breadcrumbs/fragments/BreadcrumbFragment.generated';
+import { SeoAttributesFragment } from '../../../seo/fragments/SeoAttributesFragment.generated';
 import { HreflangLinksFragment } from '../../../hreflangLinks/fragments/HreflangLinksFragment.generated';
 import { SimpleBlogCategoryFragment } from '../../../blogCategories/fragments/SimpleBlogCategoryFragment.generated';
 import { BlogArticleAuthorFragment } from './BlogArticleAuthorFragment.generated';
-export type TypeBlogArticleDetailFragment = { __typename: 'BlogArticle', id: number, uuid: string, name: string, slug: string, link: string, text: string | null, publishDate: string | null, status: string, seoTitle: string | null, seoMetaDescription: string | null, seoH1: string | null, mainBlogCategoryUuid: string, mainImage: { __typename: 'Image', name: string | null, url: string } | null, breadcrumb: Array<{ __typename: 'Link', name: string, slug: string }>, hreflangLinks: Array<{ hreflang: string, href: string }>, blogCategories: Array<{ __typename: 'BlogCategory', uuid: string, name: string, link: string, parent: { name: string } | null }>, author: { __typename: 'BlogArticleAuthor', uuid: string, name: string, jobTitle: string | null, description: string | null, mainImage: { __typename: 'Image', name: string | null, url: string } | null } | null };
+export type TypeBlogArticleDetailFragment = { __typename: 'BlogArticle', id: number, uuid: string, name: string, slug: string, link: string, text: string | null, publishDate: string | null, status: string, mainBlogCategoryUuid: string, mainImage: { __typename: 'Image', name: string | null, url: string } | null, breadcrumb: Array<{ __typename: 'Link', name: string, slug: string }>, seo: { __typename: 'SeoAttributes', title: string | null, metaDescription: string | null, h1: string | null, metaRobots: string | null, canonicalUrl: string | null }, hreflangLinks: Array<{ hreflang: string, href: string }>, blogCategories: Array<{ __typename: 'BlogCategory', uuid: string, name: string, link: string, parent: { name: string } | null }>, author: { __typename: 'BlogArticleAuthor', uuid: string, name: string, jobTitle: string | null, description: string | null, mainImage: { __typename: 'Image', name: string | null, url: string } | null } | null };
 
 export const BlogArticleDetailFragment = gql`
     fragment BlogArticleDetailFragment on BlogArticle {
@@ -28,9 +29,9 @@ export const BlogArticleDetailFragment = gql`
   text
   publishDate
   status
-  seoTitle
-  seoMetaDescription
-  seoH1
+  seo {
+    ...SeoAttributesFragment
+  }
   hreflangLinks {
     ...HreflangLinksFragment
   }
@@ -44,6 +45,7 @@ export const BlogArticleDetailFragment = gql`
 }
     ${ImageFragment}
 ${BreadcrumbFragment}
+${SeoAttributesFragment}
 ${HreflangLinksFragment}
 ${SimpleBlogCategoryFragment}
 ${BlogArticleAuthorFragment}`;
--- project-base/storefront/graphql/requests/seoPage/fragments/SeoPageFragment.generated.tsx
+++ project-base/storefront/graphql/requests/seoPage/fragments/SeoPageFragment.generated.tsx
@@ -4,16 +4,17 @@ export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' |
 import * as Types from '../../../types';
 
 import gql from 'graphql-tag';
+import { SeoAttributesFragment } from '../../seo/fragments/SeoAttributesFragment.generated';
 import { ImageFragment } from '../../images/fragments/ImageFragment.generated';
 import { HreflangLinksFragment } from '../../hreflangLinks/fragments/HreflangLinksFragment.generated';
-export type TypeSeoPageFragment = { __typename: 'SeoPage', title: string | null, metaDescription: string | null, canonicalUrl: string | null, ogTitle: string | null, ogDescription: string | null, ogImage: { __typename: 'Image', name: string | null, url: string } | null, hreflangLinks: Array<{ hreflang: string, href: string }> };
+export type TypeSeoPageFragment = { __typename: 'SeoPage', ogTitle: string | null, ogDescription: string | null, seo: { __typename: 'SeoAttributes', title: string | null, metaDescription: string | null, h1: string | null, metaRobots: string | null, canonicalUrl: string | null }, ogImage: { __typename: 'Image', name: string | null, url: string } | null, hreflangLinks: Array<{ hreflang: string, href: string }> };
 
 export const SeoPageFragment = gql`
     fragment SeoPageFragment on SeoPage {
   __typename
-  title
-  metaDescription
-  canonicalUrl
+  seo {
+    ...SeoAttributesFragment
+  }
   ogTitle
   ogDescription
   ogImage {
@@ -23,5 +24,6 @@ export const SeoPageFragment = gql`
     ...HreflangLinksFragment
   }
 }
-    ${ImageFragment}
+    ${SeoAttributesFragment}
+${ImageFragment}
 ${HreflangLinksFragment}`;
