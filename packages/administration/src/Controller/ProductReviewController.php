b590f5b4903b30c6f2a369f829c67d205952e923
Jan Górecki <jan.gorecki@shopsys.com>
2026-09-14 15:10:15 +0200

    ProductReviewController: added filters by the status, the rating, the product and the verified purchase flag
    - the list is the first CRUD list offering the filter next to its quick search, and its functional tests prove the filter from the URL down to the rows: a composed rule narrows the reviews, outranks the quick search and opens its tab, a rule of the verified purchase flag needs no value, and an invalid rule is reported instead of narrowing

--- packages/administration/src/Controller/ProductReviewController.php
+++ packages/administration/src/Controller/ProductReviewController.php
@@ -11,6 +11,9 @@ use Shopsys\AdministrationBundle\Component\Config\CrudConfig;
 use Shopsys\AdministrationBundle\Component\Crud\Form\CrudFormConfigurator;
 use Shopsys\AdministrationBundle\Component\Datagrid\Datagrid;
 use Shopsys\AdministrationBundle\Component\Datagrid\DomainControl\DomainControlType;
+use Shopsys\AdministrationBundle\Component\Datagrid\Filter\BooleanFilter;
+use Shopsys\AdministrationBundle\Component\Datagrid\Filter\ChoiceFilter;
+use Shopsys\AdministrationBundle\Component\Datagrid\Filter\ProductFilter;
 use Shopsys\AdministrationBundle\Component\Security\Role\AdminRoleSectionsProvider;
 use Shopsys\AdministrationBundle\Model\ProductReview\ProductReviewEditHandler;
 use Shopsys\FrameworkBundle\Component\EntityLog\Model\EntityLogFacade;
@@ -27,6 +30,7 @@ class ProductReviewController extends AbstractCrudController
     public function __construct(
         protected readonly EntityLogFacade $entityLogFacade,
         protected readonly ProductReviewEnabledChecker $productReviewEnabledChecker,
+        protected readonly ProductReviewStatusEnum $productReviewStatusEnum,
     ) {
     }
 
@@ -119,6 +123,17 @@ class ProductReviewController extends AbstractCrudController
                 'label' => t('Domain'),
             ]);
         }
+
+        $this->configureFilters($datagrid);
+    }
+
+    protected function configureFilters(Datagrid $datagrid): void
+    {
+        $datagrid->filters()
+            ->add(ChoiceFilter::new('status', t('Status'))->setChoices($this->productReviewStatusEnum->getAllIndexedByTranslations()))
+            ->add(ChoiceFilter::new('rating', t('Rating'))->setChoices(array_combine(range(1, 5), range(1, 5))))
+            ->add(ProductFilter::new('product', t('Product')))
+            ->add(BooleanFilter::new('isVerifiedPurchase', t('Verified purchase')));
     }
 
     #[Override]
